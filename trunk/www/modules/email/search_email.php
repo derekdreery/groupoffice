2465476e67f7980e6ba0f9a3f65d2d245e1102cf
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-01-29 09:10:32 +0000

    Allow duplicate email addresses with differt names in autocomplete

--- trunk/www/modules/email/search_email.php
+++ trunk/www/modules/email/search_email.php
@@ -22,20 +22,33 @@ $addresses=array();
 while($ab->next_record(MYSQL_ASSOC))
 {
 	$name = format_name($ab->f('last_name'),$ab->f('first_name'),$ab->f('middle_name'),'first_name');
-	if($ab->f('email')!='' && !in_array($ab->f('email'), $addresses))
+	$rcf_email =$RFC822->write_address($name, $ab->f('email'));
+	if($ab->f('email')!='')
 	{
-		$addresses[]=$ab->f('email');
-		echo '<contact><full_email>'.htmlspecialchars($RFC822->write_address($name, $ab->f('email'))).'</full_email><name>'.htmlspecialchars($name).'</name><email>'.htmlspecialchars($ab->f('email')).'</email></contact>';
-	}
-	if($ab->f('email2')!='' && !in_array($ab->f('email2'), $addresses))
+		$rcf_email =$RFC822->write_address($name, $ab->f('email'));
+		if( !in_array($rcf_email, $addresses))
+		{
+			$addresses[]=$rcf_email;
+			echo '<contact><full_email>'.htmlspecialchars($rcf_email).'</full_email><name>'.htmlspecialchars($name).'</name><email>'.htmlspecialchars($ab->f('email')).'</email></contact>';
+		}
+	}	
+	if($ab->f('email2')!='')
 	{
-		$addresses[]=$ab->f('email2');
-		echo '<contact><full_email>'.htmlspecialchars($RFC822->write_address($name, $ab->f('email2'))).'</full_email><name>'.htmlspecialchars($name).'</name><email>'.htmlspecialchars($ab->f('email2')).'</email></contact>';
-	}
-	if($ab->f('email3')!='' && !in_array($ab->f('email3'), $addresses))
+		$rcf_email =$RFC822->write_address($name, $ab->f('email2'));
+		if( !in_array($rcf_email, $addresses))
+		{
+			$addresses[]=$rcf_email;
+			echo '<contact><full_email>'.htmlspecialchars($rcf_email).'</full_email><name>'.htmlspecialchars($name).'</name><email>'.htmlspecialchars($ab->f('email2')).'</email></contact>';
+		}
+	}	
+	if($ab->f('email3')!='')
 	{
-		$addresses[]=$ab->f('email3');
-		echo '<contact><full_email>'.htmlspecialchars($RFC822->write_address($name, $ab->f('email3'))).'</full_email><name>'.htmlspecialchars($name).'</name><email>'.htmlspecialchars($ab->f('email3')).'</email></contact>';
+		$rcf_email =$RFC822->write_address($name, $ab->f('email3'));
+		if( !in_array($rcf_email, $addresses))
+		{
+			$addresses[]=$rcf_email;
+			echo '<contact><full_email>'.htmlspecialchars($rcf_email).'</full_email><name>'.htmlspecialchars($name).'</name><email>'.htmlspecialchars($ab->f('email3')).'</email></contact>';
+		}
 	}	
 }
 
@@ -45,10 +58,11 @@ if(count($addresses)<10)
 	
 	while($GO_USERS->next_record(MYSQL_ASSOC))
 	{
-		if(!in_array($GO_USERS->f('email'),$addresses))
-		{
-			$name = format_name($GO_USERS->f('last_name'),$GO_USERS->f('first_name'),$GO_USERS->f('middle_name'),'first_name');
-			echo '<contact><full_email>'.htmlspecialchars($RFC822->write_address($name, $GO_USERS->f('email'))).'</full_email><name>'.htmlspecialchars($name).'</name><email>'.htmlspecialchars($GO_USERS->f('email')).'</email></contact>';
+		$name = format_name($GO_USERS->f('last_name'),$GO_USERS->f('first_name'),$GO_USERS->f('middle_name'),'first_name');
+		$rfc_email = $RFC822->write_address($name, $GO_USERS->f('email'));
+		if(!in_array($rfc_email,$addresses))
+		{			
+			echo '<contact><full_email>'.htmlspecialchars($rcf_email).'</full_email><name>'.htmlspecialchars($name).'</name><email>'.htmlspecialchars($GO_USERS->f('email')).'</email></contact>';
 		}
 	}
 	echo '</addressses>';
