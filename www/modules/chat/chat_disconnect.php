14c4dece73548d946642b0b4c2cd12c609b8e67d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-02-02 12:32:06 +0000

    Tagged 2.16-9. Minor bugs fixes

--- www/modules/chat/chat_disconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  //disconnect
-  $userid = $_GET["userid"];
-
-  if ($userid) {
-    query("delete from chat_users where id=".($userid)."");
-    query("delete from chat_chatrooms where fromuser=".($userid)."");
-  }
-  echo "done";
--- www/modules/timeregistration/sql/timeregistration.uninstall.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `tr_hours`;
-DROP TABLE IF EXISTS `tr_projects`;
-DROP TABLE IF EXISTS `tr_timers`;
-DROP TABLE IF EXISTS `tr_settings`;
-DROP TABLE IF EXISTS `tr_template_events`;
-DROP TABLE IF EXISTS `tr_templates`;
