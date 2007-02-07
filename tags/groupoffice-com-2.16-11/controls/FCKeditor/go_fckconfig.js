8dc34f12a48309c8804dcc2a54d1055ede732af4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-02-07 11:23:59 +0000

    tagged 2.16-11

---
+++ tags/groupoffice-com-2.16-11/controls/FCKeditor/go_fckconfig.js
@@ -0,0 +1,74 @@
+FCKConfig.SpellChecker			= 'SpellerPages' ;
+
+FCKConfig.LinkBrowser=false;
+FCKConfig.LinkUpload=false;
+FCKConfig.ImageBrowser=false;
+FCKConfig.ImageUpload=false;
+FCKConfig.FlashUpload =false;
+FCKConfig.UseBROnCarriageReturn=true;
+FCKConfig.IEForceVScroll=true;
+
+FCKConfig.ToolbarSets["template"] = [
+	['Source','DocProps','-','Templates'],
+	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
+	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+	['OrderedList','UnorderedList','-','Outdent','Indent'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['Link','Unlink','Anchor'],
+	['Image', 'Table','Rule','SpecialChar','PageBreak','UniversalKey'],
+	'/',
+	['FontFormat','FontName','FontSize'],
+	['TextColor','BGColor'],['go_autodata']
+] ;
+
+FCKConfig.ToolbarSets["ImageManager"] = [
+	['Source','DocProps','-','Templates'],
+	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
+	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+	['OrderedList','UnorderedList','-','Outdent','Indent'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['Link','Unlink','Anchor'],
+	['ImageManager', 'Table','Rule','SpecialChar','PageBreak','UniversalKey'],
+	'/',
+	['FontFormat','FontName','FontSize'],
+	['TextColor','BGColor'],['go_autodata']
+] ;
+
+FCKConfig.ToolbarSets["email"] = [
+	['Source','DocProps','-','Templates'],
+	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
+	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+	['OrderedList','UnorderedList','-','Outdent','Indent'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['Link','Unlink','Anchor'],
+	['Image','Table','Rule','SpecialChar','PageBreak','UniversalKey'],
+	'/',
+	['FontFormat','FontName','FontSize'],
+	['TextColor','BGColor']
+] ;
+
+
+
+var FCKInsertHTMLCommand = function()
+{
+	this.Name = 'InsertHTML' ;
+}
+
+FCKInsertHTMLCommand.prototype.Execute = function(html)
+{
+  FCK.InsertHtml(html);
+}
+
+FCKInsertHTMLCommand.prototype.GetState = function()
+{
+	return FCK_TRISTATE_OFF ;
+}
+
+FCKCommands.RegisterCommand('InsertHTML', new FCKInsertHTMLCommand());
+
+
+FCKConfig.Plugins.Add( 'go_autodata', 'en');
+FCKConfig.Plugins.Add( 'ImageManager');
