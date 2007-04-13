efd6e4fe775171e49b4848376ad5c53c4ef31fcf
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-04-13 17:48:12 +0000

    Updated catalan

--- trunk/www/controls/jscalendar/lang/calendar-ca.js
+++ trunk/www/controls/jscalendar/lang/calendar-ca.js
@@ -1,8 +1,8 @@
 // ** I18N
 
 // Calendar CA language
-// Author: Mihai Bazon, <mihai_bazon@yahoo.com>
-// Encoding: any
+// Author: Jordi Sanfeliu, <jordi@fibranet.cat>
+// Encoding: UTF-8
 // Distributed under the same terms as the calendar itself.
 
 // For translators: please use UTF-8 if possible.  We strongly believe that
@@ -75,31 +75,31 @@ Calendar._SMN = new Array
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["INFO"] = "Sobre el calendari";
+Calendar._TT["INFO"] = "Quant al calendari";
 
 Calendar._TT["ABOUT"] =
-"DHTML Selector de Data/Hora\n" +
+"Selector de data i hora en DHTML\n" +
 "(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this this ;-)
-"For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
-"Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
+"Per la darrera versió visiteu: http://www.dynarch.com/projects/calendar/\n" +
+"Distribuït sota GNU LGPL.  Per més detalls vegeu http://gnu.org/licenses/lgpl.html" +
 "\n\n" +
-"Sel.lecció de Dates:\n" +
-"- Fes servir els botons \xab, \xbb per sel.leccionar l'any\n" +
-"- Fes servir els botons " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " per se.lecciconar el mes\n" +
-"- Manté el ratolí apretat en qualsevol dels anteriors per sel.lecció ràpida.";
+"Selecció de data:\n" +
+"- Feu servir els botons \xab i \xbb per seleccionar l'any\n" +
+"- Feu servir els botons " + String.fromCharCode(0x2039) + " i " + String.fromCharCode(0x203a) + " per seleccionar el mes\n" +
+"- Mantingueu el botó del ratolí apretat en qualsevol dels anteriors botons per una selecció més ràpida.";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
-"Time selection:\n" +
-"- claca en qualsevol de les parts de la hora per augmentar-les\n" +
-"- o Shift-click per decrementar-la\n" +
-"- or click and arrastra per sel.lecció ràpida.";
+"Selecció de l'hora:\n" +
+"- Feu clic sobre qualsevol part de l'hora per augmentar-la\n" +
+"- o majúscules-clic per reduir-la\n" +
+"- o feu clic i arrossegueu per una selecció més ràpida.";
 
-Calendar._TT["PREV_YEAR"] = "Any anterior (Mantenir per menu)";
-Calendar._TT["PREV_MONTH"] = "Mes anterior (Mantenir per menu)";
-Calendar._TT["GO_TODAY"] = "Anar a avui";
-Calendar._TT["NEXT_MONTH"] = "Mes següent (Mantenir per menu)";
-Calendar._TT["NEXT_YEAR"] = "Any següent (Mantenir per menu)";
-Calendar._TT["SEL_DATE"] = "Sel.leccionar data";
-Calendar._TT["DRAG_TO_MOVE"] = "Arrastrar per moure";
+Calendar._TT["PREV_YEAR"] = "Any anterior (mantingueu-lo apretat per menú)";
+Calendar._TT["PREV_MONTH"] = "Mes anterior (mantingueu-lo apretat per menú)";
+Calendar._TT["GO_TODAY"] = "Vés a avui";
+Calendar._TT["NEXT_MONTH"] = "Mes següent (mantingueu-lo apretat per menú)";
+Calendar._TT["NEXT_YEAR"] = "Any següent (mantingueu-lo apretat per menú)";
+Calendar._TT["SEL_DATE"] = "Selecciona data";
+Calendar._TT["DRAG_TO_MOVE"] = "Arrossega per moure";
 Calendar._TT["PART_TODAY"] = " (avui)";
 
 // the following is to inform that "%s" is to be the first day of week
@@ -113,11 +113,11 @@ Calendar._TT["WEEKEND"] = "0,6";
 
 Calendar._TT["CLOSE"] = "Tanca";
 Calendar._TT["TODAY"] = "Avui";
-Calendar._TT["TIME_PART"] = "(Shift-)Click a arrastra per canviar el valor";
+Calendar._TT["TIME_PART"] = "Feu (Shift-)Clic i arrossegueu per canviar el valor";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
 Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
 
-Calendar._TT["WK"] = "st";
+Calendar._TT["WK"] = "set";
 Calendar._TT["TIME"] = "Hora:";
