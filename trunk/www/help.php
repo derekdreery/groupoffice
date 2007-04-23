ec323e25fda1798aa8a6ad4b4d342972e49b161d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-04-23 14:13:30 +0000

    "New help system"

--- trunk/www/help.php
+++ trunk/www/help.php
@@ -1,5 +1,5 @@
 <?php
-require('Group-Office.php');
+session_start();
 $url = isset($_SESSION['GO_SESSION']['help_url']) ? $_SESSION['GO_SESSION']['help_url'] : 'http://docs.group-office.com';
 
 header('Location: '.$url);
