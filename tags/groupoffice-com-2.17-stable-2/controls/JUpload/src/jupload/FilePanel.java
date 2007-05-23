d72a936b78a8f83a597d5461eb9dbe8b4460acad
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-05-23 06:07:17 +0000

    Tagged 2.17-stable-2

---
+++ tags/groupoffice-com-2.17-stable-2/controls/JUpload/src/jupload/FilePanel.java
@@ -0,0 +1,12 @@
+package jupload;
+
+import java.io.File;
+
+public interface FilePanel
+{
+    public void addFiles(File[] f);
+    public File[] getFiles();
+    public int getFilesLength();
+    public void removeSelected();
+    public void removeAll();
+}
