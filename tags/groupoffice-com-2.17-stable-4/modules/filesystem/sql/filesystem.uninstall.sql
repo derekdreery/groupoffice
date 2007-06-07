c3a0ade6655dcbd73b7826aba29b3db164e5a018
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-06-07 10:00:58 +0000

    Tagged 2.17-stable-4

---
+++ tags/groupoffice-com-2.17-stable-4/modules/filesystem/sql/filesystem.uninstall.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `fs_shares`;
+DROP TABLE IF EXISTS `fs_links`;
+DROP TABLE IF EXISTS `fs_settings`;
+DROP TABLE IF EXISTS `fs_status_history`;
+DROP TABLE IF EXISTS `fs_statuses`;
