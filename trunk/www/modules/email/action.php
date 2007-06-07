3396f3519e06eb9ce5f4b16b838f8e2b3be91008
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-06-07 15:03:35 +0000

    ExtJS

---
+++ trunk/www/modules/email/action.php
@@ -0,0 +1,49 @@
+<?php
+/**
+* @copyright Intermesh 2007
+* @author Merijn Schering <mschering@intermesh.nl>
+* @version $Revision: 1.13 $ $Date: 2006/10/20 12:36:43 $3
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ */
+ 
+
+require_once("../../Group-Office.php");
+$GO_SECURITY->authenticate();
+$GO_MODULES->authenticate('email');
+
+//load contact management class
+require_once($GO_MODULES->class_path."email.class.inc");
+$email = new email();
+
+
+$result =array();
+switch($_REQUEST['task'])
+{
+    case 'delete':
+        
+        $selectedRows = json_decode(smart_stripslashes($_POST['selectedRows']));
+        foreach($selectedRows as $note_id)
+        {
+            $notes->delete_note($note_id);            
+        }
+        $result['success']=true;
+        $result['errors']='Messages deleted successfully';
+        
+        break;
+        
+    case 'move':
+    	$selectedRows = json_decode(smart_stripslashes($_POST['selectedRows']));
+        foreach($selectedRows as $note_id)
+        {
+            $notes->delete_note($note_id);            
+        }
+        $result['success']=true;
+        $result['errors']='Messages deleted successfully';    	
+    	break;
+}
+
+echo json_encode($result);
