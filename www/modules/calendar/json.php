1689390fbcf0c620c635e683ffa61d2e4fc595e7
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-09-10 14:59:59 +0000

    NEw calendar

--- www/modules/calendar/json.php
+++ www/modules/calendar/json.php
@@ -20,24 +20,34 @@ $GO_MODULES->authenticate('calendar');
 require_once ($GO_MODULES->class_path."calendar.class.inc");
 $cal = new calendar();
 
-$calendar_id=isset($_REQUEST['calendar_id']) ? smart_addslashes($_REQUEST['calendar_id']) : 0;
-$view_id=isset($_REQUEST['view_id']) ? smart_addslashes($_REQUEST['view_id']) : 0;
-$start_time=isset($_REQUEST['start_time']) ? strtotime($_REQUEST['start_time']) : 0;
-$end_time=isset($_REQUEST['end_time']) ? strtotime($_REQUEST['end_time']) : 0;
 
+$task=isset($_REQUEST['task']) ? smart_addslashes($_REQUEST['task']) : 0;
 
-$events = $cal->get_events_in_array($calendar_id, $view_id, 0, 
-			local_to_gmt_time($start_time), local_to_gmt_time($end_time), false,false,true,false,true);
-$results=array();
-foreach($events as $event)
+switch($task)
 {
-	$results[] = array(
-		'id'=> $event['id'],
-		'name'=> $event['name'],
-		'start_time'=> date('Y-m-d H:i', gmt_to_local_time($event['start_time'])),
-		'end_time'=> date('Y-m-d H:i', gmt_to_local_time($event['end_time'])),
-		'tooltip'=>'Dit is een beschrijving'
-	);
+	case 'events':
+		
+		$calendar_id=isset($_REQUEST['calendar_id']) ? smart_addslashes($_REQUEST['calendar_id']) : 0;
+		$view_id=isset($_REQUEST['view_id']) ? smart_addslashes($_REQUEST['view_id']) : 0;
+		$start_time=isset($_REQUEST['start_time']) ? strtotime($_REQUEST['start_time']) : 0;
+		$end_time=isset($_REQUEST['end_time']) ? strtotime($_REQUEST['end_time']) : 0;
+		
+		$events = $cal->get_events_in_array($calendar_id, $view_id, 0, 
+					local_to_gmt_time($start_time), local_to_gmt_time($end_time), false,false,true,false,true);
+		$results=array();
+		foreach($events as $event)
+		{
+			$results[] = array(
+				'id'=> $event['id'],
+				'name'=> $event['name'],
+				'start_time'=> date('Y-m-d H:i', gmt_to_local_time($event['start_time'])),
+				'end_time'=> date('Y-m-d H:i', gmt_to_local_time($event['end_time'])),
+				'tooltip'=>'Dit is een beschrijving'
+			);
+		}
+
+		break;
 }
+
 			
 echo json_encode($results);
