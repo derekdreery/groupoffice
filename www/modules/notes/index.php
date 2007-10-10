c0544a6d87a2251127ba4b0bf497ddbeaa36592b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-10-10 12:39:22 +0000

    Notes finished

--- www/modules/notes/index.php
+++ www/modules/notes/index.php
@@ -1,28 +1,16 @@
 <?php
 /**
  * @copyright Copyright Intermesh 2007
- * @version 1.0
- *
  * @author Merijn Schering <mschering@intermesh.nl>
-
-   This file is part of Group-Office.
-
-   Group-Office is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   Group-Office is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with Group-Office; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
- * @package Users
-
+ * 
+ * This file is part of Group-Office.
+ * 
+ * Group-Office is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ * 
+ * See file /LICENSE.GPL
  */
 
 require_once("../../Group-Office.php");
