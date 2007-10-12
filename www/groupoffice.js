eb6ff1642d411335a98a07eb9484cb9304fcb8a0
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-10-12 14:01:16 +0000

    Removed old files

--- www/groupoffice.js
+++ www/groupoffice.js
@@ -135,6 +135,7 @@ GroupOffice = function(){
 		     closable:true,
 		     // layout to fit child component
 		     layout:'fit', 
+		     iconCls: 'go-app-icon',
 		     // add iframe as the child component
 		     items: [ new Ext.ux.IFrameComponent({ 'id': id, 'url': url }) ]
 
