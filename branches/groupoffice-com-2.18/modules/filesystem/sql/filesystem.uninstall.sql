6c370bceddfe53f04c872b9ae8d77645f0764a01
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2007-11-19 09:37:03 +0000

    File upload notifications per folder

--- branches/groupoffice-com-2.18/modules/filesystem/sql/filesystem.uninstall.sql
+++ branches/groupoffice-com-2.18/modules/filesystem/sql/filesystem.uninstall.sql
@@ -3,3 +3,4 @@ DROP TABLE IF EXISTS `fs_links`;
 DROP TABLE IF EXISTS `fs_settings`;
 DROP TABLE IF EXISTS `fs_status_history`;
 DROP TABLE IF EXISTS `fs_statuses`;
+DROP TABLE IF EXISTS `fs_notifications`;
