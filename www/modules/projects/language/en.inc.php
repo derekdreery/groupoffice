a93282229b4915c134f123788c23e1afdcd509ae
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-09-03 13:31:11 +0000

    removed pro module

--- www/modules/projects/language/en.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lang['projects']['name']='Projects';
-$lang['projects']['description']='Put a description here';
-
-$lang['projects']['projects']='Projects';
-
-$lang['link_type'][5]=$lang['projects']['project']='Project';
-?>
