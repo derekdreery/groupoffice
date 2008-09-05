9d5f7a051ef37c1f41f03cde720cab550624dc19
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-09-05 12:29:30 +0000

    Added french translation

---
+++ trunk/www/modules/groups/language/fr.js
@@ -0,0 +1,26 @@
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id: en.js 2895 2008-08-29 15:21:02Z mschering $
+ * @author Merijn Schering <mschering@intermesh.nl>
+ *
+ * French Translation v1.0
+ * Author : Lionel JULLIEN
+ * Date : September, 05 2008
+ */
+
+Ext.namespace('GO.groups');
+
+GO.groups.lang={};
+
+GO.groups.lang.groups = 'Groupes';
+GO.groups.lang.owner = 'Propriétaire';
+GO.groups.lang.lblNew = 'Nouveau';
+GO.groups.lang.newGroupName = 'Saisissez le nom du nouveau groupe';
+GO.groups.lang.searchAllUsers = 'Rechercher tous les utilisateurs';
---
+++ trunk/www/modules/tasks/language/fr.inc.php
@@ -0,0 +1,14 @@
+<?php
+//French Translation v1.0
+//Author : Lionel JULLIEN
+//Date : September, 05 2008
+
+//Uncomment this line in new translations!
+require_once($GO_LANGUAGE->get_fallback_language_file('tasks'));
+
+$lang['tasks']['name']='Tâches';
+$lang['tasks']['description']='Saisissez une description ici';
+
+$lang['link_type'][12]=$lang['tasks']['task']='Tâche';
+$lang['tasks']['status']='Statut';
+?>
