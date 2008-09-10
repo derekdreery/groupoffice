f86fa5b49cbcb08763061d5581735d18ff056c9d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-09-10 06:37:17 +0000

    add missing tasks table

--- www/modules/tasks/install/install.sql
+++ www/modules/tasks/install/install.sql
@@ -55,3 +55,14 @@ CREATE TABLE IF NOT EXISTS `ta_tasks` (
   KEY `list_id` (`tasklist_id`),
   KEY `rrule` (`rrule`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `go_links_12`;
+CREATE TABLE IF NOT EXISTS `go_links_12` (
+  `id` int(11) NOT NULL,
+  `folder_id` int(11) NOT NULL,
+  `link_id` int(11) NOT NULL,
+  `link_type` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  KEY `link_id` (`link_id`,`link_type`),
+  KEY `id` (`id`,`folder_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
