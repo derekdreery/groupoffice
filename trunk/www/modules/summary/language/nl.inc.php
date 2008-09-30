581a9f349c861ad6f9b82f1c46398aaf74b97f8e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-09-30 13:44:39 +0000

    set windows collapsible

--- trunk/www/modules/summary/language/nl.inc.php
+++ trunk/www/modules/summary/language/nl.inc.php
@@ -3,4 +3,3 @@ $lang['summary']['name']='Startpagina';
 $lang['summary']['description']='Put a description here';
 
 $lang['summary']['default_rss_url']='http://www.nu.nl/deeplink_rss2/index.jsp?r=Algemeen';
-?>
