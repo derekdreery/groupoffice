6dd1b1d2e6e8eefbe7d1e7230ee9992762f94447
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-10-06 06:43:18 +0000

    minified

--- www/modules/summary/install/install.sql
+++ www/modules/summary/install/install.sql
@@ -10,3 +10,15 @@ CREATE TABLE `su_rss_feeds` (
 `url` VARCHAR( 255 ) NOT NULL ,
 PRIMARY KEY ( `user_id` )
 ) ENGINE = MYISAM DEFAULT CHARSET=utf8 ;
+
+CREATE TABLE IF NOT EXISTS `su_announcements` (
+  `id` int(11) NOT NULL default '0',
+  `user_id` int(11) NOT NULL default '0',
+  `due_time` int(11) NOT NULL default '0',
+  `ctime` int(11) NOT NULL default '0',
+  `mtime` int(11) NOT NULL default '0',
+  `title` varchar(50) NOT NULL default '',
+  `content` text NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `due_time` (`due_time`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
