69fe2333a4659014fb54aef19cf8bd0c82cc815a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-10-07 13:06:31 +0000

    better link presentation

--- www/modules/tasks/language/en.inc.php
+++ www/modules/tasks/language/en.inc.php
@@ -7,4 +7,13 @@ $lang['tasks']['description']='Put a description here';
 
 $lang['link_type'][12]=$lang['tasks']['task']='Task';
 $lang['tasks']['status']='Status';
+
+
+$lang['tasks']['statuses']['NEEDS-ACTION'] = 'Needs action';
+$lang['tasks']['statuses']['ACCEPTED'] = 'Accepted';
+$lang['tasks']['statuses']['DECLINED'] = 'Declined';
+$lang['tasks']['statuses']['TENTATIVE'] = 'Tentative';
+$lang['tasks']['statuses']['DELEGATED'] = 'Delegated';
+$lang['tasks']['statuses']['COMPLETED'] = 'Completed';
+$lang['tasks']['statuses']['IN-PROCESS'] = 'In process';
 ?>
