aee679dbfc2e55be2dac15410f1cf677ed7680fc
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-10-13 06:49:13 +0000

    fixed small errors in log

--- trunk/www/language/Languages.js
+++ trunk/www/language/Languages.js
@@ -2,5 +2,6 @@ GO.Languages=[
 ["de", "Deutch"],
 ["en", "English"],
 ["fr", "Francais"],
+["it", "Italiano"],
 ["nl", "Nederlands"]
 ];
--- trunk/www/language/countries/en.inc.php
+++ trunk/www/language/countries/en.inc.php
@@ -1,6 +1,6 @@
 <?php
 //Uncomment this line in new translations!
-//require_once($GO_LANGUAGE->get_fallback_base_language_file('language'));
+//require_once($GO_LANGUAGE->get_fallback_base_language_file('countries'));
 
 $countries['AF'] = 'Afghanistan';
 $countries['AL'] = 'Albania';
