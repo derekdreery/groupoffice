3549fdd13272d33bbc712d92fac4a47fef2e061a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-10-14 07:09:23 +0000

    cleaned up groups module

--- www/modules/groups/groups.js
+++ www/modules/groups/groups.js
@@ -21,10 +21,6 @@
  	{
  		config={};
  	}
- 
-	//Ext.QuickTips.init();
-	//Ext.form.Field.prototype.msgTarget = 'side';
-	
 	
 	this.storeAllGroups = new GO.data.JsonStore({
 	    url: GO.settings.modules.groups.url+'json.php',
@@ -34,21 +30,31 @@
 	    fields: ['id', 'name', 'user_id', 'user_name'],
 	    remoteSort: true
 	});			
-	
-	//this.storeAllGroups.setDefaultSort('name', 'ASC');
 
 
 	var columnModel =  new Ext.grid.ColumnModel([
-        {id:'name',header: GO.groups.lang.groups, dataIndex: 'name', width: 300},
-        {id:'user_name',header: GO.groups.lang.owner, dataIndex: 'user_name'}	        			        
+        {header: GO.groups.lang.groups, dataIndex: 'name', width: 300},
+        {header: GO.groups.lang.owner, dataIndex: 'user_name'}	        			        
     ]);	
   columnModel.defaultSortable = true;
     
 	var tbar = new Ext.Toolbar({		
 			cls:'go-head-tb',
 			items: [
-    	{iconCls: 'btn-add', text: GO.lang['cmdAdd'], cls: 'x-btn-text-icon', handler: function(){this.showGroupDialog(0);}, scope: this},
-    	{iconCls: 'btn-delete', text: GO.lang['cmdDelete'], cls: 'x-btn-text-icon', handler: function(){this.deleteSelected();},  scope: this}
+    	{
+    		iconCls: 'btn-add', 
+    		text: GO.lang['cmdAdd'], 
+    		cls: 'x-btn-text-icon', 
+    		handler: function(){this.showGroupDialog(0);},
+    		scope: this
+    	},
+    	{
+    		iconCls: 'btn-delete', 
+				text: GO.lang['cmdDelete'], 
+				cls: 'x-btn-text-icon', 
+				handler: function(){this.deleteSelected();},  
+				scope: this
+			}
       ]});
       
   config.layout='fit';
@@ -58,7 +64,11 @@
   config.sm=new Ext.grid.RowSelectionModel({singleSelect: false});
   config.tbar=tbar;
   config.paging=true;
-  config.autoExpandColumn='user_name';
+  config.viewConfig={
+  	autoFill:true,
+  	forceFit:true
+  };
+  
 
 	GO.groups.MainPanel.superclass.constructor.call(this,config);
 
@@ -68,82 +78,15 @@ Ext.extend(GO.groups.MainPanel, GO.grid.GridPanel,{
 		
 		afterRender : function(){
 			GO.groups.MainPanel.superclass.afterRender.call(this);
+			
 			this.on("rowdblclick",this.rowDoubleClick, this);
+			
 			this.storeAllGroups.load();
 		},
 		
 		rowDoubleClick : function(grid)
 		{
-			//this.groupName = this.grid.selModel.selections.items[0].data.name;
 			this.showGroupDialog(grid.selModel.selections.items[0].data);
-			//alert(this.grid.selModel.selections.items[0].data.name);
-		},
-		
-
-		
-		showGroupButtonHandler : function (btn)
-		{
-			this.newFormPanel.container.mask(GO.lang['waitMsgSave'],'x-mask-loading');
-			
-			switch(btn.id)
-			{
-				case 'ok':
-					this.saveNewGroup(true);
-				break;
-				case 'apply':
-					this.saveNewGroup();
-				break;
-			}
-		},	
-		
-		addUserButtonHandler : function (btn)
-		{
-			switch(btn.id)
-			{
-				case 'add':		
-					if(!this.addUsersDialog)
-					{
-						this.addUsersDialog = new GO.dialog.SelectUsers({
-							handler:function(allUserGrid)
-							{
-								if(allUserGrid.selModel.selections.keys.length>0)
-								{
-									this.userStore.baseParams['add_users']=Ext.encode(allUserGrid.selModel.selections.keys);
-									this.userStore.reload();
-									delete this.userStore.baseParams['add_users'];
-								}
-							},
-							scope:this				
-						});
-					}
-					this.addUsersDialog.show();
-				break;
-				case 'delete':
-					//TODO nieuwe standard delete functie gebruiken
-					GO.deleteItems({
-						url: GO.settings.modules.groups.url+'action.php',  
-						params: {action: 'delete_user_from_group', delete_users: Ext.encode(this.userGrid.selModel.selections.keys), group_id: this.group_id}, 
-						count: this.userGrid.selModel.selections.keys.length,
-						callback: function(options, success, response){						
-							this.userStore.reload();
-						}, 
-						scope: this
-					});				
-				break;
-			}
-		},
-		
-
-		deleteGroup : function()
-		{
-			GO.deleteItems({
-				url: GO.settings.modules.groups.url+'action.php',  
-				params: {action: 'delete', groups: Ext.encode(this.selModel.selections.keys)}, 
-				count: this.selModel.selections.keys.length,
-				callback: function(options, success, response){						
-					this.storeAllGroups.reload();
-				}, 
-				scope: this});
 		},
 		
 		showGroupDialog : function(group)
@@ -158,108 +101,103 @@ Ext.extend(GO.groups.MainPanel, GO.grid.GridPanel,{
 				    fields: ['id', 'user_id', 'name', 'email'],
 				    remoteSort: true
 				});			
-				
-				//this.userStore.setDefaultSort('name', 'ASC');
-			
 	
 				var columnModel =  new Ext.grid.ColumnModel([
-			        {id:'name',header: GO.lang.strUsername, dataIndex: 'name'},
-			        {id:'email',header: GO.lang.strEmail, dataIndex: 'email', width: 150}	        			        
+			        {header: GO.lang.strUsername, dataIndex: 'name'},
+			        {header: GO.lang.strEmail, dataIndex: 'email'}	        			        
 			    ]);	
-			    columnModel.defaultSortable = true;				
-				
-				var tbar = [
-			    	{id: 'add', iconCls: 'btn-add', text: GO.lang['cmdAdd'], cls: 'x-btn-text-icon', handler: this.addUserButtonHandler, scope: this},
-			    	{id: 'delete', iconCls: 'btn-delete', text: GO.lang['cmdDelete'], cls: 'x-btn-text-icon', handler: this.addUserButtonHandler,  scope: this}
-		        ];
-		        
-		        var bbar = new Ext.PagingToolbar({
-		            store: this.userStore,
-		            pageSize: 20,
-		            displayInfo: true,
-		            displayMsg: GO.lang['displayingItems'],
-		            emptyMsg: GO.lang['strNoItems']
-		        });				
-				
-				var selectionModel = new Ext.grid.RowSelectionModel({singleSelect: false});
-				var autoScroll = true;
-				var trackMouseOver = false;
-				var plugins;
-				var autoExpandColumn = 'name';
-				var autoExpandMax = 2500;
-				var enableDragDrop = false;
-				var collapsible = false;		   
-		 			
-			    this.userGrid = new Ext.grid.GridPanel({
-			    	id: 'groups-grid-overview-users',
-			    	ds: this.userStore,
-			        cm: columnModel,
-			        sm: selectionModel,
-			        height: 300,
-			    	autoScroll: (autoScroll) ? autoScroll : false,
-			    	trackMouseOver: (trackMouseOver) ? trackMouseOver : true,  
-			        plugins: (plugins) ? plugins : null,
-			        tbar: (tbar) ? tbar : null,
-			        bbar: (bbar) ? bbar : null,
-			        autoExpandColumn: (autoExpandColumn) ? autoExpandColumn : null,
-			        autoExpandMax: (autoExpandMax) ? autoExpandMax : null,		        
-			        enableDragDrop: (enableDragDrop) ? enableDragDrop : false,
-			        collapsible: (collapsible) ? collapsible : false,
-			        border: true
-			        
-			    });
+			  columnModel.defaultSortable = true;		
+					
+			  this.userGrid = new GO.grid.GridPanel({
+			  	region:'center',
+		    	ds: this.userStore,
+	        cm: columnModel,
+	        sm: new Ext.grid.RowSelectionModel({singleSelect: false}),
+		    	autoScroll:true,			    	  
+	        border: false,
+	        viewConfig:{forceFit:true, autoFill:true},
+	        tbar:[
+			    	{
+			    		iconCls: 'btn-add', 
+			    		text: GO.lang['cmdAdd'], 
+			    		cls: 'x-btn-text-icon', 
+			    		handler:function(){
+						    if(!this.addUsersDialog)
+								{
+									this.addUsersDialog = new GO.dialog.SelectUsers({
+										handler:function(allUserGrid)
+										{
+											if(allUserGrid.selModel.selections.keys.length>0)
+											{
+												this.userStore.baseParams['add_users']=Ext.encode(allUserGrid.selModel.selections.keys);
+												this.userStore.reload();
+												delete this.userStore.baseParams['add_users'];
+											}
+										},
+										scope:this				
+									});
+								}
+								this.addUsersDialog.show();
+			    		}, 
+			    		scope: this
+			    	},
+			    	{
+			    		iconCls: 'btn-delete', 
+			    		text: GO.lang['cmdDelete'], 
+			    		cls: 'x-btn-text-icon', 
+			    		handler: function(){
+			    			this.userGrid.deleteSelected();
+			    		},  
+			    		scope: this
+			    	}
+		        ]
+		    });
 							
 				this.newFormPanel = new Ext.FormPanel({
-					id: 'groups-formpanel-new-group',
+					region:'north',
+					height:30,
+					waitMsgTarget:true,
 					labelWidth: 85,
 					defaultType: 'textfield',
-	    			bodyStyle: 'padding: 5px 5px 5px 5px;',
-	    			border: true,
-	    			items:[
-	    				{
-	    					id: 'name', 
-	    					fieldLabel: GO.groups.lang.lblNew, 
-	    					name: 'name',
-	    					width: '88%',
-	    					allowBlank: false
-	    				}		
-	    			]
+    			cls:'go-form-panel',
+    			border: false,
+    			items:[
+    				{
+    					fieldLabel: GO.groups.lang.lblNew, 
+    					name: 'name',
+    					anchor: '100%',
+    					allowBlank: false
+    				}		
+    			]
 				});			
 				
 				var buttons = [
-					{id: 'ok', text: GO.lang['cmdOk'], handler: this.showGroupButtonHandler, scope: this },
-					{id: 'apply', text: GO.lang['cmdApply'], handler: this.showGroupButtonHandler, scope: this },
-					{id: 'close', text: GO.lang['cmdClose'], handler: function(){this.new_dialog.hide();}, scope: this }
+					{text: GO.lang['cmdOk'], handler: function(){this.saveNewGroup(true);}, scope: this },
+					{text: GO.lang['cmdApply'], handler: function(){this.saveNewGroup();}, scope: this },
+					{text: GO.lang['cmdClose'], handler: function(){this.new_dialog.hide();}, scope: this }
 				];			
 				
+				var focusFirstField = function(){
+					this.newFormPanel.items.items[0].focus();
+				};
+				
 				this.new_dialog = new Ext.Window({
-					id: 'groups-window-new-group',
-					renderTo: document.body,
-					layout: 'form',
+					layout: 'border',
 					modal:false,
 					shadow: false,
-					autoHeight: true,
+					height: 400,
 					width: 500,
 					plain: true,
 					closeAction: 'hide',
-					title: GO.groups.lang.newGroupName,
-					enableTabScroll: true,
+					title: GO.groups.lang.group,					
 					items: [this.newFormPanel,this.userGrid],
 					buttons: buttons,
-					focus: function(){
-			 		    Ext.get('name').focus();
-					}
-				});
-				
-				var map = new Ext.KeyMap("groups-window-new-group", {
-				    key: 13,
-				    fn: this.saveNewGroup,
-				    scope: this
+					focus: focusFirstField.createDelegate(this)
 				});
 			}
-			
-			this.setGroup(group);
 			this.new_dialog.show();
+			this.setGroup(group);
+			
 		},
 		
 		setGroup : function(group)
@@ -301,27 +239,21 @@ Ext.extend(GO.groups.MainPanel, GO.grid.GridPanel,{
 					group_id : this.group_id
 				},
 				success:function(form, action){
-					this.newFormPanel.container.unmask();
-					
 					var group = {id: action.result.group_id};
-
 					this.setGroup(group);
-
 					this.storeAllGroups.reload();
-
 					if (hide)
 					{
 						this.new_dialog.hide();
-					}
-					
-					
+					}					
 				},
 				failure: function(form, action) {					
-					this.newFormPanel.container.unmask();
-					
 					if(action.failureType != 'client')
 					{					
 						Ext.MessageBox.alert(GO.lang['strError'], action.result.feedback);			
+					}else
+					{
+						Ext.MessageBox.alert(GO.lang['strError'], 'Errors in form');
 					}
 				},
 				scope: this
