3cffaf4bd0e50367df1439289a9ffb93aa232386
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-10-15 13:38:42 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.00-unstable-13/language/Languages.js
@@ -0,0 +1,7 @@
+GO.Languages=[
+["de", "Deutsch"],
+["en", "English"],
+["fr", "Francais"],
+["it", "Italiano"],
+["nl", "Nederlands"]
+];
---
+++ tags/groupoffice-com-3.00-unstable-13/modules/files/language/fr.inc.php
@@ -0,0 +1,18 @@
+<?php
+//French Translation v1.0
+//Author : Lionel JULLIEN
+//Date : September, 04 2008
+
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('files'));
+
+$lang['files']['name'] = 'Fichiers';
+$lang['files']['description'] = 'Module gestion des fichiers. Module pour partager des fichiers entre utilisateurs de Group-Office.';
+
+$lang['files']['fileNotFound'] = 'Fichier introuvable';
+$lang['files']['folderExists'] = 'Le dossier existe déjà';
+$lang['files']['filenameExists'] = 'Le nom de fichier existe déjà';
+$lang['files']['uploadedSucces'] = 'Fichiers envoyé avec succès';
+
+$lang['files']['ootextdoc']='Document texte Open-Office';
+$lang['files']['wordtextdoc']='Document Microsoft Word';
