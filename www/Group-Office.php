04f87e8e4a8995b79048c89558d99cf203ccd3e7
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-10-23 21:42:56 +0000

    Better function to undo magic quotes

--- www/Group-Office.php
+++ www/Group-Office.php
@@ -1,12 +1,12 @@
 <?php
-/** 
+/**
  * Copyright Intermesh
- * 
+ *
  * This file is part of Group-Office. You should have received a copy of the
  * Group-Office license along with Group-Office. See the file /LICENSE.TXT
- * 
+ *
  * If you have questions write an e-mail to info@intermesh.nl
- * 
+ *
  * @copyright Copyright Intermesh
  * @version $Id$
  * @author Merijn Schering <mschering@intermesh.nl>
@@ -15,10 +15,10 @@
 /**
  * This class holds the main configuration options of Group-Office
  * Don't modify this file. The values defined here are just default values.
- * They are overwritten by the configuration options in local/config.php. 
+ * They are overwritten by the configuration options in local/config.php.
  * To edit these options use install.php.
- * 
- * @copyright Copyright Intermesh 
+ *
+ * @copyright Copyright Intermesh
  * @version $Id$
  * @author Merijn Schering <mschering@intermesh.nl>
  * @since Group-Office 1.0
@@ -45,9 +45,9 @@ if($GO_CONFIG->debug)
 
 //preload classes before session so they can be stored in the session
 if ( isset( $GO_INCLUDES ) ) {
-  while ( $include = array_shift( $GO_INCLUDES ) ) {
-    require_once( $include );
-  }
+	while ( $include = array_shift( $GO_INCLUDES ) ) {
+		require_once( $include );
+	}
 }
 
 //start session
@@ -64,8 +64,8 @@ if(!isset($_SESSION['DIR_CHECK']))
 }elseif($_SESSION['DIR_CHECK'] != $GO_CONFIG->root_path)
 {
 	go_log(LOG_DEBUG, 'Session root path check failed. Stored root path in session: '.
-		$_SESSION['DIR_CHECK'].' doesn\'t match the configured one: '.$GO_CONFIG->root_path);
-		
+	$_SESSION['DIR_CHECK'].' doesn\'t match the configured one: '.$GO_CONFIG->root_path);
+
 	session_destroy();
 	unset($_SESSION);
 }
@@ -79,7 +79,7 @@ require_once($GO_CONFIG->class_path.'base/exceptions.class.inc.php');
 require_once($GO_CONFIG->class_path.'base/auth.class.inc.php');
 require_once($GO_CONFIG->class_path.'base/security.class.inc.php');
 require_once($GO_CONFIG->class_path.'base/groups.class.inc.php');
-require_once($GO_CONFIG->class_path.'base/users.class.inc.php');	
+require_once($GO_CONFIG->class_path.'base/users.class.inc.php');
 require_once($GO_CONFIG->class_path.'base/modules.class.inc.php');
 require_once($GO_CONFIG->class_path.'base/language.class.inc.php');
 require_once($GO_CONFIG->class_path.'base/theme.class.inc.php');
@@ -101,16 +101,16 @@ $GO_SECURITY = new GO_SECURITY();
 $GO_LINKS = new GO_LINKS();
 
 if (isset($_REQUEST['SET_LANGUAGE'])){
-  $GO_LANGUAGE->set_language($_REQUEST['SET_LANGUAGE']);
+	$GO_LANGUAGE->set_language($_REQUEST['SET_LANGUAGE']);
 }
 
 require($GO_LANGUAGE->get_base_language_file('common'));
 //require($GO_LANGUAGE->get_base_language_file('filetypes'));
 
 if ( $GO_CONFIG->log ) {
-  $username = isset($_SESSION['GO_SESSION']['username']) ? $_SESSION['GO_SESSION']['username'] : 'notloggedin';
-  define_syslog_variables();
-  openlog('[Group-Office]['.date('Ymd G:i').']['.$username.']', LOG_PERROR, LOG_LOCAL0);
+	$username = isset($_SESSION['GO_SESSION']['username']) ? $_SESSION['GO_SESSION']['username'] : 'notloggedin';
+	define_syslog_variables();
+	openlog('[Group-Office]['.date('Ymd G:i').']['.$username.']', LOG_PERROR, LOG_LOCAL0);
 }
 
 
@@ -131,9 +131,23 @@ unset($type);
 
 define('GO_LOADED', true);
 
-if(get_magic_quotes_gpc())
+//undo magic quotes
+if (get_magic_quotes_gpc())
 {
-	$_POST=array_map('stripslashes', $_POST);
-	$_REQUEST=array_map('stripslashes', $_REQUEST);
-	$_GET=array_map('stripslashes', $_GET);
+
+	function stripslashes_array($data) {
+		if (is_array($data)){
+			foreach ($data as $key => $value){
+				$data[$key] = stripslashes_array($value);
+			}
+			return $data;
+		}else{
+			return stripslashes($data);
+		}
+	}
+
+	$_REQUEST=stripslashes_array($_REQUEST);
+	$_GET=stripslashes_array($_GET);
+	$_POST=stripslashes_array($_POST);
+	$_COOKIE=stripslashes_array($_COOKIE);
 }
