948d5c5f3678614179cd2cc3d422a66915111f4a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-10-29 15:24:35 +0000

    invalid tag

--- tags/groupoffice-com-3.00-unstable-16cd/modules/groups/language/de.inc.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-//Uncomment this line in new translations!
-//require_once($GO_LANGUAGE->get_fallback_language_file('files'));
-$lang['groups']['name'] = 'Gruppen';
-$lang['groups']['description'] = 'Admin-Modul; Verwalten von Benutzergruppen.';
-$lang['groups']['noDeleteAdmins'] = 'Die Gruppen-Admins können nicht gelöscht werden';
-$lang['groups']['noDeleteEveryone'] = 'Die Gruppe Everyone kann nicht gelöscht werden';
-$lang['groups']['groupNameAlreadyExists'] = 'Die Gruppe existiert bereits';
-?>
--- tags/groupoffice-com-3.00-unstable-16cd/modules/summary/language/en.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$lang['summary']['name']='Summary';
-$lang['summary']['description']='Put a description here';
-
-$lang['summary']['default_rss_url']='http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml';
-$lang['summary']['announcement']='Announcement';
-$lang['summary']['announcements']='Announcements';
