946d8b52260e105a9c9832b05a719f309fab0197
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-10-29 15:22:09 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.00-unstable-16cd/modules/groups/language/it.js
@@ -0,0 +1,9 @@
+Ext.namespace('GO.groups');
+
+GO.groups.lang={};
+
+GO.groups.lang.groups = 'Gruppi';
+GO.groups.lang.owner = 'Proprietario';
+GO.groups.lang.lblNew = 'Nuovo';
+GO.groups.lang.newGroupName = 'Inserisci il nome del nuovo gruppo';
+GO.groups.lang.searchAllUsers = 'Cerca tutti gli utenti';
