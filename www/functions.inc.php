e660c36935f2db588afbeaaa0e5272b6afb34616
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-10-30 12:58:51 +0000

    New mime detection function

--- www/functions.inc.php
+++ www/functions.inc.php
@@ -15,12 +15,17 @@
 /**
  * This file holds global functions for use inside Group-Office
  *
- * @package Framework
+ * @package go.global
  * @author   Merijn Schering <mschering@intermesh.nl>
  * @since    Group-Office 1.0
  */
 
 
+/**
+ * Attempts to autoload class files
+ *
+ * @param string $class_name
+ */
 
 function __autoload($class_name) {
 		global $GO_CONFIG;
@@ -31,6 +36,14 @@ function __autoload($class_name) {
 		}*/
     require_once $GO_CONFIG->class_path. $class_name.'.class.inc.php';
 }
+
+/**
+ * Get's the last file or directory name of a filesystem path and works
+ * with UTF-8 too unlike the basename function in PHP.
+ *
+ * @param string $path
+ * @return string basename
+ */
 
 function utf8_basename($path)
 {
@@ -44,56 +57,6 @@ function utf8_basename($path)
 		$path = substr($path,0,-1);
 	}
 	return mb_substr($path, mb_strrpos($path, '/')+1);
-}
-
-function resize_image($src, $maxsize)
-{
-	//De hoogte en breedte ophalen van het plaatje
-	$dimensions = getimagesize($src);
-
-
-	//Hoogte en breedte toekennnen aan nieuwe variabelen
-	$old_width = $dimensions[0];
-	$old_height  = $dimensions[1];
-
-	if($old_width<$maxsize && $old_height<$maxsize)
-	{
-		return true;
-	}
-
-	if($old_width>$old_height)
-	{
-		$new_width=$maxsize;
-		//De nieuwe hoogte berekenen aan de gegevens van het oude plaatje en de doel breedte
-		$new_height = ($old_height * $new_width) / $old_width;
-
-		//De hoogte, als het nodig is, afronden
-		$new_height = round($new_height, 0);
-	}else {
-		$new_height=$maxsize;
-		$new_width = ($old_width * $new_height) / $old_height;
-		$new_width = round($new_width, 0);
-	}
-
-
-	//Het plaatje inlezen in de variabele $image
-	$image = imagecreatefromjpeg($src);
-
-	//een nieuw klein plaatje maken met de gewenste grootte
-	$destination = imagecreatetruecolor($new_width, $new_height);
-
-	//Het nieuwe plaatje vullen met verkleinde plaatje
-	imagecopyresampled($destination, $image, 0, 0, 0, 0, $new_width, $new_height, $old_width, $old_height);
-
-	//Het plaatje weergeven
-	imagejpeg($destination, $src);
-
-	//Het bronplaatje verwijderen
-	imagedestroy($image);
-
-	//Het doelplaatje verwijderen
-	imagedestroy($destination);
-
 }
 
 /**
@@ -114,11 +77,22 @@ function go_log($level, $message) {
 	}
 }
 
+/**
+ * Set's the debug log location
+ *
+ * @param string $file
+ */
+
 function set_debug_log($file)
 {
 	$_SESSION['GO_SESION']['debug_log']=$file;
 }
 
+/**
+ * Write's to a debug log.
+ *
+ * @param string $text log entry
+ */
 
 function debug($text)
 {
