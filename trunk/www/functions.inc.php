736d865ca11da35f4753a83073487bb969bcf983
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-10-30 13:12:52 +0000

    documentation

--- trunk/www/functions.inc.php
+++ trunk/www/functions.inc.php
@@ -1,13 +1,4 @@
 <?php
-/** 
- * Copyright Intermesh
- * 
- * This file is part of Group-Office. You should have received a copy of the
- * Group-Office license along with Group-Office. See the file /LICENSE.TXT
- * 
- * If you have questions write an e-mail to info@intermesh.nl
- */
- 
 /**
  * This file holds global functions for use inside Group-Office
  *  
