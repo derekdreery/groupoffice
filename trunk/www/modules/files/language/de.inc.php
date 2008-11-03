d5c9a4790af5b83dedf4fcd53cef3c5a04afa8c4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-11-03 12:38:33 +0000

    Change require in translations

--- trunk/www/modules/files/language/de.inc.php
+++ trunk/www/modules/files/language/de.inc.php
@@ -1,6 +1,6 @@
 ﻿<?php
 //Uncomment this line in new translations!
-//require_once($GO_LANGUAGE->get_fallback_language_file('files'));
+//require($GO_LANGUAGE->get_fallback_language_file('files'));
 $lang['files']['name'] = 'Dateien';
 $lang['files']['description'] = '	
 Modul-Dateien; Modul für die gemeinsame Nutzung von Dateien.';
