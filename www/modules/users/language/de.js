43c2dd19fa1b3d979e7be26b1865bfe12a782ba9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-11-03 08:34:52 +0000

    corrected encoding

--- www/modules/users/language/de.js
+++ www/modules/users/language/de.js
@@ -5,9 +5,9 @@ GO.users.lang.cmdHeaderColumnName= 'Modulname';
 GO.users.lang.cmdHeaderColumnGroups= 'Gruppen';
 GO.users.lang.cmdHeaderGridUserHasAccess = 'Der Benutzer hat Zugriff auf';
 GO.users.lang.userIsMemberOf = 'Der Benutzer ist Mitglied von';
-GO.users.lang.userVisibleTo = 'Der Benutzer ist für folgende Gruppen sichtbar';
+GO.users.lang.userVisibleTo = 'Der Benutzer ist fÃ¼r folgende Gruppen sichtbar';
 
-GO.users.lang.cmdPanelPersonal = 'Persönlich';
+GO.users.lang.cmdPanelPersonal = 'PersÃ¶nlich';
 GO.users.lang.cmdPanelPassword= 'Passwort';
 GO.users.lang.cmdPanelCompany = 'Firma';
 GO.users.lang.cmdPanelPermissions = 'Berechtigungen';
@@ -17,7 +17,7 @@ GO.users.lang.cmdPanelSettings = 'Einstellungen';
 GO.users.lang.cmdPanelEnabledMail = 'Email for new enabled users';
 GO.users.lang.cmdPanelDisabledMail = 'Email for new disabled users';
 	
-GO.users.lang.cmdFieldSetPersonal = 'Persönlich';	
+GO.users.lang.cmdFieldSetPersonal = 'PersÃ¶nlich';	
 GO.users.lang.cmdUserWindowTitle= 'Bearbeite Benutzer';
 GO.users.lang.cmdNotificationWindowTitle= 'Bearbeite Benutzer Einladungsemail';
 GO.users.lang.cmdMaskSaving= 'Speichern';
@@ -26,7 +26,7 @@ GO.users.lang.cmdFormLabelSubject = 'Subject';
 GO.users.lang.cmdFormLabelUsername = 'Benutername';
 GO.users.lang.cmdFormLabelName = 'Name';
 GO.users.lang.cmdFormLabelPassword = 'Passwort';
-GO.users.lang.cmdFormLabelConfirm = 'Bestätigen';
+GO.users.lang.cmdFormLabelConfirm = 'BestÃ¤tigen';
 GO.users.lang.cmdBoxLabelEnabled = 'Aktiviert';
 	
 GO.users.lang.cmdFormLabelHomepage = 'Homepage';
@@ -47,20 +47,20 @@ GO.users.lang.timeFormat = 'zeitformat';
 GO.users.lang.cmdFormLabelFirstWeekday = 'Erster Wochentag';
 GO.users.lang.cmdFormLabelThousandSeperator = 'Tausender-Trennzeichen';
 GO.users.lang.cmdFormLabelDecimalSeperator = 'Dezimal-Trennzeichen';
-GO.users.lang.cmdFormLabelCurrency = 'Währung'
+GO.users.lang.cmdFormLabelCurrency = 'WÃ¤hrung'
 
 GO.users.lang.account = 'Konto';
-GO.users.lang.confirmPassword = 'Passwort bestätigen';
+GO.users.lang.confirmPassword = 'Passwort bestÃ¤tigen';
 GO.users.lang.companyProfile = 'Firmenprofil';
 GO.users.lang.loginInfo = 'Login-Daten';
-GO.users.lang.changePassword = 'Passwort ändern';
+GO.users.lang.changePassword = 'Passwort Ã¤ndern';
 GO.users.lang.currentPassword = 'Aktuelles Passwort';
 GO.users.lang.newPassword = 'Neues passwort';
-GO.users.lang.confirmPassword = 'Passwort bestätigen';
+GO.users.lang.confirmPassword = 'Passwort bestÃ¤tigen';
 GO.users.lang.moduleAccess = 'Modulzugriff';
 GO.users.lang.groupMembership = 'Gruppen-Mitgliedschaft';
-GO.users.lang.profile = 'Persönliches Profil';
-GO.users.lang.settings = 'Persönliche Einstellungen';
+GO.users.lang.profile = 'PersÃ¶nliches Profil';
+GO.users.lang.settings = 'PersÃ¶nliche Einstellungen';
 GO.users.lang.dmy = 'Tag-Monat-Jahr';
 GO.users.lang.mdy = 'Monat-Tag-Jahr';
 GO.users.lang.jmd = 'Jahr-Monat-Tag';
@@ -73,4 +73,4 @@ GO.users.lang.userSettings = 'Benutzer-Einstellungen';
 
 GO.users.lang.group='Gruppe';
 
-GO.users.lang.maxUsersReached='Die maximale Anzahl von Benutzern wurde erreicht. Gehen Sie zu der Registerkarte um die maximale Benutzeranzahl zu erhöhen.';
+GO.users.lang.maxUsersReached='Die maximale Anzahl von Benutzern wurde erreicht. Gehen Sie zu der Registerkarte um die maximale Benutzeranzahl zu erhÃ¶hen.';
