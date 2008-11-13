c36c200ad290c2bad007c9262c7a6052eba96706
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-11-13 14:43:22 +0000

    start in personal category

--- trunk/www/modules/notes/scripts.inc.php
+++ trunk/www/modules/notes/scripts.inc.php
@@ -7,3 +7,12 @@ if(isset($GO_MODULES->modules['customfields']))
 	$cf = new customfields();
 	echo $cf->get_javascript(4, $lang['notes']['notes']);
 }
+
+require_once($GO_MODULES->modules['notes']['class_path'].'notes.class.inc.php');
+$notes = new notes();
+
+$category = $notes->get_category();
+?>
+<script type="text/javascript">
+GO.notes.defaultCategory = {id: <?php echo $category['id']; ?>, name: "<?php echo $category['name']; ?>"};
+</script>
