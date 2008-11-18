3ba0879cb865b545949a84c8dfcfd5a1a66d9c1d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-11-18 11:18:06 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.00-unstable-20/modules/email/classes/cached_imap.class.inc.php
@@ -0,0 +1,406 @@
+<?php
+class cached_imap extends imap{
+
+	/**
+	 * E-mail module object to connect to the database
+	 *
+	 * @var unknown_type
+	 */
+	var $email;
+	
+	/**
+	 * The opened folder in the database cache
+	 *
+	 * @var unknown_type
+	 */
+	var $folder;
+	
+	var $folder_sort_cache;
+	
+	/**
+	 * E-mail account record
+	 *
+	 * @var unknown_type
+	 */
+	var $account;
+	
+	var $filters=array();
+	
+	var $filtered=0;
+
+	
+	function __construct()
+	{
+		$this->email = new email();
+		parent::__construct();
+	}
+	
+	
+	/**
+	 * Opens a connection to server
+	 *
+	 * @param	string	$host					The hostname of the mailserver
+	 * @param	string	$type					The type of the mailserver (IMAP or POP-3)
+	 * @param	int 			$port 					The port to connect to
+	 * @param	string	$username	The username
+	 * @param	string	$password		The password
+	 * @param	string	$mailbox			The mailbox to open
+	 * @param	string	$flags					Connection flags (See PHP docs imap_open()
+	 * @param	bool		$ssl						Connect in SSL mode or not
+	 * @param	bool		$novalidate_cert						Don't validate SSL certificate
+	 * @access public
+	 * @return mixed	The recource ID on success or false on failure
+	 */
+	function open($account, $mailbox='INBOX') {
+		
+		$this->account = $account;
+		
+		$conn = parent::open($account['host'], $account['type'], $account['port'], $account['username'], $account['password'], $mailbox, null, $account['use_ssl'], $account['novalidate_cert']);
+		
+		$this->folder = $this->email->get_folder($this->account['id'],$mailbox);
+		
+		$this->folder_sort_cache=json_decode($this->folder['sort'], true);
+		
+		return $conn;
+	}
+	
+	/**
+	 * Sort message UID's into $this->sort (see imap_sort() PHP docs)
+	 *
+	 * @param	int	$sort_type	The column
+	 * @param	string $reverse Reverse sorting (0 or 1)
+	 * @param	string $search Search query
+	 * @access public
+	 * @return int	 Number of sorted messages
+	 */
+	function sort($sort_type = SORTDATE, $reverse = "1", $query = '') {
+		
+		$this->sort_type=$sort_type;
+		$this->sort_reverse=$reverse;
+
+		if ($query != '') {
+			parent::sort($sort_type, $reverse, $query);
+		} else {
+			if($this->folder['msgcount']==$this->count && $this->folder['unseen']==$this->unseen && isset($this->folder_sort_cache[$sort_type.'_'.$reverse]))
+			{
+				debug('Used cached sort info');
+				$this->sort = $this->folder_sort_cache[$sort_type.'_'.$reverse];
+			}else
+			{
+				$this->sort = imap_sort($this->conn, $sort_type, $reverse, SE_UID+SE_NOPREFETCH);				
+				$this->folder_sort_cache[$sort_type.'_'.$reverse]=$this->sort;
+				
+				$up_folder['id'] = $this->folder['id'];
+				$up_folder['sort']=json_encode($this->folder_sort_cache);
+				$up_folder['unseen']=$this->unseen;
+				$up_folder['msgcount']=$this->count;
+				
+				$this->email->__update_folder($up_folder);
+			}
+		}
+	}
+	
+
+	
+	/**
+		* Delete messages from the IMAP server
+		*
+		* @param Array $messages An array of message UID's
+		* @access public
+		* @return void
+		*/
+
+	function delete($messages) {
+		if(count($messages))
+		{
+			if(parent::delete($messages))
+			{
+				$this->delete_cached_messages($messages);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+		* Move messages to another mailbox
+		*
+		* @param String $folder The mailbox where the messages need to go
+		* @param Array $messages An array of message UID's to move
+		* @access public
+		* @return bool True on success
+		*/
+	function move($folder, $messages, $expunge=true) {
+		if(count($messages))
+		{
+			if(parent::move($folder, $messages, $expunge))
+			{
+				$this->delete_cached_messages($messages);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	function delete_cached_messages($uids)
+	{
+		/*$sql = "SELECT count(*) AS count FROM em_messages_cache WHERE new='1' AND folder_id=".$this->email->escape($this->folder['id'])." AND uid IN(".$this->email->escape(implode(',',$uids)).")";
+		$this->query($sql);
+		$record = $this->next_record();*/
+		
+		$sql = "DELETE FROM em_messages_cache WHERE folder_id=".$this->email->escape($this->folder['id'])." AND uid IN(".$this->email->escape(implode(',',$uids)).")";
+		$this->email->query($sql);
+		debug('Deleted '.implode(',', $uids).' from cache');
+		if(isset($this->sort_reverse))
+		{
+			debug('Removed '.implode(',', $uids).' from sort');
+			//remove uids from cached sort
+			$sort = $this->sort;		
+			$this->sort=array();
+			$removed=0;
+			$total = count($uids);
+			foreach($sort as $uid)
+			{
+				if($total==$removed || !in_array($uid, $uids))
+				{					
+					$this->sort[]=$uid;
+					$removed++;					
+				}
+			}
+			$this->folder_sort_cache[$this->sort_type.'_'.$this->sort_reverse]=$this->sort;
+			
+			$up_folder['id'] = $this->folder['id'];
+			$up_folder['sort']=json_encode($this->folder_sort_cache);
+			$up_folder['unseen']=$this->unseen;
+			$up_folder['msgcount']=$this->count;
+			
+			$this->email->__update_folder($up_folder);
+		}
+	}
+	
+	function set_unseen_cache($uids, $new)
+	{
+		$new_val = $new ? '1' : '0';
+		
+		$sql = "UPDATE em_messages_cache SET new='".$new_val."' WHERE folder_id=".$this->email->escape($this->folder['id'])." AND uid IN(".$this->email->escape(implode(',',$uids)).")";
+		$this->email->query($sql);
+		
+		$affected_rows = $this->email->affected_rows();
+		
+		if($affected_rows>0)
+		{
+			$operator = $new ? '+' : '-';
+			
+			$sql = "UPDATE em_folders SET unseen=unseen$operator? WHERE id=?";
+			$this->email->query($sql, 'ii', array($affected_rows, $this->folder['id']));
+			debug('Adding '.$operator.$affected_rows.' unseen');		
+		}
+	}
+	
+	function set_flagged_cache($uids, $flagged)
+	{
+		$new_val = $flagged ? '1' : '0';
+		
+		$sql = "UPDATE em_messages_cache SET flagged='".$new_val."' WHERE folder_id=".$this->email->escape($this->folder['id'])." AND uid IN(".$this->email->escape(implode(',',$uids)).")";
+		$this->email->query($sql);
+	}
+
+	function get_message_headers($start, $limit, $sort_field , $sort_order, $query)
+	{
+		$uids = $this->get_message_uids($start, $limit, $sort_field , $sort_order, $query);
+
+		$messages=array();
+		$this->filtered=array();
+		
+		if(count($uids))
+		{
+			$this->get_cached_messages($this->folder['id'], $uids);
+			
+			//get messages from cache
+			while($message = $this->email->next_record())
+			{
+				$message['cached']=true;
+				$messages[$message['uid']]=$message;
+			}
+
+			debug('Got '.count($messages).' from cache');
+			
+			$uncached_uids=array();
+			for($i=0;$i<count($uids);$i++)
+			{
+				if(!isset($messages[$uids[$i]]))
+				{
+					$uncached_uids[]=$uids[$i];
+				}
+			}		
+						
+			if(count($uncached_uids))
+			{				
+				$new_messages = $this->get_filtered_message_headers($uncached_uids);
+
+				foreach($new_messages as $message)
+				{
+					$messages[$message['uid']]=$message;
+					$messages[$message['uid']]['cached']=false;
+					
+					$message['folder_id']=$this->folder['id'];
+					$message['account_id']=$this->account['id'];
+					$this->add_cached_message($message);
+				}
+			}
+			debug('Got '.count($uncached_uids).' from IMAP server');
+			
+			if(count($this->filtered))
+			{
+				debug('Extra messages start '.($this->first+$this->offset-count($this->filtered)));
+				debug('Extra messages offset '.count($this->filtered));
+					
+				$extra_messages = $this->get_message_headers($this->first+$this->offset-count($this->filtered), count($this->filtered), $sort_field , $sort_order, $query);
+				foreach($extra_messages as $uid=>$message)
+				{
+					$messages[$uid]=$message;
+				}
+			}
+		}
+		return $messages;
+	}
+	
+	function set_filters($filters)
+	{
+		$this->filters=$filters;
+	}
+	
+	function get_filtered_message_headers($uids)
+	{
+		$messages=array();
+		$this->filtered=array();		
+	
+		$new_messages = parent::get_message_headers($uids);
+		if(strtoupper($this->mailbox)!='INBOX')
+		{
+			return $new_messages;
+		}		
+
+		while($message = array_shift($new_messages))
+		{			
+			if($message['new']=='1')
+			{
+				$continue=false;
+				
+				for ($i=0;$i<sizeof($this->filters);$i++)
+				{
+					if(!isset($this->filters[$i]['uids']))
+						$this->filters[$i]['uids']=array();
+					
+					$field = $message[$this->filters[$i]["field"]];
+
+					if (stristr($field, $this->filters[$i]["keyword"]))
+					{									
+						$this->filters[$i]['uids'][]=$message['uid'];							
+						$continue=true;
+						break;					
+					}												
+				}
+				if ($continue)
+				{					
+					//message was filtered so dont't add it						
+					continue;
+				}											
+			}
+			$messages[]=$message;
+		}
+
+		for ($i=0;$i<sizeof($this->filters);$i++)
+		{
+			if(isset($this->filters[$i]['uids']) && count($this->filters[$i]['uids']))
+			{
+				if($this->filters[$i]['mark_as_read'])
+				{
+					$ret = $this->set_message_flag($this->mailbox, $this->filters[$i]['uids'], "\\Seen");
+				}
+				if(parent::move($this->filters[$i]["folder"], $this->filters[$i]['uids'],false))
+				{
+					foreach($this->filters[$i]['uids'] as $uid)
+					{
+						$this->filtered[]=$uid;
+					}
+				}
+			}
+		}
+		if(count($this->filtered))
+		{
+			$this->expunge();
+			
+			$this->unseen-=count($this->filtered);
+			$this->count-=count($this->filtered);
+			
+			$this->delete_cached_messages($this->filtered);
+		}
+		return $messages;		
+	}
+	
+	
+	
+	/**
+	 * Add a Cached message
+	 *
+	 * @param Array $cached_message Associative array of record fields
+	 *
+	 * @access public
+	 * @return int New record ID created
+	 */
+
+	function add_cached_message($cached_message)
+	{
+		return $this->email->insert_row('em_messages_cache', $cached_message);
+	}
+	
+	
+	function clear_cache($folder_id=0){
+		if($folder_id==0)
+		{
+			$sql = "DELETE FROM em_messages_cache WHERE account_id=?";
+			$this->email->query($sql, 'i', $this->account['id']);
+			
+			$sql = "UPDATE em_folders SET sort='' WHERE account_id=?";
+			$this->email->query($sql, 'i', $this->account['id']);
+		}else
+		{
+			$sql = "DELETE FROM em_messages_cache WHERE folder_id=?";
+			$this->email->query($sql, 'i', $folder_id);
+			
+			$sql = "UPDATE em_folders SET sort='' WHERE id=?";
+			$this->email->query($sql, 'i', $folder_id);
+		}
+	}
+
+	/**
+	 * Update a Cached message
+	 *
+	 * @param Array $cached_message Associative array of record fields
+	 *
+	 * @access public
+	 * @return bool True on success
+	 */
+
+	function update_cached_message($cached_message)
+	{
+		return $this->email->update_row('em_messages_cache', 'id', $cached_message);
+	}
+
+	/**
+	 * Gets a Cached message record
+	 *
+	 * @param Int $cached_message_id ID of the cached_message
+	 *
+	 * @access public
+	 * @return Array Record properties
+	 */
+
+	function get_cached_messages($folder_id, $uids)
+	{
+		//TODO dont select all fields
+		$this->email->query("SELECT * FROM em_messages_cache WHERE folder_id=".$this->email->escape($folder_id)." AND uid IN (".$this->email->escape(implode(',',$uids)).")");
+	}
+
+}
---
+++ tags/groupoffice-com-3.00-unstable-20/modules/users/language/de.js
@@ -0,0 +1,76 @@
+GO.users.lang.cmdCheckColumnRead= 'Lesen';
+GO.users.lang.cmdCheckColumnWrite= 'Schreiben';
+GO.users.lang.cmdCheckColumnGroups= 'Gruppen';
+GO.users.lang.cmdHeaderColumnName= 'Modulname';
+GO.users.lang.cmdHeaderColumnGroups= 'Gruppen';
+GO.users.lang.cmdHeaderGridUserHasAccess = 'Der Benutzer hat Zugriff auf';
+GO.users.lang.userIsMemberOf = 'Der Benutzer ist Mitglied von';
+GO.users.lang.userVisibleTo = 'Der Benutzer ist für folgende Gruppen sichtbar';
+
+GO.users.lang.cmdPanelPersonal = 'Persönlich';
+GO.users.lang.cmdPanelPassword= 'Passwort';
+GO.users.lang.cmdPanelCompany = 'Firma';
+GO.users.lang.cmdPanelPermissions = 'Berechtigungen';
+GO.users.lang.cmdPanelLogin = 'Anmelden';
+GO.users.lang.cmdPanelLookFeel = 'Look & Feel';
+GO.users.lang.cmdPanelSettings = 'Einstellungen';
+GO.users.lang.cmdPanelEnabledMail = 'Email for new enabled users';
+GO.users.lang.cmdPanelDisabledMail = 'Email for new disabled users';
+	
+GO.users.lang.cmdFieldSetPersonal = 'Persönlich';	
+GO.users.lang.cmdUserWindowTitle= 'Bearbeite Benutzer';
+GO.users.lang.cmdNotificationWindowTitle= 'Bearbeite Benutzer Einladungsemail';
+GO.users.lang.cmdMaskSaving= 'Speichern';
+	
+GO.users.lang.cmdFormLabelSubject = 'Subject';
+GO.users.lang.cmdFormLabelUsername = 'Benutername';
+GO.users.lang.cmdFormLabelName = 'Name';
+GO.users.lang.cmdFormLabelPassword = 'Passwort';
+GO.users.lang.cmdFormLabelConfirm = 'Bestätigen';
+GO.users.lang.cmdBoxLabelEnabled = 'Aktiviert';
+	
+GO.users.lang.cmdFormLabelHomepage = 'Homepage';
+GO.users.lang.cmdFormLabelRegistrationTime = 'Registrierungszeit';
+GO.users.lang.cmdFormLabelLastLogin = 'Letzte Anmeldung';
+GO.users.lang.cmdFormLabelTotalLogins = 'Gesamte Anmeldungen';
+GO.users.lang.numberOfLogins = 'Anzahl der Anmeldungen';
+GO.users.lang.cmdFormLabelTheme = 'Thema';
+GO.users.lang.cmdFormLabelStartModule = 'Beginne in Modul';
+GO.users.lang.cmdFormLabelMaximunRows = 'Maximale Anzahl von Zeilen in Listen';
+GO.users.lang.cmdFormLabelSortNamesBy = 'Nachnahme';
+GO.users.lang.cmdFormLabelLanguage = 'Sprache';
+GO.users.lang.cmdFormLabelTimezone = 'Zeitzone';
+GO.users.lang.cmdFormLabelAdjustDaylight = 'Automatische Anpassung der Zeit auf Sommerzeit';
+GO.users.lang.cmdFormLabelDateFormat = 'Datumsformat';
+GO.users.lang.cmdFormLabelDateSeperator = 'Datums-Trennzeichen';
+GO.users.lang.timeFormat = 'zeitformat';
+GO.users.lang.cmdFormLabelFirstWeekday = 'Erster Wochentag';
+GO.users.lang.cmdFormLabelThousandSeperator = 'Tausender-Trennzeichen';
+GO.users.lang.cmdFormLabelDecimalSeperator = 'Dezimal-Trennzeichen';
+GO.users.lang.cmdFormLabelCurrency = 'Währung'
+
+GO.users.lang.account = 'Konto';
+GO.users.lang.confirmPassword = 'Passwort bestätigen';
+GO.users.lang.companyProfile = 'Firmenprofil';
+GO.users.lang.loginInfo = 'Login-Daten';
+GO.users.lang.changePassword = 'Passwort ändern';
+GO.users.lang.currentPassword = 'Aktuelles Passwort';
+GO.users.lang.newPassword = 'Neues passwort';
+GO.users.lang.confirmPassword = 'Passwort bestätigen';
+GO.users.lang.moduleAccess = 'Modulzugriff';
+GO.users.lang.groupMembership = 'Gruppen-Mitgliedschaft';
+GO.users.lang.profile = 'Persönliches Profil';
+GO.users.lang.settings = 'Persönliche Einstellungen';
+GO.users.lang.dmy = 'Tag-Monat-Jahr';
+GO.users.lang.mdy = 'Monat-Tag-Jahr';
+GO.users.lang.jmd = 'Jahr-Monat-Tag';
+GO.users.lang.fullhourformat = '24 Stunden Format';
+GO.users.lang.halfhourformat = '12 Stunden Format';
+GO.users.lang.sunday = 'Sonntag';
+GO.users.lang.monday = 'Montag';
+GO.users.lang.regionalSettings = 'Regionale Einstellungen';
+GO.users.lang.userSettings = 'Benutzer-Einstellungen';
+
+GO.users.lang.group='Gruppe';
+
+GO.users.lang.maxUsersReached='Die maximale Anzahl von Benutzern wurde erreicht. Gehen Sie zu der Registerkarte um die maximale Benutzeranzahl zu erhöhen.';
