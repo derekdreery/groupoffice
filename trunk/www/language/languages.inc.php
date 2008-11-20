fd1633aebd8867b4c9b28a8cfe0ad88cb0a5af17
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-11-20 16:07:13 +0000

    Added english british

--- trunk/www/language/languages.inc.php
+++ trunk/www/language/languages.inc.php
@@ -2,6 +2,7 @@
 $languages['cn']='Chinese Simplified';
 $languages['de']='Deutsch';
 $languages['en']='English';
+$languages['en_UK']='English/British';
 $languages['fr']='Francais';
 $languages['it']='Italiano';
 $languages['nl']='Nederlands';
