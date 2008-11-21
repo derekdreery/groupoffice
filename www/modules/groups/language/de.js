a5704cebf04c0f0041439e8ff73c5961c308ab9e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-11-21 09:52:59 +0000

    tagged $MAJOR-$MINOR

--- www/modules/groups/language/de.js
+++
@@ -1,5 +0,0 @@
-﻿GO.groups.lang.groups = 'Gruppen';
-GO.groups.lang.owner = 'Eigentümer';
-GO.groups.lang.lblNew = 'Neu';
-GO.groups.lang.newGroupName = 'Neuen Gruppennamen eingeben';
-GO.groups.lang.searchAllUsers = 'Suche alle Benutzer';
