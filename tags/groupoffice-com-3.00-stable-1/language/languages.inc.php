dde3cc31bdefebe626ad6c7bdf10b4913783b2c4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-11-24 14:14:42 +0000

    Invalid tag

--- tags/groupoffice-com-3.00-stable-1/language/languages.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$languages['cn']='Chinese Simplified';
-$languages['de']='Deutsch';
-$languages['en']='English';
-$languages['en_UK']='English/British';
-$languages['fr']='Francais';
-$languages['it']='Italiano';
-$languages['nl']='Nederlands';
--- tags/groupoffice-com-3.00-stable-1/modules/email/scripts.inc.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript">
-GO.email.defaultSmtpHost="<?php echo $GO_CONFIG->smtp_server; ?>";
-</script>
