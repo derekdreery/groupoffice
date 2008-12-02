28dffcfb1cdf120a8152460bb6778e37c30449a4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-12-02 14:57:07 +0000

    Fixed onmouseout scrollbar issue in tree and grid

--- www/javascript/overrides.js
+++ www/javascript/overrides.js
@@ -81,3 +81,108 @@ Ext.override(Ext.DatePicker, {
 	startDay: parseInt(GO.settings.first_weekday)
 });
 
+
+
+/*
+ * Fix for hover in panels that stays on when you mouseout on scrollbar
+ */
+ 
+Ext.override(Ext.Element, {
+	findParent : function(simpleSelector, maxDepth, returnEl){
+		var p = this.dom, b = document.body, depth = 0, dq = Ext.DomQuery, stopEl;
+		maxDepth = maxDepth || 50;
+		if(typeof maxDepth != "number"){
+		stopEl = Ext.getDom(maxDepth);
+		maxDepth = 10;
+	}
+	try {
+		while(p && p.nodeType == 1 && depth < maxDepth && p != b && p != stopEl){
+			if(dq.is(p, simpleSelector)){
+				return returnEl ? Ext.get(p) : p;
+			}
+			depth++;
+			p = p.parentNode;
+		}
+	} catch(e) {};
+	return null;
+}
+});
+Ext.override(Ext.grid.GridView, {
+	renderUI : function(){
+		var header = this.renderHeaders();
+		var body = this.templates.body.apply({rows:''});
+		var html = this.templates.master.apply({
+			body: body,
+			header: header
+		});
+		var g = this.grid;
+		g.getGridEl().dom.innerHTML = html;
+		this.initElements();
+		Ext.fly(this.innerHd).on("click", this.handleHdDown, this);
+		this.mainHd.on("mouseover", this.handleHdOver, this);
+		this.mainHd.on("mouseout", this.handleHdOut, this);
+		this.mainHd.on("mousemove", this.handleHdMove, this);
+		this.scroller.on('scroll', this.syncScroll,  this);
+		if(g.enableColumnResize !== false){
+			this.splitone = new Ext.grid.GridView.SplitDragZone(g, this.mainHd.dom);
+		}
+		if(g.enableColumnMove){
+			this.columnDrag = new Ext.grid.GridView.ColumnDragZone(g, this.innerHd);
+			this.columnDrop = new Ext.grid.HeaderDropZone(g, this.mainHd.dom);
+		}
+		if(g.enableHdMenu !== false){
+			if(g.enableColumnHide !== false){
+				this.colMenu = new Ext.menu.Menu({id:g.id + "-hcols-menu"});
+				this.colMenu.on("beforeshow", this.beforeColMenuShow, this);
+				this.colMenu.on("itemclick", this.handleHdMenuClick, this);
+			}
+			this.hmenu = new Ext.menu.Menu({id: g.id + "-hctx"});
+			this.hmenu.add(
+				{id:"asc", text: this.sortAscText, cls: "xg-hmenu-sort-asc"},
+				{id:"desc", text: this.sortDescText, cls: "xg-hmenu-sort-desc"}
+			);
+			if(g.enableColumnHide !== false){
+				this.hmenu.add('-',
+					{id:"columns", text: this.columnsText, menu: this.colMenu, iconCls: 'x-cols-icon'}
+				);
+			}
+			this.hmenu.on("itemclick", this.handleHdMenuClick, this);
+		}
+		if(g.enableDragDrop || g.enableDrag){
+			this.dragZone = new Ext.grid.GridDragZone(g, {
+				ddGroup : g.ddGroup || 'GridDD'
+			});
+		}
+		this.updateHeaderSortState();
+		if(this.grid.trackMouseOver){
+			this.mainBody.on("mouseover", this.onRowOver, this);
+			this.mainBody.on("mouseout", this.onRowOut, this);
+		}
+	},
+	initUI : function(grid){
+		grid.on("headerclick", this.onHeaderClick, this);
+//		if(grid.trackMouseOver){
+//			grid.on("mouseover", this.onRowOver, this);
+//			grid.on("mouseout", this.onRowOut, this);
+//		}
+	}
+});
+
+Ext.override(Ext.tree.TreeEventModel, {
+	initEvents : function(){
+		var el = this.tree.getTreeEl();
+		el.on('click', this.delegateClick, this);
+		if(this.tree.trackMouseOver !== false){
+			var innerCt = Ext.fly(el.dom.firstChild);
+			innerCt.on('mouseover', this.delegateOver, this);
+			innerCt.on('mouseout', this.delegateOut, this);
+		}
+		el.on('dblclick', this.delegateDblClick, this);
+		el.on('contextmenu', this.delegateContextMenu, this);
+	}
+});
+
+/*
+ * End of fix for hover in panels that stays on when you mouseout on scrollbar
+ */
+
