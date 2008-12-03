7034ef72fce8db861c2b97fe4f813e8fbd1d1b95
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-12-03 07:46:44 +0000

    Added Portugese Brazil

---
+++ www/modules/calendar/language/pt_BR.js
@@ -0,0 +1,66 @@
+/**
+ * Don't copy the next line into a translation
+ */
+Ext.namespace('GO.calendar');
+Ext.namespace("GO.calendar.dd");
+
+GO.calendar.lang={};
+/**
+ * Copy everything below for translations
+ */
+ 
+
+GO.calendar.lang.addressbook='Contatos';
+
+GO.calendar.lang.appointment = 'Compromisso';
+GO.calendar.lang.appointments = 'Compromissos';
+GO.calendar.lang.recurrence = 'Recorrente';
+GO.calendar.lang.options = 'Opções';
+GO.calendar.lang.repeatForever = 'repetir para sempre';
+GO.calendar.lang.repeatEvery = 'Repetir a cada';
+GO.calendar.lang.repeatUntil = 'Repetir até';
+GO.calendar.lang.busy = 'Mostrar como ocupado';
+GO.calendar.lang.allDay = 'Tempo não aplicável';
+GO.calendar.lang.navigation = 'Navegação';
+GO.calendar.lang.oneDay = '1 Dia';
+GO.calendar.lang.fiveDays = '5 Dias';
+GO.calendar.lang.sevenDays = '7 Dias';
+GO.calendar.lang.month = 'Mês';
+GO.calendar.lang.recurringEvent = 'Recurring event';
+GO.calendar.lang.deleteRecurringEvent = 'Você quer apagar um evento simples ou todos os eventos recorrentes?';
+GO.calendar.lang.singleOccurence = 'Evento simples';
+GO.calendar.lang.entireSeries = 'Série de eventos';
+GO.calendar.lang.calendar = 'Calendário';
+GO.calendar.lang.calendars = 'Calendários';
+GO.calendar.lang.views = 'Vistas';
+GO.calendar.lang.administration = 'Administração';
+GO.calendar.lang.needsAction = 'Necessita de ação';
+GO.calendar.lang.accepted = 'Aceito';
+GO.calendar.lang.declined = 'Negado';
+GO.calendar.lang.tentative = 'Tentativa';
+GO.calendar.lang.delegated = 'Delegado';
+GO.calendar.lang.noRecurrence = 'Sem recorrência';
+GO.calendar.lang.notRespondedYet = 'Sem resposta';
+GO.calendar.lang.days = 'dias';
+GO.calendar.lang.weeks = 'semanas';
+GO.calendar.lang.monthsByDate = 'meses por data';
+GO.calendar.lang.monthsByDay = 'meses por dia';
+GO.calendar.lang.years = 'anos';
+GO.calendar.lang.atDays = 'Nos dias';
+GO.calendar.lang.noReminder = 'Sem lembrete';
+GO.calendar.lang.reminder='Lembrete';
+GO.calendar.lang.participants = 'Participantes';
+GO.calendar.lang.checkAvailability = 'Checar disponibilidade';
+GO.calendar.lang.sendInvitation = 'Enviar convite';
+GO.calendar.lang.emailSendingNotConfigured = 'Envio de E-mail não configurado.';
+GO.calendar.lang.privateEvent = 'Privado';
+GO.calendar.lang.noInformationAvailable = 'Sem informação disponível';
+GO.calendar.lang.noParticipantsToDisplay = 'Sem participantes para mostrar';
+GO.calendar.lang.previousDay = 'Dia anterior';
+GO.calendar.lang.nextDay = 'Próximo dia';
+GO.calendar.lang.noAppointmentsToDisplay = 'Sem compromissos para mostrar';
+GO.calendar.lang.selectCalendar = 'Selecione calendário';
+GO.calendar.lang.selectCalendarForAppointment = 'Selecion o calendário para colocar esse compromisso:';
+GO.calendar.lang.closeWindow = 'O compromisso foi aceito e agendade. Você pode fechar essa janela.';
+GO.calendar.lang.list='Lista';
+GO.calendar.lang.editRecurringEvent='você deseja editar esta ocorrência ou a série de ocorrências?';
