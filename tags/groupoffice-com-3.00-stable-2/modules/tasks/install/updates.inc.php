b0261eb4b225d309e41e4f18ef64af2e9775f8f2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-12-03 12:35:19 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.00-stable-2/modules/tasks/install/updates.inc.php
@@ -0,0 +1,13 @@
+<?php
+$updates[]="CREATE TABLE IF NOT EXISTS `go_links_12` (
+  `id` int(11) NOT NULL,
+  `folder_id` int(11) NOT NULL,
+  `link_id` int(11) NOT NULL,
+  `link_type` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  KEY `link_id` (`link_id`,`link_type`),
+  KEY `id` (`id`,`folder_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+
+$updates[]="ALTER TABLE `ta_tasks` CHANGE `completion_time` `completion_time` INT( 11 ) NULL";
+?>
