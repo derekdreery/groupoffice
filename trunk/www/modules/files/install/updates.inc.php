cb4637d91e5ae63a5bb7419d1296b686749d65bd
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-12-05 09:56:07 +0000

    template blob changed to mediumblob

--- trunk/www/modules/files/install/updates.inc.php
+++ trunk/www/modules/files/install/updates.inc.php
@@ -13,3 +13,4 @@ $updates[]="ALTER TABLE `fs_folders` ADD `thumbs` ENUM( '0', '1' ) NOT NULL DEFA
 $updates[]="ALTER TABLE `fs_folders` CHANGE `comments` `comments` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL";
 $updates[]="ALTER TABLE `fs_files` CHANGE `comments` `comments` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL";  
 $updates[]="ALTER TABLE `fs_files` CHANGE `locked_user_id` `locked_user_id` INT( 11 ) NOT NULL DEFAULT '0'";
+$updates[]="ALTER TABLE `fs_templates` CHANGE `content` `content` MEDIUMBLOB NOT NULL";
