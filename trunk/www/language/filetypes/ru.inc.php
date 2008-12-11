e860ce61d0f69d2633379d86e89639b90bce88b7
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-12-11 12:14:30 +0000

    language corrections

--- trunk/www/language/filetypes/ru.inc.php
+++ trunk/www/language/filetypes/ru.inc.php
@@ -1,46 +1,74 @@
 <?php
+/** 
+* Russian translation 
+* By Valery Yanchenko (utf-8 encoding) 
+* vajanchenko@hotmail.com 
+* 10 December 2008*/
 //Uncomment this line in new translations!
-require($GO_LANGUAGE->get_fallback_base_language_file('filetypes'));
+//require($GO_LANGUAGE->get_fallback_base_language_file('filetypes'));
 
-$ft_title = "Filetypes";
-$ft_text = "Filetypes are automatically added by Group-Office. You can change icons and names here.";
+$lang['filetypes']['unknown'] = 'Неизвестный тип фала';
+$lang['filetypes']['txt'] = 'Текстовый файл';
+$lang['filetypes']['sxw'] = 'OpenOffice.Org текст';
+$lang['filetypes']['ott'] = 'OpenOffice.Org шаблон';
+$lang['filetypes']['odc']= 'OpenOffice.Org диаграмма';
+$lang['filetypes']['odb']= 'OpenOffice.Org база данныхe';
+$lang['filetypes']['odf']= 'OpenOffice.Org формула';
+$lang['filetypes']['odg']= 'OpenOffice.Org графика';
+$lang['filetypes']['otg']= 'OpenOffice.Org графический шаблон';
+$lang['filetypes']['odi']= 'OpenOffice.Org рисунок';
+$lang['filetypes']['odp']= 'OpenOffice.Org презентация';
+$lang['filetypes']['otp']= 'OpenOffice.Org шаблон презентации';
+$lang['filetypes']['ods']= 'OpenOffice.Org таблица';
+$lang['filetypes']['ots']= 'OpenOffice.Org шаблон таблицы';
+$lang['filetypes']['odt']= 'OpenOffice.Org текст';
+$lang['filetypes']['odm']= 'OpenOffice.Org основной тект';
+$lang['filetypes']['oth']= 'OpenOffice.Org web страница';
 
-$ft_icon = "������";
-$ft_extension = "����������";
-$ft_friendly = "Friendly name";
-$ft_content = "��� ����������";
-$ft_no_filetypes = "No filetypes found.";
-$ft_upload_text = "Select the filetype icon to upload. Only GIF files are accepted.";
-$ft_no_file = "No file was recieved.";
-$ft_please_wait = "Please wait while uploading icon...";
-$ft_properties = "���������";
-$ft_filename = "��� �����";
-$ft_current_icon = "������� ������";
 
-$lang['filetypes']['unknown'] = '����������� ��� �����';
-$lang['filetypes']['txt'] = '��������� ����';
-$lang['filetypes']['sxw'] = '�������� OpenOffice.Org Writer';
-$lang['filetypes']['doc'] = '�������� Microsoft Word';
-$lang['filetypes']['xls'] = '����������� ������� Microsoft Excel';
-$lang['filetypes']['mdb'] = '���� ������ Microsoft Access';
-$lang['filetypes']['ppt'] = $lang['filetypes']['pps'] = '����������� Microsoft Powerpoint';
-$lang['filetypes']['sxc'] = '����������� ������� OpenOffice.Org Calc';
-$lang['filetypes']['tar'] = 'Tar �����';
-$lang['filetypes']['zip'] = 'ZIP compressed archive';
-$lang['filetypes']['rar'] = 'RAR compressed archive';
-$lang['filetypes']['gz'] = $lang['filetypes']['tgz'] = 'GZIP compressed archive';
-$lang['filetypes']['bz2'] = 'BZIP2 compressed archive'; 
-$lang['filetypes']['pdf'] = '�������� PDF';
-$lang['filetypes']['exe'] = '����������� Windows ����';
-$lang['filetypes']['ttf'] = '����� True type';
-$lang['filetypes']['html'] = $lang['filetypes']['htm'] = '�������� HTML';
-$lang['filetypes']['jpg'] = $lang['filetypes']['jpeg'] = '�����������';
-$lang['filetypes']['gif'] = '�����������';
-$lang['filetypes']['bmp'] = '�����������';
-$lang['filetypes']['tif'] = '�����������';
-$lang['filetypes']['png'] = '�����������';
-$lang['filetypes']['php'] = '������ PHP';
-$lang['filetypes']['ics'] = '����������� ����������';
-$lang['filetypes']['vcf'] = '���������� ����������';
-$lang['filetypes']['wav'] = $lang['filetypes']['ogg'] = $lang['filetypes']['mp3'] = $lang['filetypes']['wma'] = '�������� ����';
-$lang['filetypes']['mpg'] = $lang['filetypes']['mpeg'] = $lang['filetypes']['avi'] = $lang['filetypes']['wmv'] = $lang['filetypes']['wmf'] = '����� ����';
+$lang['filetypes']['doc'] = 'Microsoft Word документ';
+$lang['filetypes']['xls'] = 'Microsoft Excel таблица';
+$lang['filetypes']['mdb'] = 'Microsoft Access база данных';
+$lang['filetypes']['ppt'] = 'Microsoft Powerpoint презентация';
+$lang['filetypes']['pps'] = 'Microsoft Powerpoint презентация';
+$lang['filetypes']['sxc'] = 'OpenOffice.Org Calc таблица';
+$lang['filetypes']['tar'] = 'Tar архив';
+$lang['filetypes']['zip'] = 'ZIP архив';
+$lang['filetypes']['rar'] = 'RAR архив';
+$lang['filetypes']['gz'] = 'GZIP архив'; 
+$lang['filetypes']['tgz'] = 'GZIP архив';
+$lang['filetypes']['bz2'] = 'BZIP2 архив'; 
+$lang['filetypes']['pdf'] = 'PDF документ';
+$lang['filetypes']['exe'] = 'исполняемый файл Windows';
+$lang['filetypes']['ttf'] = 'True type шрифт';
+$lang['filetypes']['html'] = 'HTML документ';
+$lang['filetypes']['htm'] = 'HTML документ';
+$lang['filetypes']['jpg'] = 'Изображение';
+$lang['filetypes']['jpeg'] = 'Изображение';
+$lang['filetypes']['gif'] = 'Изображение';
+$lang['filetypes']['bmp'] = 'Изображение';
+$lang['filetypes']['tif'] = 'Изображение';
+$lang['filetypes']['png'] = 'Изображение';
+$lang['filetypes']['php'] = 'PHP скрипт';
+$lang['filetypes']['ics'] = 'Иформация для Calendar';
+$lang['filetypes']['vcf'] = 'Иформация для Contact';
+$lang['filetypes']['wav'] = 'Звуковой файл';
+$lang['filetypes']['ogg'] = 'Звуковой файл';
+$lang['filetypes']['mp3'] = 'Звуковой файл';
+$lang['filetypes']['wma'] = 'Звуковой файл';
+$lang['filetypes']['mpg'] =  'Видео файл';
+$lang['filetypes']['mpeg'] =  'Видео файл';
+$lang['filetypes']['avi'] =  'Видео файл';
+$lang['filetypes']['wmv'] = 'Видео файл'; 
+$lang['filetypes']['wmf'] = 'Видео файл';
+$lang['filetypes']['pdf'] = 'Adobe Acrobat PDF';
+$lang['filetypes']['doc'] = 'Документ Microsoft Word';
+$lang['filetypes']['dot'] = 'Документ Microsoft Word';
+$lang['filetypes']['psd'] = 'Файл Adobe Photoshop';
+$lang['filetypes']['rtf'] = 'Rich Text Format';
+$lang['filetypes']['swf'] = 'Macromedia Flash Видео';
+$lang['filetypes']['fla'] = 'Macromedia Flash Документ (исходник)';
+$lang['filetypes']['xls'] = 'Microsoft Excel таблица';
+$lang['filetypes']['xlt'] = 'Microsoft Excel таблица';
+$lang['filetypes']['ai'] = 'Файл Adobe Illustrator';
+$lang['filetypes']['eml'] = 'Сообщение E-mail';
