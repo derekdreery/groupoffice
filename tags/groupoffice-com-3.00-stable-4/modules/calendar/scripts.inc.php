fd1e1cc06ab6bc037d4dc5612c611e03a1021c64
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2008-12-16 14:46:55 +0000

    invalid tag

--- tags/groupoffice-com-3.00-stable-4/modules/calendar/scripts.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once($GO_MODULES->modules['calendar']['class_path'].'calendar.class.inc');
-$cal = new calendar();
-
-$calendar = $cal->get_calendar();
-?>
-<script type="text/javascript">
-GO.calendar.defaultCalendar = {id: <?php echo $calendar['id']; ?>, name: "<?php echo $calendar['name']; ?>"};
-</script>
