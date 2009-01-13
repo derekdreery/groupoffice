992666b420b7b9f483a5bc4a1a989e657f597822
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-01-13 15:20:16 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.00-stable-6/modules/email/language/cs.inc.php
@@ -0,0 +1,41 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('email'));
+$lang['email']['name'] = 'Email';
+$lang['email']['description'] = 'Webový e-mailový klient. Každý uživatel může  posílat a přijímat e-maily nebo přeposílat.';
+
+$lang['link_type'][9]='E-mail';
+
+$lang['email']['feedbackNoReciepent'] = 'Nezadali jste příjemce';
+$lang['email']['feedbackSMTPProblem'] = 'Došlo k problému při komunikaci s SMTP: ';
+$lang['email']['feedbackUnexpectedError'] = 'Došlo k nečekanému problému s úpravou emailu: ';
+$lang['email']['feedbackCreateFolderFailed'] = 'Chyba při vytváření složky';
+$lang['email']['feedbackDeleteFolderFailed'] = 'Chyba při mazání složky';
+$lang['email']['feedbackSubscribeFolderFailed'] = 'Nepodařilo se přihlásit složky';
+$lang['email']['feedbackUnsubscribeFolderFailed'] = 'Nepodařilo se odhlásit od složky';
+$lang['email']['feedbackCannotConnect'] = 'Nelze se připojit k %1$s<br /><br />The mail server returned: %2$s';
+$lang['email']['inbox'] = 'Příchozí pošta';
+
+$lang['email']['spam']='Spam';
+$lang['email']['trash']='Koš';
+$lang['email']['sent']='Odeslaná pošta';
+$lang['email']['drafts']='Koncepty';
+
+$lang['email']['no_subject']='Žádný předmět';
+$lang['email']['to']='Příjemce';
+$lang['email']['from']='Odesílatel';
+$lang['email']['subject']='Předmět';
+$lang['email']['no_recipients']='Žádný příjemce';
+$lang['email']['original_message']='--- Původní zpráva ---';
+$lang['email']['attachments']='Přílohy';
+
+$lang['email']['notification_subject']='Přečtení: %s';
+$lang['email']['notification_body']='Vaše zpráva s předmětem "%s" byla zobrazena v %s';
+
+$lang['email']['errorGettingMessage']='Nelze přijmout zprávy ze serveru';
+$lang['email']['no_recipients_drafts']='Žádný příjemce';
+$lang['email']['usage_limit'] = '%s z %s používáno';
+$lang['email']['usage'] = '%s používáno';
+
+$lang['email']['event']='Událost';
+$lang['email']['calendar']='kalendář';
