021d1931ec26ad032446b2cf4f3450c959df6a72
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-01-30 08:52:34 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.00-stable-15/language/languages.inc.php
@@ -0,0 +1,13 @@
+<?php
+$languages['pl']='Polski';
+$languages['en']='English';
+$languages['cn']='Chinese Simplified';
+$languages['cs']='Čeština';
+$languages['de']='Deutsch';
+$languages['en_UK']='English/British';
+$languages['es']='Español';
+$languages['fr']='Francais';
+$languages['it']='Italiano';
+$languages['nl']='Nederlands';
+$languages['pt_BR']='Português - Brazil';
+$languages['ru']='Russian';
---
+++ tags/groupoffice-com-3.00-stable-15/modules/notes/language/cs.inc.php
@@ -0,0 +1,16 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('notes'));
+
+$modules['notes']['name']='Poznámky';
+$modules['notes']['description']='Slouží k uchování rychlých zpráv.';
+$lang['notes']['note']='Poznámka';
+$lang['notes']['name']='Poznámky';
+$lang['notes']['description']='Slouží k uchování rychlých zpráv.';
+
+$lang['notes']['category']='Skupina';
+$lang['notes']['categories']='Skupiny';
+$lang['link_type'][4]=$lang['notes']['note']='Poznámka';
+$lang['notes']['notes']='Poznámky';
+
+$lang['notes']['general'] = 'Obecné';
