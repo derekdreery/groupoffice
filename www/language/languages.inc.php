9ea93ed91920a0126d956b8a4e2821e11d849b8e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-02-02 08:42:30 +0000

    Added Danish translation

--- www/language/languages.inc.php
+++ www/language/languages.inc.php
@@ -1,13 +1,14 @@
 <?php
-$languages['pl']='Polski';
-$languages['en']='English';
 $languages['cn']='Chinese Simplified';
 $languages['cs']='Čeština';
+$languages['da']='Dansk';
 $languages['de']='Deutsch';
+$languages['en']='English';
 $languages['en_UK']='English/British';
 $languages['es']='Español';
 $languages['fr']='Francais';
 $languages['it']='Italiano';
 $languages['nl']='Nederlands';
+$languages['pl']='Polski';
 $languages['pt_BR']='Português - Brazil';
-$languages['ru']='Russian';
+$languages['ru']='Русский';
