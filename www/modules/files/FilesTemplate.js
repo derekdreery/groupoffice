ba2459d095f418290ef1b79333b2de4b0e51b511
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-02-03 16:21:43 +0000

    no div inside link

--- www/modules/files/FilesTemplate.js
+++ www/modules/files/FilesTemplate.js
@@ -14,10 +14,10 @@ GO.files.filesTemplate = '<tpl if="files.length">'+
 		'<tpl for="files">'+
 			'<tr>'+
 				'<tpl if="values.extension==\'folder\'">'+										
-					'<td><a href="#" onclick="GO.files.openFolder(\'{[GO.util.add_slashes(values.path)]}\');">{grid_display}</a></td>'+
+					'<td><a class="go-grid-icon filetype-folder" href="#" onclick="GO.files.openFolder(\'{[GO.util.add_slashes(values.path)]}\');">{name}</a></td>'+
 				'</tpl>'+							
 				'<tpl if="values.extension!=\'folder\'">'+										
-					'<td><a href="#" onclick="GO.files.openFile(\'{[GO.util.add_slashes(values.path)]}\');">{grid_display}</a></td>'+
+					'<td><a class="go-grid-icon filetype-{extension}" href="#" onclick="GO.files.openFile(\'{[GO.util.add_slashes(values.path)]}\');">{name}</a></td>'+
 				'</tpl>'+
 				'<td style="white-space:nowrap">{mtime}</td>'+
 			'</tr>'+
