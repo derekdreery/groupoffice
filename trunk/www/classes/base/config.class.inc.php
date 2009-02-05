09a0f8862aca3564b7e4a3090aa6d4e544aaa091
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-02-05 14:52:10 +0000

    changelog update for 17

--- trunk/www/classes/base/config.class.inc.php
+++ trunk/www/classes/base/config.class.inc.php
@@ -630,7 +630,7 @@ class GO_CONFIG
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.00-stable-16';
+	var $version = '3.00-stable-17';
 
 
 	/**
@@ -640,7 +640,7 @@ class GO_CONFIG
 	 * @access  public
 	 */
 
-	var $mtime = '20090204';
+	var $mtime = '20090205';
 
 	/* The permissions mode to use when creating files
 	 *
