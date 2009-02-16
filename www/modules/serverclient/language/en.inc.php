efe20c4a8e149c81b53e85505d73f7506dc86c04
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-02-16 10:22:26 +0000

    open sourced the postfixadmin module

---
+++ www/modules/serverclient/language/en.inc.php
@@ -0,0 +1,4 @@
+<?php
+//Uncomment this line in new translations!
+//require($GO_LANGUAGE->get_fallback_language_file('serverclient'));
+$lang['serverclient']['connect_error']='Could not connect to %s';
