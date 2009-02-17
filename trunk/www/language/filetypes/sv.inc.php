b25d25eb0ffdd851f31cb67db9eb8027bd8fa8d6
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-02-17 08:07:47 +0000

    Added Swedish translation

--- trunk/www/language/filetypes/sv.inc.php
+++ trunk/www/language/filetypes/sv.inc.php
@@ -1,4 +1,17 @@
 <?php
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id$
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
 //Uncomment this line in new translations!
 require($GO_LANGUAGE->get_fallback_base_language_file('filetypes'));
 
