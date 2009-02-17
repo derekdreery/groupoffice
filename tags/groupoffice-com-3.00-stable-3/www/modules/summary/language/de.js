5ce3c1701f81d1ab91330994446c1c296fa7284a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-02-17 14:17:20 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.00-stable-3/www/modules/summary/language/de.js
@@ -0,0 +1,15 @@
+GO.summary.lang.summary = 'Zusammenfassung';
+GO.summary.lang.noItems = 'Sie haben keine Punkte auf Ihrer Startseite';
+GO.summary.lang.selectPortlet = 'Wähle Portlet';
+GO.summary.lang.hotTopics = 'Interessante Themen';
+GO.summary.lang.enterRssFeed = 'Bitte geben Sie die RSS URL an:';
+GO.summary.lang.notes = 'Notizen';
+GO.summary.lang.loadingFeed = 'Lade Feed...';
+GO.summary.lang.goToPost = 'Gehe zu Post';
+GO.summary.lang.today = 'Heute ';
+
+GO.summary.lang.announcement="Ankündigung";
+GO.summary.lang.announcements="Ankündigungen";
+GO.summary.lang.dueTime="Anzeige bis";
+GO.summary.lang.title="Titel";
+GO.summary.lang.manageAnnouncements='Ankündigungen verwalten';
