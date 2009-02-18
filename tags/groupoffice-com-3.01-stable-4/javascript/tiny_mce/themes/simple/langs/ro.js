e41da539fb69f60e4025f0fa49eec36d15076c58
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-02-18 12:06:24 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.01-stable-4/javascript/tiny_mce/themes/simple/langs/ro.js
@@ -0,0 +1,11 @@
+﻿tinyMCE.addI18n('ro.simple',{
+bold_desc:"Îngroşat (Ctrl+B)",
+italic_desc:"Cursiv (Ctrl+I)",
+underline_desc:"Subliniat (Ctrl+U)",
+striketrough_desc:"Barat",
+bullist_desc:"Lista neordonată",
+numlist_desc:"Listă ordonată",
+undo_desc:"Anulează (Ctrl+Z)",
+redo_desc:"Recuperează (Ctrl+Y)",
+cleanup_desc:"Curaţă codul"
+});
---
+++ tags/groupoffice-com-3.01-stable-4/modules/tools/language/it.inc.php
@@ -0,0 +1,9 @@
+<?php
+$lang['tools']['name']='Strumenti';
+$lang['tools']['description']='Un modulo per eseguire operazioni amministrative.';
+
+$lang['tools']['dbcheck']='Controllare il database';
+$lang['tools']['rm_duplicates']='Rimuovere i contatti e gli eventi duplicati';
+
+$lang['tools']['backupdb']='Backup database';
+?>
