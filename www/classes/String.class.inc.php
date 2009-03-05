9dfcde1f5793c7b7c612f33f5e9bd436306cc396
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-03-05 14:46:47 +0000

    oodoc + gota

--- www/classes/String.class.inc.php
+++ www/classes/String.class.inc.php
@@ -498,7 +498,7 @@ class String {
 		{
 			//
 			//$text = preg_replace("/(?:^|\b)(((http(s?):\/\/)|(www\.-))([\w\.-]+)([,:;%#&\/?=\w+\.\-@]+))(?:\b|$)/is", "<a href=\"http$4://$5$6$7\" target=\"_blank\" class=\"normal-link\">$1</a>", $text);
-			$text = preg_replace("/\b(https?:\/\/[a-z0-9\.&\-\/@#;`~=%?]+)/i", '<a href="$1" target="_blank" class="normal-link">$1</a>', $text);
+			$text = preg_replace("/\b(https?:\/\/[a-z0-9\.&\-\/@#;`~=%?:_]+)/i", '<a href="$1" target="_blank" class="normal-link">$1</a>', $text);
 			//$text = preg_replace("/(\A|\s)([\w\.\-]+)(@)([\w\.-]+)([A-Za-z]{2,3})\b/i", "\\1<a href=\"mailto:\\2\\3\\4\\5\">\\2\\3\\4\\5</a>", $text);
 			if($email_module)
 			{
