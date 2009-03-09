21050236ffbe45ac1f106f1bf5404c6370b16d39
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-03-09 10:11:26 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.01-stable-7/modules/notes/language/de.js
@@ -0,0 +1,12 @@
+﻿/* new table: no_categories */
+GO.notes.lang.category="Kategorie";
+GO.notes.lang.categories="Kategorien";
+
+/* new table: no_notes */
+GO.notes.lang.note="Notiz";
+GO.notes.lang.notes="Notizen";
+GO.notes.lang.category_id="Kategorie-ID";
+GO.notes.lang.content="Inhalt";
+
+
+GO.notes.lang.manageCategories="Kategorien verwalten";
