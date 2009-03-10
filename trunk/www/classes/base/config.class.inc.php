cf12351277b3170c77f42cf02bf1e1ee2e0f576b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-03-10 13:42:55 +0000

    link descriptions

--- trunk/www/classes/base/config.class.inc.php
+++ trunk/www/classes/base/config.class.inc.php
@@ -630,7 +630,7 @@ class GO_CONFIG
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.01-stable-9';
+	var $version = '3.01-stable-11';
 
 
 	/**
@@ -640,7 +640,7 @@ class GO_CONFIG
 	 * @access  public
 	 */
 
-	var $mtime = '20090310';
+	var $mtime = '200903101';
 
 	/* The permissions mode to use when creating files
 	 *
