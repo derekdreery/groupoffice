ec786420ab3d85c8ff92a029af248ae507dfb68f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-03-10 14:04:39 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.01-stable-11/modules/groups/language/de.inc.php
@@ -0,0 +1,10 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('groups'));
+
+$lang['groups']['name'] = 'Gruppen';
+$lang['groups']['description'] = 'Admin-Modul; Verwalten von Benutzergruppen.';
+$lang['groups']['noDeleteAdmins'] = 'Die Gruppen-Admins können nicht gelöscht werden';
+$lang['groups']['noDeleteEveryone'] = 'Die Gruppe Everyone kann nicht gelöscht werden';
+$lang['groups']['groupNameAlreadyExists'] = 'Die Gruppe existiert bereits';
+?>
