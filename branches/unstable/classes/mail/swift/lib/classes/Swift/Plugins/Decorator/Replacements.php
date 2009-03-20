950eb85b6a8e96debf26ebc0323d5a936215d2d9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-03-20 12:44:08 +0000

    Updated swift to 4.0.3

--- branches/unstable/classes/mail/swift/lib/classes/Swift/Plugins/Decorator/Replacements.php
+++ branches/unstable/classes/mail/swift/lib/classes/Swift/Plugins/Decorator/Replacements.php
@@ -1,77 +1,46 @@
 <?php
 
-/**
- * Swift Mailer Decorator Replacements Container
- * Please read the LICENSE file
- * @author Chris Corbyn <chris@w3style.co.uk>
- * @package Swift_Plugin
- * @subpackage Decorator
- * @license GNU Lesser General Public License
+/*
+ Replacements interface for the Decorator plugin in Swift Mailer.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
  */
 
-
 /**
- * Swift Decorator Plugin Replacements.
- * Provides and manages the list of replacements for the decorator plugin.
- * @package Swift_Plugin
- * @subpackage Decorator
- * @author Chris Corbyn <chris@w3style.co.uk>
+ * Allows customization of Messages on-the-fly.
+ * 
+ * @package Swift
+ * @subpackage Plugins
+ * 
+ * @author Chris Corbyn
  */
-class Swift_Plugins_DecoratorPluginReplacements
+interface Swift_Plugins_Decorator_Replacements
 {
-  /**
-   * The list of replacements as a 2-d array
-   * @var array,array
-   */
-  protected $replacements;
   
   /**
-   * Ctor.
-   * @param array The replacements as a 2-d array, optional
-   */
-  public function __construct($replacements = array())
-  {
-    $this->setReplacements($replacements);
-  }
-  /**
-   * Add a list of replacements for a given address.
-   * @param string The e-mail address
-   * @param array The replacements as (search => replacement) form.
-   */
-  public function addReplacements($address, $replacements)
-  {
-    $this->replacements[strtolower($address)] = (array)$replacements;
-  }
-  /**
-   * Set the complete list of replacements as a 2-d array.
-   * The array is formed thus (address => (search => replace), address => (search => replace))
-   * @param array,array The replacements.
-   */
-  public function setReplacements($replacements)
-  {
-    $this->replacements = array_change_key_case((array) $replacements, CASE_LOWER);
-  }
-  /**
-   * Get the entire list of replacements as a 2-d array
-   * @return array,array
-   */
-  public function getReplacements()
-  {
-    return $this->replacements;
-  }
-  /**
-   * Get the list of replacements for the address given.
-   * Returns an array where (search => replacement).
-   * @param string The address to get replacements for
+   * Return the array of replacements for $address.
+   * 
+   * This method is invoked once for every single recipient of a message.
+   * 
+   * If no replacements can be found, an empty value (NULL) should be returned
+   * and no replacements will then be made on the message.
+   * 
+   * @param string $address
+   * 
    * @return array
    */
-  public function getReplacementsFor($address)
-  {
-    $address = strtolower($address);
-    if (array_key_exists($address, $this->replacements))
-    {
-      return (array)$this->replacements[$address];
-    }
-    else return array();
-  }
+  public function getReplacementsFor($address);
+  
 }
