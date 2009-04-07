087608f2cbd3b412753ffdce39f073d0069bf04f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-07 11:05:06 +0000

    New combobox reset class

--- www/javascript/form/ComboReset.js
+++ www/javascript/form/ComboReset.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * Copyright Intermesh
  * 
  * This file is part of Group-Office. You should have received a copy of the
@@ -10,70 +10,35 @@
  * @copyright Copyright Intermesh
  * @author Merijn Schering <mschering@intermesh.nl>
  */
- 
+
 GO.form.ComboBoxReset = Ext.extend(GO.form.ComboBox, {
-
-	validationEvent:false,
-  validateOnBlur:false,
-  trigger1Class:'x-form-clear-trigger',
-  trigger2Class:'',
-  //hideTrigger1:true,
-  width:180,
-  hasSearch : false,
-  paramName : 'query',
-  
-	initComponent : function(){
-      GO.form.ComboBoxReset.superclass.initComponent.call(this);
-
-      this.triggerConfig = {
-          tag:'span', cls:'x-form-twin-triggers', cn:[
-          {tag: "img", src: Ext.BLANK_IMAGE_URL, cls: "x-form-trigger " + this.trigger1Class},
-          {tag: "img", src: Ext.BLANK_IMAGE_URL, cls: "x-form-trigger " + this.trigger2Class}
-      ]};
-      
-      this.addEvents ({'reset' : true});
-  },
-
-  getTrigger : function(index){
-      return this.triggers[index];
-  },
-
-  initTrigger : function(){
-      var ts = this.trigger.select('.x-form-trigger', true);
-      this.wrap.setStyle('overflow', 'hidden');
-      var triggerField = this;
-      ts.each(function(t, all, index){
-          t.hide = function(){
-              var w = triggerField.wrap.getWidth();
-              this.dom.style.display = 'none';
-              triggerField.el.setWidth(w-triggerField.trigger.getWidth());
-          };
-          t.show = function(){
-              var w = triggerField.wrap.getWidth();
-              this.dom.style.display = '';
-              triggerField.el.setWidth(w-triggerField.trigger.getWidth());
-          };
-          var triggerIndex = 'Trigger'+(index+1);
-
-          if(this['hide'+triggerIndex]){
-              t.dom.style.display = 'none';
-          }
-          t.on("click", this['on'+triggerIndex+'Click'], this, {preventDefault:true});
-          t.addClassOnOver('x-form-trigger-over');
-          t.addClassOnClick('x-form-trigger-click');
-      }, this);
-      this.triggers = ts.elements;
-  },  
-
-  onTrigger1Click : function(){      
-      this.reset();
-      this.fireEvent('reset', this);
-      this.hasSearch = false;
-  },
-
-  onTrigger2Click : function(){
-      this.onTriggerClick();
-  }
-});
+			initComponent : Ext.form.TwinTriggerField.prototype.initComponent,
+			getTrigger : Ext.form.TwinTriggerField.prototype.getTrigger,
+			initTrigger : Ext.form.TwinTriggerField.prototype.initTrigger,
+			trigger1Class : 'x-form-clear-trigger',
+			hideTrigger1 : true,
+			reset : Ext.form.Field.prototype.reset.createSequence(function() {
+					this.triggers[0].hide();
+			}),
+			onViewClick : Ext.form.ComboBox.prototype.onViewClick.createSequence(function() {
+								this.triggers[0].show();
+							}),
+			onTrigger2Click : function() {
+				this.onTriggerClick();
+			},
+			onTrigger1Click : function() {
+				this.clearValue();
+				this.triggers[0].hide();
+				this.fireEvent('clear', this);
+			},
+			setValue : function(v){
+				GO.form.ComboBoxReset.superclass.setValue.call(this, v);
+				if(v!='')
+				{					
+					this.triggers[0].show();
+				}
+			}
+			
+		});
 
 Ext.reg('comboboxreset', GO.form.ComboBoxReset);
