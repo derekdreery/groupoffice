a927069e1c9de847814bbb99d651aabfb49e7691
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-07 12:25:48 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.01-stable-24/modules/notes/language/de.inc.php
@@ -0,0 +1,16 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('notes'));
+
+$modules['notes']['name']='Notizen';
+$modules['notes']['description']='Modul zum Verwalten von Notizen';
+$lang['notes']['note']='Notiz';
+$lang['notes']['name']='Notizen';
+$lang['notes']['description']='Beschreibung hier eingeben';
+
+$lang['notes']['category']='Ordner';
+$lang['notes']['categories']='Ordner';
+$lang['link_type'][4]=$lang['notes']['note']='Notiz';
+$lang['notes']['notes']='Notizen';
+
+$lang['notes']['general'] = 'Allgemein';
