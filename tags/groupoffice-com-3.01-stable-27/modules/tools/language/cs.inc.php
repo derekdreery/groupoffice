741cff8ea6a0f6dff7225e834900211eab3d9d90
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-10 14:50:40 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.01-stable-27/modules/tools/language/cs.inc.php
@@ -0,0 +1,11 @@
+<?php
+require($GO_LANGUAGE->get_fallback_language_file('tools'));
+
+$lang['tools']['name']='Nástroje';
+$lang['tools']['description']='Prostředí pro vykonávání administrativních příkazů.';
+
+$lang['tools']['dbcheck']='Kontrola databáze';
+$lang['tools']['rm_duplicates']='Smazat duplicitní kontakty a události';
+
+$lang['tools']['backupdb']='Záloha databáze';
+?>
