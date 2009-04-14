4b7ecb9db13349001d890c8ee26068ba7ab945d2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-14 14:33:29 +0000

    update account name

--- www/modules/imapauth/classes/imapauth.class.inc.php
+++ www/modules/imapauth/classes/imapauth.class.inc.php
@@ -4,15 +4,16 @@ require_once( $GLOBALS['GO_CONFIG']->class_path.'mail/imap.class.inc' );
 class imapauth
 {
 	var $config;
-	
+
 	public function __on_load_listeners($events){
 		$events->add_listener('before_login', __FILE__, 'imapauth', 'before_login');
+		$events->add_listener('add_user', __FILE__, 'imapauth', 'add_user');
 	}
 
-	private function get_domain_config($domain)
+	protected function get_domain_config($domain)
 	{
 		global $GO_CONFIG;
-		
+
 		if(!empty($domain))
 		{
 			$conf = str_replace('config.php', 'imapauth.config.php', $GO_CONFIG->get_config_file());
@@ -47,7 +48,7 @@ class imapauth
 	public static function before_login($username, $password)
 	{
 		$ia = new imapauth();
-		
+
 		debug('IMAP auth module active');
 		$arr = explode('@', $username);
 
@@ -71,9 +72,9 @@ class imapauth
 			if ($config['remove_domain_from_username']) {
 				$mail_username = $mailbox;
 			}
-				
+
 			debug('Attempt IMAP login');
-				
+
 			if ($imap->open(
 			$config['host'],
 			$config['proto'],
@@ -89,7 +90,7 @@ class imapauth
 				$imap->close();
 
 				if ($user = $GO_USERS->get_user_by_username( $go_username ) ) {
-						
+
 					//user exists. See if the password is accurate
 					if(md5($password) != $user['password'])
 					{
@@ -107,7 +108,7 @@ class imapauth
 					$user['email'] =$email;
 					$user['username'] = $go_username;
 					$user['password'] = $password;
-					$user['sex'] = 'M';
+						
 
 					if ( !$user_id = $GO_USERS->add_user(
 					$user,
@@ -118,43 +119,7 @@ class imapauth
 					{
 						trigger_error('Failed creating user '.$go_username.' and e-mail '.$email.' with imapauth. The e-mail address probably already existed at another user.', E_USER_WARNING);
 					} else {
-						if ($config['create_email_account'])
-						{
-							if(isset($GO_MODULES->modules['email']))
-							{
-								require_once($GO_MODULES->modules['email']['class_path']."email.class.inc.php");
-								require_once($GO_LANGUAGE->get_language_file('email'));
-								$email_client = new email();
-
-								$account['user_id']=$user_id;
-								$account['type']=$config['proto'];
-								$account['host']=$config['host'];
-								$account['smtp_host']=$config['smtp_host'];
-								$account['smtp_port']=$config['smtp_port'];
-								$account['smtp_encryption']=$config['smtp_encryption'];
-								$account['smtp_username']=$config['smtp_username'];
-								$account['smtp_password']=$config['smtp_password'];
-
-								$account['port']=$config['port'];
-								$account['use_ssl']=$config['ssl'];
-								$account['novalidate_cert']=$config['novalidate_cert'];
-								$account['mbroot']=$config['mbroot'];
-								$account['username']=$mail_username;
-								$account['password']=$password;
-								$account['name']=$email;
-								$account['email']=$email;
-								//$account['auto_check']=$config['auto_check_email'];
-
-								if (!$account_id = $email_client->add_account($account))
-								{
-									trigger_error('Failed creating e-mail account for user '.$go_username.' in imapauth module.', E_USER_WARNING);
-								}else
-								{
-									$account = $email_client->get_account($account_id);
-									$email_client->synchronize_folders($account);
-								}
-							}
-						}
+						$ia->create_email_account($config, $user_id, $mail_username, $password, $email);
 					}
 				}
 			}else
@@ -162,6 +127,64 @@ class imapauth
 				debug('IMAP auth failed '.$imap->last_error());
 				$imap->clear_errors();
 			}
+		}
+	}
+
+	protected function create_email_account($config, $user_id, $username, $password, $email){
+		global $GO_MODULES, $GO_LANGUAGE;
+		if ($config['create_email_account'])
+		{
+			if(isset($GO_MODULES->modules['email']))
+			{
+				require_once($GO_MODULES->modules['email']['class_path']."email.class.inc.php");
+				require_once($GO_LANGUAGE->get_language_file('email'));
+				$email_client = new email();
+
+				$account['user_id']=$user_id;
+				$account['type']=$config['proto'];
+				$account['host']=$config['host'];
+				$account['smtp_host']=$config['smtp_host'];
+				$account['smtp_port']=$config['smtp_port'];
+				$account['smtp_encryption']=$config['smtp_encryption'];
+				$account['smtp_username']=$config['smtp_username'];
+				$account['smtp_password']=$config['smtp_password'];
+
+				$account['port']=$config['port'];
+				$account['use_ssl']=$config['ssl'];
+				$account['novalidate_cert']=$config['novalidate_cert'];
+				$account['mbroot']=$config['mbroot'];
+				$account['username']=$username;
+				$account['password']=$password;
+				$account['name']=$email;
+				$account['email']=$email;
+
+				if (!$account_id = $email_client->add_account($account))
+				{
+					trigger_error('Failed creating e-mail account for user '.$username.' in imapauth module.', E_USER_WARNING);
+				}else
+				{
+					$_SESSION['GO_SESSION']['imapauth']['new_account_id']=$account_id;
+					$account = $email_client->get_account($account_id);
+					$email_client->synchronize_folders($account);
+				}
+			}
+		}
+	}
+	
+	public static function add_user($user)
+	{
+		global $GO_MODULES;
+		
+		require_once($GO_MODULES->modules['email']['class_path']."email.class.inc.php");
+		$email_client = new email();
+		
+		if(!empty($_SESSION['GO_SESSION']['imapauth']['new_account_id']))
+		{
+			$up_account['id']=$_SESSION['GO_SESSION']['imapauth']['new_account_id'];
+			$up_account['name']=String::format_name($user);
+			$email_client->_update_account($up_account);
+			
+			unset($_SESSION['GO_SESSION']['imapauth']['new_account_id']);			
 		}
 	}
 }
