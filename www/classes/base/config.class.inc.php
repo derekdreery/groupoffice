f74230459d130e1efbc77d8835ad1fa783ff794c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-15 07:31:59 +0000

    URL rewrite didnt work for plain text

--- www/classes/base/config.class.inc.php
+++ www/classes/base/config.class.inc.php
@@ -638,7 +638,7 @@ class GO_CONFIG
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.01-stable-29';
+	var $version = '3.01-stable-31';
 
 
 	/**
@@ -648,7 +648,7 @@ class GO_CONFIG
 	 * @access  public
 	 */
 
-	var $mtime = '20090415';
+	var $mtime = '20090415-1';
 
 	/* The permissions mode to use when creating files
 	 *
