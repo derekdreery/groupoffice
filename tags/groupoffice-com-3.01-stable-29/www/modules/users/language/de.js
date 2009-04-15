25a25c3e32079e9d8ba22f816ee11ca5fcf32d05
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-15 07:08:34 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.01-stable-29/www/modules/users/language/de.js
@@ -0,0 +1,90 @@
+﻿GO.users.lang.cmdCheckColumnRead= 'Lesen';
+GO.users.lang.cmdCheckColumnWrite= 'Schreiben';
+GO.users.lang.cmdCheckColumnGroups= 'Gruppen';
+GO.users.lang.cmdHeaderColumnName= 'Modulname';
+GO.users.lang.cmdHeaderColumnGroups= 'Gruppen';
+GO.users.lang.cmdHeaderGridUserHasAccess = 'Der Benutzer hat Zugriff auf';
+GO.users.lang.userIsMemberOf = 'Der Benutzer ist Mitglied von';
+GO.users.lang.userVisibleTo = 'Der Benutzer ist sichtbar für';
+
+GO.users.lang.cmdPanelPersonal = 'Persönlich';
+GO.users.lang.cmdPanelPassword= 'Passwort';
+GO.users.lang.cmdPanelCompany = 'Firma';
+GO.users.lang.cmdPanelPermissions = 'Berechtigungen';
+GO.users.lang.cmdPanelLogin = 'Anmelden';
+GO.users.lang.cmdPanelLookFeel = 'Erscheinungsbild';
+GO.users.lang.cmdPanelSettings = 'Einstellungen';
+GO.users.lang.cmdPanelEnabledMail = 'E-Mail für neue Benutzer aktiviert';
+GO.users.lang.cmdPanelDisabledMail = 'E-Mail für neue Benutzer deaktiviert';
+	
+GO.users.lang.cmdFieldSetPersonal = 'Persönlich';	
+GO.users.lang.cmdUserWindowTitle= 'Bearbeite Benutzer';
+GO.users.lang.cmdNotificationWindowTitle= 'Benutzer-Einladung (E-Mail) bearbeiten';
+GO.users.lang.cmdMaskSaving= 'Speichern';
+	
+GO.users.lang.cmdFormLabelSubject = 'Betreff';
+GO.users.lang.cmdFormLabelUsername = 'Benutername';
+GO.users.lang.cmdFormLabelName = 'Name';
+GO.users.lang.cmdFormLabelPassword = 'Passwort';
+GO.users.lang.cmdFormLabelConfirm = 'Bestätigen';
+GO.users.lang.cmdBoxLabelEnabled = 'Aktiviert';
+	
+GO.users.lang.cmdFormLabelHomepage = 'Webseite';
+GO.users.lang.cmdFormLabelRegistrationTime = 'Registrierungsdatum';
+GO.users.lang.cmdFormLabelLastLogin = 'Letzte Anmeldung';
+GO.users.lang.cmdFormLabelTotalLogins = 'Anmeldungen gesamt';
+GO.users.lang.numberOfLogins = 'Anzahl der Anmeldungen';
+GO.users.lang.cmdFormLabelTheme = 'Design';
+GO.users.lang.cmdFormLabelStartModule = 'Beginne mit Modul';
+GO.users.lang.cmdFormLabelMaximunRows = 'Max. Zeilen in Listen';
+GO.users.lang.cmdFormLabelSortNamesBy = 'Nachname';
+GO.users.lang.cmdFormLabelLanguage = 'Sprache';
+GO.users.lang.cmdFormLabelTimezone = 'Zeitzone';
+GO.users.lang.cmdFormLabelAdjustDaylight = 'Automatisch auf Sommerzeit umstellen';
+GO.users.lang.cmdFormLabelDateFormat = 'Datum';
+GO.users.lang.cmdFormLabelDateSeperator = 'Datums-Trennzeichen';
+GO.users.lang.timeFormat = 'Uhrzeit';
+GO.users.lang.cmdFormLabelFirstWeekday = 'Erster Wochentag';
+GO.users.lang.cmdFormLabelThousandSeperator = 'Tausender-Trennzeichen';
+GO.users.lang.cmdFormLabelDecimalSeperator = 'Dezimal-Trennzeichen';
+GO.users.lang.cmdFormLabelCurrency = 'Währung';
+
+GO.users.lang.account = 'Konto';
+GO.users.lang.confirmPassword = 'Passwort bestätigen';
+GO.users.lang.companyProfile = 'Firmenprofil';
+GO.users.lang.loginInfo = 'Login-Daten';
+GO.users.lang.changePassword = 'Passwort ändern';
+GO.users.lang.currentPassword = 'Aktuelles Passwort';
+GO.users.lang.newPassword = 'Neues Passwort';
+GO.users.lang.confirmPassword = 'Passwort bestätigen';
+GO.users.lang.moduleAccess = 'Modulzugriff';
+GO.users.lang.groupMembership = 'Gruppen-Mitgliedschaft';
+GO.users.lang.profile = 'Persönliches Profil';
+GO.users.lang.settings = 'Persönliche Einstellungen';
+GO.users.lang.dmy = 'Tag-Monat-Jahr';
+GO.users.lang.mdy = 'Monat-Tag-Jahr';
+GO.users.lang.jmd = 'Jahr-Monat-Tag';
+GO.users.lang.fullhourformat = '24 Stunden Format';
+GO.users.lang.halfhourformat = '12 Stunden Format';
+GO.users.lang.sunday = 'Sonntag';
+GO.users.lang.monday = 'Montag';
+GO.users.lang.regionalSettings = 'Regionale Einstellungen';
+GO.users.lang.userSettings = 'Benutzer-Einstellungen';
+
+GO.users.lang.maxUsersReached='Die maximale Anzahl von Benutzern wurde erreicht. Gehen Sie zu der Registerkarte um die maximale Benutzeranzahl zu erhöhen.';
+
+GO.users.lang.group='Gruppe';
+
+GO.users.lang.muteSound='Keine Sounds für Ereignisse nutzen';
+
+GO.users.lang.downloadSampleCSV='Beispiel-CSV herunterladen';
+
+GO.users.lang.importText='Sie können Benutzer mittels einer CSV-Datei importieren. Um die nötige Formatierung dieser Datei einzuhalten, laden Sie sich zum Vergleich bitte die Beispieldatei herunter.<br />Die erste Zeile muss die Spaltenbezeichnungen enthalten und für jeden neuen Benutzer müssen die folgenden Daten angegeben werden:<br /><br />username, password, first_name, last_name, email';
+
+GO.users.lang.useModule= 'Nutzungs-Rechte';
+GO.users.lang.manageModule= 'Verwaltungs-Rechte';
+
+GO.users.lang.numberFormat ='Nummernformat';
+GO.users.lang.importExport ='Import / Export';
+GO.users.lang.listSeparator='Listenfeld-Trennzeichen';
+GO.users.lang.textSeparator='Textfeld-Trennzeichen';
