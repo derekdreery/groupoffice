bc99295bb551c66180cf0b908f1eb62e1a7d4025
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-16 14:27:18 +0000

    accept style property

--- www/javascript/form/HtmlComponent.js
+++ www/javascript/form/HtmlComponent.js
@@ -1,6 +1,6 @@
 GO.form.HtmlComponent = Ext.extend(Ext.BoxComponent, {
      onRender : function(ct, position){
-          this.el = ct.createChild({tag: 'div', html: this.html, cls: this.cls});
+          this.el = ct.createChild({tag: 'div', html: this.html, cls: this.cls, style:this.style});
      }
 });
 
