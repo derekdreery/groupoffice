e7ed313cf53a77163c00ac530397dffda382ca8f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-17 10:52:42 +0000

    old branch

--- branches/michiel/www/modules/tools/language/es.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lang['tools']['name']='Herramientas';
-$lang['tools']['description']='Un módulo para realizar tareas administrativas.';
-
-$lang['tools']['dbcheck']='Compruebe la base de datos';
-$lang['tools']['rm_duplicates']='Eliminar los contactos y eventos denominado';
-
-$lang['tools']['backupdb']='Copia de seguridad de base de datos';
-?>
--- branches/michiel/www/modules/tools/language/zh-tw.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lang['tools']['name']='工具';
-$lang['tools']['description']='執行管理任務的模組.';
-
-$lang['tools']['dbcheck']='資料庫檢查';
-$lang['tools']['rm_duplicates']='刪除重複的連絡人和事件';
-
-$lang['tools']['backupdb']='備份資料庫';
-?>
