bbb0cdb430a117d3344986652c764b468e01fd0b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-20 15:43:38 +0000

    Extended export query function

--- www/export_query.php
+++ www/export_query.php
@@ -1,64 +1,6 @@
 <?php
 require('Group-Office.php');
 
-$db = new db();
-
-$q = $_SESSION['GO_SESSION']['export_queries'][$_REQUEST['query']];
-
-$params = array();
-$types='';
-	
-	
-if(is_array($q))
-{
-	$extra_sql=array();
-	$sql = $q['query'];
-	if(isset($q['extra_params']))
-	{
-		foreach($q['extra_params'] as $param=>$sqlpart)
-		{
-			if(!empty($_REQUEST[$param]))
-			{
-				$params[] = $_REQUEST[$param];
-				$extra_sql[]=$sqlpart;
-			}
-		}
-	}
-	if(count($params))
-	{
-		$insert = ' ';
-		if(!strpos($sql, 'WHERE'))
-		{
-			$insert .= 'WHERE ';
-		}else
-		{
-			$insert .= ' AND ';
-		}
-		$insert .= implode(' AND ', $extra_sql);
-	
-		$pos = strpos($sql, 'ORDER');
-		
-		if(!$pos)
-		{		
-			$sql .= $insert;
-		}else
-		{
-			$sql = substr($sql, 0, $pos).$insert.' '.substr($sql, $pos);	
-		}
-		
-		$types=str_repeat('s',count($params));
-	}
-}else
-{
-	$sql = $q;
-	
-	$params=array();
-	
-}
-
-$db->query($sql,$types,$params);
-
-
 $filename = $_REQUEST['query'].'.csv';
 
 $browser = detect_browser();
@@ -73,59 +15,7 @@ if ($browser['name'] == 'MSIE')
 	header('Content-Disposition: attachment; filename="'.$filename.'"');
 }
 
-$columns=array();
-$headers=array();
-if(isset($_REQUEST['columns']))
-{
-	$indexesAndHeaders = explode(',', $_REQUEST['columns']);
-	
-	foreach($indexesAndHeaders as $i)
-	{
-		$indexAndHeader = explode(':', $i);
-		
-		$headers[]=$indexAndHeader[1];
-		$columns[]=$indexAndHeader[0];
-	}	
-	
-	echo $_SESSION['GO_SESSION']['text_separator'].implode($_SESSION['GO_SESSION']['text_separator'].$_SESSION['GO_SESSION']['list_separator'].$_SESSION['GO_SESSION']['text_separator'], $headers).$_SESSION['GO_SESSION']['text_separator']."\r\n";
-}
-
-
-while($record = $db->next_record())
-{
-	if(!count($columns))
-	{
-
-		foreach($record as $key=>$value)
-		{
-			$columns[]=$key;
-			$headers[]=$key;
-		}			
-		
-		echo $_SESSION['GO_SESSION']['text_separator'].implode($_SESSION['GO_SESSION']['text_separator'].$_SESSION['GO_SESSION']['list_separator'].$_SESSION['GO_SESSION']['text_separator'], $headers).$_SESSION['GO_SESSION']['text_separator']."\r\n";
-
-	}
-	
-	if(is_array($q))
-	{
-		if(!empty($q['require']))
-		{
-			require_once($q['require']);
-		}
-		call_user_func_array(array($q['class'], $q['method']),array(&$record));
-	}
-	
-	if(isset($record['user_id']) && isset($columns['user_id']))
-	{
-		$user = $GO_USERS->get_user($record['user_id']);
-		$record['user_id']=$user['username'];
-	}
-	$values=array();
-	foreach($columns as $index)
-	{
-		$values[] = $record[$index];
-	}
-	echo $_SESSION['GO_SESSION']['text_separator'].implode($_SESSION['GO_SESSION']['text_separator'].$_SESSION['GO_SESSION']['list_separator'].$_SESSION['GO_SESSION']['text_separator'], $values).$_SESSION['GO_SESSION']['text_separator']."\r\n";
-}
-
+$fp = fopen('php://output','w');
+export_query($fp);
+fclose($fp);
 ?>
