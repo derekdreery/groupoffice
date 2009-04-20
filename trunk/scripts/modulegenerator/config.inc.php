757a9a22329cec9ab80cccdd34288c83c32ff4f1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-20 07:21:37 +0000

    minified

--- trunk/scripts/modulegenerator/config.inc.php
+++ trunk/scripts/modulegenerator/config.inc.php
@@ -2,10 +2,10 @@
 require('../../www/Group-Office.php');
 
 //name of the module. No spaces or strange characters.
-$module = 'workflow';
+$module = 'email';
 
 //Short name of the module. The prefix of the database tables.
-$prefix = 'wf';
+$prefix = 'em';
 
 $tables=array();
 //Tables to create an interface for
@@ -14,22 +14,9 @@ $tables=array();
 $westpanel = array(
 	'mainpanel_tag'=> 'GRID',
 	'template'=>'GridPanel.tpl',
-	'name'=>'wf_processes', 
-	'friendly_single'=>'process', 
-	'friendly_multiple'=>'processes',
-	'authenticate'=>true,
-	'paging'=>true,
-	'autoload'=>false,
-	'files'=>false);
-
-$tables[] = $westpanel;
-
-$westpanel = array(
-	'mainpanel_tag'=> 'GRID',
-	'template'=>'GridPanel.tpl',
-	'name'=>'wf_steps', 
-	'friendly_single'=>'step', 
-	'friendly_multiple'=>'steps',
+	'name'=>'em_aliases', 
+	'friendly_single'=>'alias', 
+	'friendly_multiple'=>'aliases',
 	'authenticate'=>false,
 	'paging'=>true,
 	'autoload'=>false,
@@ -37,18 +24,4 @@ $westpanel = array(
 
 $tables[] = $westpanel;
 
-$westpanel = array(
-	'mainpanel_tag'=> 'GRID',
-	'template'=>'GridPanel.tpl',
-	'name'=>'wf_step_history', 
-	'friendly_single'=>'step_history_item', 
-	'friendly_multiple'=>'step_history',
-	'authenticate'=>false,
-	'paging'=>true,
-	'autoload'=>false,
-	'files'=>false);
-
-$tables[] = $westpanel;
-
-$main_template='SimpleMainPanel.tpl'; //The template for MainPanel.js
-
+//$main_template='SimpleMainPanel.tpl'; //The template for MainPanel.js
