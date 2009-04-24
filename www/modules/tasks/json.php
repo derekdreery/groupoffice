35864d9998204401c0306c6e49632dd97511a1ab
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-04-24 07:46:46 +0000

    delete tasks from summary

--- www/modules/tasks/json.php
+++ www/modules/tasks/json.php
@@ -28,40 +28,40 @@ try{
 	{
 		case 'task_with_items':
 		case 'task':
-			
+
 			require($GO_CONFIG->class_path.'ical2array.class.inc');
 			require($GO_CONFIG->class_path.'Date.class.inc.php');
 			require_once($GO_LANGUAGE->get_language_file('tasks'));
-			
+
 			$task = $tasks->get_task(($_REQUEST['task_id']));
 			$tasklist = $tasks->get_tasklist($task['tasklist_id']);
-				
+
 			$response['data']=$task;
-				
+
 			$response['data']['tasklist_name']=$tasklist['name'];
-			
+
 			$response['data']['status_text']=isset($lang['tasks']['statuses'][$task['status']]) ? $lang['tasks']['statuses'][$task['status']] : $lang['tasks']['statuses']['NEEDS-ACTION'];
-				
+
 			$response['data']['write_permission']=$GO_SECURITY->has_permission($GO_SECURITY->user_id, $tasklist['acl_write']);
 			if(!$response['data']['write_permission'] && !$GO_SECURITY->has_permission($GO_SECURITY->user_id, $tasklist['acl_read']))
 			{
 				throw new AccessDeniedException();
 			}
-			
+
 			$response['data']['subject']=$response['data']['name'];
 
 			$due_time = $response['data']['due_time'];
 
 			$response['data']['due_date']=date($_SESSION['GO_SESSION']['date_format'], $due_time);
 			$response['data']['start_date']=date($_SESSION['GO_SESSION']['date_format'], $response['data']['start_time']);
-				
+
 			$response['data']['repeat_every'] = 1;
 			$response['data']['repeat_forever'] = 0;
 			$response['data']['repeat_type'] = REPEAT_NONE;
 			$response['data']['repeat_end_time'] = 0;
 			$response['data']['month_time'] = 0;
 
-			
+
 			if (!empty($response['data']['rrule']) && $rrule = ical2array::parse_rrule($response['data']['rrule']))
 			{
 				if(isset($rrule['FREQ']))
@@ -142,18 +142,18 @@ try{
 							}
 							break;
 
-						case 'YEARLY':
-							$response['data']['repeat_type'] = REPEAT_YEARLY;
-							break;
+									case 'YEARLY':
+										$response['data']['repeat_type'] = REPEAT_YEARLY;
+										break;
 					}
 				}
 			}
 
 			$response['data']['repeat_end_date']=$response['data']['repeat_end_time']>0 ? date($_SESSION['GO_SESSION']['date_format'], $response['data']['repeat_end_time']) : '';
-				
+
 
 			$response['data']['remind']=$response['data']['reminder']>0;
-			
+
 			if($response['data']['remind'])
 			{
 				$response['data']['remind_date']=date($_SESSION['GO_SESSION']['date_format'], $response['data']['reminder']);
@@ -163,7 +163,7 @@ try{
 				$response['data']['remind_date']=date($_SESSION['GO_SESSION']['date_format'], $response['data']['start_time']);
 				$response['data']['remind_time']=date($_SESSION['GO_SESSION']['time_format'], 28800);
 			}
-				
+
 			if($GO_MODULES->modules['files'])
 			{
 				require_once($GO_MODULES->modules['files']['class_path'].'files.class.inc.php');
@@ -174,34 +174,34 @@ try{
 				$full_path = $GO_CONFIG->file_storage_path.$response['data']['files_path'];
 				$fs->check_share($full_path, $response['data']['user_id'], $tasklist['acl_read'], $tasklist['acl_write'],true);
 			}
-			
+
 			if($_task!='task')
 			{
 				$response['data']['description']=String::text_to_html($response['data']['description']);
 
 				require_once($GO_CONFIG->class_path.'/base/search.class.inc.php');
 				$search = new search();
-			
-				$links_json = $search->get_latest_links_json($GO_SECURITY->user_id, $response['data']['id'], 12);				
+					
+				$links_json = $search->get_latest_links_json($GO_SECURITY->user_id, $response['data']['id'], 12);
 				$response['data']['links']=$links_json['results'];
-				
+
 				if(isset($GO_MODULES->modules['files']))
 				{
 					$response['data']['files']=$fs->get_content_json($full_path);
 				}else
 				{
-					$response['data']['files']=array();				
+					$response['data']['files']=array();
 				}
-				
+
 				if(isset($GO_MODULES->modules['comments']))
 				{
 					require_once ($GO_MODULES->modules['comments']['class_path'].'comments.class.inc.php');
 					$comments = new comments();
-					
+
 					$response['data']['comments']=$comments->get_comments_json($response['data']['id'], 12);
 				}
 			}
-				
+
 			$response['success']=true;
 			break;
 
@@ -274,7 +274,7 @@ try{
 										$response['write_permission']=true;
 										if(isset($_REQUEST['tasklist_id']))
 										{
-											$tasklist_id = ($_REQUEST['tasklist_id']);
+											$tasklist_id = $_REQUEST['tasklist_id'];
 											$user_id=0;
 											$tasklists = array($tasklist_id);
 
@@ -282,38 +282,40 @@ try{
 											$tasklist = $tasks->get_tasklist($tasklist_id);
 
 											$response['write_permission']=$GO_SECURITY->has_permission($GO_SECURITY->user_id, $tasklist['acl_write']);
-											
+
 											if(!$GO_SECURITY->has_permission($GO_SECURITY->user_id, $tasklist['acl_read']) && !$response['write_permission'])
 											{
 												throw new AccessDeniedException();
 											}
+
+
 												
-
-											if(isset($_POST['delete_keys']) && $GO_SECURITY->has_permission($GO_SECURITY->user_id, $tasklist['acl_write']))
-											{
-												try{
-													$response['deleteSuccess']=true;
-													$delete_tasks = json_decode($_POST['delete_keys']);
-
-													foreach($delete_tasks as $task_id)
-													{
-														$old_task = $tasks->get_task($task_id);
-														if(!$GO_SECURITY->has_permission($GO_SECURITY->user_id, $old_task['acl_write']))
-														{
-															throw new AccessDeniedException();
-														}
-														$tasks->delete_task($task_id);
-													}
-												}catch(Exception $e)
-												{
-													$response['deleteSuccess']=false;
-													$response['deleteFeedback']=$e->getMessage();
-												}
-											}
 										}else
 										{
-											$user_id = ($_REQUEST['user_id']);
+											$user_id = $_REQUEST['user_id'];
 											$tasklists = array();
+										}
+
+										if(isset($_POST['delete_keys']))
+										{
+											try{
+												$response['deleteSuccess']=true;
+												$delete_tasks = json_decode($_POST['delete_keys']);
+
+												foreach($delete_tasks as $task_id)
+												{
+													$old_task = $tasks->get_task($task_id);
+													if(!$GO_SECURITY->has_permission($GO_SECURITY->user_id, $old_task['acl_write']))
+													{
+														throw new AccessDeniedException();
+													}
+													$tasks->delete_task($task_id);
+												}
+											}catch(Exception $e)
+											{
+												$response['deleteSuccess']=false;
+												$response['deleteFeedback']=$e->getMessage();
+											}
 										}
 											
 											
@@ -321,7 +323,7 @@ try{
 										{
 											$task=array();
 											$task['id']=$_POST['completed_task_id'];
-											
+
 											$old_task = $tasks->get_task($task['id']);
 											if(!$GO_SECURITY->has_permission($GO_SECURITY->user_id, $old_task['acl_write']))
 											{
@@ -353,7 +355,7 @@ try{
 											
 										//$show_completed=isset($_POST['show_completed']) && $_POST['show_completed']=='true';
 										//$show_inactive=isset($_POST['show_inactive']) && $_POST['show_inactive']=='true';
-										
+
 										if(isset($_POST['show_completed']))
 										{
 											$GO_CONFIG->save_setting('tasks_show_completed', $_POST['show_completed'], $GO_SECURITY->user_id);
@@ -367,14 +369,14 @@ try{
 											
 										$response['total'] = $tasks->get_tasks($tasklists,$user_id, $show_completed, $sort, $dir, $start, $limit,$show_inactive);
 										$response['results']=array();
-										
+
 										$now=time();
-										
+
 										while($tasks->next_record(DB_ASSOC))
 										{
 											$task = $tasks->record;
 											$task['completed']=$tasks->f('completion_time')>0;
-											$task['late']=!$task['completed'] && $task['due_time']<$now;	
+											$task['late']=!$task['completed'] && $task['due_time']<$now;
 											$task['due_time']=date($_SESSION['GO_SESSION']['date_format'], $tasks->f('due_time'));
 											$task['description']=String::text_to_html(String::cut_string($task['description'],500));
 											$response['results'][] = $task;
