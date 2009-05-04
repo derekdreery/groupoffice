4e3fb677245add2684de6590e8d37d439fcf1373
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-05-04 07:01:53 +0000

    updated german

--- modules/gnupg/language/de.inc.php
+++ modules/gnupg/language/de.inc.php
@@ -2,6 +2,6 @@
 //Uncomment this line in new translations!
 require($GO_LANGUAGE->get_fallback_language_file('gnupg'));
 $lang['gnupg']['name']='GnuPG';
-$lang['gnupg']['description']='Modul zur Aktivierung PGP verschlüsselter E-Mail-Nachrichten';
+$lang['gnupg']['description']='Modul zur Verwendung PGP verschlüsselter E-Mail-Nachrichten';
 
 $lang['gnupg']['importSuccessful']='Schlüssel erfolgreich importiert';
