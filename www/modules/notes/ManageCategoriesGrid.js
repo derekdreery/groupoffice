4f04a825bcffb6bcef11b3994e266698349dbf12
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-05-26 15:23:44 +0000

    It was possible to manage categories with only read permission for the notes module

--- www/modules/notes/ManageCategoriesGrid.js
+++ www/modules/notes/ManageCategoriesGrid.js
@@ -64,6 +64,7 @@ GO.notes.ManageCategoriesGrid = function(config){
 			iconCls: 'btn-add',							
 			text: GO.lang['cmdAdd'],
 			cls: 'x-btn-text-icon',
+			disabled:!GO.settings.notes.write_permission,
 			handler: function(){				
 	    	this.categoryDialog.show();	    	
 			},
@@ -73,6 +74,7 @@ GO.notes.ManageCategoriesGrid = function(config){
 			iconCls: 'btn-delete',
 			text: GO.lang['cmdDelete'],
 			cls: 'x-btn-text-icon',
+			disabled:!GO.settings.notes.write_permission,
 			handler: function(){
 				this.deleteSelected();
 				this.changed=true;				
