47e7188513c7188230cc877a319cf754db533564
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-05-27 18:54:38 +0000

    no ssl

--- scripts/builddebs.sh
+++ scripts/builddebs.sh
@@ -11,6 +11,9 @@ cd godebs
 svn export https://mschering@group-office.svn.sourceforge.net/svnroot/group-office/trunk/debian-groupoffice
 svn export https://mschering@group-office.svn.sourceforge.net/svnroot/group-office/trunk/debian-groupoffice-mailserver
 
+chmod 775 debian-groupoffice/DEBIAN/postinst
+chmod 775 debian-groupoffice-mailserver/DEBIAN/postinst
+
 dpkg --build debian-groupoffice
 dpkg --build debian-groupoffice-mailserver
 
