f2819be2b23f87b6ebaf4c9c628292f329ef8120
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-05-28 13:28:12 +0000

    vacation works

--- debian-groupoffice-mailserver/usr/share/groupoffice-mailserver/tpl/etc/groupoffice/vacation.pl
+++ debian-groupoffice-mailserver/usr/share/groupoffice-mailserver/tpl/etc/groupoffice/vacation.pl
@@ -1,14 +1,14 @@
 # leave empty for connection via UNIX socket
-our $db_host = '';
+$db_host = '';
 
 # connection details
-our $db_username = '{db_user}';
-our $db_password = '{db_pass}';
-our $db_name     = '{db_name}';
+$db_username = '{db_user}';
+$db_password = '{db_pass}';
+$db_name     = '{db_name}';
 
 # path to logfile, when empty logging is supressed
-our $logfile='';
-#my $logfile = "/var/log/vacation.log";
+$logfile='';
+#$logfile = "/var/log/vacation.log";
 # path to file for debugging, debug supressed when empty
-our $debugfile='';
-#my $debugfile = "/var/log/vacation-debug.log";
+$debugfile='';
+#$debugfile = "/var/log/vacation-debug.log";
