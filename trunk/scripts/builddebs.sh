9d449a35e700f7589728b66afa5ea2794fbf5bbf
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-05-28 12:29:50 +0000

    config script

--- trunk/scripts/builddebs.sh
+++ trunk/scripts/builddebs.sh
@@ -23,12 +23,12 @@ cd godebs
 svn export https://mschering@group-office.svn.sourceforge.net/svnroot/group-office/trunk/debian-groupoffice
 svn export https://mschering@group-office.svn.sourceforge.net/svnroot/group-office/trunk/debian-groupoffice-mailserver
 
-#chmod 775 debian-groupoffice/DEBIAN/postinst
-#chmod 775 debian-groupoffice-mailserver/DEBIAN/postinst
-#chmod 775 debian-groupoffice-mailserver/DEBIAN/postrm
+chmod 775 debian-groupoffice/DEBIAN/postinst
+chmod 775 debian-groupoffice-mailserver/DEBIAN/postinst
+chmod 775 debian-groupoffice-mailserver/DEBIAN/postrm
 
 dpkg --build debian-groupoffice
 dpkg --build debian-groupoffice-mailserver
 
-mv debian-groupoffice.deb $OLDPWD/groupoffice.deb
-mv debian-groupoffice-mailserver.deb $OLDPWD/groupoffice-mailserver.deb
+mv debian-groupoffice.deb $FULLPATH/groupoffice.deb
+mv debian-groupoffice-mailserver.deb $FULLPATH/groupoffice-mailserver.deb
