9bb2a25982f5f112e66552f1efa42bb70aecb49e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-06-23 13:27:00 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.01-unstable-12/index.php
@@ -0,0 +1,43 @@
+<?php
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id$
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
+require_once("Group-Office.php");
+
+if(isset($_REQUEST['task']) && $_REQUEST['task']=='logout')
+{
+	$GO_SECURITY->logout();	
+	if(isset($_COOKIE['GO_FULLSCREEN']) && $_COOKIE['GO_FULLSCREEN']=='1')
+	{
+		?>
+		<script type="text/javascript">
+		window.close();
+		</script>
+		<?php
+		exit();
+	}
+}
+//$config_file = $GO_CONFIG->get_config_file();
+if(empty($GO_CONFIG->title))
+{
+	header('Location: install/');
+	exit();
+}
+
+if(!is_dir($GO_CONFIG->tmpdir))
+{
+	mkdir($GO_CONFIG->tmpdir,0755, true);
+}
+
+
+require_once($GO_THEME->theme_path."layout.inc.php");
