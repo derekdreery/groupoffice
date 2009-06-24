9559d7173f76b3360242498024c6a4e03ea7c829
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-06-24 13:20:27 +0000

    Created tag $1

---
+++ tags/groupoffice-com-3.02-stable-1/modules/modules/language/de.inc.php
@@ -0,0 +1,8 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('modules'));
+
+	$lang['modules']['name'] = 'Module';
+	$lang['modules']['description'] = 'Modul zum Verwalten anderer Module';
+	$lang['modules']['deleteModule'] = 'Dieses Modul kann nicht gelöscht werden!';
+?>
