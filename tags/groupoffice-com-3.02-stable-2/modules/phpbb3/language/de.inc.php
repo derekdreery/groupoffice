65669267ec95a0d4ab35f09d708b48c687d287d4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-06-30 13:44:38 +0000

    Created tag 3.02-stable-2

---
+++ tags/groupoffice-com-3.02-stable-2/modules/phpbb3/language/de.inc.php
@@ -0,0 +1,5 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('email'));
+$lang['phpbb3']['name'] = 'phpBB';
+$lang['phpbb3']['description'] = 'Modul zur Interaktion zwischen GroupOffice und phpBB (ab Version 3)';
