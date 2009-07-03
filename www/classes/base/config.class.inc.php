0ad75df9a775a0c2f8fce1f63c4a94927e511a5f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-07-03 08:56:00 +0000

    file search index ficed

--- www/classes/base/config.class.inc.php
+++ www/classes/base/config.class.inc.php
@@ -608,7 +608,7 @@ class GO_CONFIG
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.02-stable-2';
+	var $version = '3.02-stable-3';
 
 
 	/* The permissions mode to use when creating files
@@ -634,7 +634,7 @@ class GO_CONFIG
 	 * @access  public
 	 */
 
-	var $mtime = '20090626';
+	var $mtime = '20090703';
 
 	#group configuration
 	/**
