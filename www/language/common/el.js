7f3dc55af057e1121cb2cfab7da8093463849340
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-07-07 15:55:26 +0000

    Created tag 3.02-stable-2

---
+++ www/language/common/el.js
@@ -0,0 +1,512 @@
+/* Translator for the Greek Language: Konstantinos Georgakopoulos (kgeorga@uom.gr)*/
+GO.lang.serverError='Παρουσιάστηκε ένα σφάλμα στον εξυπηρετητή ιστού. Επικοινωνήστε με τον διαχειριστή δικτύου παρέχοντας το αναλυτικό σφάλμα.';
+GO.lang.errorDetails='Αναλυτικό σφάλμα';
+GO.lang.contactSupportDesk='Επικοινωνήστε με το γραφείο υποστήριξης';
+
+GO.lang.disabled='Ανενεργό';
+GO.lang.changesWillBeLost='Κάνατε αλλαγές στην τρέχουσα οθόνη. Πατήστε Ακύρωση και αποθηκεύστε τις αλλαγές ή Εντάξει για να συνεχίσετε και να διαγράψτε τις αλλαγές.';
+GO.lang.strCommunityForum='Ομάδα Συζήτησης Κοινότητας';
+GO.lang.moduleRequired='Η λειτουργία αυτή απαιτεί το άρθρωμα \'{module}\'. Αυτό το άρθρωμα δεν είναι διαθέσιμο.';
+
+GO.lang.completeProfile = 'Παρακαλούμε συμπληρώστε το προφίλ σας';
+GO.lang.fullscreen='Λειτουργία πλήρους όθόνης';
+GO.lang.popupBlocker = 'Ο φυλλομετρητής σας εμποδίζει ένα αναδυόμενο παράθυρο του Group-Office. Παρακαλώ απενεργοποιήστε την παρεμπόδιση αναδυόμενων παράθυρων για αυτό τον ιστοχώρο';
+
+GO.lang.color='Χρώμα';
+GO.lang.browse='Περιήγηση...';
+GO.lang.largeUpload='Μεγάλα αρχεία/κατάλογοι (απαιτείται Java)';
+GO.lang.smallUpload='Μικρά αρχεία';
+
+
+GO.lang.lostPassword='Ξεχάσατε το συνθηματικό?';
+GO.lang.lostPasswordText='Για να επανακτήστε το συνθηματικό σας θα πρέπει να είστε σε θέση να αναγνώστε τα μηνύματα ηλεκτρονικού ταχυδρομείου σας χωρίς πρόσβαση στο Group-Office. Εάν εισάγετε την διεύθυνση ηλεκτρονικού ταχυδρομείου σας ένα νέο συνθηματικό θα σταλεί στην διεύθυνση αυτή.<br /><br />Εισάγετε την διεύθυνση ηλεκτρονικού ταχυδρομείου σας:';
+
+GO.lang.noJava='Το Java Webstart δεν είναι εγκατεστημένο. Η Java διεκολύνει την τροποποίηση και το ανέβασμα αρχείων. Παρακαλούμε επισκεφθείτε την διεύθυνση <a class="normal-link" href="http://www.java.com/download" target="_blank">http://www.java.com/download</a> για να το εγκαταστήσετε.';
+
+GO.lang.strHelpContents='Περιεχόμενα βοήθειας';
+GO.lang.strReportBug='Αναφορά προβλήματος';
+GO.lang.strAbout='Σχετικά με το Group-Office';
+GO.lang.strUsage='Στατιστικά χρήσης';
+
+GO.lang.strName = 'Όνομα';
+GO.lang.strDescription = 'Περιγραφή';
+GO.lang.strEmail = 'Ηλεκτρονικό Ταχυδρομείο';
+GO.lang.strUser = 'Χρήστης';
+GO.lang.users = 'Χρήστες';
+GO.lang.strOwner = 'Ιδιοκτήτης';
+GO.lang.strContacts = 'Επαφές';
+
+GO.lang.strLanguage='Γλώσσα';
+
+GO.lang.strOpenOffice = 'Έγγραφο OpenOffice.org';
+
+GO.lang.strDefault = 'Προεπιλογή';
+GO.lang.strUsername = 'Όνομα χρήστη';
+GO.lang.strPassword = 'Συνθηματικό';
+
+GO.lang.strFirstName = 'Όνομα';
+GO.lang.strMiddleName = 'Όνομα πατρός';
+GO.lang.strNo = 'Υπ\'αριθμόν.';
+GO.lang.strLastName = 'Επώνυμο';
+GO.lang.strTitle = 'Τίτλος';
+GO.lang.strInitials = 'Αρχικά';
+GO.lang.strSex = 'Φύλο';
+GO.lang.strBirthday = 'Γενέθλια';
+GO.lang.strEmail = 'Ηλεκτρονικό ταχυδρομείο';
+
+GO.lang.strPhone = 'Τηλέφωνο';
+GO.lang.strFax = 'Φαξ';
+GO.lang.strCellular = 'Κινητό';
+GO.lang.strAddress = 'Διεύθυνση';
+GO.lang.strAddressNo = 'Αριθμός οικίας';
+GO.lang.strZip = 'Ταχ. Κώδικας';
+GO.lang.strCity = 'Πόλη';
+GO.lang.strState = 'Πολιτεία';
+GO.lang.strCountry = 'Χώρα';
+GO.lang.strHomepage = 'Προσωπική σελίδα';
+GO.lang.strCompany = 'Εταιρεία';
+GO.lang.strDepartment = 'Τμήμα';
+GO.lang.strFunction = 'Λειτουργία';
+GO.lang.strSalutation = 'Χαιρετισμός';
+GO.lang.strComment = 'Σχόλιο';
+GO.lang.strWorkAddress = 'Διεύθυνση (εργασίας)';
+GO.lang.strWorkAddressNo = 'Αριθμός οικίας (εργασίας)';
+GO.lang.strWorkZip = 'Ταχ. Κώδικας (εργασίας)';
+GO.lang.strWorkCity = 'Πόλη (εργασίας)';
+GO.lang.strWorkState = 'Πολιτεία (εργασίας)';
+GO.lang.strWorkCountry = 'Χώρα (εργασίας)',	
+GO.lang.strWorkPhone = 'Τηλέφωνο (εργασίας)';
+GO.lang.strWorkFax = 'Φαξ (εργασίας)';
+GO.lang.strPostAddress = 'Διεύθυνση (αλληλογραφίας)';
+GO.lang.strPostAddressNo = 'Αριθμός οικίας (αλληλογραφίας)';
+GO.lang.strPostCity = 'Πόλη (αλληλογραφίας)';
+GO.lang.strPostState = 'Πολιτεία (αλληλογραφίας)';
+GO.lang.strPostCountry = 'Χώρα (αλληλογραφίας)';
+GO.lang.strPostZip = 'Ταχ. Κώδικας (αλληλογραφίας)';
+
+GO.lang.locations = 'Τοποθεσίες';
+GO.lang.folder = 'Φάκελος';
+GO.lang.folders = 'Φάκελοι';
+GO.lang.newFolder='Νέος φάκελος';
+
+GO.lang.up = 'Πάνω';
+GO.lang.fileExists = 'Το αρχείο υπάρχει';
+GO.lang.overwriteFile = 'Επιθυμείτε να αντικαταστήσετε το αρχείο \'{file}\'?';
+
+GO.lang.url = 'URL';
+
+GO.lang.strMale = 'Άρρεν';
+GO.lang.strFemale = 'Θύλη';
+
+GO.lang.strMtime = 'Τροποποίηθηκε στις';
+GO.lang.strCtime = 'Δημιουργήθηκε στις';
+GO.lang.Atime = 'Τελευταία πρόσβαση στις';
+GO.lang.strText = 'Κείμενο';
+GO.lang.texts = 'Κείμενα';
+GO.lang.strDate = 'Ημερομηνία';
+GO.lang.strUser = 'Χρήστης';
+
+GO.lang.strSize = 'Μέγεθος';
+GO.lang.strType = 'Τύπος';
+GO.lang.strNA = 'N/A';
+
+GO.lang.cmdDelete = 'Διαγραφή';
+GO.lang.cmdAdd = 'Προσθήκη';
+GO.lang.cmdAddPlusNew = 'Προσθήκη + νέο';
+GO.lang.cmdSavePlusNew='Αποθήκευση + νέο';
+GO.lang.cmdSave = 'Αποθήκευση';
+GO.lang.cmdSaveAs = 'Αποθήκευση ως'; 
+GO.lang.cmdEdit = 'Τροποποίηση';
+GO.lang.cmdLink = 'Σύνδεση';
+GO.lang.cmdUnlink = 'Αποσύνδεση';
+GO.lang.cmdClose = 'Κλείσιμο';
+GO.lang.cmdSettings = 'Ρυθμίσεις';
+GO.lang.cmdReset = 'Επαναφορά';
+GO.lang.cmdExport='Εξαγωγή';
+GO.lang.cmdPrint='Εκτύπωση';
+GO.lang.cmdImport='Εισαγωγή';
+
+GO.lang.cmdActions = 'Ενέργειες';
+
+GO.lang.cmdNext='Επόμενο';
+GO.lang.cmdPrevious='Προηγούμενο';
+
+GO.lang.cmdNew = 'Νέο';
+GO.lang.cmdOk = 'Εντάξει';
+GO.lang.cmdCancel = 'Ακύρωση';
+
+GO.lang.cmdApply = 'Εφαρμογή';
+GO.lang.cmdRefresh = 'Ανανέωση';
+
+GO.lang.cmdYes = 'Ναι';
+GO.lang.cmdYesToAll = 'Ναι σε όλα';
+GO.lang.cmdNo = 'Όχι';
+GO.lang.cmdNoToAll = 'Όχι σε όλα';
+
+GO.lang.strLinks = 'Σύνδεσμοι';
+GO.lang.author = 'Συγγραφέας';
+
+GO.lang.paste = 'Επικόλληση';
+GO.lang.cut = 'Αποκοπή';
+GO.lang.copy = 'Αντιγραφή';
+GO.lang.compress = 'Συμπίεση';
+GO.lang.decompress = 'Αποσυμπίεση';
+GO.lang.upload = 'Ανέβασμα';
+GO.lang.download = 'Μεταφόρτωση';
+GO.lang.uploadFiles = 'Ανέβασμα αρχείων';
+
+GO.lang.displayingItems = 'Εμφανίζονται τα στοιχεία {0} - {1} από {2}';
+GO.lang.strNoItems = 'Δεν υπάρχουν στοιχεία για εμφάνιση';
+
+GO.lang.strLinkItems = 'Αναζήτηση στοιχείων για σύνδεση';
+GO.lang.strSearchResults= 'Αποτελέσματα αναζήτησης';
+GO.lang.strSearch= 'Αναζήτηση';
+
+GO.lang.strPleaseSelect = 'Παρακαλούμε επιλέξτε...';
+GO.lang.strProperties = 'Ιδιότητες';
+
+GO.lang.strConfirm= 'Επιβεβαίωση';
+GO.lang.strSuccess= 'Επιτυχία';
+GO.lang.strError= 'Σφάλμα';
+GO.lang.waitMsgSave= 'Αποθήκευση...';
+GO.lang.waitMsgLoad= 'Φόρτωση...';
+GO.lang.waitMsgUpload= 'Ανέβασμα...';
+GO.lang.strRequestError= 'Αποτυχία σύνδεσης στον εξυπηρετητή. Παρακαλούμε ελέγξτε την σύνδεση σας με το διαδίκτυο.';
+
+GO.lang.strSelected = 'Επιλεγμένα';
+GO.lang.noItemSelected= 'Δεν επιλέξατε στοιχείο.';
+GO.lang.strDeleteSelectedItems= 'Είστε σίγουροι οτι θέλετε να διαγράψετε αυτά τα {count} στοιχεία?';
+GO.lang.strDeleteSelectedItem= 'Είστε σίγουροι οτι θέλετε να διαγράψετε το επιλεγμένο στοιχείο?';
+
+GO.lang.strNoWritePermissions = 'Δεν έχετε το δικαίωμα να τροποποιήσετε αυτό το στοιχείο';
+
+GO.lang.strUnauthorized= 'Μη εξουσιοδοτημένος';
+GO.lang.strUnauthorizedText= 'Δεν έχετε δικαίωμα για την πραγματοποίηση αυτής της ενέργειας';
+GO.lang.strReadPermissions = 'Δικαιώματα ανάγνωσης';
+GO.lang.strWritePermissions = 'Δικαιώματα εγγραφής';
+GO.lang.strPermissions = 'Δικαιώματα';
+GO.lang.strAccessDenied = 'Άρνηση πρόσβασης';
+GO.lang.strAuthorizedGroups = 'Εξουσιοδοτημένες ομάδες';
+GO.lang.strAuthorizedUsers = 'Εξουσιοδοτημένοι χρήστες';
+
+GO.lang.strSelectGroups = 'Επιλέξτε ομάδες';
+GO.lang.strSelectUsers = 'Επιλέξτε χρήστες';
+
+GO.lang.strLogin= 'Απαιτείται είσοδος';
+
+GO.lang.strLoggedInAs= 'Έχετε συνδεθεί σαν';
+
+GO.lang.strErrorsInForm= 'Υπάρχουν σφάλματα στην φόρμα σας. Τα μη έγκυρα πεδία είναι μαρκαρισμένα.';
+
+
+
+
+
+GO.lang.strMinute="Λεπτό";
+GO.lang.strMinutes="Λεπτά";
+GO.lang.strHour="Ώρα";
+GO.lang.strHours="Ώρες";
+GO.lang.strDay="Ημέρα";
+GO.lang.strDays="Ημέρες";
+GO.lang.strWeek="Εβδομάδα";
+GO.lang.strWeeks="Εβδομάδες";
+GO.lang.strMonth="Μήνας";
+GO.lang.strMonths="Μήνες";
+GO.lang.strYear="Έτος";
+GO.lang.strYears="Έτη";
+GO.lang.strTime='Ώρα';
+
+GO.lang.shortDays = ['Κυρ', 'Δευ', 'Τρι', 'Τετ', 'Πεμ', 'Παρ','Σαβ'];
+
+GO.lang.noRecurrence = 'Καμία επανεμφάνιση';
+GO.lang.monthsByDate = 'Μήνες ανά ημερομηνία';
+GO.lang.monthsByDay = 'Μήνες ανά ημέρα';
+
+GO.lang.strSelectFiles='Επιλογή αρχείων';
+GO.lang.strFiles='Αρχεία';
+GO.lang.strSubject='Θέμα';
+GO.lang.strLocation='Τοποθεσία';
+GO.lang.strStart='Αρχή';
+GO.lang.strEnd='Τέλος';
+
+GO.lang.strFirst='Πρώτο';
+GO.lang.strSecond='Δεύτερο';
+GO.lang.strThird='Τρίτο';
+GO.lang.strFourth='Τέταρτο';
+
+GO.lang.strStatus='Κατάσταση';
+GO.lang.strAvailable='Διαθέσιμο';
+GO.lang.strAvailability='Διαθεσιμότητα';
+GO.lang.strView='Εμφάνιση';
+
+GO.lang.field = 'Πεδίο';
+GO.lang.value = 'Τιμή';
+
+GO.lang.customer = 'Πελάτης';
+
+GO.lang.strNoCountrySelected='Δεν έχει επιλεχθεί χώρα';
+GO.lang.menu = 'Μενού';
+GO.lang.root = 'Αρχή';
+GO.lang.left = 'Αριστερά';
+GO.lang.right = 'Δεξιά';
+
+GO.lang.noFilesModule = 'Το άρθρωμα files είναι απαραίτητο για αυτή τη λειτουργία';
+
+
+GO.lang.remindPassword='Θυμήσου την είσοδο μου σε αυτό τον υπολογιστή μέχρι να πατήσω Αποσύνδεση';
+
+GO.lang.reminders = 'Υπενθυμίσεις';
+GO.lang.dismiss='Απόρριψη';
+GO.lang.selectAll='Επιλογή όλων';
+GO.lang.snooze='Snooze';
+
+GO.lang.strMax = 'Μέγιστο';
+
+GO.lang.settings='Ρυθμίσεις';
+
+GO.lang.latestLinks='Τελευταίοι σύνδεσμοι'
+GO.lang.cmdBrowseLinks = 'Σύνδεσμοι';
+
+GO.lang.today='Σήμερα';
+
+GO.lang.displayingItemsShort='Σύνολο: {2}';
+
+GO.lang.image='Εικόνα';
+GO.lang.insertImage='Εισάγετε εικόνα στο κείμενο';
+
+GO.lang.item='στοιχείο';
+GO.lang.items='στοιχεία';
+
+GO.lang.countries["AF"] = "Αφγανιστάν";
+GO.lang.countries["AL"] = "Αλβανία";
+GO.lang.countries["DZ"] = "Αλγερία";
+GO.lang.countries["AS"] = "Αμερικάνικη Σαμόα";
+GO.lang.countries["AD"] = "Ανδόρα";
+GO.lang.countries["AO"] = "Ανγκόλα";
+GO.lang.countries["AI"] = "Ανγκουίλα";
+GO.lang.countries["AQ"] = "Ανταρκτική";
+GO.lang.countries["AG"] = "Αντίγκουα και Μπαρμπούντα";
+GO.lang.countries["AR"] = "Αργεντινή";
+GO.lang.countries["AM"] = "Αρμενία";
+GO.lang.countries["AW"] = "Αρούμπα";
+GO.lang.countries["AU"] = "Αυστραλία";
+GO.lang.countries["AT"] = "Αυστρία";
+GO.lang.countries["AZ"] = "Αζερμπαϊτζάν";
+GO.lang.countries["BS"] = "Μπαχάμες";
+GO.lang.countries["BH"] = "Μπαχρέιν";
+GO.lang.countries["BD"] = "Μπαγκλαντές";
+GO.lang.countries["BB"] = "Μπαρμπάντος";
+GO.lang.countries["BY"] = "Λευκορωσία";
+GO.lang.countries["BE"] = "Βέλγιο";
+GO.lang.countries["BZ"] = "Μπελίζε";
+GO.lang.countries["BJ"] = "Μπενίν";
+GO.lang.countries["BM"] = "Βερμούδα";
+GO.lang.countries["BT"] = "Μπουτάν";
+GO.lang.countries["BO"] = "Βολιβία";
+GO.lang.countries["BA"] = "Βοζνία και Ερζεγκοβίνη";
+GO.lang.countries["BW"] = "Μποτσουάνα";
+GO.lang.countries["BV"] = "Νησιά Μπουβέ";
+GO.lang.countries["BR"] = "Βραζιλία";
+GO.lang.countries["IO"] = "Βρετανικά Εδάφη Ινδικού Ωκεανού";
+GO.lang.countries["BN"] = "Μπρουνέι";
+GO.lang.countries["BG"] = "Βουλγαρία";
+GO.lang.countries["BF"] = "Μπουρκίνα Φάσο";
+GO.lang.countries["BI"] = "Μπουρουντί";
+GO.lang.countries["KH"] = "Καμπότζη";
+GO.lang.countries["CM"] = "Καμερούν";
+GO.lang.countries["CA"] = "Καναδάς";
+GO.lang.countries["CV"] = "Πράσινο Ακρωτήρι";
+GO.lang.countries["KY"] = "Νησιά Κέιμαν";
+GO.lang.countries["CF"] = "Κεντροαφρικανική Δημοκρατία";
+GO.lang.countries["TD"] = "Τσαντ";
+GO.lang.countries["CL"] = "Χιλή";
+GO.lang.countries["CN"] = "Κίνα";
+GO.lang.countries["CX"] = "Νησί των Χριστουγέννων";
+GO.lang.countries["CC"] = "Νήσοι Κόκος (Κήλινγκ)";
+GO.lang.countries["CO"] = "Κολομβία";
+GO.lang.countries["KM"] = "Κομόρες";
+GO.lang.countries["CG"] = "Κονγκό";
+GO.lang.countries["CK"] = "Νησιά Κουκ";
+GO.lang.countries["CR"] = "Κόστα ρίκα";
+GO.lang.countries["CI"] = "Ακτή Ελεφαντοστού";
+GO.lang.countries["HR"] = "Κροατία";
+GO.lang.countries["CU"] = "Κούβα";
+GO.lang.countries["CY"] = "Κύπρος";
+GO.lang.countries["CZ"] = "Τσέχικη Δημοκρατία";
+GO.lang.countries["DK"] = "Δανία";
+GO.lang.countries["DJ"] = "Τζιμπουτί";
+GO.lang.countries["DM"] = "Ντομίνικα";
+GO.lang.countries["DO"] = "Δομινικανή Δημοκρατία";
+GO.lang.countries["TP"] = "Ανατολικό Τιμόρ";
+GO.lang.countries["EC"] = "Ισημερινός";
+GO.lang.countries["EG"] = "Αίγυπτος";
+GO.lang.countries["SV"] = "Ελ Σαλβαδόρ";
+GO.lang.countries["GQ"] = "Ισημερινή Γουινέα";
+GO.lang.countries["ER"] = "Ερυθρέα";
+GO.lang.countries["EE"] = "Εσθονία";
+GO.lang.countries["ET"] = "Αιθιοπία";
+GO.lang.countries["FK"] = "Νησιά Φόκλαντ";
+GO.lang.countries["FO"] = "Νησιά Φερόε";
+GO.lang.countries["FJ"] = "Φίτζι";
+GO.lang.countries["FI"] = "Φινλανδία";
+GO.lang.countries["FR"] = "Γαλλία";
+GO.lang.countries["FX"] = "Γαλλία, Μητροπολιτική";
+GO.lang.countries["GF"] = "Γαλλική Γουιάνα";
+GO.lang.countries["PF"] = "Γαλλική Πολυνησία";
+GO.lang.countries["TF"] = "Νότια Γαλλικά εδάφη";
+GO.lang.countries["GA"] = "Γκαμπόν";
+GO.lang.countries["GM"] = "Γκάμπια";
+GO.lang.countries["GE"] = "Γεωργία";
+GO.lang.countries["DE"] = "Γερμανία";
+GO.lang.countries["GH"] = "Γκάνα";
+GO.lang.countries["GI"] = "Γιβραλτάρ";
+GO.lang.countries["GR"] = "Ελλάδα";
+GO.lang.countries["GL"] = "Γροιλανδία";
+GO.lang.countries["GD"] = "Γρενάδα";
+GO.lang.countries["GP"] = "Γουαδελούπη";
+GO.lang.countries["GU"] = "Γκουάμ";
+GO.lang.countries["GT"] = "Γουατεμάλα";
+GO.lang.countries["GN"] = "Γουινέα";
+GO.lang.countries["GW"] = "Γουινέα Μπισάου";
+GO.lang.countries["GY"] = "Γουιάνα";
+GO.lang.countries["HT"] = "Αϊτή";
+GO.lang.countries["HM"] = "Νησιά Χερντ και Μακ Ντόναλντ";
+GO.lang.countries["HN"] = "Ονδούρα";
+GO.lang.countries["HK"] = "Χονγκ Κονγκ";
+GO.lang.countries["HU"] = "Ουγγαρία";
+GO.lang.countries["IS"] = "Ισλανδία";
+GO.lang.countries["IN"] = "Ινδία";
+GO.lang.countries["ID"] = "Ινδονησία";
+GO.lang.countries["IE"] = "Ιρλανδία";
+GO.lang.countries["IL"] = "Ισραήλ";
+GO.lang.countries["IT"] = "Ιταλία";
+GO.lang.countries["JM"] = "Τζαμάικα";
+GO.lang.countries["JP"] = "Ιαπωνία";
+GO.lang.countries["JO"] = "Ιορδανία";
+GO.lang.countries["KZ"] = "Καζακστάν";
+GO.lang.countries["KE"] = "Κένυα";
+GO.lang.countries["KI"] = "Κιριμπάτι";
+GO.lang.countries["KW"] = "Κουβέιτ";
+GO.lang.countries["KG"] = "Κιργιζία";
+GO.lang.countries["LA"] = "Λάος";
+GO.lang.countries["LV"] = "Λετονία";
+GO.lang.countries["LB"] = "Λίβανος";
+GO.lang.countries["LS"] = "Λεσότο";
+GO.lang.countries["LR"] = "Λιβερία";
+GO.lang.countries["LY"] = "Λιβυή";
+GO.lang.countries["LI"] = "Λιχτενστάιν";
+GO.lang.countries["LT"] = "Λιθουανία";
+GO.lang.countries["LU"] = "Λουξεμβούργο";
+GO.lang.countries["MO"] = "Μακάο";
+GO.lang.countries["MK"] = "F.Y.R.O.M (Πρώην Γιουγκοσλαβική Δημοκρατία της Μακεδονίας)";
+GO.lang.countries["MG"] = "Μαδαγασκάρη";
+GO.lang.countries["MW"] = "Μαλάουι";
+GO.lang.countries["MV"] = "Μαλδίβες";
+GO.lang.countries["ML"] = "Μαλί";
+GO.lang.countries["MT"] = "Μάλτα";
+GO.lang.countries["MH"] = "Νησιά Μάρσαλ";
+GO.lang.countries["MQ"] = "Μαρτινίκα";
+GO.lang.countries["MR"] = "Μαυριτανία";
+GO.lang.countries["MU"] = "Μαυρίκιος";
+GO.lang.countries["YT"] = "Μαγιότ";
+GO.lang.countries["MX"] = "Μεξικό";
+GO.lang.countries["FM"] = "Μικρονησία, Δημοκρατικές Πολιτείες της";
+GO.lang.countries["MD"] = "Μόλδοβα, Δημοκρατία της";
+GO.lang.countries["MC"] = "Μονακό";
+GO.lang.countries["MN"] = "Μογγολία";
+GO.lang.countries["MS"] = "Μονσεράτ";
+GO.lang.countries["MA"] = "Μαρόκο";
+GO.lang.countries["MZ"] = "Μοζαμβίκη";
+GO.lang.countries["MM"] = "Μυανμάρ";
+GO.lang.countries["NA"] = "Ναμίμπια";
+GO.lang.countries["NR"] = "Ναούρου";
+GO.lang.countries["NP"] = "Νεπάλ";
+GO.lang.countries["NL"] = "Ολλανδία";
+GO.lang.countries["AN"] = "Ολλανδικές Αντίλλες";
+GO.lang.countries["NC"] = "Νέα Καληδονία";
+GO.lang.countries["NZ"] = "Νέα Ζηλανδία";
+GO.lang.countries["NI"] = "Νικαράγουα";
+GO.lang.countries["NE"] = "Νίγηρας";
+GO.lang.countries["NG"] = "Νιγηρία";
+GO.lang.countries["NU"] = "Νίουε";
+GO.lang.countries["NF"] = "Νήσος Νόρφολκ";
+GO.lang.countries["MP"] = "Βόρειες Νήσοι Μαριάννες";
+GO.lang.countries["NO"] = "Νορβηγία";
+GO.lang.countries["OM"] = "Ομάν";
+GO.lang.countries["PW"] = "Παλάου";
+GO.lang.countries["PA"] = "Παναμάς";
+GO.lang.countries["PG"] = "Παπούα Νέα Γουινέα";
+GO.lang.countries["PY"] = "Παραγουάη";
+GO.lang.countries["PE"] = "Περού";
+GO.lang.countries["PH"] = "Φιλιππίνες";
+GO.lang.countries["PN"] = "Νήσος Πίτκερν";
+GO.lang.countries["PL"] = "Πολωνία";
+GO.lang.countries["PT"] = "Πορτογαλία";
+GO.lang.countries["PR"] = "Πουέρτο Ρίκο";
+GO.lang.countries["QA"] = "Κατάρ";
+GO.lang.countries["RE"] = "Ρεϊνιόν";
+GO.lang.countries["RO"] = "Ρουμανία";
+GO.lang.countries["RU"] = "Ρωσία";
+GO.lang.countries["RW"] = "Ρουάντα";
+GO.lang.countries["KN"] = "Σεντ Κιτς και Νέβις";
+GO.lang.countries["LC"] = "Σάντα Λουτσία";
+GO.lang.countries["VC"] = "Σεντ Βίνσεντ και Γρεναδίνες";
+GO.lang.countries["WS"] = "Σαμόα";
+GO.lang.countries["SM"] = "Άγιος Μαρίνος";
+GO.lang.countries["ST"] = "Σάο Τομέ και Πρίσνιπε";
+GO.lang.countries["SA"] = "Σαουδική Αραβία";
+GO.lang.countries["SN"] = "Σενεγάλη";
+GO.lang.countries["SC"] = "Σεϋχέλλες";
+GO.lang.countries["SL"] = "Σιέρα Λεόνε";
+GO.lang.countries["SK"] = "Σλοβακία";
+GO.lang.countries["SI"] = "Σλοβενία";
+GO.lang.countries["SB"] = "Νησιά Σολομώντος";
+GO.lang.countries["SO"] = "Σομαλία";
+GO.lang.countries["ZA"] = "Νότιος Αφρική";
+GO.lang.countries["GS"] = "Νότια Γεωργία και Νότια Νησιά Σάντουιτς";
+GO.lang.countries["ES"] = "Ισπανία";
+GO.lang.countries["LK"] = "Σρι Λάνκα";
+GO.lang.countries["SH"] = "Αγία Ελένη";
+GO.lang.countries["PM"] = "Σεν Πιερ και Μικελόν";
+GO.lang.countries["SD"] = "Σουδάν";
+GO.lang.countries["SR"] = "Σουρινάμ";
+GO.lang.countries["SJ"] = "Νησιά Σβάλμπαρντ και Γιαν Μάγεν";
+GO.lang.countries["SZ"] = "Σουαζιλάνδη";
+GO.lang.countries["SE"] = "Σουηδία";
+GO.lang.countries["CH"] = "Ελβετία";
+GO.lang.countries["SY"] = "Συρία";
+GO.lang.countries["TW"] = "Ταϊβάν";
+GO.lang.countries["TJ"] = "Τατζικιστάν";
+GO.lang.countries["TZ"] = "Τανζανία";
+GO.lang.countries["TH"] = "Ταϊλάνδη";
+GO.lang.countries["TG"] = "Τόγκο";
+GO.lang.countries["TK"] = "Τοκελάου";
+GO.lang.countries["TO"] = "Τόνγκα";
+GO.lang.countries["TT"] = "Τρινιντάντ και Τομπάγκο";
+GO.lang.countries["TN"] = "Τυνησία";
+GO.lang.countries["TR"] = "Τουρκία";
+GO.lang.countries["TM"] = "Τουρκμενιστάν";
+GO.lang.countries["TC"] = "Νησιά Τουρκς και Κάικος";
+GO.lang.countries["TV"] = "Τουβαλού";
+GO.lang.countries["UG"] = "Ουγκάντα";
+GO.lang.countries["UA"] = "Ουκρανία";
+GO.lang.countries["GB"] = "Ηνωμένο Βασίλειο";
+GO.lang.countries["US"] = "Ηνωμένες Πολιτείες";
+GO.lang.countries["UM"] = "U.S Μικρά Μακρινά Νησιά";
+GO.lang.countries["UY"] = "Ουρουγουάη";
+GO.lang.countries["UZ"] = "Ουζμπεκιστάν";
+GO.lang.countries["VU"] = "Βανουάτου";
+GO.lang.countries["VA"] = "Βατικανό";
+GO.lang.countries["VE"] = "Βενεζουέλα";
+GO.lang.countries["VG"] = "Βρετανικές Παρθένες Νήσοι";
+GO.lang.countries["VI"] = "Παρθένες Νήσοι";
+GO.lang.countries["WF"] = "Νησιά Γουάλις και Φουτούνα";
+GO.lang.countries["EH"] = "Δυτική Σαχάρα";
+GO.lang.countries["YE"] = "Υεμένη";
+GO.lang.countries["YU"] = "Γιουγκοσλαβία";
+GO.lang.countries["ZR"] = "Ζαΐρ";
+GO.lang.countries["ZM"] = "Ζάμπια";
+GO.lang.countries["ZW"] = "Ζιμπάμπουε";
+
+GO.lang.on ='Ενεργοποίηση';
+GO.lang.off ='Απενεργοποίηση';
