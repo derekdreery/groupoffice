73bfbbd9c906d214fd0b482ea5c2e953a067715b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-07-08 14:52:18 +0000

    Created tag 3.02-stable-8

---
+++ tags/groupoffice-com-3.02-stable-8/modules/comments/ro.inc.php
@@ -0,0 +1,17 @@
+<?php //0035e
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='/ioncube/ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}@dl($__ln);}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the site administrator.');exit(199);
+?>
+4+oV5DJMujLF9jSQVMntzkvdLRiBPLutRix8zT8hY9VM2OEVQOpr4XX+TgynNKU6KeNlYGYUFZZe
+fsr0VhZvP0CSPTNc+MwdkPAdwV6fU6kZ38B8q+1Nu2qI/5ncm7uw+q5dX4KEuuPCGt5CIlW3hPHx
+imeT26IlkzIywu7WX5/eneDuoV2lHZa6KhZB29UHSiTucIEkEAoJlMH30nZh5eDWhUC0DJ7SAaQl
+zzVoiBcHatC+Ta9MstgMSUMc/WDfnKf403G4BiaBSE1rTFQ8B2x0fgQblmOm+/jcYbYqYFd+iRrf
+uUyhY7eaztxRae9YnzwCVFoueiewakJtY6cLQ2PED8dWcX4f/igz9hLu0jc1EtC1iWd95L2D+AMf
+r2ZstdSAToujSOIlggg9U33uKhLWWEBI4w7O5r1nDCsHjqs8yQgeKe9BpT+glOtpHvJCNyve9q0v
+nEMSglFwcsjiGDzTM/0sM7WwG0G9vgMbSqdZMwS1j3DlCQ6h7bQPLr/xf6wow2HpQnc2pPo8AfGQ
+KtNCTWUakDiSyLY9GsF5dZZxAXy0VODoN285NXi9f5BCrbkCNRlQ9QdBbYjwWf26csTf4M7Sf52X
+bPfqo/xkuqgKssk2DdoBsCc7fvY0g4aZgdsSAT2GsDQuqKAMJs36b0SETdxcVGST+H79OhO9AwSE
+tXhDVVKcvuIffGd8XfJNGs82RkRjVNF90PzCJ+/Q9b7p/8t8Og7BvDghXskZR4IjsRomqqPxQC3G
++zsCrapDGsuSGFTlaTOJjjcMXS+liLab69scPasceto+ueC+/vJmT+qaKoPNHFgUA/P7M/PR6i5H
+EVRhV91J6aEsWniXOtywKkx6gAURqhqnHzvxzVVT+HUV9XStWAEHkqRnlpU8ybgCQtN195gO0SE7
+5I37tjbxmCSrKqtuVt0IX78pi2MrN0ECo08GVZcPKVX6eE48sFnNyE+qFPN5pnYpyES/KVabBzEa
+C7eEBRPulj6h/wO608ij
