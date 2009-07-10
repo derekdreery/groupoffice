ae7f93085198942bc80f37a57984364451cdc297
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-07-10 13:06:24 +0000

    Created tag 3.02-stable-11

---
+++ tags/groupoffice-com-3.02-stable-11/language/common/it.js
@@ -0,0 +1,506 @@
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id: en.js 2942 2008-09-02 12:24:54Z mschering $
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
+GO.lang.strCommunityForum='Forum';
+GO.lang.moduleRequired='Questa caratteristica richiede il modulo \'{module}\' . Questo modulo non è disponibile.';
+GO.lang.completeProfile= 'Completa il tuo profilo';
+GO.lang.cmdImport='Importa';
+
+GO.lang.fullscreen='Modalità schermo intero';
+GO.lang.popupBlocker = 'Il tuo browser blocca un popup di Group-Office. Disabilita il blocco-popup per questo sito';
+
+GO.lang.color='Colore';
+GO.lang.browse='Sfoglia...';
+GO.lang.largeUpload='Grande dimensione di file/cartelle (richiesto Java)';
+GO.lang.smallUpload='Piccola dimensione';
+
+
+GO.lang.lostPassword='Hai perso la password?';
+GO.lang.lostPasswordText='Per recuperare la password devi poter leggere la tua e-mail senza accedere a Group-Office. Se inserisci il tuo indirizzo e-mail una nuova password sarà inviata al tuo indirizzo e-mail.<br /><br />Inserisci il tuo indirizzo e-mail:';
+
+GO.lang.noJava='Java Webstart non è installato. Java consente di facilitare la modifica ed il caricamento dei file. Visita <a class="normal-link" href="http://www.java.com/download" target="_blank">http://www.java.com/download</a> per installarlo.';
+
+GO.lang.strHelpContents='Guida in linea';
+GO.lang.strReportBug='Segnala un bug';
+GO.lang.strAbout='Informazioni su Group-Office';
+GO.lang.strUsage='Statistiche di utilizzo';
+
+GO.lang.strName = 'Nome';
+GO.lang.strDescription = 'Descrizione';
+GO.lang.strEmail = 'E-mail';
+GO.lang.strUser = 'Utente';
+GO.lang.users = 'Utenti';
+GO.lang.strOwner = 'Proprietario';
+GO.lang.strContacts = 'Contatti';
+
+GO.lang.strLanguage='Lingua';
+
+GO.lang.strOpenOffice = 'Documento OpenOffice.org';
+
+GO.lang.strDefault = 'Predefinito';
+GO.lang.strUsername = 'Username';
+GO.lang.strPassword = 'Password';
+
+GO.lang.strFirstName = 'Nome';
+GO.lang.strMiddleName = 'Secondo nome';
+GO.lang.strNo = 'No.';
+GO.lang.strLastName = 'Cognome';
+GO.lang.strTitle = 'Titolo';
+GO.lang.strInitials = 'Iniziali';
+GO.lang.strSex = 'Sesso';
+GO.lang.strBirthday = 'Data di nascita';
+GO.lang.strEmail = 'E-mail';
+
+GO.lang.strPhone = 'Telefono';
+GO.lang.strFax = 'Fax';
+GO.lang.strCellular = 'Cellulare';
+GO.lang.strAddress = 'Indirizzo';
+GO.lang.strAddressNo = 'Numero civico';
+GO.lang.strZip = 'CAP';
+GO.lang.strCity = 'Città';
+GO.lang.strState = 'Provincia';
+GO.lang.strCountry = 'Nazione';
+GO.lang.strHomepage = 'Homepage';
+GO.lang.strCompany = 'Azienda';
+GO.lang.strDepartment = 'Ufficio';
+GO.lang.strFunction = 'Funzione';
+GO.lang.strSalutation = 'Formula di apertura';
+GO.lang.strComment = 'Note';
+GO.lang.strWorkAddress = 'Indirizzo (Lavoro)';
+GO.lang.strWorkAddressNo = 'Numero civico (lavoro)';
+GO.lang.strWorkZip = 'CAP (lavoro)';
+GO.lang.strWorkCity = 'Città (lavoro)';
+GO.lang.strWorkState = 'Provincia (lavoro)';
+GO.lang.strWorkCountry = 'Nazione (lavoro)',	
+GO.lang.strWorkPhone = 'Telefono (lavoro)';
+GO.lang.strWorkFax = 'Fax (lavoro)';
+GO.lang.strPostAddress = 'Indirizzo';
+GO.lang.strPostAddressNo = 'Numero civico';
+GO.lang.strPostCity = 'Città';
+GO.lang.strPostState = 'Provincia';
+GO.lang.strPostCountry = 'Nazione';
+GO.lang.strPostZip = 'CAP';
+
+GO.lang.locations = 'Percorso';
+GO.lang.folder = 'Cartella';
+GO.lang.folders = 'Cartelle';
+
+GO.lang.up = 'Livello superiore';
+GO.lang.fileExists = 'Il file esiste';
+GO.lang.overwriteFile = 'Vuoi sovrascrivere il file \'{file}\'?';
+
+GO.lang.url = 'URL';
+
+GO.lang.strMale = 'Maschio';
+GO.lang.strFemale = 'Femmina';
+
+GO.lang.strMtime = 'Modificato alle';
+GO.lang.strCtime = 'Creato alle';
+GO.lang.Atime = 'Ultimo accesso';
+GO.lang.strText = 'Testo';
+GO.lang.texts = 'Testi';
+GO.lang.strDate = 'Data';
+GO.lang.strUser = 'Utente';
+
+GO.lang.strSize = 'Dimensione';
+GO.lang.strType = 'Tipo';
+GO.lang.strNA = 'N/A';
+
+GO.lang.cmdDelete = 'Cancella';
+GO.lang.cmdAdd = 'Aggiungi';
+GO.lang.cmdAddPlusNew = 'Aggiungi + nuovo';
+GO.lang.cmdSavePlusNew='Salva + nuovo';
+GO.lang.cmdSave = 'Salva';
+GO.lang.cmdEdit = 'Modifica';
+GO.lang.cmdLink = 'Collegamento';
+GO.lang.cmdUnlink = 'Togli il collegamento';
+GO.lang.cmdClose = 'Chiudi';
+GO.lang.cmdSettings = 'Opzioni';
+GO.lang.cmdReset = 'Reset';
+GO.lang.cmdExport='Esporta';
+GO.lang.cmdPrint='Stampa';
+
+GO.lang.cmdActions = 'Azioni';
+
+GO.lang.cmdNext='Avanti';
+GO.lang.cmdPrevious='Indietro';
+
+GO.lang.cmdNew = 'Nuovo';
+GO.lang.cmdOk = 'Ok';
+GO.lang.cmdCancel = 'Cancella';
+
+GO.lang.cmdApply = 'Applica';
+GO.lang.cmdRefresh = 'Ricarica';
+
+GO.lang.cmdYes = 'Si';
+GO.lang.cmdYesToAll = 'Si a tutto';
+GO.lang.cmdNo = 'No';
+GO.lang.cmdNoToAll = 'No a tutto';
+
+GO.lang.strLinks = 'Collegamenti';
+GO.lang.author = 'Autore';
+
+GO.lang.paste = 'Incolla';
+GO.lang.cut = 'Taglia';
+GO.lang.copy = 'Copia';
+GO.lang.compress = 'Comprimi';
+GO.lang.decompress = 'Decomprimi';
+GO.lang.upload = 'Carica';
+GO.lang.download = 'Scarica';
+GO.lang.uploadFiles = 'Carica file';
+
+GO.lang.displayingItems = 'Visualizzati {0} - {1} di {2}';
+GO.lang.strNoItems = 'Non ci sono oggetti da visualizzare';
+
+GO.lang.strLinkItems = 'Ricerca di oggetti da collegare';
+GO.lang.strSearchResults= 'Cerca i risultati';
+GO.lang.strSearch= 'Cerca';
+
+GO.lang.strPleaseSelect = 'Seleziona...';
+GO.lang.strProperties = 'Proprietà';
+
+GO.lang.strConfirm= 'Conferma';
+GO.lang.strSuccess= 'Successo';
+GO.lang.strError= 'Errore';
+GO.lang.waitMsgSave= 'Salvataggio in corso...';
+GO.lang.waitMsgLoad= 'Caricamento in corso...';
+GO.lang.waitMsgUpload= 'Caricamento...';
+
+
+GO.lang.strSelected = 'Selezionato';
+GO.lang.noItemSelected= 'Non hai selezionato nessun oggetto.';
+GO.lang.strDeleteSelectedItems= 'Sei sicuro di voler cancellate {count} oggetti?';
+GO.lang.strDeleteSelectedItem= 'Sei sicuro di voler cancellare l\'oggetto selezionato?';
+
+GO.lang.strNoWritePermissions = 'Non sei autorizzato a modificare quest\'oggetto.';
+
+GO.lang.strUnauthorized= 'Non autorizzato';
+GO.lang.strUnauthorizedText= 'Non hai il permesso di eseguire questa azione';
+GO.lang.strReadPermissions = 'Permessi di lettura';
+GO.lang.strWritePermissions = 'Permessi di scrittura';
+GO.lang.strPermissions = 'Permessi';
+GO.lang.strAccessDenied = 'Accesso negato';
+GO.lang.strAuthorizedGroups = 'Gruppi autorizzati';
+GO.lang.strAuthorizedUsers = 'Utenti autorizzati';
+
+GO.lang.strSelectGroups = 'Seleziona i gruppi';
+GO.lang.strSelectUsers = 'Seleziona gli utenti';
+
+GO.lang.strLogin= 'Login richiesto';
+
+GO.lang.strLoggedInAs= 'Accesso eseguito da';
+
+GO.lang.strErrorsInForm= 'Ci sono errori nel modulo. I campi non validi sono contrassegnati.';
+
+
+
+Ext.MessageBox.buttonText.yes = GO.lang['cmdYes'];
+Ext.MessageBox.buttonText.no = GO.lang['cmdNo'];
+Ext.MessageBox.buttonText.ok = GO.lang['cmdOk'];
+Ext.MessageBox.buttonText.cancel = GO.lang['cmdCancel'];
+
+
+GO.lang.strMinute="Minuto";
+GO.lang.strMinutes="Minuti";
+GO.lang.strHour="Ora";
+GO.lang.strHours="Ore";
+GO.lang.strDay="Giorno";
+GO.lang.strDays="Giorni";
+GO.lang.strWeek="Settimana";
+GO.lang.strWeeks="Settimane";
+GO.lang.strMonth="Mese";
+GO.lang.strMonths="Mesi";
+GO.lang.strYear="Anno";
+GO.lang.strYears="Anni";
+GO.lang.strTime='Tempo';
+
+GO.lang.shortDays = ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven','Sab'];
+
+GO.lang.noRecurrence = 'Nessuna ricorrenza';
+GO.lang.monthsByDate = 'Months by date';
+GO.lang.monthsByDay = 'Months by day';
+
+GO.lang.strSelectFiles='Seleziona i file';
+GO.lang.strFiles='File';
+GO.lang.strSubject='Oggetto';
+GO.lang.strLocation='Posizione';
+GO.lang.strStart='Inizio';
+GO.lang.strEnd='Fine';
+
+GO.lang.strFirst='Primo';
+GO.lang.strSecond='Secondo';
+GO.lang.strThird='Terzo';
+GO.lang.strFourth='Quarto';
+
+GO.lang.strStatus='Stato';
+GO.lang.strAvailable='Disponibile';
+GO.lang.strAvailability='Disponibilità';
+GO.lang.strView='Vedi';
+
+GO.lang.field = 'Campo';
+
+GO.lang.customer = 'Cliente';
+
+GO.lang.strNoCountrySelected='Nessuna nazione selezionata';
+GO.lang.menu = 'Menù';
+GO.lang.root = 'Root';
+GO.lang.left = 'Sinistra';
+GO.lang.right = 'Destra';
+
+GO.lang.noFilesModule = 'IL modulo dei file è richiesto per questa funzione';
+
+
+GO.lang.remindPassword='Ricorda il mio accesso su questo computer';
+
+GO.lang.reminders = 'Promemoria';
+GO.lang.dismiss='Dismiss';
+GO.lang.snooze='Snooze';
+
+GO.lang.strMax = 'Massimo';
+
+GO.lang.settings='Opzioni';
+
+GO.lang.latestLinks='Ultimi collegamenti'
+GO.lang.cmdBrowseLinks = 'Sfoglia collegamenti';
+
+
+GO.lang.countries["AF"] = "Afghanistan";
+GO.lang.countries["AL"] = "Albania";
+GO.lang.countries["DZ"] = "Algeria";
+GO.lang.countries["AS"] = "Samoa Americana";
+GO.lang.countries["AD"] = "Andorra";
+GO.lang.countries["AO"] = "Angola";
+GO.lang.countries["AI"] = "Anguilla";
+GO.lang.countries["AQ"] = "Antartico";
+GO.lang.countries["AG"] = "Antigua e Barbuda";
+GO.lang.countries["AR"] = "Argentina";
+GO.lang.countries["AM"] = "Armenia";
+GO.lang.countries["AW"] = "Aruba";
+GO.lang.countries["AU"] = "Australia";
+GO.lang.countries["AT"] = "Austria";
+GO.lang.countries["AZ"] = "Azerbaijan";
+GO.lang.countries["BS"] = "Bahamas";
+GO.lang.countries["BH"] = "Bahrain";
+GO.lang.countries["BD"] = "Bangladesh";
+GO.lang.countries["BB"] = "Barbados";
+GO.lang.countries["BY"] = "Bielorussia";
+GO.lang.countries["BE"] = "Belgio";
+GO.lang.countries["BZ"] = "Belize";
+GO.lang.countries["BJ"] = "Benin";
+GO.lang.countries["BM"] = "Bermuda";
+GO.lang.countries["BT"] = "Bhutan";
+GO.lang.countries["BO"] = "Bolivia";
+GO.lang.countries["BA"] = "Bosnia-Herzegovina";
+GO.lang.countries["BW"] = "Botswana";
+GO.lang.countries["BV"] = "Isola Bouvet";
+GO.lang.countries["BR"] = "Brasile";
+GO.lang.countries["IO"] = "Territori Inglesi dell\'Oceano Atlantico";
+GO.lang.countries["BN"] = "Brunei";
+GO.lang.countries["BG"] = "Bulgaria";
+GO.lang.countries["BF"] = "Burkina Faso";
+GO.lang.countries["BI"] = "Burundi";
+GO.lang.countries["KH"] = "Cambogia";
+GO.lang.countries["CM"] = "Cameroon";
+GO.lang.countries["CA"] = "Canada";
+GO.lang.countries["CV"] = "Capo Verde";
+GO.lang.countries["KY"] = "Isole Cayman";
+GO.lang.countries["CF"] = "Repubblica Centrafricana";
+GO.lang.countries["TD"] = "Chad";
+GO.lang.countries["CL"] = "Cile";
+GO.lang.countries["CN"]= "Cina";
+GO.lang.countries["CX"] = "Isole Christmas";
+GO.lang.countries["CC"] = "Isole Cocos";
+GO.lang.countries["CO"] = "Colombia";
+GO.lang.countries["KM"] = "Comore";
+GO.lang.countries["CG"] = "Congo";
+GO.lang.countries["CK"] = "Isole Cook";
+GO.lang.countries["CR"] = "Costa Rica";
+GO.lang.countries["CI"] = "Costa D'Avorio";
+GO.lang.countries["HR"] = "Croazia";
+GO.lang.countries["CU"] = "Cuba";
+GO.lang.countries["CY"] = "Cipro";
+GO.lang.countries["CZ"] = "Repubblica Ceca";
+GO.lang.countries["DK"] = "Danimarca";
+GO.lang.countries["DJ"] = "Gibuti";
+GO.lang.countries["DM"] = "Dominica";
+GO.lang.countries["DO"] = "Repubblica Dominicana";
+GO.lang.countries["TP"] = "Timor Est";
+GO.lang.countries["EC"] = "Ecuador";
+GO.lang.countries["EG"] = "Egitto";
+GO.lang.countries["SV"] = "El Salvador";
+GO.lang.countries["GQ"] = "Guinea Equatoriale";
+GO.lang.countries["ER"] = "Eritrea";
+GO.lang.countries["EE"] = "Estonia";
+GO.lang.countries["ET"] = "Etiopia";
+GO.lang.countries["FK"] = "Isole Falkland";
+GO.lang.countries["FO"] = "Isole Faroe";
+GO.lang.countries["FJ"] = "Fiji";
+GO.lang.countries["FI"] = "Finlandia";
+GO.lang.countries["FR"] = "Francia";
+GO.lang.countries["FX"] = "Francia Metropolitana";
+GO.lang.countries["GF"] = "Guiana Francese";
+GO.lang.countries["PF"] = "Polinesia Francese";
+GO.lang.countries["TF"] = "Territori Australi Francesi";
+GO.lang.countries["GA"] = "Gabon";
+GO.lang.countries["GM"] = "Gambia";
+GO.lang.countries["GE"] = "Georgia";
+GO.lang.countries["DE"] = "Germania";
+GO.lang.countries["GH"] = "Ghana";
+GO.lang.countries["GI"] = "Gibilterra";
+GO.lang.countries["GR"] = "Grecia";
+GO.lang.countries["GL"] = "Groenlandia";
+GO.lang.countries["GD"] = "Grenada";
+GO.lang.countries["GP"] = "Guadalupe";
+GO.lang.countries["GU"] = "Guam";
+GO.lang.countries["GT"] = "Guatemala";
+GO.lang.countries["GN"] = "Guinea";
+GO.lang.countries["GW"] = "Guinea-bissau";
+GO.lang.countries["GY"] = "Guyana";
+GO.lang.countries["HT"] = "Haiti";
+GO.lang.countries["HM"] = "Isole Heard e Mc Donald";
+GO.lang.countries["HN"] = "Honduras";
+GO.lang.countries["HK"] = "Hong Kong";
+GO.lang.countries["HU"] = "Ungheria";
+GO.lang.countries["IS"] = "Islanda";
+GO.lang.countries["IN"] = "India";
+GO.lang.countries["ID"] = "Indonesia";
+GO.lang.countries["IE"] = "Irlanda";
+GO.lang.countries["IL"] = "Israele";
+GO.lang.countries["IT"] = "Italia";
+GO.lang.countries["JM"] = "Jamaica";
+GO.lang.countries["JP"] = "Giappone";
+GO.lang.countries["JO"] = "Giordania";
+GO.lang.countries["KZ"] = "Kazakistan";
+GO.lang.countries["KE"] = "Kenya";
+GO.lang.countries["KI"] = "Kiribati";
+GO.lang.countries["KW"] = "Kuwait";
+GO.lang.countries["KG"] = "Kyrghizistan";
+GO.lang.countries["LA"] = "Repubblica Popolare Democratica del Laos";
+GO.lang.countries["LV"] = "Lettonia";
+GO.lang.countries["LB"] = "Libano";
+GO.lang.countries["LS"] = "Lesotho";
+GO.lang.countries["LR"] = "Liberia";
+GO.lang.countries["LY"] = "Libia";
+GO.lang.countries["LI"] = "Liechtenstein";
+GO.lang.countries["LT"] = "Lituania";
+GO.lang.countries["LU"] = "Lussemburgo";
+GO.lang.countries["MO"] = "Macao";
+GO.lang.countries["MK"] = "Macedonia";
+GO.lang.countries["MG"] = "Madagascar";
+GO.lang.countries["MW"] = "Malawi";
+GO.lang.countries["MV"] = "Maldive";
+GO.lang.countries["ML"] = "Mali";
+GO.lang.countries["MT"] = "Malta";
+GO.lang.countries["MH"] = "Isole Marshall";
+GO.lang.countries["MQ"] = "Martinica";
+GO.lang.countries["MR"] = "Mauritania";
+GO.lang.countries["MU"] = "Mauritius";
+GO.lang.countries["YT"] = "Mayotte";
+GO.lang.countries["MX"] = "Messico";
+GO.lang.countries["FM"] = "Micronesia";
+GO.lang.countries["MD"] = "Moldavia";
+GO.lang.countries["MC"] = "Monaco";
+GO.lang.countries["MN"] = "Mongolia";
+GO.lang.countries["MS"] = "Montserrat";
+GO.lang.countries["MA"] = "Marocco";
+GO.lang.countries["MZ"] = "Mozambico";
+GO.lang.countries["MM"] = "Myanmar";
+GO.lang.countries["NA"] = "Namibia";
+GO.lang.countries["NR"] = "Nauru";
+GO.lang.countries["NP"] = "Nepal";
+GO.lang.countries["NL"] = "Paesi Bassi";
+GO.lang.countries["AN"] = "Antille olandesi";
+GO.lang.countries["NC"] = "Nuova Caledonia";
+GO.lang.countries["NZ"] = "Nuova Zelanda";
+GO.lang.countries["NI"] = "Nicaragua";
+GO.lang.countries["NE"] = "Niger";
+GO.lang.countries["NG"] = "Nigeria";
+GO.lang.countries["NU"] = "Niue";
+GO.lang.countries["NF"] = "Isole Norfolk";
+GO.lang.countries["MP"] = "Isole Marianne Settentrionali";
+GO.lang.countries["NO"] = "Norvegia";
+GO.lang.countries["OM"] = "Oman";
+GO.lang.countries["PW"] = "Palau";
+GO.lang.countries["PA"] = "Panama";
+GO.lang.countries["PG"] = "Papua Nuova Guinea";
+GO.lang.countries["PY"] = "Paraguay";
+GO.lang.countries["PE"] = "Perù";
+GO.lang.countries["PH"] = "Filippine";
+GO.lang.countries["PN"] = "Pitcairn";
+GO.lang.countries["PL"] = "Polonia";
+GO.lang.countries["PT"] = "Portogallo";
+GO.lang.countries["PR"] = "Puerto Rico";
+GO.lang.countries["QA"] = "Qatar";
+GO.lang.countries["RE"] = "Reunion";
+GO.lang.countries["RO"] = "Romania";
+GO.lang.countries["RU"] = "Russia";
+GO.lang.countries["RW"] = "Ruanda";
+GO.lang.countries["KN"] = "Saint Kitts e Nevis";
+GO.lang.countries["LC"] = "Saint Lucia";
+GO.lang.countries["VC"] = "Saint Vincent e Grenadine";
+GO.lang.countries["WS"] = "Samoa";
+GO.lang.countries["SM"] = "San Marino";
+GO.lang.countries["ST"] = "Sao Tome and Principe";
+GO.lang.countries["SA"] = "Arabia Saudita";
+GO.lang.countries["SN"] = "Senegal";
+GO.lang.countries["SC"] = "Seychelles";
+GO.lang.countries["SL"] = "Sierra Leone";
+GO.lang.countries["SK"] = "Slovacchia";
+GO.lang.countries["SI"] = "Slovenia";
+GO.lang.countries["SB"] = "Isole Solomon";
+GO.lang.countries["SO"] = "Somalia";
+GO.lang.countries["ZA"] = "Sudafrica";
+GO.lang.countries["GS"] = "Georgia del Sud e Isole Sandwich Meridionali";
+GO.lang.countries["ES"] = "Spagna";
+GO.lang.countries["LK"] = "Sri Lanka";
+GO.lang.countries["SH"] = "Sant'Elena";
+GO.lang.countries["PM"] = "St. Pierre et Miquelon";
+GO.lang.countries["SD"] = "Sudan";
+GO.lang.countries["SR"] = "Suriname";
+GO.lang.countries["SJ"] = "Isole Svalbard e Jan Mayen";
+GO.lang.countries["SZ"] = "Swaziland";
+GO.lang.countries["SE"] = "Svezia";
+GO.lang.countries["CH"] = "Svizzera";
+GO.lang.countries["SY"] = "Siria";
+GO.lang.countries["TW"] = "Taiwan";
+GO.lang.countries["TJ"] = "Tagikistan";
+GO.lang.countries["TZ"] = "Tanzania";
+GO.lang.countries["TG"] = "Togo";
+GO.lang.countries["TK"] = "Tokelau";
+GO.lang.countries["TO"] = "Tonga";
+GO.lang.countries["TT"] = "Trinidad e Tobago";
+GO.lang.countries["TN"] = "Tunisia";
+GO.lang.countries["TR"] = "Turchia";
+GO.lang.countries["TM"] = "Turkmenistan";
+GO.lang.countries["TC"] = "Isole Turks e Caicos";
+GO.lang.countries["TV"] = "Tuvalu";
+GO.lang.countries["UG"] = "Uganda";
+GO.lang.countries["UA"] = "Ucraina";
+GO.lang.countries["GB"] = "Regno Unito";
+GO.lang.countries["US"] = "Stati Uniti";
+GO.lang.countries["UM"] = "Isole Minori degli Sati Uniti";
+GO.lang.countries["UY"] = "Uruguay";
+GO.lang.countries["UZ"] = "Uzbekistan";
+GO.lang.countries["VU"] = "Vanuatu";
+GO.lang.countries["VA"] = "Città del Vaticano";
+GO.lang.countries["VE"] = "Venezuela";
+GO.lang.countries["VG"] = "Isole Vergini (UK)";
+GO.lang.countries["VI"] = "Isole Vergini (USA)";
+GO.lang.countries["WF"] = "Isole Wallis e Futuna";
+GO.lang.countries["EH"] = "Sahara Occidentale";
+GO.lang.countries["YE"] = "Yemen";
+GO.lang.countries["YU"] = "Yugoslavia";
+GO.lang.countries["ZR"] = "Zaire";
+GO.lang.countries["ZM"] = "Zambia";
+GO.lang.countries["ZW"] = "Zimbabwe";
+GO.lang.countries["SA"] = "Arabia Saudita";
