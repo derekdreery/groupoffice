b7b986a5c8f49403984c0f36c1af3691d91082e3
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-07-13 06:42:37 +0000

    ExtJS 3 compatibility

--- trunk/www/modules/tasks/Portlet.js
+++ trunk/www/modules/tasks/Portlet.js
@@ -1,84 +1,85 @@
 GO.tasks.SimpleTasksPanel = function(config)
-{
-	if(!config)
 	{
-		config = {};
-	}
+		if(!config)
+		{
+			config = {};
+		}
 	
-	config.store = new GO.data.JsonStore({
-		url: GO.settings.modules.tasks.url+'json.php',
-		baseParams: {
-			'task': 'tasks',
-			'user_id' : GO.settings.user_id,
-			'active_only' : true
+		config.store = new GO.data.JsonStore({
+			url: GO.settings.modules.tasks.url+'json.php',
+			baseParams: {
+				'task': 'tasks',
+				'user_id' : GO.settings.user_id,
+				'active_only' : true
 			},
-		root: 'results',
-		totalProperty: 'total',
-		id: 'id',
-		fields:['id', 'name','completed','due_time','description']
-	});
+			root: 'results',
+			totalProperty: 'total',
+			id: 'id',
+			fields:['id', 'name','completed','due_time','description']
+		});
 	
 	
-	var checkColumn = new GO.grid.CheckColumn({
-     header: '',
-     dataIndex: 'completed',
-     width: 30,
-     header: '<div class="tasks-complete-icon"></div>'
-  });
+		var checkColumn = new GO.grid.CheckColumn({
+			header: '',
+			dataIndex: 'completed',
+			width: 30,
+			header: '<div class="tasks-complete-icon"></div>'
+		});
   
-  checkColumn.on('change', function(record, checked){
-  	this.store.baseParams['completed_task_id']=record.data.id;
-  	this.store.baseParams['checked']=checked;
+		checkColumn.on('change', function(record, checked){
+			this.store.baseParams['completed_task_id']=record.data.id;
+			this.store.baseParams['checked']=checked;
   	
-  	//dirty, but it works for updating all the grids
-  	this.store.reload({
-  		callback:function(){
-  			GO.tasks.taskDialog.fireEvent('save', GO.tasks.taskDialog, record.data.id);
-  		},
-  		scope:this
-  	});
+			//dirty, but it works for updating all the grids
+			this.store.reload({
+				callback:function(){
+					GO.tasks.taskDialog.fireEvent('save', GO.tasks.taskDialog, record.data.id);
+				},
+				scope:this
+			});
   	
-  	delete this.store.baseParams['completed_task_id'];
-  	delete this.store.baseParams['checked'];
-  }, this);
+			delete this.store.baseParams['completed_task_id'];
+			delete this.store.baseParams['checked'];
+		}, this);
 	
 	
 	
-	config.paging=false,			
-	config.plugins=checkColumn;
-	config.autoExpandColumn=1;
-	config.autoExpandMax=2500;
-	config.enableColumnHide=false;
-  config.enableColumnMove=false;
-	config.columns=[
+		config.paging=false,
+		config.plugins=checkColumn;
+		config.autoExpandColumn='task-portlet-name-col';
+		config.autoExpandMax=2500;
+		config.enableColumnHide=false;
+		config.enableColumnMove=false;
+		config.columns=[
 		checkColumn,
 		{
+			id:'task-portlet-name-col',
 			header:GO.lang['strName'],
 			dataIndex: 'name',
 			renderer:function(value, p, record){
 				if(!GO.util.empty(record.data.description))
 				{
-		     p.attr = 'ext:qtip="'+Ext.util.Format.htmlEncode(record.data.description)+'"';
+					p.attr = 'ext:qtip="'+Ext.util.Format.htmlEncode(record.data.description)+'"';
 				}
-		    return value;
+				return value;
 			}
 		},{
 			header:GO.tasks.lang.dueDate,
 			dataIndex: 'due_time',
 			width:100
 		}];
-	config.view=new Ext.grid.GridView({
+		config.view=new Ext.grid.GridView({
 		
-		emptyText: GO.tasks.lang.noTask
-	}),
-	config.sm=new Ext.grid.RowSelectionModel();
-	config.loadMask=true;
-	config.autoHeight=true;
+			emptyText: GO.tasks.lang.noTask
+		}),
+		config.sm=new Ext.grid.RowSelectionModel();
+		config.loadMask=true;
+		config.autoHeight=true;
 	
 	
-	GO.tasks.SimpleTasksPanel.superclass.constructor.call(this, config);
+		GO.tasks.SimpleTasksPanel.superclass.constructor.call(this, config);
 	
-};
+	};
 
 
 Ext.extend(GO.tasks.SimpleTasksPanel, GO.grid.GridPanel, {
@@ -90,17 +91,18 @@ Ext.extend(GO.tasks.SimpleTasksPanel, GO.grid.GridPanel, {
 		GO.tasks.SimpleTasksPanel.superclass.afterRender.call(this);
 		
 		GO.tasks.taskDialog.on('save', function(){
-					this.store.reload();
-				}, this);
+			this.store.reload();
+		}, this);
     
-    this.on("rowdblclick", function(grid, rowClicked, e){	    
-				GO.linkHandlers[12].call(this, grid.selModel.selections.keys[0]);
-			}, this);
+
+		this.on("rowdblclick", function(grid, rowClicked, e){
+			GO.linkHandlers[12].call(this, grid.selModel.selections.keys[0]);
+		}, this);
 			
 		Ext.TaskMgr.start({
-		    run: this.store.load,
-		    scope:this.store,
-		    interval:960000
+			run: this.store.load,
+			scope:this.store,
+			interval:960000
 		});
 	}
 	
@@ -117,14 +119,14 @@ GO.mainLayout.onReady(function(){
 		GO.summary.portlets['portlet-tasks']=new GO.summary.Portlet({
 			id: 'portlet-tasks',
 			//iconCls: 'go-module-icon-tasks',
-		 	title: GO.tasks.lang.tasks,
+			title: GO.tasks.lang.tasks,
 			layout:'fit',
 			tools: [{
-		        id:'close',
-		        handler: function(e, target, panel){
-		            panel.removePortlet();
-		        }
-		    }],
+				id:'close',
+				handler: function(e, target, panel){
+					panel.removePortlet();
+				}
+			}],
 			items: tasksGrid,
 			autoHeight:true
 		});
