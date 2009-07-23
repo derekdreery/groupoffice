88cea3d25a06c1e77ae63c0ba1146e5a217796ba
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-07-23 07:39:15 +0000

    use trunk

--- trunk/debian-groupoffice/builddeb.sh
+++ trunk/debian-groupoffice/builddeb.sh
@@ -26,9 +26,12 @@ svn export https://mschering@group-office.svn.sourceforge.net/svnroot/group-offi
 
 if [ "$1" == "real" ]; then
 	#svn export https://mschering@group-office.svn.sourceforge.net/svnroot/group-office/tags/groupoffice-com-$VERSION
+	#mv groupoffice-com-$VERSION debian-groupoffice/usr/share/groupoffice
+
 	svn export https://mschering@group-office.svn.sourceforge.net/svnroot/group-office/trunk/www
-	mv groupoffice-com-$VERSION debian-groupoffice/usr/share/groupoffice
-	rm debian-groupoffice/usr/share/groupoffice/LICENSE.TXT
+	mv www debian-groupoffice/usr/share/groupoffice
+
+rm debian-groupoffice/usr/share/groupoffice/LICENSE.TXT
 fi
 
 
