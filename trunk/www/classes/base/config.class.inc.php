4649214359d8507c771035885a810d407bbeb4b6
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-07-29 08:20:42 +0000

    Updated version number

--- trunk/www/classes/base/config.class.inc.php
+++ trunk/www/classes/base/config.class.inc.php
@@ -608,7 +608,7 @@ class GO_CONFIG
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.2.15';
+	var $version = '3.2.16';
 
 
 	/* The permissions mode to use when creating files
@@ -634,7 +634,7 @@ class GO_CONFIG
 	 * @access  public
 	 */
 
-	var $mtime = '20090728';
+	var $mtime = '20090729';
 
 	#group configuration
 	/**
