cf679ce699dd541cc65ac3eb942d0a01d3df2e3b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-07-31 13:30:35 +0000

    minified

--- trunk/www/classes/base/config.class.inc.php
+++ trunk/www/classes/base/config.class.inc.php
@@ -608,7 +608,7 @@ class GO_CONFIG
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.2.16';
+	var $version = '3.2.17';
 
 
 	/* The permissions mode to use when creating files
@@ -634,7 +634,7 @@ class GO_CONFIG
 	 * @access  public
 	 */
 
-	var $mtime = '20090729';
+	var $mtime = '20090730';
 
 	#group configuration
 	/**
