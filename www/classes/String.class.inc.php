dbe58626dac2bc295e737ee83410c6813473732c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-08-05 07:27:36 +0000

    Finally created a well working UTF-8 cleaning function!

--- www/classes/String.class.inc.php
+++ www/classes/String.class.inc.php
@@ -25,27 +25,170 @@
 class String {
 
 
+	function clean_utf8($str, $source_charset='UTF-8') {
 
-
-	function clean_utf8($s){
-		//Does not always work
-
-		$s = preg_replace('/\p{Cc}/u', '', $s);
-
-		if(!$s)
+		//Does not always work. We suppress the:
+		//Notice:  iconv() [function.iconv]: Detected an illegal character in input string in /var/www/community/trunk/www/classes/String.class.inc.php on line 31
+		
+		$c = @iconv($source_charset, 'UTF-8//IGNORE', $str);
+		if(!empty($c))
 		{
-			die('Clean failed');
+			$str=$c;
+		}
+		//Check if preg validates it as UTF8
+		if(preg_match('/^.{1}/us', $str)){
+			return $str;
 		}
 
-		return preg_replace( '/[\x00-\x08\x0B-\x0C\x0E-\x1F\x7F-\x9F]/', '', $s );
+
+		//Not valid still so we are going to validate each utf byte sequence with
+		//help from Henri Sivonen http://hsivonen.iki.fi/php-utf8/
+		
+
+		$mState = 0;     // cached expected number of octets after the current octet
+		// until the beginning of the next UTF8 character sequence
+		$mUcs4  = 0;     // cached Unicode character
+		$mBytes = 1;     // cached expected number of octets in the current sequence
+
+		$out = '';
+		$chr = '';
+
+		$len = strlen($str);
+
+		for($i = 0; $i < $len; $i++) {
+
+			$chr.=$str{$i};
+
+			$in = ord($str{$i});
+			if ( $mState == 0) {
 
 
-		$c = iconv('UTF-8', 'UNICODE//IGNORE', $s);
-		if(!empty($c))
-			return $c;
-		else
-			return $s;
+
+			// When mState is zero we expect either a US-ASCII character or a
+			// multi-octet sequence.
+				if (0 == (0x80 & ($in))) {
+				// US-ASCII, pass straight through.
+					$mBytes = 1;
+
+					$out .= $chr;
+
+					$chr='';
+
+				} elseif (0xC0 == (0xE0 & ($in))) {
+				// First octet of 2 octet sequence
+					$mUcs4 = ($in);
+					$mUcs4 = ($mUcs4 & 0x1F) << 6;
+					$mState = 1;
+					$mBytes = 2;
+
+				} elseif (0xE0 == (0xF0 & ($in))) {
+				// First octet of 3 octet sequence
+					$mUcs4 = ($in);
+					$mUcs4 = ($mUcs4 & 0x0F) << 12;
+					$mState = 2;
+					$mBytes = 3;
+
+				} elseif (0xF0 == (0xF8 & ($in))) {
+				// First octet of 4 octet sequence
+					$mUcs4 = ($in);
+					$mUcs4 = ($mUcs4 & 0x07) << 18;
+					$mState = 3;
+					$mBytes = 4;
+
+				} elseif (0xF8 == (0xFC & ($in))) {
+							 /* First octet of 5 octet sequence.
+							 *
+							 * This is illegal because the encoded codepoint must be either
+							 * (a) not the shortest form or
+							 * (b) outside the Unicode range of 0-0x10FFFF.
+							 * Rather than trying to resynchronize, we will carry on until the end
+							 * of the sequence and let the later error handling code catch it.
+							 */
+					$mUcs4 = ($in);
+					$mUcs4 = ($mUcs4 & 0x03) << 24;
+					$mState = 4;
+					$mBytes = 5;
+
+
+				} elseif (0xFC == (0xFE & ($in))) {
+				// First octet of 6 octet sequence, see comments for 5 octet sequence.
+					$mUcs4 = ($in);
+					$mUcs4 = ($mUcs4 & 1) << 30;
+					$mState = 5;
+					$mBytes = 6;
+
+				} else {
+							 /* Current octet is neither in the US-ASCII range nor a legal first
+								* octet of a multi-octet sequence.
+								*/
+					//return FALSE;
+					$out .= '?';
+
+				}
+
+			} else {
+
+			// When mState is non-zero, we expect a continuation of the multi-octet
+			// sequence
+				if (0x80 == (0xC0 & ($in))) {
+
+				// Legal continuation.
+					$shift = ($mState - 1) * 6;
+					$tmp = $in;
+					$tmp = ($tmp & 0x0000003F) << $shift;
+					$mUcs4 |= $tmp;
+
+					/**
+					 * End of the multi-octet sequence. mUcs4 now contains the final
+					 * Unicode codepoint to be output
+					 */
+					if (0 == --$mState) {
+
+									 /*
+									 * Check for illegal sequences and codepoints.
+									 */
+					// From Unicode 3.1, non-shortest form is illegal
+						if (((2 == $mBytes) && ($mUcs4 < 0x0080)) ||
+								((3 == $mBytes) && ($mUcs4 < 0x0800)) ||
+								((4 == $mBytes) && ($mUcs4 < 0x10000)) ||
+								(4 < $mBytes) ||
+								// From Unicode 3.2, surrogate characters are illegal
+								(($mUcs4 & 0xFFFFF800) == 0xD800) ||
+								// Codepoints outside the Unicode range are illegal
+								($mUcs4 > 0x10FFFF)) {
+
+							//return FALSE;
+							$out .= '?';
+
+						}else
+						{
+							//echo $chr."\n";
+							$out .= $chr;
+						}
+
+						//initialize UTF8 cache
+						$mState = 0;
+						$mUcs4  = 0;
+						$mBytes = 1;
+						$chr='';
+					}
+
+				} else {
+				/**
+				 *((0xC0 & (*in) != 0x80) && (mState != 0))
+				 * Incomplete multi-octet sequence.
+				 */
+
+					//return FALSE;
+					$out .= '?';
+				}
+			}
+		}
+		
+		return $out;
 	}
+
+
 
 	function replace_once($search, $replace, $subject) {
 		$firstChar = strpos($subject, $search);
