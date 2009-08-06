99eca7d6c1d8f3ba5d2d6eea3f11f198f0837432
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-08-06 14:20:31 +0000

    Created tag 3.2.20

---
+++ tags/groupoffice-com-3.2.20/language/languages.inc.php
@@ -0,0 +1,22 @@
+<?php
+$languages['cn']='Chinese Simplified';
+$languages['zh-tw']='Chinese Traditional';
+$languages['cs']='Čeština';
+$languages['da']='Dansk';
+$languages['de']='Deutsch';
+$languages['en']='English';
+$languages['en_UK']='English/British';
+$languages['es']='Español';
+$languages['el']='Ελληνικά';
+$languages['fr']='Francais';
+$languages['it']='Italiano';
+$languages['nl']='Nederlands';
+$languages['nb']='Norsk bokmål';
+$languages['pl']='Polski';
+$languages['pt_BR']='Português - Brazil';
+$languages['ru']='Русский';
+$languages['ro']='Romanian';
+$languages['fi']='Suomi - Finland';
+$languages['sv']='Svenska';
+$languages['tr']='Türkçe';
+?>
