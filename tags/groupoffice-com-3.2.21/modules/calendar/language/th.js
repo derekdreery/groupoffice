e3789268830e59524e74b7de552f848053cc0eb6
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-08-13 10:29:45 +0000

    Created tag 3.2.21

---
+++ tags/groupoffice-com-3.2.21/modules/calendar/language/th.js
@@ -0,0 +1,74 @@
+/**
+ * Don't copy the next line into a translation
+ */
+
+/**
+ * Copy everything below for translations
+ */
+
+
+GO.calendar.lang.addressbook='สมุดที่อยู่';
+
+GO.calendar.lang.appointment = 'ตารางการนัดหมาย';
+GO.calendar.lang.appointments = 'ตารางการนัดหมาย';
+GO.calendar.lang.recurrence = 'การทำรายการ';
+GO.calendar.lang.options = 'ตัวเลือก';
+GO.calendar.lang.repeatForever = 'แสดงตลอดไป';//repeat forever
+GO.calendar.lang.repeatEvery = 'แสดงซ้ำ';//Repeat every
+GO.calendar.lang.repeatUntil = 'แสดงจนถึง';//Repeat until
+GO.calendar.lang.busy = 'ไม่ว่าง';
+GO.calendar.lang.allDay = 'เลือกทั้งวัน';//Time is not applicable
+GO.calendar.lang.navigation = 'เมนู';
+GO.calendar.lang.oneDay = '1 วัน';
+GO.calendar.lang.fiveDays = '5 วัน';
+GO.calendar.lang.sevenDays = '7 วัน';
+GO.calendar.lang.month = 'เดือน';
+GO.calendar.lang.recurringEvent = 'เหตุการณ์ที่มีการกระทำเป็นประจำ';
+GO.calendar.lang.deleteRecurringEvent = 'ต้องการลบรายการนี้รายการเดียว หรือ รายการการนัดหมายที่เกี่ยวข้องนี้ทั้งหมด?';//Do you want to delete a single instance or all instances of this recurring event
+GO.calendar.lang.singleOccurence = 'รายการเดียว';
+GO.calendar.lang.entireSeries = 'ทั้งหมด';
+GO.calendar.lang.calendar = 'ปฏิทิน';
+GO.calendar.lang.calendars = 'ปฏิทน';
+GO.calendar.lang.views = 'การเรียกดู';
+GO.calendar.lang.administration = 'การจัดการ';
+GO.calendar.lang.needsAction = 'เร่งดำเนินการ';
+GO.calendar.lang.accepted = 'ตอบรับ';
+GO.calendar.lang.declined = 'ไม่ตอบรับ';
+GO.calendar.lang.tentative = 'ยืนยังอีกครั้ง';
+GO.calendar.lang.delegated = 'มอบหมาย';
+GO.calendar.lang.noRecurrence = 'ไม่ได้เลือกรายการ';
+GO.calendar.lang.notRespondedYet = 'ยังไม่ตอบกลับ';
+GO.calendar.lang.days = 'วัน';
+GO.calendar.lang.weeks = 'สัปดาห์';
+GO.calendar.lang.monthsByDate = 'วันที:เดือน';
+GO.calendar.lang.monthsByDay = 'วัน:เดือน';
+GO.calendar.lang.years = 'ปี';
+GO.calendar.lang.atDays = 'วันที่';
+GO.calendar.lang.noReminder = 'ไม่ได้เลือกรายการเตือน';
+GO.calendar.lang.reminder='การเตือน';
+GO.calendar.lang.participants = 'ผู้เข้าร่วม';
+GO.calendar.lang.checkAvailability = 'ตรวจสอบข้อมูล';
+GO.calendar.lang.sendInvitation = 'ส่งการเชิญ';
+GO.calendar.lang.emailSendingNotConfigured = 'เกิดข้อผิดพลาดในการส่งอีเมล กรุณาตรวจสอบความถูกต้อง.';
+GO.calendar.lang.privateEvent = 'ส่วนตัว';
+GO.calendar.lang.noInformationAvailable = 'ไม่มีข้อมูล';
+GO.calendar.lang.noParticipantsToDisplay = 'ไม่มีผู้เข้าร่วม';
+GO.calendar.lang.previousDay = 'ย้อนกลับ วัน';
+GO.calendar.lang.nextDay = 'ถัดไป วัน';
+GO.calendar.lang.noAppointmentsToDisplay = 'ไม่มีตารางการนัดหมาย';
+GO.calendar.lang.selectCalendar = 'เลือกปฏิทิน';
+GO.calendar.lang.selectCalendarForAppointment = 'เพิ่มการนัดหมายลงในปฏิทิน:';
+GO.calendar.lang.closeWindow = 'การเพิ่มการนัดหมายเสร็จสิ้น.';
+GO.calendar.lang.list='รายการ';
+GO.calendar.lang.editRecurringEvent='ต้องการแก้ไขเพียงรายการนี้รายการเดียว หรือ ทุกรายการที่เกี่ยวข้อง?';//Do you want to edit this occurence or the entire series
+GO.calendar.lang.selectIcalendarFile='เลือกปฏิทิน (*.ics) ไฟล์';
+
+GO.calendar.lang.location='สถานที่';
+GO.calendar.lang.startsAt='วันเริ่มต้น';
+GO.calendar.lang.endsAt='วันสิ้นสุด';
+
+GO.calendar.lang.eventDefaults='กำหนดค่าเริ่มต้นสำหรับปฏิทิน';
+
+GO.calendar.lang.importToCalendar='เพิ่มเหตุการณ์ลงไปยังปฏิทินโดยตรงอัตโนมัติ';
+GO.calendar.lang.default_calendar='ปฏิทินเริ่มต้นที่ใช้';
+
