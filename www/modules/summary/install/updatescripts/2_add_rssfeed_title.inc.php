00e1c33b08279d0ed101abac7308bdca7e1cac7f
MysticD <MysticD@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-08-17 10:58:20 +0000

    Added possibility to add multiple RSS feeds and added missing countries

--- www/modules/summary/install/updatescripts/2_add_rssfeed_title.inc.php
+++ www/modules/summary/install/updatescripts/2_add_rssfeed_title.inc.php
@@ -1,6 +1,5 @@
 <?php
-$GO_LANGUAGE->get_language_file('summary');
+require($GO_LANGUAGE->get_language_file('summary'));
 
-$db = new db();
 $sql = "UPDATE su_rss_feeds SET title = \"".$lang['summary']['default_rss_title']."\"";
 $db->query($sql);
