858891586ed74b7f902f8ac0ff5cd382802ce6a4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-08-17 14:51:06 +0000

    Created tag 3.2.23

---
+++ tags/groupoffice-com-3.2.23/modules/tasks/language/fr.js
@@ -0,0 +1,57 @@
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id$
+ * @author Merijn Schering <mschering@intermesh.nl>
+ *
+ * French Translation v1.0
+ * Author : Lionel JULLIEN
+ * Date : September, 05 2008
+ *
+ * Update for 3.02-stable-10
+ * Author : Cyril DUCHENOY
+ * Date : July, 21 200
+ */
+
+
+
+
+
+GO.tasks.lang.task = 'Tâche';
+GO.tasks.lang.noTask = 'Aucune tâche à afficher';
+GO.tasks.lang.tasks = 'Tâches';
+GO.tasks.lang.addTask = 'Ajouter une tâche...';
+GO.tasks.lang.tasklist = 'Liste de tâches'
+GO.tasks.lang.tasklists = 'Listes de tâches';
+GO.tasks.lang.showCompletedTasks = 'Afficher les tâches terminées';
+GO.tasks.lang.filter = 'Filtrer';
+GO.tasks.lang.dueDate = 'Date de fin';
+GO.tasks.lang.dueAt = 'Termine le';
+GO.tasks.lang.needsAction = 'Action nécessaire';
+GO.tasks.lang.accepted = 'Accepté';
+GO.tasks.lang.declined = 'Décliné';
+GO.tasks.lang.tentative = 'Tentative';
+GO.tasks.lang.delegated = 'Délégué';
+GO.tasks.lang.completed = 'Terminé';
+GO.tasks.lang.inProcess = 'En cours';
+GO.tasks.lang.repeatEvery = 'Répéter chaque';
+GO.tasks.lang.atDays = 'Aux jours';
+GO.tasks.lang.repeatUntil = 'Répéter jusqu\'à';
+GO.tasks.lang.repeatForever = 'Répéter pour toujours';
+GO.tasks.lang.recurrence = 'Récurrence';
+GO.tasks.lang.options = 'Options';
+GO.tasks.lang.createLink = 'Créer un lien';
+GO.tasks.lang.startsAt='Débute le';
+GO.tasks.lang.showInactiveTasks='Afficher les tâches inactives';
+GO.tasks.lang.remindMe= 'Ajouter un rappel';
+GO.tasks.lang.scheduleCall='Planifier un appel téléphonique';
+GO.tasks.lang.call='Appel';
+GO.tasks.lang.taskDefaults='Paramètres par défaut pour les tâches';
+GO.tasks.lang.daysBeforeStart='Rappel en jours avant l\'échéance';
+GO.tasks.lang.defaultTasklist='Afficher par défaut les tâches de';
