4ec47a3bc05b2363b5ce7f9d409883a5c0f8464d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-08-24 07:35:40 +0000

    Created tag 3.2.27

---
+++ tags/groupoffice-com-3.2.27/language/common/th.js
@@ -0,0 +1,308 @@
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id: en.js 1827 2009-02-05 14:30:48Z mschering $
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
+/**
+ * Don't copy the next line into a translation
+ */
+//GO.lang = {};
+/**
+ * Copy everything below for translations
+ */
+
+GO.lang.serverError='เกิดข้อผิดพลาด กรุณาติดต่อผู้ดูแลระบบและแจ้งรายละเอียดข้อผิดพลาด';//An error occurred on the webserver. Contact your system administrator and supply the detailed error
+GO.lang.errorDetails='รายละเอียดข้อผิดพลาด';
+GO.lang.contactSupportDesk='ติดต่อฝ่ายเทคนิค';//Contact support desk
+
+GO.lang.disabled='ปิดการใช้งาน';
+GO.lang.changesWillBeLost='ต้องการเปลียนแปลงรายการ หรือเลือกยกเลิก และบันทึกการเปลียนแปลง หรือตกลงเพื่อทำรายการต่อและลบรายการที่ทำการเปลียนแปลง';//You made changes in the current screen. Press cancel and save changes or Ok to continue and delete your changes
+GO.lang.strCommunityForum='เว็บบอร์ด';//Community forum
+GO.lang.moduleRequired='คุณสมบัติเมนู \'{module}\' ที่ต้องการ .ไม่สามารถใช้งานได้.';
+GO.lang.completeProfile = 'โปรดกรอกข้อมูลของคุณ';
+GO.lang.fullscreen='โหมด Fullscreen';
+GO.lang.popupBlocker = 'เว็บเบราเซอร์มีการตั้งค่าป้องกันป๊อบอัป Group-Office. กรุณาเปลี่ยนแปลงการตั้งค่าป้องกันป๊อบอัป';//Your browser is blocking a popup from Group-Office. Please disable the popup blocker for this site
+
+GO.lang.color='สี';
+GO.lang.browse='เลือก...';
+GO.lang.largeUpload='ไฟล์ขนาดใหญ่/โฟลเดอร์ (เครื่องคอมพิวเตอร์ต้องรองรับการทำงานจาวา)';
+GO.lang.smallUpload='ไฟล์ขนาดเล็ก';
+
+
+GO.lang.lostPassword='ลืมรหัสผ่าน?';
+GO.lang.lostPasswordText='เพื่อการเข้าใช้งาน Group-Office หากท่านลืมรหัสผ่านและต้องการรหัสผ่านใหม่ โดยรหัสผ่านใหม่จะถูกส่งไปยังอีเมลของท่าน<br /><br />กรุณากรอกอีเมล:';//To recover your password you must be able to read your e-mail without access to Group-Office.If you enter your e-mail address a new password will be sent to your e-mail address.Enter your e-mail address
+
+GO.lang.noJava='เครื่องคอมพิวเตอร์ไม่รองรับการทำงานจาวา เพื่อความสะดวกในการแก้ไขและการอัพโหลดไฟล์ กรุณาเข้าเว็บ <a class="normal-link" href="http://www.java.com/download" target="_blank">http://www.java.com/download</a>เพื่อทำการดาวน์โหลดและติดตั้ง';
+
+GO.lang.strHelpContents='ช่วยเหลือ';
+GO.lang.strReportBug='รายงานปัญหา';
+GO.lang.strAbout='เกี่ยวกับ Group-Office';
+GO.lang.strUsage='สถิติการใช้งาน';
+
+GO.lang.strName = 'ชื่อ';
+GO.lang.strDescription = 'คำอธิบาย';
+GO.lang.strEmail = 'อีเมล';
+GO.lang.strUser = 'ผู้ใช้งาน';
+GO.lang.users = 'ผู้ใช้งาน';
+GO.lang.strOwner = 'เจ้าของ';
+GO.lang.strContacts = 'รายชื่อ';
+
+GO.lang.strLanguage='ภาษา';
+
+GO.lang.strOpenOffice = 'เอกสาร OpenOffice.org';
+
+GO.lang.strDefault = 'ค่าเริ่มต้น';
+GO.lang.strUsername = 'ผู้ใช้งาน ';
+GO.lang.strPassword = 'รหัสผ่าน';
+
+GO.lang.strFirstName = 'ชื่อ';
+GO.lang.strMiddleName = 'ชื่อเล่น';
+GO.lang.strNo = 'เลขที่';
+GO.lang.strLastName = 'นามสกุล';
+GO.lang.strTitle = 'คำนำหน้าชื่อ';
+GO.lang.strInitials = 'ชื่อย่อ';
+GO.lang.strSex = 'เพศ';
+GO.lang.strBirthday = 'วันเดือนปีเกิด';
+GO.lang.strEmail = 'อีเมล';
+
+GO.lang.strPhone = 'หมายเลขโทรศัพท์';
+GO.lang.strFax = 'หมายเลขแฟ็กซ์';
+GO.lang.strCellular = 'โทรศัพท์มือถือ';
+GO.lang.strAddress = 'ที่อยู่';
+GO.lang.strAddressNo = 'เลขที่';
+GO.lang.strZip = 'รหัสไปรษณีย์';
+GO.lang.strCity = 'อำเภอ/เขต';
+GO.lang.strState = 'จังหวัด';
+GO.lang.strCountry = 'ประเทศ';
+GO.lang.strHomepage = 'โฮมเพจ';
+GO.lang.strCompany = 'บริษัท';
+GO.lang.strDepartment = 'แผนก';
+GO.lang.strFunction = 'ตำแหน่ง';
+GO.lang.strSalutation = 'คำขึ้นต้น';
+GO.lang.strComment = 'ข้อความ';
+GO.lang.strWorkAddress = 'ที่อยู่';//(ที่ทำงาน)
+GO.lang.strWorkAddressNo = 'เลขที่';//(ที่ทำงาน
+GO.lang.strWorkZip = 'รหัสไปรษณีย์ ';//(ที่ทำงาน)
+GO.lang.strWorkCity = 'อำเภอ/เขต ';//(ที่ทำงาน)
+GO.lang.strWorkState = 'จังหวัด';// (ที่ทำงาน)
+GO.lang.strWorkCountry = 'ประเทศ';
+GO.lang.strWorkPhone = 'หมายเลขโทรศัพท์';// (ที่ทำงาน)
+GO.lang.strWorkFax = 'หมายเลขแฟ็กซ์';// (ที่ทำงาน)
+GO.lang.strPostAddress = 'ที่อยู่';// (ที่ทำงาน)
+GO.lang.strPostAddressNo = 'เลขที่';// (ที่ทำงาน)
+GO.lang.strPostCity = 'อำเภอ/เขต';// (ที่ทำงาน)
+GO.lang.strPostState = 'จังหวัด';// (ที่ทำงาน)
+GO.lang.strPostCountry = 'ประเทศ';// (ที่ทำงาน)
+GO.lang.strPostZip = 'รหัสไปรษณีย์';// (ที่ทำงาน)
+
+
+GO.lang.locations = 'สถานที่';
+GO.lang.folder = 'โฟลเดอร์';
+GO.lang.folders = 'โฟลเดอร์';
+
+GO.lang.up = 'อัพ';
+GO.lang.fileExists = 'ที่อยู่ไฟล์';
+GO.lang.overwriteFile = 'คุณต้องการบันทึก \'{file}\'?';
+
+GO.lang.url = 'URL';
+
+GO.lang.strMale = 'ชาย';
+GO.lang.strFemale = 'หญิง';
+
+GO.lang.strMtime = 'แก้ไขล่าสุด';
+GO.lang.strCtime = 'สร้าง';
+GO.lang.Atime = 'เข้าถึงล่าสุด';
+GO.lang.strText = 'ข้อความ';
+GO.lang.texts = 'ข้อความ';
+GO.lang.strDate = 'วัน';
+GO.lang.strUser = 'ชื่อผู้ใช้';
+
+GO.lang.strSize = 'ขนาด';
+GO.lang.strType = 'ประเภท';
+GO.lang.strNA = 'N/A';
+
+GO.lang.cmdDelete = 'ลบ';
+GO.lang.cmdAdd = 'เพิ่ม';
+GO.lang.cmdAddPlusNew = 'บันทึกเป็น';
+GO.lang.cmdSavePlusNew='บันทึกเป็น';
+GO.lang.cmdSave = 'บันทึก';
+GO.lang.cmdEdit = 'แก้ไข';
+GO.lang.cmdLink = 'เชื่อมโยง';
+GO.lang.cmdUnlink = 'ยกเลิกเชื่อมโยง';
+GO.lang.cmdClose = 'ปิด';
+GO.lang.cmdSettings = 'การตั่งค่า';
+GO.lang.cmdReset = 'ล้าง';
+GO.lang.cmdExport='ส่งออก';
+GO.lang.cmdPrint='พิมพ์';
+GO.lang.cmdImport='นำเข้า';
+
+GO.lang.cmdActions = 'ผลการดำเนินการ';
+
+GO.lang.cmdNext='ต่อไป';
+GO.lang.cmdPrevious='ย้อนกลับ';
+
+GO.lang.cmdNew = 'เพิ่มไปยัง';
+GO.lang.cmdOk = 'ตกลง';
+GO.lang.cmdCancel = 'ยกเลิก';
+
+GO.lang.cmdApply = 'ปรับปรุง';
+GO.lang.cmdRefresh = 'รีเฟรช';
+
+GO.lang.cmdYes = 'ตกลง';
+GO.lang.cmdYesToAll = 'ตกลง ทั้งหมด';
+GO.lang.cmdNo = 'ยกเลิก';
+GO.lang.cmdNoToAll = 'ยกเลิก ทั้งหมด';
+
+GO.lang.strLinks = 'เชื่อมโยง';
+GO.lang.author = 'อื่นๆ';
+
+GO.lang.paste = 'วาง';
+GO.lang.cut = 'ตัด';
+GO.lang.copy = 'คัดลอก';
+GO.lang.compress = 'บีบอัดไฟล์';
+GO.lang.decompress = 'ขยายไฟล์บีบอัด';
+GO.lang.upload = 'อัพโหลด';
+GO.lang.download = 'ดาวน์โหลด';
+GO.lang.uploadFiles = 'อัพโหลดไฟล์';
+
+GO.lang.displayingItems = 'รายการ {0} - {1} จาก {2}';
+GO.lang.strNoItems = 'ไม่มีรายการในวันนี้';
+
+GO.lang.strLinkItems = 'ค้นหารายการเชื่อมโยง';
+GO.lang.strSearchResults= 'ค้นหาจากข้อความ';
+GO.lang.strSearch= 'ค้นหา';
+
+GO.lang.strPleaseSelect = 'กรุณา เลือก...';
+GO.lang.strProperties = 'คุณสมบัติ';
+
+GO.lang.strConfirm= 'ยืนยัน';
+GO.lang.strSuccess= 'เสร็จสิ้น';
+GO.lang.strError= 'เกิดข้อผิดพลาด';
+GO.lang.waitMsgSave= 'กำลังบันทึก...';
+GO.lang.waitMsgLoad= 'กำลังโหลด...';
+GO.lang.waitMsgUpload= 'กำลังอัพโหลด...';
+GO.lang.strRequestError= 'ไม่สามารถดำเนินการได้. สถานนะตอบกลับของเซิร์ฟเวอร์  {0}';//Could not send request. Server returned status 
+
+GO.lang.strSelected = 'สิ่งที่เลือก';
+GO.lang.noItemSelected= 'ไม่ได้ทำการเลือกรายการ.';
+GO.lang.strDeleteSelectedItems= 'ต้องการลบจำนวน {count}รายการ?';
+GO.lang.strDeleteSelectedItem= 'ต้องการลบรายการที่เลือก?';
+
+GO.lang.strNoWritePermissions = 'ไม่ได้รับสิทธิ์ให้แก้ไขรายการนี้.';
+
+GO.lang.strUnauthorized= 'ไม่ได้ีรับสิทธิ์ในการเข้าถึง';
+GO.lang.strUnauthorizedText= 'ไม่ได้รับสิทธิ์ให้ดำเนินการในรายการนี้';
+GO.lang.strReadPermissions = 'การเพิ่มสิทธิ์ในการอ่าน';
+GO.lang.strWritePermissions = 'การเพิ่มสิทธิ์ในการแก้ไข';
+GO.lang.strPermissions = 'การเพิ่มสิทธิ์';
+GO.lang.strAccessDenied = 'การเข้าถึงถูกปฏิเสธ';
+GO.lang.strAuthorizedGroups = 'กลุ่มที่ได้รับสิทธิ์';
+GO.lang.strAuthorizedUsers = 'ผู้ที่ได้รับสิทธ์ิ';
+
+GO.lang.strSelectGroups = 'เลือกกลุ่มผู้ใช้งาน';
+GO.lang.strSelectUsers = 'เลือกผู้ใช้งานรายบุคคล';
+
+GO.lang.strLogin= 'เข้าสู่ระบบ';
+
+GO.lang.strLoggedInAs= 'เข้าสู่ระบบ';//Logged in as
+
+GO.lang.strErrorsInForm= 'เกิดข้อผิดพลาด.กรุณากรอกรายละเอียดสำคัญให้ครบทุกรายการ';//You have errors in your form. The invalid fields are marked
+GO.lang.strMinute="นาที";
+GO.lang.strMinutes="นาที";
+GO.lang.strHour="ชั่วโมง";
+GO.lang.strHours="ชั่วโมง";
+GO.lang.strDay="วัน";
+GO.lang.strDays="วัน";
+GO.lang.strWeek="สัปดาห์";
+GO.lang.strWeeks="สัปดาห์";
+GO.lang.strMonth="เดือน";
+GO.lang.strMonths="เดือน";
+GO.lang.strYear="ปี";
+GO.lang.strYears="ปี";
+GO.lang.strTime='เวลา';
+
+GO.lang.shortDays = ['จ','อ','พ','พฤ','ศ','ส','อา'];
+GO.lang.noRecurrence = 'ไม่ได้เลือกรายการ';
+GO.lang.monthsByDate = 'วันที่ : เดือน';
+GO.lang.monthsByDay = 'วัน :เดือน';
+
+GO.lang.strSelectFiles='เลือกไฟล์';
+GO.lang.strFiles='ไฟล์';
+GO.lang.strSubject='หัวข้อ';
+GO.lang.strLocation='ที่จัดเก็บ';
+GO.lang.strStart='เริ่มต้น';
+GO.lang.strEnd='สิ้นสุด';
+
+GO.lang.strFirst='ระดับ1';
+GO.lang.strSecond='ระดับ2';
+GO.lang.strThird='ระดับ3';
+GO.lang.strFourth='ระดับ4';
+
+GO.lang.strStatus='สถานะ';
+GO.lang.strAvailable='เวลาว่าง';//Available
+GO.lang.strAvailability='ช่วงเวลาที่ว่าง';//Availability
+GO.lang.strView='มุมมอง';
+
+GO.lang.field = 'ฟิล์ด';
+
+GO.lang.customer = 'ลูกค้า';
+
+GO.lang.strNoCountrySelected='ไม่ได้เลือกประเทศ';
+GO.lang.menu = 'เมนู';
+GO.lang.root = 'Root';
+GO.lang.left = 'ซ้าย';
+GO.lang.right = 'ขวา';
+
+GO.lang.noFilesModule = 'ฟังก์ชั่นไฟล์จะถูกเรียกใช้ในฟังก์ชั่นนี้';//The files module is required for this function
+
+
+GO.lang.remindPassword='จดจำค่าล็อกอินปัจจุบันจนกระทั้งมีการกดปุ่มล็อกเอาท์';// Remember my login on this computer until I press logout
+
+GO.lang.reminders = 'ตั้งค่าการเตือน';
+GO.lang.dismiss='ยกเลิก';
+GO.lang.snooze='เลื่อนออกไป';//Snooze
+
+GO.lang.strMax = 'ค่าสูงสุด';
+
+GO.lang.settings='การตั้งค่า';
+
+GO.lang.latestLinks='เชื่อมโยงล่าสุด'
+GO.lang.cmdBrowseLinks = 'เปิดหน้าต่างเชื่อมโยง';
+
+GO.lang.today='วันนี้';
+
+GO.lang.displayingItemsShort='รวม: {2}';
+
+GO.lang.image='รูปภาพ';
+GO.lang.insertImage='เพิ่มรูปภาพ';
+
+GO.lang.item='รายการ';
+GO.lang.items='รายการ';
+
+//* Top เพิ่ม 22-07-2009
+GO.lang.exportHiddenColumns='ส่งออกคอลั่มที่ซ้อนอยู่ด้วย';
+GO.lang.searchQuery='ค้นหา';
+GO.lang.pleaseApplyFirst='กรุณาบันทึกก่อนคลิ๊ก \'Apply\'';
+GO.lang.passwordMatchError='รหัสผ่านไม่สัมพันธ์กัน';
+GO.lang.newFolder='สร้างโฟลเดอร์ใหม่';
+GO.lang.cmdSaveAs= 'บันทึกเป็น';
+GO.lang.value= 'ค่า';
+GO.lang.selectAll='เลือกทั้งหมด';
+GO.lang.more='เพิ่มเติม';
+GO.lang.countries['IR']= 'Iran';
+GO.lang.countries['IQ']= 'Iraq';
+GO.lang.countries["MY"]= "Malaysia";
+GO.lang.countries["CD"]= "Congo";
+GO.lang.countries["RS"]= "Serbia";
+GO.lang.countries["AE"]= "United Arab Emirates";
+GO.lang.on='เปิด';
+GO.lang.off='ปิด';
+GO.lang.keyword='คีร์เวิร์ด';
