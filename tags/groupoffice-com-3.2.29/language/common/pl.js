7f7009e3450c1f9e63d16d776ed8b3180bc86c7b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-08-26 13:42:46 +0000

    Created tag 3.2.29

---
+++ tags/groupoffice-com-3.2.29/language/common/pl.js
@@ -0,0 +1,277 @@
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id: en.js 1591 2008-12-15 09:34:29Z mschering $
+ * @author Merijn Schering <mschering@intermesh.nl>
+ *
+ * Polish Translation v1.0
+ * Author : Robert GOLIAT info@robertgoliat.com  info@it-administrator.org
+ * Date : January, 20 2009
+*/
+
+GO.lang.more='Więcej';
+GO.lang.strCommunityForum='Forum';
+GO.lang.moduleRequired='Ta funkcja wymaga modułu \'{module}\'. Moduł nie jest dostępny.';
+
+GO.lang.completeProfile = 'Proszę wprowadzić dane do swojego profilu';
+GO.lang.fullscreen='Tryb pełnoekranowy';
+GO.lang.popupBlocker = 'Twója przeglądarka blokuje popup z Group-Office. Proszę wyłączyć blokadę wyskakujących okienek (popup) dla tej strony';
+
+GO.lang.color='Kolor';
+GO.lang.browse='Wybierz ...';
+GO.lang.largeUpload='Duże pliki/foldery (wymagana Java)';
+GO.lang.smallUpload='Małe pliki';
+
+
+GO.lang.lostPassword='Zapomiane hasło?';
+GO.lang.lostPasswordText='To recover your password you must be able to read your e-mail without access to Group-Office. If you enter your e-mail address a new password will be sent to your e-mail address.<br /><br />Enter your e-mail address:';
+
+GO.lang.noJava='Java Webstart is not installed. Java enables easier editing of files and easier file uploading. Please visit <a class="normal-link" href="http://www.java.com/download" target="_blank">http://www.java.com/download</a> to install it.';
+
+GO.lang.strHelpContents='Zasięgnij pomocy';
+GO.lang.strReportBug='Zgłoś błąd';
+GO.lang.strAbout='Informacje o Group-Office';
+GO.lang.strUsage='Statystyki użytkowania';
+
+GO.lang.strName = 'Nazwa';
+GO.lang.strDescription = 'Opis';
+GO.lang.strEmail = 'E-mail';
+GO.lang.strUser = 'Użytkownik';
+GO.lang.users = 'Użytkownicy';
+GO.lang.strOwner = 'Właściciel';
+GO.lang.strContacts = 'Kontakty';
+
+GO.lang.strLanguage='Język';
+
+GO.lang.strOpenOffice = 'Dokument OpenOffice.org';
+
+GO.lang.strDefault = 'Domyślnie';
+GO.lang.strUsername = 'Użytkownik';
+GO.lang.strPassword = 'Hasło';
+
+GO.lang.strFirstName = 'Imię';
+GO.lang.strMiddleName = 'Drugie imię';
+GO.lang.strNo = 'Nr.';
+GO.lang.strLastName = 'Nazwisko';
+GO.lang.strTitle = 'Tytuł';
+GO.lang.strInitials = 'Inicjały';
+GO.lang.strSex = 'Płeć';
+GO.lang.strBirthday = 'Data urodzenia';
+GO.lang.strEmail = 'E-mail';
+
+GO.lang.strPhone = 'Telefon';
+GO.lang.strFax = 'Fax';
+GO.lang.strCellular = 'Telefon komórkowy';
+GO.lang.strAddress = 'Adres';
+GO.lang.strAddressNo = 'Nr domu';
+GO.lang.strZip = 'Kod pocztowy';
+GO.lang.strCity = 'Miasto';
+GO.lang.strState = 'Województwo';
+GO.lang.strCountry = 'Kraj';
+GO.lang.strHomepage = 'Strona domowa';
+GO.lang.strCompany = 'Firma';
+GO.lang.strDepartment = 'Dział';
+GO.lang.strFunction = 'Funkcja';
+GO.lang.strSalutation = 'Powitanie';
+GO.lang.strComment = 'Uwagi';
+GO.lang.strWorkAddress = 'Adres (praca)';
+GO.lang.strWorkAddressNo = 'Nr domu (praca)';
+GO.lang.strWorkZip = 'Kod pocztowy (praca)';
+GO.lang.strWorkCity = 'Miasto (praca)';
+GO.lang.strWorkState = 'Województwo (praca)';
+GO.lang.strWorkCountry = 'Kraj (praca)',	
+GO.lang.strWorkPhone = 'Telefon (praca)';
+GO.lang.strWorkFax = 'Fax (praca)';
+GO.lang.strPostAddress = 'Adres (poczta)';
+GO.lang.strPostAddressNo = 'Nr domu (poczta)';
+GO.lang.strPostCity = 'Miasto (poczta)';
+GO.lang.strPostState = 'Województwo (poczta)';
+GO.lang.strPostCountry = 'Kraj (poczta)';
+GO.lang.strPostZip = 'Kod pocztowy (poczta)';
+
+GO.lang.locations = 'Lokalizacje';
+GO.lang.folder = 'Folder';
+GO.lang.folders = 'Foldery';
+
+GO.lang.up = 'Wyżej';
+GO.lang.fileExists = 'Plik istnieje';
+GO.lang.overwriteFile = 'Czy chcesz nadpisać plik \'{file}\'?';
+
+GO.lang.url = 'URL';
+
+GO.lang.strMale = 'Mężczyzna';
+GO.lang.strFemale = 'Kobieta';
+
+GO.lang.strMtime = 'Zmodyfikowano';
+GO.lang.strCtime = 'Utworzono';
+GO.lang.Atime = 'Dostęp';
+GO.lang.strText = 'Tekst';
+GO.lang.texts = 'Teksty';
+GO.lang.strDate = 'Data';
+GO.lang.strUser = 'Użytkownik';
+
+GO.lang.strSize = 'Rozmiar';
+GO.lang.strType = 'Typ';
+GO.lang.strNA = 'N/A';
+
+GO.lang.cmdDelete = 'Usuń';
+GO.lang.cmdAdd = 'Dodaj';
+GO.lang.cmdAddPlusNew = 'Dodaj + nowy';
+GO.lang.cmdSavePlusNew='Zapisz + nowy';
+GO.lang.cmdSave = 'Zapisz';
+GO.lang.cmdEdit = 'Popraw';
+GO.lang.cmdLink = 'Połacz';
+GO.lang.cmdUnlink = 'Rozłącz';
+GO.lang.cmdClose = 'Zamknij';
+GO.lang.cmdSettings = 'Ustawienia';
+GO.lang.cmdReset = 'Reset';
+GO.lang.cmdExport='Export';
+GO.lang.cmdPrint='Drukuj';
+GO.lang.cmdImport='Import';
+
+GO.lang.cmdActions = 'Czynności';
+
+GO.lang.cmdNext='Dalej';
+GO.lang.cmdPrevious='Wstecz';
+
+GO.lang.cmdNew = 'Nowy';
+GO.lang.cmdOk = 'Ok';
+GO.lang.cmdCancel = 'Rezygnuj';
+
+GO.lang.cmdApply = 'Zastosuj';
+GO.lang.cmdRefresh = 'Odświez';
+
+GO.lang.cmdYes = 'Tak';
+GO.lang.cmdYesToAll = 'Tak na wszystko';
+GO.lang.cmdNo = 'Nie';
+GO.lang.cmdNoToAll = 'Nie na wszystko';
+
+GO.lang.strLinks = 'Linki';
+GO.lang.author = 'Autor';
+
+GO.lang.paste = 'Klej';
+GO.lang.cut = 'Wytnij';
+GO.lang.copy = 'Kopiuj';
+GO.lang.compress = 'Spakuj';
+GO.lang.decompress = 'Rozpakuj';
+GO.lang.upload = 'Wyślij';
+GO.lang.download = 'Pobierz';
+GO.lang.uploadFiles = 'Wyslij pliki';
+
+GO.lang.displayingItems = 'Wyświetlane pozycje {0} - {1} of {2}';
+GO.lang.strNoItems = 'Brak danych do wyswietlenia';
+
+GO.lang.strLinkItems = 'Szukanie pozycji do połączenia';
+GO.lang.strSearchResults= 'Wysziki szukania';
+GO.lang.strSearch= 'Szukaj';
+
+GO.lang.strPleaseSelect = 'Proszę wybrać ...';
+GO.lang.strProperties = 'Właściwości';
+
+GO.lang.strConfirm= 'Potwierdź';
+GO.lang.strSuccess= 'Sukces';
+GO.lang.strError= 'Błąd';
+GO.lang.waitMsgSave= 'Zapisywanie ...';
+GO.lang.waitMsgLoad= 'Ładowanie ...';
+GO.lang.waitMsgUpload= 'Wysyłanie ...';
+
+
+GO.lang.strSelected = 'Wybrane';
+GO.lang.noItemSelected= 'Nie wybrano żadnych pozycji.';
+GO.lang.strDeleteSelectedItems= 'Na pewno chcesz usunąć {count} pozycji?';
+GO.lang.strDeleteSelectedItem= 'Na pewno chcesz usunąć wybraną pozycję?';
+
+GO.lang.strNoWritePermissions = 'Nie masz uprawnień, aby poprawiać tę pozycję.';
+
+GO.lang.strUnauthorized= 'Nieautoryzowany';
+GO.lang.strUnauthorizedText= 'Nie masz uprawnień, aby wykonać tę operację';
+GO.lang.strReadPermissions = 'Uprawnienia do odczytu';
+GO.lang.strWritePermissions = 'Uprawnienia do zapisu';
+GO.lang.strPermissions = 'Uprawnienia';
+GO.lang.strAccessDenied = 'Brak dostepu';
+GO.lang.strAuthorizedGroups = 'Autoryzowane grupy';
+GO.lang.strAuthorizedUsers = 'Autoryzowani użytkownicy';
+
+GO.lang.strSelectGroups = 'Wybierz grupy';
+GO.lang.strSelectUsers = 'Wybierz użytkowników';
+
+GO.lang.strLogin= 'Logowanie do systemu';
+
+GO.lang.strLoggedInAs= 'Zalogowano jako ';
+
+GO.lang.strErrorsInForm= 'Wystąpiły błędy w formularzu. Pola wypełnione nieprawidłowo zostały zaznaczone.';
+
+
+
+
+
+GO.lang.strMinute="Minuta";
+GO.lang.strMinutes="Minut";
+GO.lang.strHour="Godzina";
+GO.lang.strHours="Godzin";
+GO.lang.strDay="Dzień";
+GO.lang.strDays="Dni";
+GO.lang.strWeek="Tydzień";
+GO.lang.strWeeks="Tygodni";
+GO.lang.strMonth="Miesiąc";
+GO.lang.strMonths="Miesięcy";
+GO.lang.strYear="Rok";
+GO.lang.strYears="Lat";
+GO.lang.strTime='Czas';
+
+GO.lang.shortDays = ['Nie', 'Pon', 'Wto', 'Śro', 'Czw', 'Pią','Sob'];
+
+GO.lang.noRecurrence = 'Brak powtarzania';
+GO.lang.monthsByDate = 'Miesięcznie wg daty';
+GO.lang.monthsByDay = 'Miesięcznie wg dnia';
+
+GO.lang.strSelectFiles='Wybrane pliki';
+GO.lang.strFiles='Pliki';
+GO.lang.strSubject='Temat';
+GO.lang.strLocation='Lokalizacja';
+GO.lang.strStart='Poczatek';
+GO.lang.strEnd='Koniec';
+
+GO.lang.strFirst='Pierwszy';
+GO.lang.strSecond='Drugi';
+GO.lang.strThird='Trzeci';
+GO.lang.strFourth='Czwarty';
+
+GO.lang.strStatus='Status';
+GO.lang.strAvailable='Dostępny';
+GO.lang.strAvailability='Dostępność';
+GO.lang.strView='Widok';
+
+GO.lang.field = 'Pole';
+
+GO.lang.customer = 'Klient';
+
+GO.lang.strNoCountrySelected='Nie wybrano kraju';
+GO.lang.menu = 'Menu';
+GO.lang.root = 'Root';
+GO.lang.left = 'Lewo';
+GO.lang.right = 'Prawo';
+
+GO.lang.noFilesModule = 'Dla tej operacji wymagane są pliki modułu';
+
+
+GO.lang.remindPassword='Zapamietaj mój login na tym komputerze';
+
+GO.lang.reminders = 'Przypomnienia';
+GO.lang.dismiss='Odrzuć';
+GO.lang.snooze='Drzemka';
+
+GO.lang.strMax = 'Maksimum';
+
+GO.lang.settings='Ustawienia';
+
+GO.lang.latestLinks='Ostatnie linki'
+GO.lang.cmdBrowseLinks = 'Linki';
+
+GO.lang.displayingItemsShort='Total: {2}';
---
+++ tags/groupoffice-com-3.2.29/language/common/pt_BR.js
@@ -0,0 +1,272 @@
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id: en.js 1465 2008-11-24 10:47:46Z mschering $
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
+/**
+ * Copy everything below for translations
+ */
+
+
+GO.lang.strCommunityForum='Fórum comunitário';
+GO.lang.moduleRequired='Esta função requer o \'{module}\' módulo. Este módulo não está disponível.';
+
+GO.lang.completeProfile = 'Por favor complete seu perfil';
+GO.lang.fullscreen='Tela cheia';
+GO.lang.popupBlocker = 'Seu browser está bloqueando um popup do Group-Office. Por favor, desabilite o bloqueador de popup para este site';
+
+GO.lang.color='Cor';
+GO.lang.browse='Procurar...';
+GO.lang.largeUpload='Arquivos grandes/pastas (requer Java)';
+GO.lang.smallUpload='Arquivos pequenos';
+
+
+GO.lang.lostPassword='Perdeu a senha?';
+GO.lang.lostPasswordText='Para recuperar sua senha você precisa ter acesso ao seu e-mail sem acessar ao Group-Office. Se você informar o e-mail, uma nova senha será enviada para ele.<br /><br />Informe seu e-mail:';
+
+GO.lang.noJava='Java Webstart não instalado. Java habilita a edição e envio fácil de arquivos . Visite <a class="normal-link" href="http://www.java.com/download" target="_blank">http://www.java.com/download</a> para instalar.';
+
+GO.lang.strHelpContents='Conteúdo de ajuda';
+GO.lang.strReportBug='Informar um erro';
+GO.lang.strAbout='Sobre Group-Office';
+GO.lang.strUsage='Estatística de uso';
+
+GO.lang.strName = 'Nome';
+GO.lang.strDescription = 'Descrição';
+GO.lang.strEmail = 'E-mail';
+GO.lang.strUser = 'Usuário';
+GO.lang.users = 'Usuários';
+GO.lang.strOwner = 'Dono';
+GO.lang.strContacts = 'Contatos';
+
+GO.lang.strLanguage='Língua';
+
+GO.lang.strOpenOffice = 'Documento OpenOffice.org';
+
+GO.lang.strDefault = 'Padrão';
+GO.lang.strUsername = 'Usuário';
+GO.lang.strPassword = 'Senha';
+
+GO.lang.strFirstName = 'Primeiro nome';
+GO.lang.strMiddleName = 'Nome do meio';
+GO.lang.strNo = 'Nro.';
+GO.lang.strLastName = 'Último nome';
+GO.lang.strTitle = 'Título';
+GO.lang.strInitials = 'Iniciais';
+GO.lang.strSex = 'Sexo';
+GO.lang.strBirthday = 'Aniversário';
+GO.lang.strEmail = 'E-mail';
+
+GO.lang.strPhone = 'Telefone';
+GO.lang.strFax = 'Fax';
+GO.lang.strCellular = 'Celular';
+GO.lang.strAddress = 'Endereço';
+GO.lang.strAddressNo = 'Número da casa';
+GO.lang.strZip = 'CEP';
+GO.lang.strCity = 'Cidade';
+GO.lang.strState = 'Estado';
+GO.lang.strCountry = 'Pais';
+GO.lang.strHomepage = 'Homepage';
+GO.lang.strCompany = 'Empresa';
+GO.lang.strDepartment = 'Departamento';
+GO.lang.strFunction = 'Função';
+GO.lang.strSalutation = 'Saudação';
+GO.lang.strComment = 'Comentário';
+GO.lang.strWorkAddress = 'Endereço (trabalho)';
+GO.lang.strWorkAddressNo = 'Número da casa (trabalho)';
+GO.lang.strWorkZip = 'CEP (trabalho)';
+GO.lang.strWorkCity = 'Cidade (trabalho)';
+GO.lang.strWorkState = 'Estado (trabalho)';
+GO.lang.strWorkCountry = 'Pais (trabalho)',	
+GO.lang.strWorkPhone = 'Telefone (trabalho)';
+GO.lang.strWorkFax = 'Fax (trabalho)';
+GO.lang.strPostAddress = 'Endereço (postal)';
+GO.lang.strPostAddressNo = 'Número da casa (postal)';
+GO.lang.strPostCity = 'Cidade (postal)';
+GO.lang.strPostState = 'Estado (postal)';
+GO.lang.strPostCountry = 'Pais (postal)';
+GO.lang.strPostZip = 'CEP (postal)';
+
+GO.lang.locations = 'Localização';
+GO.lang.folder = 'Pasta';
+GO.lang.folders = 'Pastas';
+
+GO.lang.up = 'Subir';
+GO.lang.fileExists = 'Arquivo existente';
+GO.lang.overwriteFile = 'Deseja sobrepor o arquivo \'{file}\'?';
+
+GO.lang.url = 'URL';
+
+GO.lang.strMale = 'Homem';
+GO.lang.strFemale = 'Mulher';
+
+GO.lang.strMtime = 'Modificado em';
+GO.lang.strCtime = 'Criado em';
+GO.lang.Atime = 'Acessado em';
+GO.lang.strText = 'Texto';
+GO.lang.texts = 'Textos';
+GO.lang.strDate = 'Data';
+GO.lang.strUser = 'Usuário';
+
+GO.lang.strSize = 'Tamanho';
+GO.lang.strType = 'Tipo';
+GO.lang.strNA = 'N/A';
+
+GO.lang.cmdDelete = 'Apagar';
+GO.lang.cmdAdd = 'Adicionar';
+GO.lang.cmdAddPlusNew = 'Adicionar + novo';
+GO.lang.cmdSavePlusNew='Salvar + novo';
+GO.lang.cmdSave = 'Salvar';
+GO.lang.cmdEdit = 'Editar';
+GO.lang.cmdLink = 'Link';
+GO.lang.cmdUnlink = 'Unlink';
+GO.lang.cmdClose = 'Fechar';
+GO.lang.cmdSettings = 'Configurações';
+GO.lang.cmdReset = 'Reset';
+GO.lang.cmdExport='Exportar';
+GO.lang.cmdPrint='Imprimir';
+GO.lang.cmdImport='Importar';
+
+GO.lang.cmdActions = 'Ações';
+
+GO.lang.cmdNext='Próximo';
+GO.lang.cmdPrevious='Anterior';
+
+GO.lang.cmdNew = 'Novo';
+GO.lang.cmdOk = 'Ok';
+GO.lang.cmdCancel = 'Cancela';
+
+GO.lang.cmdApply = 'Aplicar';
+GO.lang.cmdRefresh = 'Atualizar';
+
+GO.lang.cmdYes = 'Sim';
+GO.lang.cmdYesToAll = 'Sim para todos';
+GO.lang.cmdNo = 'Não';
+GO.lang.cmdNoToAll = 'Não para todos';
+
+GO.lang.strLinks = 'Links';
+GO.lang.author = 'Autor';
+
+GO.lang.paste = 'Colar';
+GO.lang.cut = 'Cortar';
+GO.lang.copy = 'Copiar';
+GO.lang.compress = 'Comprimir';
+GO.lang.decompress = 'Descomprimir';
+GO.lang.upload = 'Enviar';
+GO.lang.download = 'Baixar';
+GO.lang.uploadFiles = 'Enviar arquivos';
+
+GO.lang.displayingItems = 'Exibir itens {0} - {1} of {2}';
+GO.lang.strNoItems = 'Sem itens para exibir';
+
+GO.lang.strLinkItems = 'Procurar itens para o link';
+GO.lang.strSearchResults= 'Resultados da busca';
+GO.lang.strSearch= 'Busca';
+
+GO.lang.strPleaseSelect = 'Por favor selecione...';
+GO.lang.strProperties = 'Propriedades';
+
+GO.lang.strConfirm= 'Confirme';
+GO.lang.strSuccess= 'Sucesso';
+GO.lang.strError= 'Erro';
+GO.lang.waitMsgSave= 'Salvando...';
+GO.lang.waitMsgLoad= 'Carregando...';
+GO.lang.waitMsgUpload= 'Enviando...';
+
+
+GO.lang.strSelected = 'Selecionado';
+GO.lang.noItemSelected= 'Você não pode selecionar um item.';
+GO.lang.strDeleteSelectedItems= 'Você quer apagar o(s) {count} item(s)?';
+GO.lang.strDeleteSelectedItem= 'Você quer apagar o(s) item(s) selecionado(s)?';
+
+GO.lang.strNoWritePermissions = 'Você não tem permissão para editar este item.';
+
+GO.lang.strUnauthorized= 'Desautorizado';
+GO.lang.strUnauthorizedText= 'Você não tem permissão para executar essa ação';
+GO.lang.strReadPermissions = 'Permissões de leitura';
+GO.lang.strWritePermissions = 'Permissões de gravação';
+GO.lang.strPermissions = 'Permissões';
+GO.lang.strAccessDenied = 'Acesso negado';
+GO.lang.strAuthorizedGroups = 'Grupos autorizados';
+GO.lang.strAuthorizedUsers = 'Usuários autorizados';
+
+GO.lang.strSelectGroups = 'Grupos selecionados';
+GO.lang.strSelectUsers = 'Usuários selecionados';
+
+GO.lang.strLogin= 'Login requerido';
+
+GO.lang.strLoggedInAs= 'Logado como';
+
+GO.lang.strErrorsInForm= 'Você tem erros no seu formulário. Os campos inválidos estão marcados.';
+
+
+GO.lang.strMinute="Minuto";
+GO.lang.strMinutes="Minutos";
+GO.lang.strHour="Hora";
+GO.lang.strHours="Horas";
+GO.lang.strDay="Dia";
+GO.lang.strDays="Dias";
+GO.lang.strWeek="Semana";
+GO.lang.strWeeks="Semanas";
+GO.lang.strMonth="Mês";
+GO.lang.strMonths="Meses";
+GO.lang.strYear="Ano";
+GO.lang.strYears="Anos";
+GO.lang.strTime='Tempo';
+
+GO.lang.shortDays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex','Sab'];
+
+GO.lang.noRecurrence = 'Sem periodicidade';
+GO.lang.monthsByDate = 'Meses por data';
+GO.lang.monthsByDay = 'Meses por dia';
+
+GO.lang.strSelectFiles='Arquivos selecionados';
+GO.lang.strFiles='Arquivos';
+GO.lang.strSubject='Assunto';
+GO.lang.strLocation='Localização';
+GO.lang.strStart='Início';
+GO.lang.strEnd='Fim';
+
+GO.lang.strFirst='Primeiro';
+GO.lang.strSecond='Segundo';
+GO.lang.strThird='Terceiro';
+GO.lang.strFourth='Quarto';
+
+GO.lang.strStatus='Status';
+GO.lang.strAvailable='Disponível';
+GO.lang.strAvailability='Disponibilidade';
+GO.lang.strView='Vista';
+
+GO.lang.field = 'Campos';
+
+GO.lang.customer = 'Cliente';
+
+GO.lang.strNoCountrySelected='Nenhum pais selecionado';
+GO.lang.menu = 'Menu';
+GO.lang.root = 'Root';
+GO.lang.left = 'Esquerda';
+GO.lang.right = 'Direita';
+
+GO.lang.noFilesModule = 'Os arquivos do módulo são necessários para essa função';
+
+
+GO.lang.remindPassword='Lembrar meu login neste computador';
+
+GO.lang.reminders = 'Lembretes';
+GO.lang.dismiss='Desistir';
+GO.lang.snooze='Snooze';
+
+GO.lang.strMax = 'Máximo';
+
+GO.lang.settings='Configurações';
+
+GO.lang.latestLinks='Últimos links'
+GO.lang.cmdBrowseLinks = 'Links';
