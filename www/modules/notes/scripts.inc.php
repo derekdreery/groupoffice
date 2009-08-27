093309725000e0e23a2bd9148995f5afcb0a1945
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-08-27 10:31:30 +0000

    put script output from scripts.inc.php into one js file

--- www/modules/notes/scripts.inc.php
+++ www/modules/notes/scripts.inc.php
@@ -5,14 +5,11 @@ if(isset($GO_MODULES->modules['customfields']))
 {
 	require_once($GO_MODULES->modules['customfields']['class_path'].'customfields.class.inc.php');
 	$cf = new customfields();
-	echo $cf->get_javascript(4, $lang['notes']['notes']);
+	$GO_SCRIPTS_JS .= $cf->get_javascript(4, $lang['notes']['notes']);
 }
 
 require_once($GO_MODULES->modules['notes']['class_path'].'notes.class.inc.php');
 $notes = new notes();
 
 $category = $notes->get_category();
-?>
-<script type="text/javascript">
-GO.notes.defaultCategory = {id: <?php echo $category['id']; ?>, name: "<?php echo $category['name']; ?>"};
-</script>
+$GO_SCRIPTS_JS .= 'GO.notes.defaultCategory = {id: '.$category['id'].', name: "'.$category['name'].'"};';
