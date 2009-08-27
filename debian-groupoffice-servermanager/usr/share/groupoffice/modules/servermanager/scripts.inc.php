06ceb9d99d0688365fd3a6be51246ea49e49d325
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-08-27 10:57:43 +0000

    put script output from scripts.inc.php into one js file

--- debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/scripts.inc.php
+++ debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/scripts.inc.php
@@ -1,12 +1,9 @@
-<script type="text/javascript">
-GO.servermanager.config={};
-<?php 
+<?php
+
+$GO_SCRIPTS_JS .= 'GO.servermanager.config={};';
 require('/etc/groupoffice/servermanager.inc.php');
 
 foreach($default_config as $key=>$value)
 {
-	echo 'GO.servermanager.config["'.$key.'"]="'.$value.'";';
+	$GO_SCRIPTS_JS .= 'GO.servermanager.config["'.$key.'"]="'.$value.'";';
 }
-
-?>
-</script>
