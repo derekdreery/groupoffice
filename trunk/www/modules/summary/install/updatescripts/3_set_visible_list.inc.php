94be48060e32c6aef512c2fd98757a88a8696c42
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-08-31 14:52:04 +0000

    get default calendar and tasklist works in upgrade now

--- trunk/www/modules/summary/install/updatescripts/3_set_visible_list.inc.php
+++ trunk/www/modules/summary/install/updatescripts/3_set_visible_list.inc.php
@@ -10,7 +10,7 @@ if(isset($GO_MODULES->modules['tasks'])){
 
 	while($settings = $db1->next_record())
 	{
-		$tasklist = $tasks->get_tasklist($settings['default_tasklist_id']);
+		$tasklist = $tasks->get_tasklist($settings['default_tasklist_id'], $db1->f('user_id'));
 		$db2->query('INSERT INTO su_visible_lists(tasklist_id, user_id) VALUES("'.$tasklist['id'].'", "'.$db1->f('user_id').'")');
 	}
 }
