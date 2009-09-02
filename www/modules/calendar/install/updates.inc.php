aba0c9fdf2af0b0e43951483af1d7353ce28ed40
sjmeut <sjmeut@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-02 14:59:49 +0000

    Update script

--- www/modules/calendar/install/updates.inc.php
+++ www/modules/calendar/install/updates.inc.php
@@ -21,3 +21,14 @@ $updates[]="ALTER TABLE `cal_settings` ADD `calendar_id` INT NOT NULL;";
 $updates[]="ALTER TABLE `cal_settings` ADD INDEX ( `calendar_id` )";
 
 $updates[]="ALTER TABLE `cal_calendars` ADD `shared_acl` BOOL NOT NULL ";
+
+
+$updates[]="DROP TABLE IF EXISTS `cal_groups`;";
+$updates[]="CREATE TABLE IF NOT EXISTS `cal_groups` (
+  `id` int(11) NOT NULL default '0',
+  `user_id` int(11) NOT NULL default '0',
+  `name` varchar(50) default NULL,
+  `acl_admin` int(11) NOT NULL,
+  `fields` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
