3f24624817b296daac877b2477bb267a3846f948
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-02 12:30:10 +0000

    correct user_ids with folders

--- www/modules/users/classes/users.class.inc.php
+++ www/modules/users/classes/users.class.inc.php
@@ -67,7 +67,7 @@ class users extends db
 
 				//correct user_id on child folders
 				echo "Applying user_id recursively".$line_break;
-				$files->update_child_folders_recursively($up_folder['id'], array('user_id'=>$GO_USERS->f('user_id')));
+				$files->update_child_folders_recursively($up_folder['id'], array('user_id'=>$GO_USERS->f('id')));
 				//$files->set_readonly($folder['id']);
 
 				$home_dir = 'adminusers/'.$GO_USERS->f('username');
