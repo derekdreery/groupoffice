dbf81e53192cb14a1c29176074132ded8efa618b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-03 09:21:43 +0000

    Created tag 3.2.31

--- debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/scripts.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$GO_SCRIPTS_JS .= 'GO.servermanager.config={};';
-require('/etc/groupoffice/servermanager.inc.php');
-
-foreach($default_config as $key=>$value)
-{
-	$GO_SCRIPTS_JS .= 'GO.servermanager.config["'.$key.'"]="'.$value.'";';
-}
