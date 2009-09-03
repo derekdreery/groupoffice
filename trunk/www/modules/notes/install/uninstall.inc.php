ecf3176a7433959b4114e2dd9d2903e55dd93fd2
sjmeut <sjmeut@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-03 14:55:27 +0000

    Notes: uninstall scripts remove corresponding customfields.

---
+++ trunk/www/modules/notes/install/uninstall.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+global $GO_MODULES;
+require_once ($GO_MODULES->modules['customfields']['class_path'].'customfields.class.inc.php');
+
+$cf = new customfields();
+
+$cf->delete_link_type(4);
