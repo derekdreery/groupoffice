1547a53323753d37edb81ece681a0b4f248acdbc
sjmeut <sjmeut@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-03 14:56:15 +0000

    Calendar: uninstall scripts remove corresponding customfields.

--- trunk/www/modules/calendar/install/uninstall.inc.php
+++ trunk/www/modules/calendar/install/uninstall.inc.php
@@ -1,7 +1,8 @@
 <?php
 
-$module = $this->get_module('customfields');
-require_once($module['class_path'].'customfields.class.inc.php');
+global $GO_MODULES;
+require_once ($GO_MODULES->modules['customfields']['class_path'].'customfields.class.inc.php');
+
 $cf = new customfields();
 
 $cf->delete_link_type(1);
