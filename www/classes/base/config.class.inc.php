361f72a9976bc21c28c60237fb7f92b9e2b84ab3
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-04 14:43:58 +0000

    merged community bugfixes

--- www/classes/base/config.class.inc.php
+++ www/classes/base/config.class.inc.php
@@ -607,7 +607,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.2.32';
+	var $version = '3.2.33';
 
 
 	/* The permissions mode to use when creating files
@@ -633,7 +633,7 @@ class GO_CONFIG {
 	 * @access  public
 	 */
 
-	var $mtime = '20090902';
+	var $mtime = '20090904';
 
 	#group configuration
 	/**
