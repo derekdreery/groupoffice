7a03cf34612cfbbb927a445ab142704c9a27a821
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-04 15:01:23 +0000

    updated changelogs

--- scripts/updatepromodules.sh
+++ scripts/updatepromodules.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd `dirname "$0"`
 cd ../www/modules
-svn update sync/ billing/ legalhorizons/ professional/ cms/ projects/ webshop/ gota/ customfields/ dealermanager/ licenses hoursapproval timeregistration 
+svn update sync/ billing/ legalhorizons/ professional/ cms/ projects/ webshop/ gota/ customfields/ dealermanager/ licenses hoursapproval timeregistration mailings 
