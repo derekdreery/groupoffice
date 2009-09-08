872b74a27781c9afbfff4a273843a79a3fbe7ad0
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-08 14:38:00 +0000

    Cached css

--- trunk/www/themes/ExtJS/default_head.inc.php
+++ trunk/www/themes/ExtJS/default_head.inc.php
@@ -1,11 +1,11 @@
 <title><?php echo $GO_CONFIG->title; ?></title>
 <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
-<link href="<?php echo $GO_CONFIG->host; ?>ext/resources/css/ext-all.css" type="text/css" rel="stylesheet" />
 <?php
-foreach($GO_MODULES->modules as $module)
-{
-	echo $GO_THEME->get_stylesheet($module['id']);	
-}?>
+$GO_THEME->add_stylesheet($GO_CONFIG->root_path.'ext/resources/css/ext-all.css');
+$GO_THEME->add_stylesheet($GO_CONFIG->root_path.'themes/Default/style.css');
+$GO_THEME->add_stylesheet($GO_CONFIG->root_path.'themes/ExtJS/style.css');
+$GO_THEME->load_module_stylesheets();
+
+$GO_THEME->get_cached_css();
+?>
 <link href="<?php echo $GO_CONFIG->theme_url; ?>Default/images/favicon.ico" rel="shotcut icon" />
-<link href="<?php echo $GO_CONFIG->theme_url; ?>Default/style.css" type="text/css" rel="stylesheet" />
-<link href="<?php echo $GO_THEME->theme_url; ?>style.css" type="text/css" rel="stylesheet" />
