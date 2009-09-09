df38128839466168b118f91f0e5512ae3bc6a961
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-09 13:18:10 +0000

    new plugin

--- trunk/www/javascript/plugins/InsertAtCursorTextarea.js
+++ trunk/www/javascript/plugins/InsertAtCursorTextarea.js
@@ -1,3 +1,17 @@
+/**
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @version $Id: InsertAtCursorTextareaPlugin.js 2353 2009-04-15 11:05:51Z mschering $
+ * @copyright Copyright Intermesh
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
+ 
 GO.plugins.InsertAtCursorTextareaPlugin = function (){
 	return {
 		init : function(textarea){
