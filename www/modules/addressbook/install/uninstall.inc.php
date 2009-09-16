83b084d67583d54611dcfff17828c2383802230e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-16 09:11:54 +0000

    Created tag 3.2.34

--- www/modules/addressbook/install/uninstall.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-global $GO_MODULES;
-require_once ($GO_MODULES->modules['customfields']['class_path'].'customfields.class.inc.php');
-
-$cf = new customfields();
-
-$cf->delete_link_type(2);
-$cf->delete_link_type(3);
--- www/modules/files/install/uninstall.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-global $GO_MODULES;
-require_once ($GO_MODULES->modules['customfields']['class_path'].'customfields.class.inc.php');
-
-$cf = new customfields();
-
-$cf->delete_link_type(6);
