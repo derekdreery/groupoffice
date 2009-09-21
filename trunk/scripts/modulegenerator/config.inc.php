837268830f15d93f7557f7b8043a4c286c1dc125
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-09-21 12:17:48 +0000

    Generated currencies module

--- trunk/scripts/modulegenerator/config.inc.php
+++ trunk/scripts/modulegenerator/config.inc.php
@@ -2,27 +2,25 @@
 require('../../www/Group-Office.php');
 
 //name of the module. No spaces or strange characters.
-$module = 'projects';
+$module = 'currencies';
 
 //Short name of the module. The prefix of the database tables.
-$prefix = 'pm';
+$prefix = 'cu';
 
 $tables=array();
 //Tables to create an interface for
 
-
 $westpanel = array(
 	'mainpanel_tag'=> 'GRID',
 	'template'=>'GridPanel.tpl',
-	'name'=>'pm_custom_reports',
-	'friendly_single'=>'custom_report',
-	'friendly_multiple'=>'custom_reports',
-	'link_type'=>'14',
+	'name'=>'cu_currencies',
+	'friendly_single'=>'currency',
+	'friendly_multiple'=>'currencies',
 	'authenticate'=>false,
 	'paging'=>true,
-	'autoload'=>true,
+	'autoload'=>false,
 	'files'=>false);
 
 $tables[] = $westpanel;
 
-//$main_template='SimpleMainPanel.tpl'; //The template for MainPanel.js
+$main_template='SimpleMainPanel.tpl'; //The template for MainPanel.js
