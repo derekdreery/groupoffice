2586e9a35023deffb30c50c25d5312710bff1018
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-10-06 20:20:11 +0000

    Wrong acl stored for search index

--- branches/groupoffice-3.2/scripts/updatepromodules.sh
+++ branches/groupoffice-3.2/scripts/updatepromodules.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd `dirname "$0"`
 cd ../www/modules
-svn update sync/ billing/ legalhorizons/ professional/ cms/ projects/ webshop/ gota/ customfields/ dealermanager/ licenses
+svn update sync/ billing/ legalhorizons/ professional/ cms/ projects/ webshop/ gota/ customfields/ dealermanager/ licenses mailings
