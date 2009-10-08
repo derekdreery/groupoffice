3adb65686b52c4b9a73ab102508083caff22001e
sjmeut <sjmeut@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-10-08 11:28:34 +0000

    Systemusers: all system commands executed from 1 script sudo.php (add to sudoers)

--- trunk/www/modules/systemusers/classes/systemusers.class.inc.php
+++ trunk/www/modules/systemusers/classes/systemusers.class.inc.php
@@ -10,42 +10,47 @@ class systemusers extends db {
 	}
 
 
-	public static function before_add_user($user) {
-		global $GO_CONFIG, $GO_LANGUAGE;
-
-		exec($GO_CONFIG->cmd_sudo.' useradd -m '.$user['username'], $arg2, $arg3);
-
-		if($arg3) {
-			require($GLOBALS['GO_LANGUAGE']->get_language_file('systemusers'));
-			throw new Exception(str_replace('{USERNAME}', $user['username'], $lang['systemusers']['return_error']));
-		}
-
-		exec('echo '.$user['username'].':'.$user['password'].' | '.$GO_CONFIG->cmd_sudo.' '.$GO_CONFIG->cmd_chpasswd);
-
-		if(!empty($GO_CONFIG->cmd_edquota) && !empty($GO_CONFIG->quota_protouser)) {
-			exec($GO_CONFIG->cmd_sudo.' '.$GO_CONFIG->cmd_edquota.' -p '.$GO_CONFIG->quota_protouser.' '.$user['username']);
-		}
-	}
-
-	public static function update_user($user) {
-		global $GO_CONFIG, $GO_USERS;
-
-		if(!empty($user['password'])) {
-			if(!isset($user['username'])) {
-				$GO_USERS->get_user($user['id']);
-				$user['username'] = $GO_USERS->f('username');
-			}
-
-			exec('echo '.$user['username'].':'.$user['password'].' | '.$GO_CONFIG->cmd_sudo.' '.$GO_CONFIG->cmd_chpasswd);
-		}
-	}
-
-	public static function user_delete($user) {
-		global $GO_CONFIG;
+	public static function before_add_user($user)
+	{
+		global $GO_CONFIG, $GO_MODULES, $lang;
 		
-		exec($GO_CONFIG->cmd_sudo.' userdel '.$user['username']);
+		if(!$user['username'] || !$user['password'])
+		{
+			throw new Exception($lang['common']['missingField']);
+		}
+
+		exec($GO_CONFIG->cmd_sudo.' '.$GO_MODULES->modules['systemusers']['path'].'sudo.php add_user '.$user['username'].' '.$user['password'], $output);
+		if($output)
+		{
+			throw new Exception($output[0]);
+		}				
 	}
 
+    public static function update_user($user)
+    {
+		global $GO_CONFIG, $GO_MODULES;
+
+		$user['password'] = '1';
+		if(isset($user['password']) && $user['id'])
+		{
+			exec($GO_CONFIG->cmd_sudo.' '.$GO_MODULES->modules['systemusers']['path'].'sudo.php update_user '.$user['id'].' '.$user['password'], $output);
+			if($output)
+			{
+				throw new Exception($output[0]);
+			}
+		}
+    }
+
+    public static function user_delete($user)
+	{
+		global $GO_CONFIG, $GO_MODULES;
+		
+		exec($GO_CONFIG->cmd_sudo.' '.$GO_MODULES->modules['systemusers']['path'].'sudo.php delete_user '.$user['username'], $output);
+		if($output)
+		{
+			throw new Exception(str_replace('<br />',"\n", $output[0]));
+		}
+	}
 
 	function get_vacation($account_id) {
 		$this->query("SELECT * FROM syu_vacation WHERE account_id=?", 'i', $account_id);
--- trunk/www/modules/systemusers/language/en.inc.php
+++ trunk/www/modules/systemusers/language/en.inc.php
@@ -3,5 +3,4 @@
 //require($GO_LANGUAGE->get_fallback_language_file('users'));
 $lang['systemusers']['name']='System users';
 $lang['systemusers']['description']='Creates system users for every Group-Office user';
-$lang['systemusers']['return_error']='System user {USERNAME} could not be created.';
 $lang['systemusers']['vacation_not_executable_error']='Vacation is not installed or not executable.';
