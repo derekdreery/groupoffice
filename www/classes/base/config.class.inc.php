9c60392154dca7ff38c0ebb43bdbe98474a32143
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-10-20 07:44:04 +0000

    module permissions cache bug

--- www/classes/base/config.class.inc.php
+++ www/classes/base/config.class.inc.php
@@ -607,7 +607,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.2.38';
+	var $version = '3.2.39';
 
 
 	/* The permissions mode to use when creating files
@@ -633,7 +633,7 @@ class GO_CONFIG {
 	 * @access  public
 	 */
 
-	var $mtime = '20091014';
+	var $mtime = '20091020';
 
 	#group configuration
 	/**
