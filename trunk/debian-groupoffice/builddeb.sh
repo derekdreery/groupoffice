3a29a622888d3ec4715a1d055c87541d063d3d23
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-10-20 15:24:15 +0000

    new repos location

--- trunk/debian-groupoffice/builddeb.sh
+++ trunk/debian-groupoffice/builddeb.sh
@@ -47,7 +47,7 @@ cd groupoffice-com-$VERSION
 if [ "$2" == "send" ]; then
 	debuild -rfakeroot
 	cd ..
-	scp *.deb mschering@imfoss.nl:/var/www/groupoffice/repos.groupoffice.eu/groupoffice/binary/
+	scp *.deb mschering@imfoss.nl:/var/www/groupoffice/repos.groupoffice.eu/groupoffice/pool/main/
 
 	#ssh mschering@imfoss.nl "dpkg-scanpackages /var/www/groupoffice/repos.groupoffice.eu/groupoffice/binary /dev/null | gzip -9c > /var/www/groupoffice/repos.groupoffice.eu/groupoffice/binary/Packages.gz"
 else
