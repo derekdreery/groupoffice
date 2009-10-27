7675fbfcc05236f1cfea3e6f05b221ae19465ac4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-10-27 13:59:35 +0000

    download headers

--- branches/groupoffice-3.2/www/modules/email/tnef.php
+++ branches/groupoffice-3.2/www/modules/email/tnef.php
@@ -65,7 +65,7 @@ if ($browser['name'] == 'MSIE')
 	header('Pragma: public');
 }else
 {
-	header('Content-Type: application/download');
+	header('Content-Type: '.File::get_mime($path));
 	header('Pragma: no-cache');
 	header('Content-Disposition: attachment; filename="'.$lang['email']['attachments'].'.zip"');
 }
