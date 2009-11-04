24780defc507e38c3300f98efee7a76eb5755e60
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-11-04 11:03:25 +0000

    Created tag 3.2.43

---
+++ tags/groupoffice-com-3.2.43/modules/log/language/nb.inc.php
@@ -0,0 +1,6 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('log'));
+$lang['log']['name']='Logg';
+$lang['log']['description']='Loggfører alle handlinger av typen ny, endre og slette';
+
