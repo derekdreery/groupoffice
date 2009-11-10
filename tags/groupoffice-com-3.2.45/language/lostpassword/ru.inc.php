64f66d1e840b7b5cf4c18e72c174e16243cc89d4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-11-10 07:43:59 +0000

    Created tag 3.2.45

---
+++ tags/groupoffice-com-3.2.45/language/lostpassword/ru.inc.php
@@ -0,0 +1,29 @@
+<?php
+//Uncomment this line in new translations!
+//require($GO_LANGUAGE->get_fallback_base_language_file('lostpassword'));
+
+$lang['lostpassword']['success']='<h1>Пароль изменен</h1><p>Ваш пароль успешно изменен. Теперь Вы можете перейти на страницу входа.</p>';
+$lang['lostpassword']['send']='Отправить';
+$lang['lostpassword']['login']='Имя пользователя';
+
+$lang['lostpassword']['lost_password_subject']='Запрос нового пароля';
+$lang['lostpassword']['lost_password_body']='%s,
+
+Вы запросили создать новый пароль для %s. Имя пользователя "%s".
+
+Нажмите на ссылку ниже(или скопируйте ее в адресную строку интернет браузера) чтобы изменить Ваш пароль:
+
+%s
+
+Если Вы не запрашивали замену пароля удалите это письмо.';
+
+$lang['lostpassword']['lost_password_error']='В базе данных нет указанного e-mail адреса.';
+$lang['lostpassword']['lost_password_success']='Письмо с инструкцией выслано на Ваш e-mail адрес.';
+
+$lang['lostpassword']['enter_password']='Введите новый пароль';
+
+$lang['lostpassword']['new_password']='Новый пароль';
+$lang['lostpassword']['lost_password']='Забыли пароль';
+
+$lang['lostpassword']['confirm_password']='Подтвердите пароль';
+?>
