c09470df3acb71c2d40f15334ec1400a4dc515e2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-11-10 14:07:55 +0000

    Extend exports with custom classes

--- trunk/www/export_query.php
+++ trunk/www/export_query.php
@@ -1,8 +1,33 @@
 <?php
-require('Group-Office.php');
+/**
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @version $Id: Date.class.inc.php 3589 2009-11-05 13:02:37Z mschering $
+ * @copyright Copyright Intermesh
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
 
+require('Group-Office.php');
 require_once($GO_CONFIG->class_path.'export_query.class.inc.php');
-$eq = new export_query();
+
+if($_REQUEST['type']!='PDF' && $_REQUEST['type']!='CSV'){
+	
+	$file = $GO_CONFIG->file_storage_path.'customexports/'.$_REQUEST['type'].'.class.inc.php';
+	if(!file_exists($file)){
+		die('Custom export class not found.');
+	}
+	require_once($file);
+	$eq = new $_REQUEST['type']();
+}else
+{
+	$eq = new export_query();
+}
+
 $eq->download_headers();
 
 $fp = fopen('php://output','w');
