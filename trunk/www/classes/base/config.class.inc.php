734dfaa05b5c1ad1bae2ecc3296da1e8f220f606
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-11-11 11:44:24 +0000

    Merging r3614 through r3621 from stable

--- trunk/www/classes/base/config.class.inc.php
+++ trunk/www/classes/base/config.class.inc.php
@@ -604,7 +604,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.2.45';
+	var $version = '3.2.46';
 	
 
 	/* The permissions mode to use when creating files
@@ -630,7 +630,7 @@ class GO_CONFIG {
 	 * @access  public
 	 */
 
-	var $mtime = '20091110';
+	var $mtime = '20091111';
 
 	#group configuration
 	/**
