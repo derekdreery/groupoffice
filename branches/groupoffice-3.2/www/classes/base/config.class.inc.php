4e58c837db0014b63253dd11bda8c498268741ff
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-11-12 13:44:11 +0000

    empty fields on client

--- branches/groupoffice-3.2/www/classes/base/config.class.inc.php
+++ branches/groupoffice-3.2/www/classes/base/config.class.inc.php
@@ -618,7 +618,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.2.46';
+	var $version = '3.2.47';
 
 
 	/* The permissions mode to use when creating files
@@ -644,7 +644,7 @@ class GO_CONFIG {
 	 * @access  public
 	 */
 
-	var $mtime = '20091111';
+	var $mtime = '20091112';
 
 	#group configuration
 	/**
