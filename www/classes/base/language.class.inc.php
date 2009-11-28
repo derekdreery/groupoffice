979d14335cde8cae9b9d88608f42555ef3916c51
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-11-28 12:42:46 +0000

    check if object is created

--- www/classes/base/language.class.inc.php
+++ www/classes/base/language.class.inc.php
@@ -102,8 +102,8 @@ class GO_LANGUAGE extends db {
 				$_COOKIE['GO_LANGUAGE']=$this->language;
 				SetCookie("GO_LANGUAGE",$language,time()+3600*24*30,"/",'',!empty($_SERVER['HTTPS']),false);
 			}
-
-			require($GO_LANGUAGE->get_base_language_file('common'));
+			if(is_object($GO_LANGUAGE))
+				require($GO_LANGUAGE->get_base_language_file('common'));
 				
 			return true;
 		}else
