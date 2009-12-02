3fe2f2d4b32f0f4ec6313610fc97602ab70f217a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-12-02 11:02:13 +0000

    Merged from stable

--- trunk/www/modules/email/classes/email.class.inc.php
+++ trunk/www/modules/email/classes/email.class.inc.php
@@ -353,7 +353,7 @@ class email extends db
 		if($this->_add_folder($account['mbroot'].$lang['email']['trash'], $mailbox_names, $subscribed_names))
 		{
 			$account['trash'] = $account['mbroot'].$lang['email']['trash'];
-		}elseif($account['mbroot'] = $this->mail->check_mbroot('INBOX'))
+		}elseif($account['mbroot'] = $this->mail->check_mbroot($mailbox_names[0]))
 		{
 			if($this->_add_folder($account['mbroot'].$lang['email']['trash'], $mailbox_names, $subscribed_names))
 			{
