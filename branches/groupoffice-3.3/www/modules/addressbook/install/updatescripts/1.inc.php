3bb82b5625cdcb7550cef60fb6b01eaaeb883d30
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-12-04 11:41:18 +0000

    fixed upgrade2to3.php

--- branches/groupoffice-3.3/www/modules/addressbook/install/updatescripts/1.inc.php
+++ branches/groupoffice-3.3/www/modules/addressbook/install/updatescripts/1.inc.php
@@ -5,11 +5,22 @@ if(is_dir($GO_CONFIG->module_path.'mailings'))
 	$module['version']='0';
 	$module['id']='mailings';
 	$module['sort_order'] = count($GO_MODULES->modules)+1;
-	$module['acl_read']=$GO_SECURITY->get_new_acl();
-	$module['acl_write']=$GO_SECURITY->get_new_acl();
+
+	$GO_MODULES->load_modules();
+
+	if(isset($GO_MODULES->modules['users']['acl_id'])){
+		$module['acl_id']=$GO_SECURITY->get_new_acl();
+		$GO_SECURITY->copy_acl($GO_MODULES->modules['addressbook']['acl_id'], $module['acl_id']);
+	}else
+	{
+		$module['acl_read']=$GO_SECURITY->get_new_acl();
+		$module['acl_write']=$GO_SECURITY->get_new_acl();
+
+		$GO_SECURITY->copy_acl($GO_MODULES->modules['addressbook']['acl_read'], $module['acl_read']);
+		$GO_SECURITY->copy_acl($GO_MODULES->modules['addressbook']['acl_write'], $module['acl_write']);
+	}
 	
-	$GO_SECURITY->copy_acl($GO_MODULES->modules['addressbook']['acl_read'], $module['acl_read']);
-	$GO_SECURITY->copy_acl($GO_MODULES->modules['addressbook']['acl_write'], $module['acl_write']);
+	
 	
 	$db->insert_row('go_modules', $module);
 
