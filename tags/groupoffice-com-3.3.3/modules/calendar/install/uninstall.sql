a2e5cc51d77939c04bdbcb93887cd0106b92f9c6
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-12-08 11:15:38 +0000

    Created tag 3.3.3

---
+++ tags/groupoffice-com-3.3.3/modules/calendar/install/uninstall.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `cal_backgrounds`;
+DROP TABLE IF EXISTS `cal_calendars`;
+DROP TABLE IF EXISTS `cal_events`;
+DROP TABLE IF EXISTS `cal_participants`;
+DROP TABLE IF EXISTS `cal_views`;
+DROP TABLE IF EXISTS `cal_exceptions`;
+DROP TABLE IF EXISTS `cal_views_calendars`;
+DROP TABLE IF EXISTS `go_links_1`;
+DROP TABLE IF EXISTS `cal_settings`;
+DROP TABLE IF EXISTS `cal_groups`;
+DROP TABLE IF EXISTS `cf_1`;
