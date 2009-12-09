06a49263b1eea9c527428fe0440f809a31084cc8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-12-09 13:24:56 +0000

    invalid chmod

--- debian-groupoffice-servermanager/usr/share/groupoffice-servermanager/configure.php
+++ debian-groupoffice-servermanager/usr/share/groupoffice-servermanager/configure.php
@@ -35,7 +35,7 @@ if(!file_exists('/etc/groupoffice/servermanager.inc.php'))
 	create_file('/etc/groupoffice/servermanager.inc.php', 'tpl/etc/groupoffice/servermanager.inc.php', $replacements);
 
 chgrp('/etc/groupoffice/servermanager.inc.php', 'www-data');
-chmod('/etc/groupoffice/servermanager.inc.php', 640);
+chmod('/etc/groupoffice/servermanager.inc.php', 0640);
 
 if(!file_exists('/etc/apache2/sites-enabled/000-groupoffice'))
 	create_file('/etc/apache2/sites-enabled/000-groupoffice', 'tpl/etc/apache2/sites-enabled/000-groupoffice', $replacements);
