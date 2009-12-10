e8fd53f2d9d79e84a429b0bf435ebe7848c41e5b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-12-10 15:58:10 +0000

    Updated German translation

--- www/modules/users/language/de.inc.php
+++ www/modules/users/language/de.inc.php
@@ -10,6 +10,16 @@ $lang['users']['error_username_exists']='Der Benutzername ist bereits registrier
 $lang['users']['error_email_exists']='Die E-Mail-Adresse ist bereits registriert.';
 $lang['users']['error_match_pass']='Die Passwörter sind nicht identisch';
 $lang['users']['error_email']='Sie haben eine ungültige E-Mail-Adresse angegeben';
+$lang['users']['error_user']='Benutzer konnte nicht angelegt werden';
 $lang['users']['imported']='%s Benutzer importiert';
 $lang['users']['failed']='Fehlgeschlagen';
 $lang['users']['incorrectFormat']='Datei hatte nicht das erforderliche CSV-Format';
+$lang['users']['register_email_subject']='Ihre GroupOffice Accountdaten';
+$lang['users']['register_email_body']='Ein GroupOffice Account wurde für Sie auf {url} angelegt
+Ihre Logindaten sind:
+
+Benutzername: {username}
+Passwort: {password}';
+
+
+$lang['users']['max_users_reached']='Für dieses System wurde die maximale Benutzeranzahl erreicht.';
