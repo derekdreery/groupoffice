ac9855ede2232db28933be48d15ebf098f24d51e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-12-10 12:01:45 +0000

    Created tag 3.3.6

---
+++ tags/groupoffice-com-3.3.6/language/common/nl.js
@@ -0,0 +1,311 @@
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id$
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
+GO.lang.exportHiddenColumns='Exporteer verborgen kolommen ook';
+GO.lang.searchQuery='zoekterm';
+GO.lang.pleaseApplyFirst='Sla eerst op aub. door op \'Toepassen\' te klikken';
+GO.lang.passwordMatchError='De wachtwoorden kwamen niet overeen';
+GO.lang.serverError='Er is een fout opgetreden op de webserver. Neem contact op met uw systeembeheerder geef de gedetailleerde foutmelding.';
+GO.lang.errorDetails='Gedetailleerde foutmelding';
+GO.lang.contactSupportDesk='Neem contact op met de helpdesk';
+GO.lang.disabled='Uitgeschakeld';
+GO.lang.changesWillBeLost='U heeft wijzigingen in het huidige scherm gemaakt. Druk op annuleren en sla de wijzigingen op of druk op Ok om door te gaan en uw wijzigingen te verwijderen.';
+GO.lang.more='Meer';
+GO.lang.strCommunityForum='Community forum';
+
+GO.lang.moduleRequired='Deze functie vereist de \'{module}\' module. Deze module is niet beschikbaar.';
+ 
+GO.lang.completeProfile = 'Vul uw profiel aan a.u.b.';
+GO.lang.fullscreen='Volledig scherm modus';
+
+GO.lang.color='Kleur';
+GO.lang.browse='Bladeren...';
+
+GO.lang.largeUpload='Grote bestanden/mappen (Java Vereist)';
+GO.lang.smallUpload='Kleine bestanden';
+
+GO.lang.lostPassword='Wachtwoord vergeten?';
+GO.lang.lostPasswordText='Om uw wachtwoord opnieuw in te stellen heeft u toegang tot uw e-mail nodig zonder toegang tot Group-Office. Als u hier uw e-mail adres invult wordt een e-mail met instructies naar uw e-mail adres verzonden.<br /><br />Vul uw e-mail adres in:';
+
+
+GO.lang.noJava='Java Webstart is niet geïnstalleerd. Java maakt het mogelijk om gemakkelijk bestanden te uploaden en te bewerken. Ga naar <a class="normal-link" href="http://www.java.com/download" target="_blank">http://www.java.com/download</a> om Java op uw computer te installeren.';
+GO.lang.item='item';
+GO.lang.items='items';
+ 
+GO.lang.strDefault = 'Standaard';
+
+GO.lang.strHelpContents='Helpinhoud';
+GO.lang.strReportBug='Meld een programmafout';
+GO.lang.strAbout='Over Group-Office';
+
+GO.lang.strName = 'Naam';
+GO.lang.strDescription = 'Beschrijving';
+GO.lang.strEmail = 'E-mail';
+GO.lang.strUser = 'Gebruiker';
+GO.lang.strOwner = 'Eigenaar';
+
+GO.lang.strLanguage='Taal';
+
+GO.lang.strOpenOffice = 'OpenOffice.org document';
+
+
+GO.lang.strUsername = 'Gebruikersnaam';
+GO.lang.strFirstName = 'Voornaam';
+GO.lang.strMiddleName = 'Tussenvoegsel';
+GO.lang.strNo = 'Nr.';
+GO.lang.strLastName = 'Achternaam';
+GO.lang.strTitle = 'Titel';
+GO.lang.strInitials = 'Voorletters';
+GO.lang.strSex = 'Geslacht';
+GO.lang.strBirthday = 'Geboortedatum';
+GO.lang.strEmail = 'E-mail';
+
+GO.lang.strPhone = 'Tel.';
+GO.lang.strFax = 'Fax';
+GO.lang.strCellular = 'Mobiel';
+GO.lang.strAddress = 'Adres';
+GO.lang.strAddressNo = 'Huisnummer';
+GO.lang.strZip = 'Postcode';
+GO.lang.strCity = 'Woonplaats';
+GO.lang.strState = 'Provincie';
+GO.lang.strCountry = 'Land';
+GO.lang.strAddressFormat = 'Adres formaat';
+GO.lang.strHomepage = 'Homepage';
+GO.lang.strCompany = 'Bedrijf';
+GO.lang.strDepartment = 'Afdeling';
+GO.lang.strFunction = 'Functie';
+GO.lang.strSalutation = 'Aanhef';
+GO.lang.strComment = 'Opmerking';
+GO.lang.strWorkAddress = 'Adres (werk)';
+GO.lang.strWorkAddressNo = 'Huisnummer (werk)';
+GO.lang.strWorkZip = 'Postcode (werk)';
+GO.lang.strWorkCity = 'Woonplaats (werk)';
+GO.lang.strWorkState = 'Provincie (werk)';
+GO.lang.strWorkCountry = 'Land (werk)',	
+GO.lang.strWorkPhone = 'Tel. (werk)';
+GO.lang.strWorkFax = 'Fax (werk)';
+GO.lang.strPostAddress = 'Adres (post)';
+GO.lang.strPostAddressNo = 'Huisnummer (post)';
+GO.lang.strPostCity = 'Stad (post)';
+GO.lang.strPostState = 'Provincie (post)';
+GO.lang.strPostCountry = 'Land (post)';
+GO.lang.strPostZip = 'Postcode (post)';
+
+GO.lang.locations = 'Locaties';
+
+GO.lang.strMale = 'Man';
+GO.lang.strFemale = 'Vrouw',	
+
+GO.lang.strType = 'Type';
+GO.lang.strMtime = 'Gewijzigd op';
+GO.lang.strCtime = 'Gemaakt op';
+GO.lang.strText = 'Tekst';
+GO.lang.strDate = 'Datum';
+GO.lang.strUser = 'Gebruiker';
+GO.lang.newFolder='Nieuwe map';
+
+GO.lang.cmdDelete = 'Verwijderen';
+GO.lang.cmdAdd = 'Toevoegen';
+GO.lang.cmdSave = 'Opslaan';
+GO.lang.cmdSaveAs = 'Opslaan als';
+GO.lang.cmdEdit = 'Bewerken';
+GO.lang.cmdLink = 'Koppelen';
+GO.lang.cmdUnlink = 'Ontkoppelen';
+GO.lang.cmdClose = 'Sluiten';
+GO.lang.cmdSettings = 'Instellingen';
+GO.lang.cmdReset = 'Wissen';
+GO.lang.cmdImport='Importeren';
+
+GO.lang.cmdActions = 'Acties';
+
+GO.lang.cmdNew = 'Nieuw';
+GO.lang.cmdOk = 'Ok';
+GO.lang.cmdCancel = 'Annuleren';
+
+GO.lang.cmdApply = 'Toepassen';
+GO.lang.cmdRefresh = 'Vernieuwen';
+
+GO.lang.cmdYes = 'Ja';
+GO.lang.cmdNo = 'Nee';
+
+GO.lang.strLinks = 'Koppelingen';
+
+
+GO.lang.displayingItems = 'Toont items {0} - {1} van {2}';
+GO.lang.strNoItems = 'Geen items om weer te geven';
+
+GO.lang.strLinkItems = 'Zoeken naar items om te koppelen';
+GO.lang.strSearchResults= 'Zoekresultaten';
+GO.lang.strSearch= 'Zoeken';
+
+GO.lang.strPleaseSelect = 'Selecteer a.u.b...';
+GO.lang.strProperties = 'Eigenschappen';
+
+GO.lang.strConfirm= 'Bevestigen';
+GO.lang.strSuccess= 'Succes';
+GO.lang.strError= 'Fout';
+GO.lang.waitMsgSave= 'Bezig met opslaan...';
+GO.lang.waitMsgLoad= 'Bezig met laden...';
+
+GO.lang.strRequestError= 'Kon geen verbinding maken met de server. Controleer uw internet verbinding a.u.b.';
+
+GO.lang.noItemSelected= 'U heeft niets geselecteerd';
+GO.lang.strDeleteSelectedItems= 'Weet u zeker dat u de {count} geselecteerde items wilt verwijderen?';
+GO.lang.strDeleteSelectedItem= 'Weet u zeker dat u het geselecteerde item wilt verwijderen?';
+
+GO.lang.strNoWritePermissions = 'U heeft geen toegangsrechten om dit item te bewerken.';
+
+GO.lang.strUnauthorized= 'U bent ongeautoriseerd';
+GO.lang.strUnauthorizedText= 'U heeft onvoldoende toegangsrechten om deze actie uit te voeren';
+GO.lang.strReadPermissions = 'Leesrechten';
+GO.lang.strWritePermissions = 'Schrijfrechten';
+GO.lang.strPermissions = 'Toegangsrechten';
+GO.lang.strAccessDenied = 'Geen toegang';
+GO.lang.strAuthorizedGroups = 'Geautoriseerde groepen';
+GO.lang.strAuthorizedUsers = 'Geautoriseerde gebruikers';
+
+GO.lang.strSelectGroups = 'Selecteer groepen';
+GO.lang.strSelectUsers = 'Selecteer gebruikers';
+
+GO.lang.strLogin= 'Inloggen vereist';
+
+GO.lang.strLoggedInAs= 'Ingelogd als';
+
+GO.lang.strErrorsInForm= 'U heeft het formulier onvolledig of foutief ingevuld. De foutieve velden zijn gemarkeerd.';
+
+GO.lang.strLocation='Locatie';
+
+GO.lang.remindPassword='Onthoud mijn login op deze PC tot ik op uitloggen druk';
+
+GO.lang.users= 'Gebruikers';
+GO.lang.strContacts= 'Contacten';
+GO.lang.strPassword= 'Wachtwoord';
+GO.lang.folder= 'Map';
+GO.lang.folders= 'Mappen';
+GO.lang.up= 'Omhoog';
+GO.lang.fileExists= 'Bestand bestaat al';
+GO.lang.overwriteFile= 'Wilt u het bestand \'{file}\' overschrijven?';
+GO.lang.url= 'URL';
+GO.lang.Atime= 'Laatst geopend';
+GO.lang.texts= 'Teksten';
+GO.lang.strSize= 'Grootte';
+GO.lang.strNA= 'n.v.t.';
+GO.lang.cmdAddPlusNew= 'Toevoegen + nieuw';
+GO.lang.cmdSavePlusNew='Opslaan + nieuw';
+GO.lang.cmdExport='Exporteren';
+GO.lang.cmdPrint='Printen';
+GO.lang.cmdNext='Volgende';
+GO.lang.cmdPrevious='Vorige';
+GO.lang.cmdYesToAll= 'Ja op alles';
+GO.lang.cmdNoToAll= 'Nee op alles';
+GO.lang.author= 'Auteur';
+GO.lang.paste= 'Plakken';
+GO.lang.cut= 'Knippen';
+GO.lang.copy= 'Kopiëren';
+GO.lang.compress= 'Inpakken';
+GO.lang.decompress= 'Uitpakken';
+GO.lang.upload= 'Upload';
+GO.lang.download= 'Download';
+GO.lang.uploadFiles= 'Upload bestanden';
+GO.lang.waitMsgUpload= 'Bezig met uploaden...';
+GO.lang.strSelected= 'Geselecteerde';
+
+GO.lang.strMinute="Minuut";
+GO.lang.strMinutes="Minuten";
+GO.lang.strHour="Uur";
+GO.lang.strHours="Uren";
+GO.lang.strDay="Dag";
+GO.lang.strDays="Dagen";
+GO.lang.strWeek="Week";
+GO.lang.strWeeks="Weken";
+GO.lang.strMonth="Maand";
+GO.lang.strMonths="Maanden";
+GO.lang.strYear="Jaar";
+GO.lang.strYears="Jaren";
+GO.lang.strTime='Tijd';
+GO.lang.shortDays= ['Zon', 'Maa', 'Din', 'Woe', 'Don', 'Vri','Zat'];
+
+GO.lang.selectWeek='Selecteer week';
+
+GO.lang.noRecurrence= 'Geen herhaling';
+GO.lang.monthsByDate= 'Maanden op datum';
+GO.lang.monthsByDay= 'Maanden op dag';
+GO.lang.strSelectFiles='Selecteer bestanden';
+GO.lang.strSelectFile='Selecteer bestand';
+GO.lang.strFiles='Bestanden';
+GO.lang.strSubject='Onderwerp';
+GO.lang.strStart='Start';
+GO.lang.strEnd='Einde';
+GO.lang.strFirst='Eerste';
+GO.lang.strSecond='Tweede';
+GO.lang.strThird='Derde';
+GO.lang.strFourth='Vierde';
+GO.lang.strStatus='Status';
+GO.lang.strAvailable='Beschikbaar';
+GO.lang.strAvailability='Beschikbaarheid';
+GO.lang.strView='Beeld';
+GO.lang.field= 'Veld';
+GO.lang.value = 'Waarde';
+GO.lang.customer= 'Klant';
+GO.lang.strNoCountrySelected='Geen land geselecteerd';
+GO.lang.menu= 'Menu';
+GO.lang.root= 'Hoofdmap';
+GO.lang.left= 'Links';
+GO.lang.right= 'Rechts';
+GO.lang.reminders= 'Herinneringen';
+GO.lang.dismiss='Verwerp';
+GO.lang.selectAll='Selecteer alles';
+GO.lang.snooze='Uitstel';
+GO.lang.latestLinks='Nieuwste links'
+GO.lang.cmdBrowseLinks= 'Koppelingen';
+
+GO.lang.noFilesModule= 'De files-module is nodig voor deze functie';
+GO.lang.strMax= 'Maximum';
+
+GO.lang.settings='Instellingen';
+
+
+GO.lang.popupBlocker= 'Uw browser blokkeert een popup van Group-Office. Schakel de popup blokkering uit voor Group-Office a.u.b.';
+GO.lang.strUsage='Gebruiksstatistieken';
+GO.lang.today='Vandaag';
+
+GO.lang.image='Afbeelding';
+GO.lang.insertImage='Afbeelding invoegen in de tekst';
+
+GO.lang.on ='Aan';
+GO.lang.off ='Uit';
+
+GO.lang.displayingItemsShort='Totaal: {2}';
+
+GO.lang.keyword='Trefwoord';
+
+if(Ext.form.BasicForm)
+{
+	Ext.form.BasicForm.prototype.waitTitle = "Even wachten a.u.b...";
+}
+
+GO.lang.closeApps='Applicaties afsluiten';
+GO.lang.rightClickToClose='Klik met de rechtermuisknop op een tabblad kop om deze af te sluiten.';
+
+GO.lang.administration='Beheer';
+
+GO.lang.total='Totaal';
+GO.lang.adminMenu= 'Beheermenu';
+GO.lang.extra='Extra';
+
+GO.lang.permissionsLevel='Niveau';
+
+GO.lang.permissionRead='Alleen lezen';
+GO.lang.permissionWrite='Schrijven';
+GO.lang.permissionDelete='Schrijven en verwijderen';
+GO.lang.permissionManage='Beheren';
