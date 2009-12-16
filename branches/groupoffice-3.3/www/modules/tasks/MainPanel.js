6588888ad1de6959e2534b3c7de87a98b99079f2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2009-12-16 16:21:08 +0000

    thumb url for ppt files

--- branches/groupoffice-3.3/www/modules/tasks/MainPanel.js
+++ branches/groupoffice-3.3/www/modules/tasks/MainPanel.js
@@ -104,6 +104,12 @@ GO.tasks.MainPanel = function(config){
 			task_id: record.data.id
 		});
 	}, this);
+
+	this.gridPanel.on('checked', function(grid, task_id){
+		if(this.taskPanel.data && this.taskPanel.data.id==task_id)
+				this.taskPanel.reload();
+			
+	}, this);
 			
 	this.gridPanel.store.on('load', function(store){
 		this.deleteButton.setDisabled(!store.reader.jsonData.write_permission);
@@ -141,16 +147,7 @@ GO.tasks.MainPanel = function(config){
 				text: GO.lang['cmdAdd'],
 				cls: 'x-btn-text-icon',
 				handler: function(){
-					if(!GO.tasks.taskDialog)
-					{
-						GO.tasks.taskDialog = new GO.tasks.TaskDialog();
-					}
-					if(!GO.tasks.taskDialog.hasListener('save'))
-					{
-						GO.tasks.taskDialog.on('save', function(){
-							this.gridPanel.store.reload();
-						}, this);
-					}
+
 					GO.tasks.taskDialog.show({
 						tasklist_id: this.tasklist_id,
 						tasklist_name: this.tasklist_name
