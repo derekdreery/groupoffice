819270f4040d3dec683be41599cc4a610fe0b919
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-01-05 10:08:29 +0000

    Created tag 3.3.9

---
+++ tags/groupoffice-com-3.3.9/modules/postfixadmin/language/ru.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Russian translation
+ * By Valery Yanchenko (utf-8 encoding)
+ * vajanchenko@hotmail.com
+ * 10 December 2008
+*/
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('postfixadmin'));
+$lang['postfixadmin']['name']='Администрирование Postfix';
+$lang['postfixadmin']['description']='Модуль для управления виртуальными пользователями postfix в базе данных Group-Office';
+$lang['postfixadmin']['alias']='Псевдоним';
+$lang['postfixadmin']['aliases']='Псевдонимы';
+$lang['postfixadmin']['domain']='Домен';
+$lang['postfixadmin']['domains']='Домены';
+$lang['postfixadmin']['fetchmail_config']='Конфигурация Fetchmail';
+$lang['postfixadmin']['fetchmail_configs']='Конфигурации Fetchmail';
+$lang['postfixadmin']['mailbox']='Почтовый ящик';
+$lang['postfixadmin']['mailboxes']='Почтовые ящики';
+$lang['postfixadmin']['vacation_config']='Конфигурация Vacation';
+$lang['postfixadmin']['vacation_configs']='Конфигурации Vacation';
