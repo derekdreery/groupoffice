bff360d7aa609ed58071d844266f75e3507dcca0
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-01-12 08:38:07 +0000

    Search query Addressbook: fixed bug of email default value, enabled gridpanels store load on beforeshow, and tightened the layout.

---
+++ trunk/www/modules/addressbook/SearchQueryPanel.js
@@ -0,0 +1,155 @@
+GO.addressbook.SearchQueryPanel = function(config)
+	{
+		if(!config){
+			config = {};
+		}
+
+		config.title = 'Nieuwe zoekopdracht';
+
+		this.typesStore = new GO.data.JsonStore({
+			url: GO.settings.modules.addressbook.url+'json.php',
+			baseParams: {
+				task: "ab_fields",
+				type:""
+			},
+			root: 'results',
+			id: 'field',
+			fields: ['field', 'label', 'value', 'type', 'options'],
+			remoteSort: true
+		});
+
+		//config.layout='table';
+		config.split=true;
+
+		this.typesStore.load();
+
+		var comparators = this.getComparators();
+
+		this.queryField = new Ext.form.TextArea({
+			fieldLabel : 'Zoekopdracht',
+			width : 585,
+			height : 150
+		});
+
+		this.queryMakerPanel = new Ext.Panel({
+			layout: 'table',
+			items: [this.typesBox = new Ext.form.ComboBox({
+				store: this.typesStore,
+				valueField:'field',
+				displayField:'label',
+				mode: 'local',
+				triggerAction: 'all',
+				editable: true,
+				selectOnFocus:true,
+				forceSelection:true,
+				emptyText: 'kies type'
+			}),this.comparatorBox = new Ext.form.ComboBox({
+				store: new Ext.data.SimpleStore({
+					fields: ['value'],
+					data : comparators
+				}),
+				valueField:'value',
+				displayField:'value',
+				value: '=',
+				width: 50,
+				mode: 'local',
+				triggerAction: 'all',
+				editable: true,
+				selectOnFocus:true,
+				forceSelection:true
+			}),this.criteriumField = new Ext.form.TextField({
+				//fieldLabel: GO.lang['strFirstName'],
+				name: 'criterium',
+				emptyText: 'criterium',
+				width: 190,
+				panel: this
+			}),this.operatorBox = new Ext.form.ComboBox({
+				store: new Ext.data.SimpleStore({
+					fields: ['value','text'],
+					data : [
+					['','--'],
+					['AND','AND'],
+					['OR','OR']
+					]
+				}),
+				valueField:'value',
+				displayField:'text',
+				width: 60,
+				mode: 'local',
+				triggerAction: 'all',
+				editable: true,
+				selectOnFocus:true,
+				forceSelection:true
+			}),new Ext.Button({
+				handler: function()
+				{
+					var text = this.queryField.getValue();
+					if (this.typesBox.value) text = text + ' ' + this.typesBox.value;
+					if (this.comparatorBox.value) text = text + ' ' + this.comparatorBox.value;
+					if (this.criteriumField.getValue()) text = text + ' \'' + this.criteriumField.getValue() + '\'';
+					if (this.operatorBox.value) text = text + ' ' + this.operatorBox.value;
+					this.queryField.setValue(text);
+				},
+				text: 'voeg toe',
+				scope: this
+			})]
+		});
+
+		this.buttonsPanel = new Ext.Panel({
+			layout: 'table',
+			items: [new Ext.Button({
+				handler: function()
+				{
+					Ext.Ajax.request({
+						url:GO.settings.modules.addressbook.url +'action.php',
+						params:{
+							task:'save_sql',
+							sql: this.queryField.getValue()
+						},
+						success: function(response, options)
+						{
+							var responseParams = Ext.decode(response.responseText);
+							if(!responseParams.success)
+							{
+								alert(responseParams.feedback);
+							}else
+							{
+							}
+						},
+						scope:this
+					})
+				},
+				text: 'opslaan',
+				scope: this
+			}),new Ext.Button({
+				handler: function()
+				{
+					this.queryField.setValue('');
+				},
+				text: 'reset',
+				scope: this
+			})]
+		});
+
+		// 	config.defaults={border: false, cls:'ab-search-form-panel'};
+		config.items= [this.queryField,this.buttonsPanel,this.queryMakerPanel];
+
+		GO.addressbook.SearchQueryPanel.superclass.constructor.call(this, config);
+	}
+
+Ext.extend(GO.addressbook.SearchQueryPanel, Ext.Panel, {
+
+	getComparators : function() {
+		return [
+					['LIKE'],
+					['NOT LIKE'],
+					['='],
+					['!='],
+					['>'],
+					['>='],
+					['<'],
+					['<=']
+					];
+	}
+
+	});
