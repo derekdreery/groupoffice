385ebd57dc3e269c1cdafb2fb6bee67a2e81c6a4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-01-13 11:13:08 +0000

    script to send users invitations

--- branches/groupoffice-3.3/www/classes/mail/swift/lib/preferences.php
+++ branches/groupoffice-3.3/www/classes/mail/swift/lib/preferences.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__FILE__).'/../../../../../Group-Office.php');
+require_once(dirname(__FILE__).'/../../../../Group-Office.php');
 
 // Sets the default charset so that setCharset() is not needed elsewhere
 Swift_Preferences::getInstance()->setCharset('utf-8');
