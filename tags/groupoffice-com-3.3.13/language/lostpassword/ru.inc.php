88a1616dfeff2c3fa7e2680fb2e4df19b1d94c96
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-01-19 16:22:25 +0000

    Created tag 3.3.13

---
+++ tags/groupoffice-com-3.3.13/language/lostpassword/ru.inc.php
@@ -0,0 +1,29 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_base_language_file('lostpassword'));
+
+$lang['lostpassword']['success']='<h1>Ïàðîëü èçìåíåí</h1><p>Âàø ïàðîëü óñïåøíî èçìåíåí. Òåïåðü Âû ìîæåòå ïåðåéòè íà ñòðàíèöó âõîäà.</p>';
+$lang['lostpassword']['send']='Îòïðàâèòü';
+$lang['lostpassword']['login']='Èìÿ ïîëüçîâàòåëÿ';
+
+$lang['lostpassword']['lost_password_subject']='Çàïðîñ íîâîãî ïàðîëÿ';
+$lang['lostpassword']['lost_password_body']='%s,
+
+Âû çàïðîñèëè ñîçäàòü íîâûé ïàðîëü äëÿ %s. Èìÿ ïîëüçîâàòåëÿ "%s".
+
+Íàæìèòå íà ññûëêó íèæå(èëè ñêîïèðóéòå åå â àäðåñíóþ ñòðîêó èíòåðíåò áðàóçåðà) ÷òîáû èçìåíèòü Âàø ïàðîëü:
+
+%s
+
+Åñëè Âû íå çàïðàøèâàëè çàìåíó ïàðîëÿ óäàëèòå ýòî ïèñüìî.';
+
+$lang['lostpassword']['lost_password_error']='Â áàçå äàííûõ íåò óêàçàííîãî e-mail àäðåñà.';
+$lang['lostpassword']['lost_password_success']='Ïèñüìî ñ èíñòðóêöèåé âûñëàíî íà Âàø e-mail àäðåñ.';
+
+$lang['lostpassword']['enter_password']='Ââåäèòå íîâûé ïàðîëü';
+
+$lang['lostpassword']['new_password']='Íîâûé ïàðîëü';
+$lang['lostpassword']['lost_password']='Çàáûëè ïàðîëü';
+
+$lang['lostpassword']['confirm_password']='Ïîäòâåðäèòå ïàðîëü';
+?>
