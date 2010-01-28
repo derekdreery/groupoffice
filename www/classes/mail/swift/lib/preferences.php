1962146b79bab3e67db5cb61b4797de892365acd
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-01-28 13:57:09 +0000

    address go_config in global scope

--- www/classes/mail/swift/lib/preferences.php
+++ www/classes/mail/swift/lib/preferences.php
@@ -5,5 +5,5 @@ require_once(dirname(__FILE__).'/../../../../Group-Office.php');
 Swift_Preferences::getInstance()->setCharset('utf-8');
 
 Swift_Preferences::getInstance()
-    -> setTempDir($GO_CONFIG->tmpdir)
+    -> setTempDir($GLOBALS['GO_CONFIG']->tmpdir)
     -> setCacheType('disk');
