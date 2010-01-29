530e9d9bdf95d466ba6b4867268e52c2bb5982c8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-01-29 16:18:59 +0000

    don't count already unread/read mails when manually marked

--- branches/groupoffice-3.3/www/modules/email/EmailClient.js
+++ branches/groupoffice-3.3/www/modules/email/EmailClient.js
@@ -1096,7 +1096,7 @@ Ext.extend(GO.email.EmailClient, Ext.Panel,{
 			}
 			
 			var current = this.notificationEl.dom.innerHTML;
-			
+
 			if(current!='' && inbox_new-this.justMarkedUnread>current)
 			{
 				GO.playAlarm();
@@ -1548,14 +1548,21 @@ Ext.extend(GO.email.EmailClient, Ext.Panel,{
 								case 'mark_as_read':							
 									field='new';
 									value=false;
+
+									for(var i=0;i<records.length;i++){
+										if(records[i].get('new')=='1')
+											this.justMarkedUnread--;
+									}
 									
-									this.justMarkedUnread-=records.length;
 									break;
 								case 'mark_as_unread':
 									field='new';
 									value=true;
 									
-									this.justMarkedUnread+=records.length;
+									for(var i=0;i<records.length;i++){
+										if(records[i].get('new')!='1')
+											this.justMarkedUnread++;
+									}
 									break;
 									
 								case 'flag':					
