37e549b257c82b92f5177b2babba2259072829e8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-02 15:51:08 +0000

    Created tag 3.4.7

---
+++ tags/groupoffice-com-3.4.7/modules/files/language/fr.js
@@ -0,0 +1,49 @@
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id$
+ * @author Merijn Schering <mschering@intermesh.nl>
+ *
+ * French Translation v1.0
+ * Author : Lionel JULLIEN
+ * Date : September, 04 2008
+ *
+ * Update for 3.02-stable-10
+ * Author : Cyril DUCHENOY
+ * Date : July, 21 200
+ */
+
+
+
+
+
+GO.files.lang.files='Fichiers';
+
+GO.files.lang.template = 'Modèle'
+GO.files.lang.manageTemplates = 'Gérer les modèles';
+GO.files.lang.downloadTemplate = 'Télécharger le modèle';
+GO.files.lang.enterName = 'Saisir un nom';
+GO.files.lang.pleaseEnterName = 'Veuillez saisir un nom';
+GO.files.lang.pleaseEnterNameArchive = 'Veuillez saisir un nom pour l\'archive';
+GO.files.lang.startTransfer = 'Débuter le transfert';
+GO.files.lang.addFolder = 'Ajouter un dossier';
+GO.files.lang.errorOneItem = 'Veuillez sélectionner un seul élément';
+GO.files.lang.activateSharing = 'Activer le partage';
+GO.files.lang.notifyChanges = 'Me notifier les changements sur ce dossier';
+GO.files.lang.comments = 'Commentaires';
+GO.files.lang.downloadGOTA = 'Télécharger avec GOTA';
+GO.files.lang.fileBrowser = 'Explorateur de fichiers';
+GO.files.lang.thumbnails = 'Miniatures';
+GO.files.lang.saveFile='Sauvegarder le fichier';
+GO.files.lang.confirmOverwrite='{filename} existe déjà, voulez vous écraser ce fichier ?';
+GO.files.lang.file='Fichier';
+GO.files.lang.emptyList='Liste vide';
+GO.files.lang.newFiles='Nouveau';
+
+GO.files.lang.olderVersions='Versions antérieures';
