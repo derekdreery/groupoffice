3f55a0d7141d9d48cff59996e9960f9b5864a86c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-04 16:11:23 +0000

    typo in export

--- branches/groupoffice-3.4/www/classes/base/config.class.inc.php
+++ branches/groupoffice-3.4/www/classes/base/config.class.inc.php
@@ -629,7 +629,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.4.7';
+	var $version = '3.4.8';
 
 
 	/* The permissions mode to use when creating files
@@ -654,7 +654,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20100302-1';
+	var $mtime = '20100304';
 
 	#group configuration
 	/**
