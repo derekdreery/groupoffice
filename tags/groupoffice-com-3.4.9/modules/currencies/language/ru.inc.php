37e24241681b2a4663064796596a4a8f85b262b1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-08 08:06:27 +0000

    Removed invalid tag 3.4.9

--- tags/groupoffice-com-3.4.9/modules/currencies/language/ru.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Uncomment this line in new translations!
-require($GO_LANGUAGE->get_fallback_language_file('currencies'));
-$lang['currencies']['name']='Валюты';
-$lang['currencies']['description']='Валюты';
-$lang['currencies']['currency']='Валюта';
-$lang['currencies']['currencies']='Валюты';
--- tags/groupoffice-com-3.4.9/modules/serverclient/language/fr.js
+++
@@ -1,6 +0,0 @@
-/**
- * Copy everything below for translations
- */
-
-GO.serverclient.lang.mailboxes='Comptes de messagerie';
-GO.serverclient.lang.createMailbox='Créer un compte de messagerie pour le domaine';
