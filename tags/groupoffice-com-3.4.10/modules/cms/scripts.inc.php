34827a14a7e93017f603bc6e1013258bd21819b9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-15 12:09:31 +0000

    Removed invalid tag 3.4.10

--- tags/groupoffice-com-3.4.10/modules/cms/scripts.inc.php
+++
@@ -1,3 +0,0 @@
-<script   src="<?php echo $GO_CONFIG->host; ?>javascript/tiny_mce/tiny_mce_gzip.js" type="text/javascript"></script>
-<script type="text/javascript">tinyMCE_GZ.init({plugins : 'style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,pagebreak,template',themes : 'simple,advanced',languages : 'en',disk_cache : true,debug : true});</script>
-<script	src="<?php echo $GO_CONFIG->host; ?>javascript/form/TinyMCE.js" type="text/javascript"></script>
--- tags/groupoffice-com-3.4.10/modules/comments/language/de.js
+++
@@ -1,4 +0,0 @@
-GO.comments.lang.comment='Kommentar';
-GO.comments.lang.comments='Kommentare';
-GO.comments.lang.fiveLatestComments='Letzte Kommentare';
-GO.comments.lang.browseComments='Kommentare anschauen';
