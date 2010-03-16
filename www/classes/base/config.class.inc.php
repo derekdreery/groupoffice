44b885a449b6602d059b8796814f483d12838c1c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-16 13:09:05 +0000

    Only create accounts for other users as superadmin

--- www/classes/base/config.class.inc.php
+++ www/classes/base/config.class.inc.php
@@ -629,7 +629,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.4.10';
+	var $version = '3.4.11';
 
 
 	/* The permissions mode to use when creating files
@@ -654,7 +654,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20100315';
+	var $mtime = '20100316';
 
 	#group configuration
 	/**
