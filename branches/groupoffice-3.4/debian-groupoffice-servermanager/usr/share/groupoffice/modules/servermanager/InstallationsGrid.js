1dc99c0dcfdc01df8a2648e1e869b45c4271c3d0
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-16 15:35:39 +0000

    Show if installation is disabled

--- branches/groupoffice-3.4/debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/InstallationsGrid.js
+++ branches/groupoffice-3.4/debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/InstallationsGrid.js
@@ -30,7 +30,7 @@ GO.servermanager.InstallationsGrid = function(config){
 	    root: 'results',
 	    id: 'id',
 	    totalProperty:'total',
-	    fields: ['id','status','name','webmaster_email','title','default_country','language','default_timezone','default_currency','default_date_format','default_date_separator','default_thousands_separator','theme','allow_themes','allow_password_change','allow_registration','allow_duplicate_email','auto_activate_accounts','notify_admin_of_registration','registration_fields','required_registration_fields','register_modules_read','register_modules_write','register_user_groups','register_visible_user_groups','max_users','ctime','mtime','count_users', 'install_time','lastlogin','total_logins','database_usage','file_storage_usage','mailbox_usage','total_usage', 'comment', 'ctime', 'mailbox_domains', 'features', 'billing', 'professional'],
+	    fields: ['id','status','name','webmaster_email','title','default_country','language','default_timezone','default_currency','default_date_format','default_date_separator','default_thousands_separator','theme','allow_themes','allow_password_change','allow_registration','allow_duplicate_email','auto_activate_accounts','notify_admin_of_registration','registration_fields','required_registration_fields','register_modules_read','register_modules_write','register_user_groups','register_visible_user_groups','max_users','ctime','mtime','count_users', 'install_time','lastlogin','total_logins','database_usage','file_storage_usage','mailbox_usage','total_usage', 'comment', 'ctime', 'mailbox_domains', 'features', 'billing', 'professional', 'enabled'],
 	    remoteSort: true
 	});
 	
@@ -116,7 +116,12 @@ GO.servermanager.InstallationsGrid = function(config){
 	config.view=new Ext.grid.GridView({
 		autoFill: true,
 		forceFit: true,
-		emptyText: GO.lang['strNoItems']		
+		emptyText: GO.lang['strNoItems'],
+		getRowClass : function(record, rowIndex, p, store){
+			if(GO.util.empty(record.data.enabled)){
+				return 'installation-disabled';
+			}
+		}
 	});
 	config.sm=new Ext.grid.RowSelectionModel();
 	config.loadMask=true;
