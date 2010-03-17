5106154293d42a229aee9e8f6469fc5620e169cb
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-17 14:42:43 +0000

    Only set reminders for the future

--- branches/groupoffice-3.4/www/modules/calendar/classes/calendar.class.inc.php
+++ branches/groupoffice-3.4/www/modules/calendar/classes/calendar.class.inc.php
@@ -1162,7 +1162,7 @@ class calendar extends db
 
 			$this->cache_event($event['id']);
 
-			if(!empty($event['reminder']))
+			if(!empty($event['reminder']) && $event['reminder']>time())
 			{
 				global $GO_CONFIG;
 
@@ -1304,7 +1304,7 @@ class calendar extends db
 				$rm->delete_reminder($existing_reminder['id']);
 			}
 
-			if(!empty($event['reminder']))
+			if(!empty($event['reminder'])  && $event['reminder']>time())
 			{
 				if(!$calendar)
 				{
