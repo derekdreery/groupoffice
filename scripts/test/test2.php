06d8f56972600e68437cd01ffebdf56ed3e28fa7
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-25 08:49:23 +0000

    prepared trial system for alternate configs

--- scripts/test/test2.php
+++ scripts/test/test2.php
@@ -1,22 +1,10 @@
+#!/usr/bin/php
 <?php
-if(isset($argv[1]))
-{
-	define('CONFIG_FILE', $argv[1]);
-}
+chdir(dirname(__FILE__));
+require('../../www/cli-functions.inc.php');
 
+$args = parse_cli_args($argv);
 
 require('../../www/Group-Office.php');
 
-
-$rfc822 = '';
-
-
-require_once($GO_CONFIG->class_path.'mail/GoSwift.class.inc.php');
-require_once($GO_MODULES->modules['email']['class_path']."email.class.inc.php");
-require_once($GO_MODULES->modules['sync']['class_path']."settings.class.inc.php");
-$email = new email();
-$sync_settings = new sync_settings();
-$settings = $sync_settings->get_settings(1);
-$account = $email->get_account($settings['account_id']);
-$goswift = new GoSwiftImport($rfc822,true, $account['default_alias_id']);
-$goswift->sendmail();
+var_dump($args);
