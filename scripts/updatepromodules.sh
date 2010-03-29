7acb202578142169e99bce060cf3c894e8e6b4e0
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-29 08:06:20 +0000

    Created tag 3.4.14

--- scripts/updatepromodules.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd `dirname "$0"`
-cd ../www/modules
-
-svn update sync/ billing/ legalhorizons/ professional/ cms/ projects/ webshop/ gota/ customfields/ dealermanager/ licenses hoursapproval timeregistration mailings tickets z-push
