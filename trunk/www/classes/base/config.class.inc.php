e6235a1b7933b545c21c8b5dd71511528c05227e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-29 08:04:01 +0000

    Merging r4498 through r4507

--- trunk/www/classes/base/config.class.inc.php
+++ trunk/www/classes/base/config.class.inc.php
@@ -629,7 +629,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.4.13';
+	var $version = '3.4.14';
 
 
 	/* The permissions mode to use when creating files
@@ -654,7 +654,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20100326';
+	var $mtime = '20100329';
 
 	#group configuration
 	/**
