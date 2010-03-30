8164ccf6a0ebdcba4469d25532ce67f7c3aa2226
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-30 16:15:10 +0000

    removed cms

--- trunk/scripts/updatepromodules.sh
+++ trunk/scripts/updatepromodules.sh
@@ -2,4 +2,4 @@
 cd `dirname "$0"`
 cd ../www/modules
 
-svn update sync/ billing/ legalhorizons/ professional/ cms/ projects/ webshop/ gota/ customfields/ dealermanager/ licenses hoursapproval timeregistration mailings tickets z-push
+svn update sync/ billing/ legalhorizons/ professional/ projects/ webshop/ gota/ customfields/ dealermanager/ licenses hoursapproval timeregistration mailings tickets z-push
