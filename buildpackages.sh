ae1f2a1d6efb88565bd9695b1334fbd00882d851
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-03-30 08:32:41 +0000

    script to build all

--- buildpackages.sh
+++ buildpackages.sh
@@ -2,4 +2,4 @@
 ./debian-groupoffice-servermanager/builddeb.sh send
 ./debian-groupoffice-mailserver/builddeb.sh send
 ./debian-groupoffice-pro/builddeb.sh send
-./debian-groupoffice-com/builddeb.sh real send
+./debian-groupoffice/builddeb.sh real send
