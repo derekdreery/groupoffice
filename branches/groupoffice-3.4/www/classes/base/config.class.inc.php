600067e46edf8090cd8393c37b1ef9094232b019
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-04-08 11:24:53 +0000

    Safer file functions

--- branches/groupoffice-3.4/www/classes/base/config.class.inc.php
+++ branches/groupoffice-3.4/www/classes/base/config.class.inc.php
@@ -629,7 +629,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.4.15';
+	var $version = '3.4.16';
 
 
 	/* The permissions mode to use when creating files
@@ -654,7 +654,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20100406';
+	var $mtime = '20100407';
 
 	#group configuration
 	/**
