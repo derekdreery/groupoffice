8b63ce3ff07f965a2dffeca12693506c64348b32
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-04-14 09:07:49 +0000

    broken text_to_html function

--- www/classes/base/config.class.inc.php
+++ www/classes/base/config.class.inc.php
@@ -635,7 +635,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.4.19';
+	var $version = '3.4.20';
 
 
 	/* The permissions mode to use when creating files
@@ -660,7 +660,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20100413';
+	var $mtime = '20100414';
 
 	#group configuration
 	/**
