3b51ffd22b1ddea47f5290efdd6542dbfcf04123
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-04-14 09:07:49 +0000

    broken text_to_html function

--- branches/groupoffice-3.4/www/classes/String.class.inc.php
+++ branches/groupoffice-3.4/www/classes/String.class.inc.php
@@ -711,13 +711,17 @@ class String {
 	public static function text_to_html($text, $convert_links=true) {
 		global $GO_CONFIG, $GO_MODULES;
 
+		//replace repeating spaces with &nbsp;		
+		$text = htmlspecialchars($text, ENT_COMPAT, 'UTF-8');
+		$text = str_replace('  ', '&nbsp;&nbsp;', $text);
+
 		if($convert_links)
 		{
 			$text = preg_replace("/\b(https?:\/\/[\pL0-9\.&\-\/@#;`~=%?:_\+,]+)\b/ui", '{lt}a href={quot}$1{quot} target={quot}_blank{quot} class={quot}normal-link{quot}{gt}$1{lt}/a{gt}', $text."\n");
 			$text = preg_replace("/\b([\pL0-9\._\-]+@[\pL0-9\.\-_]+\.[a-z]{2,4})(\s)/ui", "{lt}a class={quot}normal-link{quot} href={quot}mailto:$1{quot}{gt}$1{lt}/a{gt}$2", $text);
 		}
 
-		$text = htmlspecialchars($text, ENT_COMPAT, 'UTF-8');
+		
 		$text = nl2br(trim($text));
 		//$text = str_replace("\r", "", $text);
 		//$text = str_replace("\n", "", $text);
@@ -728,11 +732,6 @@ class String {
 		$text = str_replace("{quot}", '"', $text);
 		$text = str_replace("{lt}", "<", $text);
 		$text = str_replace("{gt}", ">", $text);
-
-
-		//replace repeating spaces with &nbsp;
-		$text = str_replace(' ', '&nbsp;', $text);
-
 
 		return ($text);
 	}
