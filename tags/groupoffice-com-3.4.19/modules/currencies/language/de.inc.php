e7af2db2565f61ec5574399f8e6b13b562523a91
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-04-14 07:29:17 +0000

    Removed invalid tag 3.4.19

--- tags/groupoffice-com-3.4.19/modules/currencies/language/de.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Uncomment this line in new translations!
-require($GO_LANGUAGE->get_fallback_language_file('currencies'));
-$lang['currencies']['name']='Waehrungen';
-$lang['currencies']['description']='Put a description here';
-$lang['currencies']['currency']='Währung';
-$lang['currencies']['currencies']='Währungen';
