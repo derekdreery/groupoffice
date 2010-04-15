a64b4e8956e1daefb394ef2354606e0804a59ecc
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-04-15 18:22:59 +0000

    Improved link type filer grid

--- www/javascript/links/LinkTypeFilterPanel.js
+++ www/javascript/links/LinkTypeFilterPanel.js
@@ -25,7 +25,8 @@ GO.LinkTypeFilterPanel = function(config)
 		store: new Ext.data.JsonStore({
 			root: 'results',
 			data: {"results":GO.linkTypes},
-			fields: ['id','name', 'checked']
+			fields: ['id','name', 'checked'],
+			id:'id'
 		}),
 		columns: [
 				checkColumn,
@@ -35,7 +36,13 @@ GO.LinkTypeFilterPanel = function(config)
 				}				
 			],
 		plugins: [checkColumn],
-		autoExpandColumn:1		
+		autoExpandColumn:1,
+		listeners:{
+			scope:this,
+			delayedrowselect:function(grid, rowIndex, r){
+				this.applyFilter(r.id);
+			}
+		}
 	});
 	
 	
@@ -43,21 +50,7 @@ GO.LinkTypeFilterPanel = function(config)
 	var applyButton = new Ext.Button({
 		text:GO.lang.cmdApply,
 		handler:function(){
-			
-			var types = [];
-			
-			for (var i = 0; i < this.filterGrid.store.data.items.length;  i++)
-			{
-				var checked = this.filterGrid.store.data.items[i].get('checked');
-				if(checked=="1")
-				{
-					types.push(this.filterGrid.store.data.items[i].get('id'));	
-				}				
-			}
-			
-			this.fireEvent('change', this, types);
-			
-			this.filterGrid.store.commitChanges();			
+			this.applyFilter();
 		},
 		scope: this
 	});
@@ -78,5 +71,36 @@ GO.LinkTypeFilterPanel = function(config)
 	this.addEvents({change : true});
 }
 
-Ext.extend(GO.LinkTypeFilterPanel, Ext.Panel);
+Ext.extend(GO.LinkTypeFilterPanel, Ext.Panel,{
+	applyFilter : function(type_id){
+
+		var types = [], checked, current_type_id;
+
+		for (var i = 0; i < this.filterGrid.store.data.items.length;  i++)
+		{
+			current_type_id = this.filterGrid.store.data.items[i].get('id');
+			if(type_id && type_id != current_type_id){
+				checked=false;
+				this.filterGrid.store.data.items[i].set('checked', "0");
+			}else
+			{
+				if(type_id ==current_type_id){
+					checked="1";
+					this.filterGrid.store.data.items[i].set('checked', "1");
+				}else
+				{
+					checked = this.filterGrid.store.data.items[i].get('checked');
+				}
+			}
+			if(checked=="1")
+			{
+				types.push(this.filterGrid.store.data.items[i].get('id'));
+			}
+		}
+
+		this.fireEvent('change', this, types);
+
+		this.filterGrid.store.commitChanges();
+	}
+});
 
