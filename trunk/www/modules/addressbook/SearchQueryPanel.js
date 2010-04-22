b7a8dae45f976f30c4ab4f70235b63c27b28856e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-04-22 15:24:29 +0000

    License headers

--- trunk/www/modules/addressbook/SearchQueryPanel.js
+++ trunk/www/modules/addressbook/SearchQueryPanel.js
@@ -1,3 +1,16 @@
+/**
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @version $Id$
+ * @copyright Copyright Intermesh
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
 GO.addressbook.SearchQueryPanel = function(config)
 	{
 		if(!config){
