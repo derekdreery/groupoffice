b12d93a98e42f5fa2d76bdb576325e3a65694ac2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-04-28 16:59:52 +0000

    remove pass

--- scripts/imaptestold.php
+++ scripts/imaptestold.php
@@ -3,8 +3,7 @@ require('../www/Group-Office.php');
 require($GO_CONFIG->class_path.'mail/imap.class.inc');
 
 $imap = new imap();
-//$ret = $imap->open('mail.imfoss.nl','IMAP', 143, 'test@intermesh.nl', 'test');
-$ret = $imap->open('imap.domeneshop.no', 'IMAP', 143, 'nri3', '7MQPHvwz');
+$ret = $imap->open('mail.imfoss.nl','IMAP', 143, 'test@intermesh.nl', 'test');
 $imap->sort();
 
 $uids = $imap->get_message_uids(0, 30);
