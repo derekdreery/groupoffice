d273a1852e4cf27017a8c6bd097dbb529eade707
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-04-28 08:15:16 +0000

    Add company_id field

--- www/modules/addressbook/SelectContact.js
+++ www/modules/addressbook/SelectContact.js
@@ -29,7 +29,7 @@ GO.addressbook.SelectContact = function(config){
 	    root: 'results',
 	    id: 'id',
 	    totalProperty:'total',	    
-        fields: ['id', 'cf', 'name', 'salutation', 'email', 'first_name', 'middle_name','last_name', 'home_phone', 'work_phone', 'cellular'],
+        fields: ['id', 'cf', 'name', 'salutation', 'email', 'first_name', 'middle_name','last_name', 'home_phone', 'work_phone', 'cellular', 'company_id'],
 	    remoteSort: true
 	});
 	
