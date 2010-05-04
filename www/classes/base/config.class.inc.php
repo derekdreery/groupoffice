a205c897fdd7912c079312f50946778cdcd37dc8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-05-04 12:49:52 +0000

    3.4.23

--- www/classes/base/config.class.inc.php
+++ www/classes/base/config.class.inc.php
@@ -635,7 +635,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.4.22';
+	var $version = '3.4.23';
 
 
 	/* The permissions mode to use when creating files
@@ -660,7 +660,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20100426';
+	var $mtime = '20100504';
 
 	#group configuration
 	/**
