22066b82b24766cf027474434255a1c9334b9a43
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-05-13 13:18:03 +0000

    old stufff

--- branches/extjs3/modules/serverclient/language/de.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Uncomment this line in new translations!
-require($GO_LANGUAGE->get_fallback_language_file('serverclient'));
-
-$lang['serverclient']['name']='Postfix-Client';
-$lang['serverclient']['description']='Modul zur automatischen Mailbox-Erstellung innerhalb einer GroupOffice-Postfix-Serverumgebung';
-$lang['serverclient']['connect_error']='Konnte keine Verbindung mit %s herstellen';
