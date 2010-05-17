8febbec724333b20d1b62b018efcf3788e21bc52
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-05-17 12:00:27 +0000

    Added cryptastic class

--- trunk/scripts/test/test2.php
+++ trunk/scripts/test/test2.php
@@ -1,6 +1,11 @@
 #!/usr/bin/php
 <?php
-echo crypt('test');
-
+require('../../www/classes/cryptastic.class.inc.php');
+$c = new cryptastic();
+$enc = $c->encrypt('Secret message', 'mysecretkey', true);
+echo $c->decrypt($enc, 'mysecretkey', true);
 echo "\n";
+
+
+
 
