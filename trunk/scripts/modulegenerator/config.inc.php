aec24ebbacc075b3f91f1048b735842800cdf1b0
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-05-17 15:26:59 +0000

    IP blacklist module

--- trunk/scripts/modulegenerator/config.inc.php
+++ trunk/scripts/modulegenerator/config.inc.php
@@ -1,10 +1,10 @@
 <?php
 require('../../www/Group-Office.php');
 //name of the module. No spaces or strange characters.
-$module = 'filesearch';
+$module = 'blacklist';
 
 //Short name of the module. The prefix of the database tables.
-$prefix = 'fs';
+$prefix = 'bl';
 
 $tables=array();
 //Tables to create an interface for
@@ -19,17 +19,17 @@ $tables=array();
 
 $westpanel = array(
 	'template'=>'GridPanel.tpl', //The template to use for the grid. This is the only option at the moment
-	'name'=>'fs_docbundles',  //Name of the table
-	'friendly_single'=>'docbundle', //Name for a single item in this table. Must be lower case and alphanummeric
-	'friendly_multiple'=>'docbundles',//Name for a multiple items in this table. Must be lower case and alphanummeric
-	'authenticate'=>true,//Secure these items with authentication? If true then acl_read and acl_write columns must be defined in the table
+	'name'=>'bl_ips',  //Name of the table
+	'friendly_single'=>'ip', //Name for a single item in this table. Must be lower case and alphanummeric
+	'friendly_multiple'=>'ips',//Name for a multiple items in this table. Must be lower case and alphanummeric
+	'authenticate'=>false,//Secure these items with authentication? If true then acl_read and acl_write columns must be defined in the table
 	'paging'=>true, //Use pagination in the grid?
 	'autoload'=>false, //Automatically load this table with data after rendering?
 	'files'=>false, //Can files be uploaded to these items?
-	'link_type'=>16
+	'link_type'=>false
 	);
 
 $tables[] = $westpanel;
 
-//$main_template='SimpleMainPanel.tpl'; //The template for MainPanel.js
+$main_template='SimpleMainPanel.tpl'; //The template for MainPanel.js
 ?>
---
+++ trunk/www/modules/blacklist/language/en.js
@@ -0,0 +1,8 @@
+Ext.namespace('GO.blacklist');
+GO.blacklist.lang={};
+GO.blacklist.lang.blacklist='IP blacklist';
+
+/* table: bl_ips */
+GO.blacklist.lang.ip="IP address";
+GO.blacklist.lang.ips="IP addresses";
+GO.blacklist.lang.count="Count";
