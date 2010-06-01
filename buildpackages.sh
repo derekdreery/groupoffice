d3a7fc67b24926f9135f76eb375baab747c5d6c9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-06-01 15:59:38 +0000

    Update changelogs automatically

--- buildpackages.sh
+++ buildpackages.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+php ./createchangelogs.php
+
+svn commit -m 'Updated changelogs'
+
 ./debian-groupoffice-servermanager/builddeb.sh send
 ./debian-groupoffice-mailserver/builddeb.sh send
 ./debian-groupoffice-pro/builddeb.sh send
