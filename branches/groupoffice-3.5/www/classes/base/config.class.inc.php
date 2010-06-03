673f2df4672d83a5d272bbf8681e4639beb8acb1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-06-03 13:01:58 +0000

    Handle client access for domains properly

--- branches/groupoffice-3.5/www/classes/base/config.class.inc.php
+++ branches/groupoffice-3.5/www/classes/base/config.class.inc.php
@@ -643,7 +643,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.5.5';
+	var $version = '3.5.6';
 
 
 	/* The permissions mode to use when creating files
@@ -666,7 +666,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20100602';
+	var $mtime = '20100603';
 
 	#group configuration
 	/**
