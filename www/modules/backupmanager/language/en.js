e22cf7c086a574315ef87b8f330ad1e6d7cce179
sjmeut <sjmeut@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-06-04 10:35:49 +0000

    New module Backup Manager and cronjob added to the trunk.

---
+++ www/modules/backupmanager/language/en.js
@@ -0,0 +1,17 @@
+Ext.namespace('GO.backupmanager');
+GO.backupmanager.lang={};
+
+GO.backupmanager.lang.backupmanager='Backup Manager';
+GO.backupmanager.lang.rmachine='Remote machine';
+GO.backupmanager.lang.rport='Port';
+GO.backupmanager.lang.rtarget='Target folder';
+GO.backupmanager.lang.sources='Source folders';
+GO.backupmanager.lang.rotations='Rotations';
+GO.backupmanager.lang.quiet='Quiet';
+GO.backupmanager.lang.emailaddresses='Email addresses';
+GO.backupmanager.lang.emailsubject='Email subject';
+GO.backupmanager.lang.rhomedir='Remote homedir';
+GO.backupmanager.lang.rpassword='Wachtwoord';
+GO.backupmanager.lang.publish='Publish';
+GO.backupmanager.lang.publishkey='Publish public key';
+GO.backupmanager.lang.publishSuccess='Public key has succesfully been uploaded.';
