c561cf4558a2da5c1e58b10529bf7a90c2711475
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-06-07 07:41:47 +0000

    Updated Spanish translation

--- branches/groupoffice-3.5/www/modules/addressbook/language/es.inc.php
+++ branches/groupoffice-3.5/www/modules/addressbook/language/es.inc.php
@@ -2,15 +2,15 @@
 //Uncomment this line in new translations!
 require($GO_LANGUAGE->get_fallback_language_file('addressbook'));
 $lang['addressbook']['name'] = 'Contactos';
-$lang['addressbook']['description'] = 'Módulo para gestionar todos los contactos.';
+$lang['addressbook']['description'] = 'Módulo para gestionarlibretas de contactos.';
 
 
 
-$lang['addressbook']['allAddressbooks'] = 'Todas las secciones';
-$lang['common']['addressbookAlreadyExists'] = 'La partida que está intentando crear ya existe';
-$lang['addressbook']['notIncluded'] = 'No de importación';
+$lang['addressbook']['allAddressbooks'] = 'Todos las libretas';
+$lang['common']['addressbookAlreadyExists'] = 'La libreta que está intentando crear ya existe';
+$lang['addressbook']['notIncluded'] = 'No importar';
 
-$lang['addressbook']['comment'] = 'Notes';
+$lang['addressbook']['comment'] = 'Comentarios';
 $lang['addressbook']['bankNo'] = 'Número de cuenta bancaria'; 
 $lang['addressbook']['vatNo'] = 'IVA';
 $lang['addressbook']['contactsGroup'] = 'Grupo';
@@ -20,10 +20,17 @@ $lang['link_type'][3]=$lang['addressbook']['company'] = 'Empresa';
 
 $lang['addressbook']['customers'] = 'Clientes';
 $lang['addressbook']['suppliers'] = 'Proveedores';
-$lang['addressbook']['prospects'] = 'Los clientes potenciales';
+$lang['addressbook']['prospects'] = 'Potenciales clientes';
 
 
-$lang['addressbook']['contacts'] = 'Contacto';
-$lang['addressbook']['companies'] = 'Empresa';
+$lang['addressbook']['contacts'] = 'Contactos';
+$lang['addressbook']['companies'] = 'Empresas';
 
+$lang['addressbook']['newContactAdded']='Contacto nuevo agregado';
+$lang['addressbook']['newContactFromSite']='Un nuevo contacto fue agregado a través del formulario web';
+$lang['addressbook']['clickHereToView']='Haga click aca para ver el contacto';
+
+$lang['addressbook']['contactFromAddressbook']='Contacto de %s';
+$lang['addressbook']['companyFromAddressbook']='Empresa de %s';
+$lang['addressbook']['defaultSalutation']='Estimado/a [Sr./Sra.] {middle_name} {last_name}';
 ?>
