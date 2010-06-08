c14f50cab5ed2ecec75f5cc191e1a88c2b637c1e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-06-08 15:16:53 +0000

    Created tag 3.5.6

---
+++ action.php
@@ -0,0 +1,340 @@
+<?php
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id$
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
+require_once("Group-Office.php");
+$GO_SECURITY->authenticate();
+
+
+
+$response =array();
+try{
+
+	switch($_REQUEST['task'])
+	{
+
+		case 'update_level':
+
+			if(!$GO_SECURITY->has_permission_to_manage_acl($GO_SECURITY->user_id, $_POST['acl_id'])){
+				throw new AccessDeniedException();
+			}
+
+			if(!empty($_POST['user_id'])){
+
+				$acl = $GO_SECURITY->get_acl($_POST['acl_id']);
+
+				if($_POST['user_id']==$acl['user_id'] || $_POST['user_id']==$GO_SECURITY->user_id){
+					throw new Exception($lang['common']['dontChangeOwnersPermissions']);
+				}
+
+				$response['success']=$GO_SECURITY->add_user_to_acl($_POST['user_id'], $_POST['acl_id'], $_POST['level']);
+			}else
+			{
+				if($_POST['group_id']==$GO_CONFIG->group_root){
+					throw new Exception($lang['common']['dontChangeAdminsPermissions']);
+				}
+				$response['success']=$GO_SECURITY->add_group_to_acl($_POST['group_id'], $_POST['acl_id'], $_POST['level']);
+			}
+
+			break;
+
+		
+		case 'complete_profile':
+			
+			$user['id']=$GO_SECURITY->user_id;
+			$user['first_name']=$_POST['first_name'];
+			$user['middle_name']=$_POST['middle_name'];
+			$user['last_name']=$_POST['last_name'];
+			
+			$user['title'] = isset($_POST["title"]);
+			$user['initials'] = isset($_POST["initials"]);
+			$user['sex'] = isset($_POST["sex"]);
+			$user['birthday'] = isset($_POST['birthday']) ? Date::to_db_date($_POST['birthday']) : '';
+			$user['address'] = isset($_POST["address"]) ? $_POST["address"] : '';
+			$user['address_no'] = isset($_POST["address_no"]) ? $_POST["address_no"] : '';
+			$user['zip'] = isset($_POST["zip"]) ? $_POST["zip"] : '';
+			$user['city'] = isset($_POST["city"]) ? $_POST["city"] : '';
+			$user['state'] = isset($_POST["state"]) ? $_POST["state"] : '';
+			$user['country'] = isset($_POST["country"]) ? $_POST["country"] : '';
+
+			$user['email'] = isset($_POST["email"]) ? $_POST["email"] : '';
+			$user['home_phone'] = isset($_POST["home_phone"]) ? $_POST["home_phone"] : '';
+			$user['fax'] = isset($_POST["fax"]) ? $_POST["fax"] : '';
+			$user['cellular'] = isset($_POST["cellular"]) ? $_POST["cellular"] : '';
+			
+			$user['company'] = isset($_POST["company"]) ? $_POST["company"] : '';
+			$user['department'] = isset($_POST["department"]) ? $_POST["department"] : '';
+			$user['function'] = isset($_POST["function"]) ? $_POST["function"] : '';
+			$user['work_address'] = isset($_POST["work_address"]) ? $_POST["work_address"] : '';
+			$user['work_address_no'] = isset($_POST["work_address_no"]) ? $_POST["work_address_no"] : '';
+			$user['work_zip'] = isset($_POST["work_zip"]) ? $_POST["work_zip"] : '';
+			$user['work_city'] = isset($_POST["work_city"]) ? $_POST["work_city"] : '';
+			$user['work_state'] = isset($_POST["work_state"]) ? $_POST["work_state"] : '';
+			$user['work_country'] = isset($_POST["work_country"]) ? $_POST["work_country"] : '';
+			$user['work_phone'] = isset($_POST["work_phone"]) ? $_POST["work_phone"] : '';
+			$user['work_fax'] = isset($_POST["work_fax"]) ? $_POST["work_fax"] : '';
+			$user['homepage'] = isset($_POST["homepage"]) ? $_POST["homepage"] : '';
+
+			$GO_USERS->update_profile($user, true);
+			
+			$response['success']=true;
+			
+			break;
+		
+		case 'lost_password':
+
+			require($GO_LANGUAGE->get_base_language_file('lostpassword'));
+			
+			if($user = $GO_USERS->get_user_by_email(($_POST['email'])))
+			{
+				$url = $GO_CONFIG->full_url.'change_lost_password.php?username='.$user['username'].'&code1='.md5($user['password']).'&code2='.md5($user['lastlogin'].$user['registration_time']);
+
+				$salutation = $lang['common']['default_salutation'][$user['sex']];
+				if(!empty($user['middle_name']))
+					$salutation .= ' '.$user['middle_name'];
+				$salutation .= ' '.$user['last_name'];
+
+				$mail_body = sprintf($lang['lostpassword']['lost_password_body'],
+					$salutation,
+					$GO_CONFIG->title,
+					$user['username'],
+					$url);
+				
+				require_once($GO_CONFIG->class_path.'mail/GoSwift.class.inc.php');
+				$swift = new GoSwift($user['email'], $lang['lostpassword']['lost_password_subject']);
+				$swift->set_body($mail_body,'plain');
+				$swift->set_from($GO_CONFIG->webmaster_email, $GO_CONFIG->title);
+				$swift->sendmail();				
+				
+				$response['success']=true;
+				$response['feedback']=$lang['lostpassword']['lost_password_success'];
+			}else
+			{
+				$response['success']=false;
+				$response['feedback']=$lang['lostpassword']['lost_password_error'];
+			}
+			
+			break;
+		
+		case 'save_settings':			
+
+			$GO_EVENTS->fire_event('save_settings');
+
+			$response['success']=true;
+			break;
+			
+		case 'snooze_reminders':
+			
+			require($GO_CONFIG->class_path.'base/reminder.class.inc.php');
+			$rm = new reminder();
+			
+			$reminders = json_decode(($_POST['reminders']));
+			
+			$snooze_time = ($_POST['snooze_time']);
+			
+			foreach($reminders as $reminder_id)
+			{
+				$reminder['id']=$reminder_id;
+				$reminder['time']=time()+$snooze_time;
+				$rm->update_reminder($reminder);
+			}
+			$response['success']=true;			
+			break;
+		case 'dismiss_reminders':
+			
+			require($GO_CONFIG->class_path.'base/reminder.class.inc.php');
+			$rm = new reminder();
+			
+			$reminders = json_decode(($_POST['reminders']));
+			
+			foreach($reminders as $reminder_id)
+			{				
+				$reminder = $rm->get_reminder($reminder_id);
+				
+				//other modules can do something when a reminder is dismissed
+				//eg. The calendar module sets a next reminder for a recurring event.
+				$GO_EVENTS->fire_event('reminder_dismissed', array($reminder));
+				$rm->delete_reminder($reminder_id);
+			}
+			
+			$response['success']=true;
+			break;
+		
+		case 'login':
+
+			$response['success']=false;
+
+			$username = ($_POST['username']);
+			$password = ($_POST['password']);
+
+			if (!$username || !$password)
+			{
+				throw new Exception($lang['common']['missingField']);
+			}
+
+			//attempt login using security class inherited from index.php
+			//$params = isset( $auth_sources[$auth_source]) ?  $auth_sources[$auth_source] : false;
+			if (!$GO_AUTH->login($username, $password))
+			{
+				throw new Exception($lang['common']['badLogin']);
+			}
+			//login is correct final check if login registration was ok
+			if (!$GO_SECURITY->logged_in())
+			{
+				throw new Exception($lang['common']['saveError']);
+			}
+			if (isset($_POST['remind']))
+			{
+				SetCookie("GO_UN",$username,time()+3600*24*30,"/",'',!empty($_SERVER['HTTPS']),false);
+				SetCookie("GO_PW",$password,time()+3600*24*30,"/",'',!empty($_SERVER['HTTPS']),false);
+			}
+			
+			$fullscreen = isset($_POST['fullscreen']) ? '1' : '0';
+			
+			SetCookie("GO_FULLSCREEN",$fullscreen,time()+3600*24*30,"/",'',!empty($_SERVER['HTTPS']),false);
+				
+			$response['user_id']=$GO_SECURITY->user_id;
+			$response['name']=$_SESSION['GO_SESSION']['name'];
+			$response['email']=$_SESSION['GO_SESSION']['email'];
+			//$response['sid']=session_id();
+			
+			
+			//$response['fullscreen']=isset($_POST['fullscreen']);
+				
+			//$response['settings'] = $GO_CONFIG->get_client_settings();
+			
+			
+			
+			require_once($GO_CONFIG->class_path.'cache.class.inc.php');
+			$cache = new cache();
+			$cache->cleanup();
+			
+			$response['success']=true;
+
+			break;
+
+		case 'logout':
+			$GO_SECURITY->logout();
+
+			unset($_SESSION);
+			unset($_COOKIE);
+
+			break;
+
+		case 'link':
+
+			$fromLinks = json_decode(($_POST['fromLinks']),true);
+			$toLinks = json_decode(($_POST['toLinks']),true);
+			$from_folder_id=isset($_POST['folder_id']) ? ($_POST['folder_id']) : 0;
+			$to_folder_id=isset($_POST['to_folder_id']) ? ($_POST['to_folder_id']) : 0;
+
+			foreach($fromLinks as $fromLink)
+			{
+				foreach($toLinks as $toLink)
+				{
+					$GO_LINKS->add_link($fromLink['link_id'], $fromLink['link_type'], $toLink['link_id'], $toLink['link_type'],$from_folder_id, $to_folder_id, $_POST['description'], $_POST['description']);
+				}
+			}
+
+			$response['success']=true;
+				break;
+		case 'updatelink': 
+			
+			$link['id']=$_POST['link_id1'];
+			$link['link_id']=$_POST['link_id2'];
+			$link['link_type']=$_POST['link_type2'];
+			$link['description']=$_POST['description'];
+
+			$GO_LINKS->update_link($_POST['link_type1'],$link);
+			
+			$link['id']=$_POST['link_id2'];
+			$link['link_id']=$_POST['link_id1'];
+			$link['link_type']=$_POST['link_type1'];
+
+			$GO_LINKS->update_link($_POST['link_type2'],$link);
+			
+			$response['success']=true;
+		break;
+		
+		case 'move_links':
+			
+			$move_links = json_decode(($_POST['selections']), true);
+			$target = json_decode(($_POST['target']), true);
+			
+			$response['moved_links']=array();
+			
+			foreach($move_links as $link_and_type)
+			{
+				$link = explode(':', $link_and_type);
+				$link_type = $link[0];
+				$link_id = $link[1];
+				
+				if($link_type=='folder')
+				{
+					if($target['folder_id'] != $link_id && !$GO_LINKS->is_sub_folder($link_id, $target['folder_id']))
+					{
+						$folder['id']=$link_id;
+						$folder['parent_id']=$target['folder_id'];
+						$GO_LINKS->update_folder($folder);
+						
+						$response['moved_links'][]=$link_and_type;
+					}
+				}else
+				{
+					$update_link['link_type']=$link_type;
+					$update_link['link_id']=$link_id;
+					$update_link['id']=$target['link_id'];
+					$update_link['folder_id']=$target['folder_id'];
+					$GO_LINKS->update_link($target['link_type'], $update_link);
+					
+					$response['moved_links'][]=$link_and_type;
+				}
+			}
+			$response['success']=true;
+			
+			
+			break;
+
+		case 'save_link_folder':
+			$folder['id']=isset($_POST['folder_id']) ? ($_POST['folder_id']) : 0;
+			$folder['name']=$_POST['name'];
+			$folder['parent_id']=isset($_POST['parent_id']) ? ($_POST['parent_id']) : 0;
+			$folder['link_id']=isset($_POST['link_id']) ? ($_POST['link_id']) : 0;
+			$folder['link_type']=isset($_POST['link_type']) ? ($_POST['link_type']) : 0;
+
+
+			
+			if($folder['id']>0)
+			{
+				$GO_LINKS->update_folder($folder);
+			}else
+			{
+				if($GO_LINKS->get_folder_by_name($folder['name'],$folder['link_id'], $folder['link_type'], $folder['parent_id']))
+				{
+					throw new Exception($lang['common']['theFolderAlreadyExists']);
+				}
+
+				$response['folder_id']=$GO_LINKS->add_folder($folder);
+			}
+
+			$response['success']=true;
+
+			break;
+	}
+}
+catch(Exception $e)
+{
+	$response['feedback']=$e->getMessage();
+	$response['success']=false;
+}
+echo json_encode($response);
---
+++ language/common/es.inc.php
@@ -0,0 +1,165 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_base_language_file('common'));
+
+$lang['common']['htmldirection']= 'ltr';
+
+$lang['common']['errorsInForm'] = 'Hay errores en el formulario. Por favor, corrijalos y vuelva a intentar.';
+
+$lang['common']['loadingCore']= 'Cargando el sistema base';
+$lang['common']['loadingLogin'] = 'Cargando ventana de acceso';
+$lang['common']['renderInterface']='Renderizando Interfaz';
+$lang['common']['loadingModule'] = 'Cargando Módulo';
+
+$lang['common']['loggedInAs'] = 'Logueado como';
+$lang['common']['search']='Buscar';
+$lang['common']['settings']='Opciones';
+$lang['common']['adminMenu']='Menú de Administrador';
+$lang['common']['startMenu']='Menú de Inicio';
+$lang['common']['help']='Ayuda';
+$lang['common']['logout']='Salir';
+$lang['common']['badLogin'] = 'Nombre de usuario o contraseña incorrectos';
+$lang['common']['badPassword'] = 'La contraseña es incorrecta';
+
+$lang['common']['passwordMatchError']='Las contraseñas no coinciden';
+$lang['common']['accessDenied']='Acceso denegado';
+$lang['common']['saveError']='Error al guardar los datos';
+$lang['common']['deleteError']='Error al eliminar los datos';
+$lang['common']['selectError']='Error de lectura de datos';
+$lang['common']['missingField'] = 'Usted no ha completado todos los campos obligatorios.';
+
+$lang['common']['salutation']='Saludo';
+$lang['common']['firstName'] = 'Nombre';
+$lang['common']['lastName'] = 'Apellido';
+$lang['common']['middleName'] = 'Segundo Nombre';
+$lang['common']['sirMadam']['M'] = 'Señor';
+$lang['common']['sirMadam']['F'] = 'Señora';
+$lang['common']['initials'] = 'Iniciales';
+$lang['common']['sex'] = 'Sexo';
+$lang['common']['birthday'] = 'Fecha de nacimiento';
+$lang['common']['sexes']['M'] = 'Hombre';
+$lang['common']['sexes']['F'] = 'Mujer';
+$lang['common']['title'] = 'Título';
+$lang['common']['addressNo'] = 'Número';
+$lang['common']['workAddressNo'] = 'Número (trabajo)';
+$lang['common']['postAddress'] = 'Dirección';
+$lang['common']['postAddressNo'] = 'Número';
+$lang['common']['postCity'] = 'Ciudad';
+$lang['common']['postState'] = 'Provincia';
+$lang['common']['postCountry'] = 'País';
+$lang['common']['postZip'] = 'Código Postal';
+$lang['common']['visitAddress'] = 'Visit Dirección';
+$lang['common']['postAddressHead'] = 'Post Dirección';
+$lang['common']['name'] = 'Nombre';
+$lang['common']['user'] = 'Usuario';
+$lang['common']['username'] = 'Nombre de usuario';
+$lang['common']['password'] = 'Contraseña';
+$lang['common']['authcode'] = 'Código de autorización';
+$lang['common']['country'] = 'País';
+$lang['common']['state'] = 'Provincia';
+$lang['common']['city'] = 'Ciudad';
+$lang['common']['zip'] = 'Código Postal';
+$lang['common']['address'] = 'Dirección';
+$lang['common']['email'] = 'E-mail';
+$lang['common']['phone'] = 'Teléfono';
+$lang['common']['workphone'] = 'Teléfono (trabajo)';
+$lang['common']['cellular'] = 'Celular';
+$lang['common']['company'] = 'Compañía';
+$lang['common']['department'] = 'Oficina';
+$lang['common']['function'] = 'Función';
+$lang['common']['question'] = 'Pregunta';
+$lang['common']['answer'] = 'Respuesta';
+$lang['common']['fax'] = 'Fax';
+$lang['common']['workFax'] = 'Fax (trabajo)';
+$lang['common']['homepage'] = 'Sitio web';
+$lang['common']['workAddress'] = 'Dirección (trabajo)';
+$lang['common']['workZip'] = 'Código Postal (trabajo)';
+$lang['common']['workCountry'] = 'País (trabajo)';
+$lang['common']['workState'] = 'Provincia (trabajo)';
+$lang['common']['workCity'] = 'Ciudad (trabajo)';
+$lang['common']['today'] = 'Hoy';
+$lang['common']['tomorrow'] = 'Mañana';
+
+$lang['common']['SearchAll'] = 'Todos los campos';
+$lang['common']['total'] = 'total';
+$lang['common']['results'] = 'Resultados';
+
+
+$lang['common']['months'][1]='Enero';
+$lang['common']['months'][2]='Febrero';
+$lang['common']['months'][3]='Marzo';
+$lang['common']['months'][4]='Abril';
+$lang['common']['months'][5]='Mayo';
+$lang['common']['months'][6]='Junio';
+$lang['common']['months'][7]='Julio';
+$lang['common']['months'][8]='Agosto';
+$lang['common']['months'][9]='Septiembre';
+$lang['common']['months'][10]='Octubre';
+$lang['common']['months'][11]='Noviembre';
+$lang['common']['months'][12]='Diciembre';
+
+$lang['common']['short_days'][0]="Do";
+$lang['common']['short_days'][1]="Lu";
+$lang['common']['short_days'][2]="Ma";
+$lang['common']['short_days'][3]="Mi";
+$lang['common']['short_days'][4]="Ju";
+$lang['common']['short_days'][5]="Vi";
+$lang['common']['short_days'][6]="Sa";
+
+
+$lang['common']['full_days'][0] = "Domingo";
+$lang['common']['full_days'][1] = "Lunes";
+$lang['common']['full_days'][2] = "Martes";
+$lang['common']['full_days'][3] = "Miércoles";
+$lang['common']['full_days'][4] = "Jueves";
+$lang['common']['full_days'][5] = "Viernes";
+$lang['common']['full_days'][6] = "Sábado";
+
+$lang['common']['default']='Predeterminado';
+$lang['common']['description']='Descripción';
+$lang['common']['date']='Fecha';
+
+$lang['common']['default_salutation']['M']='Estimado señor';
+$lang['common']['default_salutation']['F']='Señora';
+
+
+$lang['common']['mins'] = 'Mins';
+$lang['common']['hour'] = 'Hora';
+$lang['common']['hours'] = 'Horas';
+$lang['common']['day'] = 'día';
+$lang['common']['days'] = 'días';
+$lang['common']['week'] = 'semana';
+$lang['common']['weeks'] = 'semanas';
+
+$lang['common']['group_everyone']='Todos';
+$lang['common']['group_admins']='Administradores';
+$lang['common']['group_internal']='Interno';
+
+$lang['common']['admin']='Administrador';
+
+$lang['common']['beginning']='Saludo';
+
+$lang['common']['max_emails_reached'] = 'Se alcanzó el número máximo de e-mails diarios en el servidor %s. Cantidad %s';
+$lang['common']['usage_stats']='Uso del disco por %s';
+$lang['common']['usage_text']='This Group-Office installation is using';
+
+$lang['common']['database']='Base de datos';
+$lang['common']['files']='Archivo';
+$lang['common']['email']='E-mail';
+$lang['common']['total']='Total';
+
+$lang['common']['lost_password_subject']='Nueva Contraseña';
+$lang['common']['lost_password_body']='%s,<br />
+<br />
+Ha solicitado una nueva contraseña para %s.<br />
+<br />
+Su nuevo ingreso es:<br />
+<br />
+Nombre de usuario: %s<br />
+Contraseña: %s';
+
+$lang['common']['lost_password_error']='No se puede encontrar su dirección de correo electrónico.';
+$lang['common']['lost_password_success']='Una nueva contraseña ha sido enviado a su e-mail.';
+
+$lang['common']['loadingModules']='Cargando modulos';
+$lang['common']['loadingModule'] = 'Cargando modulo';
