e5c24705974a046f8abe7dd8d5c731dd18e8d54c
sjmeut <sjmeut@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-06-29 14:38:16 +0000

    Users: add selectUser as customfield

---
+++ trunk/www/modules/users/CustomField.js
@@ -0,0 +1,17 @@
+GO.moduleManager.onModuleReady('customfields', function(){
+    GO.customfields.nonGridTypes.push('user');
+    GO.customfields.dataTypes.user={
+	label : GO.lang.strUser,
+	getFormField : function(customfield, config){
+	    return {
+		xtype: 'selectuser',
+		fieldLabel: customfield.name,
+		hiddenName:customfield.dataname,
+		forceSelection:true,
+		anchor:'-20',
+		valueField:'cf'
+	    }
+	}
+    }
+
+}, this);
