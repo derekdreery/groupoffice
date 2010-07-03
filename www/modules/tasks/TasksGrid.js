af86fe29363665f56a328d87b772a3d13f4d70ef
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-03 09:39:50 +0000

    new TasksObservable

--- www/modules/tasks/TasksGrid.js
+++ www/modules/tasks/TasksGrid.js
@@ -20,9 +20,8 @@ GO.tasks.TasksPanel = function(config)
   	
 			//dirty, but it works for updating all the grids
 			this.store.reload({
-				callback:function(){
-					GO.tasks.taskDialog.fireEvent('save', GO.tasks.taskDialog, record.data.id);
-					this.fireEvent('checked', this, record.data.id);
+				callback:function(){					
+					GO.tasks.tasksObservable.fireEvent('save', this, this.task_id, this.store);
 				},
 				scope:this
 			});
@@ -285,9 +284,8 @@ Ext.extend(GO.tasks.TasksPanel, GO.grid.GridPanel, {
 							alert(reponseParams.feedback);
 						}else
 						{
-							//dirty, but it works for updating other grids like on the summary
-							if(GO.tasks.taskDialog)
-								GO.tasks.taskDialog.fireEvent('save', GO.tasks.taskDialog, reponseParams.task_id);
+		
+							GO.tasks.tasksObservable.fireEvent('save', this, this.task_id, this.store);
 							
 									
 							this.store.reload();
