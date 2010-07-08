bc3afcb4205572de307d99f53f7096da2d337e7a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-08 14:26:36 +0000

    configure WP url

--- www/modules/wordpress/classes/wordpress.class.inc.php
+++ www/modules/wordpress/classes/wordpress.class.inc.php
@@ -66,22 +66,22 @@ class wordpress extends db{
 		return $this->next_record();
 	}
 
-	public function set_wp_user($id, $wp_user_id){
-		$r['id']=$id;
+	public function set_contact_wp_user($contact_id, $wp_user_id){
+		$r['contact_id']=$contact_id;
 		$r['wp_user_id']=$wp_user_id;
 
-		return $this->replace_row('gw_wp_users', $r);
+		return $this->replace_row('gw_contacts_wp_users', $r);
 	}
 
-	public function get_id_by_wp_user_id($wp_user_id){
-		$sql = "SELECT id FROM gw_wp_users WHERE wp_user_id=".intval($wp_user_id);
+	public function get_contact_id_by_wp_user_id($wp_user_id){
+		$sql = "SELECT contact_id FROM gw_contacts_wp_users WHERE wp_user_id=".intval($wp_user_id);
 		$this->query($sql);
 
 		$record = $this->next_record();
 		if(!$record)
 			return false;
 
-		return $record['id'];
+		return $record['contact_id'];
 	}
 
 }
