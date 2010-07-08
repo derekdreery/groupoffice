582f4f868c0dc6f3a9dec2bcf9e7e049e43376b2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-08 07:05:57 +0000

    Save settings

--- www/modules/wordpress/MainPanel.js
+++ www/modules/wordpress/MainPanel.js
@@ -1,8 +1,21 @@
 Ext.namespace('GO.wordpress');
 
-GO.moduleManager.addModule('wordpress', GO.panel.IFrameComponent, {
+GO.moduleManager.addModule('wordpress', Ext.Panel, {
 	title : 'Wordpress',
 	iconCls : 'go-tab-icon-forum',
-	url:GO.settings.modules.wordpress.url+'redirect.php',
-	border:false
+	layout:'fit',
+	items:[new GO.panel.IFrameComponent({
+			url:GO.settings.modules.wordpress.url+'redirect.php'
+	})],
+	border:false,
+	tbar:[{
+		iconCls:'btn-settings',
+		text:GO.lang.cmdSettings,
+		handler:function(){
+			if(!this.adminDialog){
+				this.adminDialog = new GO.wordpress.AdminDialog();
+			}
+			this.adminDialog.show();
+		}
+	}]
 });
