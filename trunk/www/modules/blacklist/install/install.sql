a1bd63db5b5e13780a0d778ef4c828932e65ae7d
sjmeut <sjmeut@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-12 13:06:00 +0000

    Blacklist: Captcha implemented

--- trunk/www/modules/blacklist/install/install.sql
+++ trunk/www/modules/blacklist/install/install.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `bl_ips` (
   `ip` varchar(15) NOT NULL,
   `mtime` int(11) NOT NULL,
   `count` int(11) NOT NULL,
-  PRIMARY KEY (`ip`),
+  `userid` int(11) NOT NULL,
+  PRIMARY KEY (`ip`,`userid`),
   KEY `count` (`count`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
