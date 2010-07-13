2e2396e4cff5c7b5f7bf594f709c37f2975ed111
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-13 15:45:15 +0000

    Confirmation e-mail

--- trunk/www/modules/wordpress/install/install.sql
+++ trunk/www/modules/wordpress/install/install.sql
@@ -1,9 +1,16 @@
 
-CREATE TABLE IF NOT EXISTS `gw_contacts_wp_users` (
+DROP TABLE IF EXISTS `wp_contacts_wp_users`;
+CREATE TABLE IF NOT EXISTS `wp_contacts_wp_users` (
   `contact_id` int(11) NOT NULL,
   `wp_user_id` int(11) NOT NULL,
   PRIMARY KEY (`contact_id`,`wp_user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_posts`
+--
 
 DROP TABLE IF EXISTS `wp_posts`;
 CREATE TABLE IF NOT EXISTS `wp_posts` (
@@ -14,16 +21,23 @@ CREATE TABLE IF NOT EXISTS `wp_posts` (
   `publish` tinyint(1) NOT NULL,
   `title` varchar(100) NOT NULL,
   `content` text NOT NULL,
+  `categories` varchar(255) NOT NULL,
   KEY `id` (`id`,`link_type`),
   KEY `updated` (`updated`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_posts_custom`
+--
 
 DROP TABLE IF EXISTS `wp_posts_custom`;
-CREATE TABLE `wp_posts_custom` (
-`id` INT NOT NULL ,
-`link_id` INT NOT NULL ,
-`key` VARCHAR( 100 ) NOT NULL ,
-`value` VARCHAR( 255 ) NOT NULL ,
-PRIMARY KEY ( `id` , `link_id` )
-) ENGINE = MYISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `wp_posts_custom` (
+  `id` int(11) NOT NULL,
+  `link_type` int(11) NOT NULL,
+  `key` varchar(100) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  KEY `id` (`id`,`link_type`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
