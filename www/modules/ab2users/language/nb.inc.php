d0336e49333982e8d5cec84382c758a82f7bc40a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-15 11:38:25 +0000

    theme images

--- www/modules/ab2users/language/nb.inc.php
+++ www/modules/ab2users/language/nb.inc.php
@@ -2,4 +2,4 @@
 //Uncomment this line in new translations!
 require($GO_LANGUAGE->get_fallback_language_file('ab2users'));
 $lang['ab2users']['name']='Addressbook2Users';
-$lang['cms']['description']='Legger til en hurtigknapp for å opprette en bruker fra en kontaktperson eller et firma';
+$lang['ab2users']['description']='Legger til en hurtigknapp for å opprette en bruker fra en kontaktperson eller et firma';
