0171219e8872d2984c1444f53cc77b1a581622e4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-16 11:14:49 +0000

    Created tag 3.5.11

---
+++ tags/groupoffice-com-3.5.11/modules/log/language/fr.inc.php
@@ -0,0 +1,6 @@
+<?php
+//Uncomment this line in new translations!
+//require($GO_LANGUAGE->get_fallback_language_file('log'));
+$lang['log']['name']='Log';
+$lang['log']['description']='Conserve une trace de tous les ajouts, mises à jour et suppressions';
+
