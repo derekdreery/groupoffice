0b1ef103e3d9705ea058365676b9ee9fb830ee7a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-19 07:00:21 +0000

    broken print button and stream blocking in gnupg

--- branches/groupoffice-3.5/www/classes/base/config.class.inc.php
+++ branches/groupoffice-3.5/www/classes/base/config.class.inc.php
@@ -643,7 +643,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.5.11';
+	var $version = '3.5.12';
 
 
 	/* The permissions mode to use when creating files
@@ -666,7 +666,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20100716-1';
+	var $mtime = '20100719';
 
 	#group configuration
 	/**
