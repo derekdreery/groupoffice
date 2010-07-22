4b27aec580cbbbc99153eb039ca354c3bee86c66
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-22 11:14:34 +0000

    Added events

--- www/modules/groups/action.php
+++ www/modules/groups/action.php
@@ -1,5 +1,6 @@
 <?php
-/** 
+
+/**
  * Copyright Intermesh
  * 
  * This file is part of Group-Office. You should have received a copy of the
@@ -11,7 +12,6 @@
  * @copyright Copyright Intermesh
  * @author Merijn Schering <mschering@intermesh.nl>
  */
-
 require_once("../../Group-Office.php");
 $GO_SECURITY->json_authenticate('groups');
 require_once($GO_LANGUAGE->get_language_file('groups'));
@@ -23,7 +23,7 @@ $action = isset($_REQUEST['action']) ? ($_REQUEST['action']) : null;
 /* case delete */
 $groups = isset($_REQUEST['groups']) ? json_decode(($_REQUEST['groups']), true) : null;
 
-/* case save_group*/
+/* case save_group */
 $name = isset($_REQUEST['name']) ? ($_REQUEST['name']) : null;
 
 /* case delete user from group */
@@ -35,40 +35,41 @@ $users = isset($_REQUEST['users']) ? json_decode(($_REQUEST['users']), true) : n
 
 $feedback = null;
 
-switch($action)
-{
-	case 'save_group':
-		$result['success'] = true;
-		$result['feedback'] = $feedback;
+try {
+	switch ($action) {
+		case 'save_group':
+			$response['success'] = true;
+			$response['feedback'] = $feedback;
 
-		if ($group_id < 1)
-		{
-			//insert
-			if ($GO_GROUPS->get_group_by_name($name))
-			{
-				$result['feedback'] = $lang['groups']['groupNameAlreadyExists'];
-				$result['success'] = false;
-			} else {
-				$new_group_id = $GO_GROUPS->add_group($GO_SECURITY->user_id, $name);
-				if (!$new_group_id)
-				{
-					$result['feedback'] = $lang['common']['saveError'];
-					$result['success'] = false;
+			if ($group_id < 1) {
+				//insert
+				if ($GO_GROUPS->get_group_by_name($name)) {
+					$response['feedback'] = $lang['groups']['groupNameAlreadyExists'];
+					$response['success'] = false;
 				} else {
-					$result['group_id'] = $new_group_id;
+					$new_group_id = $GO_GROUPS->add_group($GO_SECURITY->user_id, $name);
+					if (!$new_group_id) {
+						throw new DatabaseInsertException();
+					} else {
+						$response['group_id'] = $new_group_id;
+					}
+				}
+			} else {
+				// update
+				if (!$GO_GROUPS->update_group($group_id, $name)) {
+					throw new DatabaseUpdateException();
+				} else {
+					$response['group_id'] = $group_id;
 				}
 			}
-		} else {
-			// update
-			if(!$GO_GROUPS->update_group($group_id, $name))
-			{
-				$result['feedback'] = $lang['common']['saveError'];
-				$result['success'] = false;
-			} else {
-				$result['group_id'] = $group_id;
-			}
-		}
-		echo json_encode($result);
-		break;
+
+			$GO_EVENTS->fire_event('save_group', array($group_id, $name, &$response));
+
+
+			break;
+	}
+} catch (Exception $e) {
+	$response['feedback'] = $e->getMessage();
+	$response['success'] = false;
 }
-?>
+echo json_encode($response);
