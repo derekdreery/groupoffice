92e93af385458fc20915fe000db2c0cfe82b7bdf
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-27 08:24:46 +0000

    Created tag 3.5.14

--- www/modules/wordpress/install/updates.inc.php
+++
@@ -1,2 +0,0 @@
-<?php
-$updates[]="ALTER TABLE `wp_posts` ADD `categories` VARCHAR( 255 ) NOT NULL ";
--- www/modules/wordpress/language/nl.js
+++
@@ -1,3 +0,0 @@
-GO.wordpress.lang.wordpress='Website';
-GO.wordpress.lang.wordpressAdmin='Beheer';
-GO.wordpress.lang.websitePublish='Website publicatie';
