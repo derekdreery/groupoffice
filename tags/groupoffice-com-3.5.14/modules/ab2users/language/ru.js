cb56e4481810d2c29c0669b8a77465a504354643
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-27 08:24:46 +0000

    Created tag 3.5.14

---
+++ tags/groupoffice-com-3.5.14/modules/ab2users/language/ru.js
@@ -0,0 +1,9 @@
+/**
+ * Russian translation
+ * By Valery Yanchenko (utf-8 encoding)
+ * vajanchenko@hotmail.com
+ * 10 December 2008
+*/
+
+GO.ab2users.lang.createUser='Создать пользователя';
+GO.ab2users.lang.companyUserFirstName='Компания';
