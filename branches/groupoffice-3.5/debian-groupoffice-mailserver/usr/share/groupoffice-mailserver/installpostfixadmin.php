b3ae2319dcd67a044d605aff198e45122d053db0
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-29 12:40:24 +0000

    Don't bail out when adding a module fails

--- branches/groupoffice-3.5/debian-groupoffice-mailserver/usr/share/groupoffice-mailserver/installpostfixadmin.php
+++ branches/groupoffice-3.5/debian-groupoffice-mailserver/usr/share/groupoffice-mailserver/installpostfixadmin.php
@@ -3,14 +3,18 @@
 require('/etc/groupoffice/config.php');
 require($config['root_path'].'Group-Office.php');
 
+try{
+	if(!isset($GO_MODULES->modules['postfixadmin']))
+	{
+		$GO_MODULES->add_module('postfixadmin');
+	}
 
-if(!isset($GO_MODULES->modules['postfixadmin']))
-{
-	$GO_MODULES->add_module('postfixadmin');
+	if(!isset($GO_MODULES->modules['serverclient']))
+	{
+		$GO_MODULES->add_module('serverclient');
+	}
 }
-
-if(!isset($GO_MODULES->modules['serverclient']))
-{
-	$GO_MODULES->add_module('serverclient');
+catch(Exception $e){
+	echo 'ERROR: '.$e->getMessage();
 }
 
