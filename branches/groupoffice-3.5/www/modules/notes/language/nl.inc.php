8543252ebb0ee02021a6b038dd95c8806540d1be
sjmeut <sjmeut@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-07-30 11:21:23 +0000

    Note: language update

--- branches/groupoffice-3.5/www/modules/notes/language/nl.inc.php
+++ branches/groupoffice-3.5/www/modules/notes/language/nl.inc.php
@@ -4,18 +4,12 @@ require_once($GLOBALS['GO_LANGUAGE']->get_fallback_language_file('notes'));
 
 $lang['notes']['category']='Categorie';
 $lang['notes']['categories']='Categorieën';
-$lang['notes']['note']='Notitie';
+$lang['link_type'][4]=$lang['notes']['note']='Notitie';
 $lang['notes']['notes']='Notities';
 
 $lang['notes']['general'] = 'Algemeen';
 
-$lang['notes']['name']='Notes';
+$lang['notes']['name']='Notities';
 $lang['notes']['description']='Een module om notities te maken in verschillende categorieën.';
 
-$lang['notes']['category']='Category';
-$lang['notes']['categories']='Categories';
-$lang['link_type'][4]=$lang['notes']['note']='Note';
-$lang['notes']['notes']='Notes';
-
-
-$lang['notes']['general'] = 'General';
+$lang['notes']['incomplete_delete']='U heeft niet voldoende rechten om alle geselecteerde notities te verwijderen';
