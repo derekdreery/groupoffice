54d2a212802a088808ec2b36cb4a0dc270ccac24
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-08-06 09:52:45 +0000

    Use default props

--- trunk/www/modules/users/CustomField.js
+++ trunk/www/modules/users/CustomField.js
@@ -1,19 +1,25 @@
 GO.moduleManager.onModuleReady('customfields', function(){
     GO.customfields.nonGridTypes.push('user');
     GO.customfields.dataTypes.user={
-	label : GO.lang.strUser,
-	getFormField : function(customfield, config){
-	    return {
-		xtype: 'selectuser',
-		idValuePair:true,
-		startBlank:true,
-		fieldLabel: customfield.name,
-		hiddenName:customfield.dataname,
-		forceSelection:true,
-		anchor:'-20',
-		valueField:'cf'
-	    }
-	}
+		label : GO.lang.strUser,
+		getFormField : function(customfield, config){
+
+			var f = GO.customfields.dataTypes.text.getFormField(customfield, config);
+
+			delete f.name;
+
+			f=Ext.apply(f, {
+				xtype: 'selectuser',
+				idValuePair:true,
+				startBlank:true,
+				forceSelection:true,
+				hiddenName:customfield.dataname,
+				anchor:'-20',
+				valueField:'cf'
+			});
+
+			return f;
+		}
     }
 
 }, this);
