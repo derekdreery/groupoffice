e9ca25474eb7e4c2a51be990e2fb4e183a50ecb9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-08-10 09:59:25 +0000

    Removed invalid tag 3.5.19

--- tags/groupoffice-com-3.5.19/modules/serverclient/language/fr.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Uncomment this line in new translations!
-require($GO_LANGUAGE->get_fallback_language_file('serverclient'));
-
-$lang['serverclient']['name']='Serverclient';
-$lang['serverclient']['description']='Permet de créer automatiquement les boites de messagerie si Group-Office gère le serveur e-mail avec le module postfixadmin';
-$lang['serverclient']['connect_error']='Impossible de se connecter à %s';
