16b1269cb6e380a51891d8d1004bb456e6c52518
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-08-10 10:00:17 +0000

    Created tag 3.5.19

---
+++ tags/groupoffice-com-3.5.19/modules/serverclient/language/fr.js
@@ -0,0 +1,6 @@
+/**
+ * Copy everything below for translations
+ */
+
+GO.serverclient.lang.mailboxes='Comptes de messagerie';
+GO.serverclient.lang.createMailbox='Créer un compte de messagerie pour le domaine';
