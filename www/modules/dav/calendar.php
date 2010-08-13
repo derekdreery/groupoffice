539e4a13a69020ae56068452fc469a9eafc64618
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-08-13 14:26:20 +0000

    Corrected headers

--- www/modules/dav/calendar.php
+++ www/modules/dav/calendar.php
@@ -1,12 +1,16 @@
 <?php
-
-/*
-
-CalendarServer example
-
-This server features CalDAV and ACL support
-
-*/
+/**
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @version $Id$
+ * @copyright Copyright Intermesh
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
 
 // settings
 require('../../Group-Office.php');
