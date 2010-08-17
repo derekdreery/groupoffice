75e511ddf8c9b6a0914a34c78ee2d4b6158152a8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-08-17 14:13:32 +0000

    Tasklists work too

--- trunk/www/modules/dav/SabreDAV/install/install.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `dav_events`;
-CREATE TABLE IF NOT EXISTS `dav_events` (
-  `uuid` varchar(100) NOT NULL,
-  `data` text NOT NULL,
-  `mtime` int(11) NOT NULL,
-  PRIMARY KEY (`uuid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
