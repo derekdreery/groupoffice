0d068b65854d70dd79f4e18e3ea0d49e9c8e15ea
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-08-17 09:08:07 +0000

    Meer tekst

--- www/modules/wordpress/plugin/GroupOffice/GroupOffice.php
+++ www/modules/wordpress/plugin/GroupOffice/GroupOffice.php
@@ -358,7 +358,7 @@ function groupoffice_get_contact_form($post_extra_info=false){
 				$to = get_option('admin_email').',test@intermesh.nl';
 				$subject='Reactie op vacature '.$post_title;
 
-				$message='<a href="go:showContact('.$r['contact_id'].');">Bekijk gegevens van '.$current_user->first_name.' '.$current_user->last_name.' ('.$current_user->user_email.')</a>';
+				$message='<p>Dit is een geautomatiseerd bericht. Er heeft iemand op een vacature gereageerd. Klik op onderstaande link om de contactgegevens te bekijken.</p><a href="go:showContact('.$r['contact_id'].');">Bekijk gegevens van '.$current_user->first_name.' '.$current_user->last_name.' ('.$current_user->user_email.')</a>';
 				$headers="From: Keystaff (Recruity) <noreply@keystaff.nl>\n".
 					"Content-Type: text/html";
 
