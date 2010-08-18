ee65eb28065bf8bdacbb063a327a621fde3589e1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-08-18 13:25:24 +0000

    Created tag 3.5.23

---
+++ tags/groupoffice-com-3.5.23/modules/blacklist/language/sv.js
@@ -0,0 +1,10 @@
+GO.blacklist.lang.blacklist='Svartlistade IP-adresser';
+/**
+ * Copy everything below for translations
+ */
+
+
+/* table: bl_ips */
+GO.blacklist.lang.ip="IP-adress";
+GO.blacklist.lang.ips="IP-adresser";
+GO.blacklist.lang.count="Antal";
