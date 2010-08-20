ac998c64266519ed62d449af1ac781c28bfa131c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-08-20 14:06:10 +0000

    Created tag 3.5.24

--- scripts/test/test2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('../../www/Group-Office.php');
-
-$ns=UUID::v4();
-
-echo UUID::v5($ns, 1)."\n";
-echo UUID::v5($ns, 2)."\n";
-
--- www/modules/log/language/th.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Uncomment this line in new translations!
-require($GO_LANGUAGE->get_fallback_language_file('log'));
-$lang['log']['name']='Log';
-$lang['log']['description']='การเพิ่ม Log,การอัพเดทและการลบรายการ';//Logs all add, update and delete actions
-
