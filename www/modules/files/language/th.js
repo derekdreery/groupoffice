9394d0bf79e9fde753a2afa6852176408f852210
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-08-20 08:05:54 +0000

    updated thai translation

--- www/modules/files/language/th.js
+++ www/modules/files/language/th.js
@@ -18,15 +18,21 @@ GO.files.lang.startTransfer = 'เริ่มต้น';
 GO.files.lang.addFolder = 'เพิ่มโฟลเดอร์';
 GO.files.lang.errorOneItem = 'เลือกเพียงรายการเดียว';
 GO.files.lang.activateSharing = 'เปิดการใช้งานร่วมกัน';
-GO.files.lang.notifyChanges = 'แจ้งเมื่อมีการเปลียนแปลงโฟลเดอร์';//Notify me about changes in this folder
+GO.files.lang.notifyChanges = 'แจ้งเมื่อมีการเปลียนแปลงข้อมูลในโฟลเดอร์นี้';//Notify me about changes in this folder
 GO.files.lang.comments = 'คำอธิบาย';
-GO.files.lang.downloadGOTA = 'ดาวน์โหลดด้วย GOTA';//Download with GOTA
+GO.files.lang.downloadGOTA = 'เปิดไฟล์ด้วย GOTA';//Download with GOTA
 GO.files.lang.fileBrowser = 'ไฟล์ เบราเซอร์';
 GO.files.lang.thumbnails = 'รูปย่อ';
 GO.files.lang.saveFile='บันทึกไฟล์';
-GO.files.lang.confirmOverwrite='มี{filename}แล้ว.ต้องการบันทึกซ้ำหรือไม่  ?';//already exists, do you want to overwrite the file
+GO.files.lang.confirmOverwrite='มีไฟล์ชื่อ{filename}แล้ว.ต้องการบันทึกซ้ำหรือไม่  ?';//already exists, do you want to overwrite the file
 GO.files.lang.file='ไฟล์';
-GO.files.lang.emptyList='รายการว่าง';
-GO.files.lang.newFiles='New';
+GO.files.lang.emptyList='รีเซ็ตรายการ';
+GO.files.lang.newFiles='ไฟล์ใหม่';
 
 GO.files.lang.olderVersions='เวอร์ชั่นที่เก่ากว่า';
+
+GO.files.lang.uploadProperties='คุณสมบัติด้านล่างนี้จะใช้กับทุกไฟล์อัพโหลดที่เลือกตัวเลือก"แฟ้มขนาดเล็ก"';
+GO.files.lang.selectFiles= 'เลือกไฟล์';
+GO.files.lang.fitImage='แสดงภาพขนาดที่พอเหมาะ';
+GO.files.lang.normalSize='แสดงภาพขนาดปกติ';
+GO.files.lang.imageViewer='แสดงภาพ';
--- www/modules/notes/language/th.inc.php
+++ www/modules/notes/language/th.inc.php
@@ -2,15 +2,13 @@
 //Uncomment this line in new translations!
 require($GO_LANGUAGE->get_fallback_language_file('notes'));
 
-$modules['notes']['name']='โน๊ต';
-$modules['notes']['description']='สามารถแสดงรายละเอียดและข้อความ  . เมนูที่ใช้งานในรูปแบบส่วนตัวแต่ละบุคคล';
-$lang['notes']['note']='โน๊ต';
-$lang['notes']['name']='โน๊ต';
+$lang['notes']['note']='บันทึกย่อ';
+$lang['notes']['name']='บันทึกย่อ';
 $lang['notes']['description']='สามารถแสดงรายละเอียดและข้อความ  . เมนูที่ใช้งานในรูปแบบส่วนตัวแต่ละบุคคล';
 
 $lang['notes']['category']='หมวด';
 $lang['notes']['categories']='หมวดหมู่';
-$lang['link_type'][4]=$lang['notes']['note']='โน๊ต';
-$lang['notes']['notes']='โน๊ต';
+$lang['link_type'][4]=$lang['notes']['note']='บันทึกย่อ';
+$lang['notes']['notes']='บันทึกย่อ';
 
 $lang['notes']['general'] = 'ทั่วไป';
