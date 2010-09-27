505de4b2ede424006daabbe752f840f0a6e34c74
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-09-27 11:22:09 +0000

    Created tag 3.5.28

---
+++ tags/groupoffice-com-3.5.28/modules/blacklist/language/en.inc.php
@@ -0,0 +1,9 @@
+<?php
+//Uncomment this line in new translations!
+//require($GO_LANGUAGE->get_fallback_language_file('blacklist'));
+$lang['blacklist']['name']='IP blacklist';
+$lang['blacklist']['description']='A module that blocks IP addresses after 5 login failures in a row.';
+$lang['blacklist']['ip']='IP';
+$lang['blacklist']['ips']='IP addresses';
+
+$lang['blacklist']['blacklisted']='Your IP address %s is b being blocked because there were 5 consecutive login failures from this IP address. Contact the system administrator and supply your IP address to unblock it.';
