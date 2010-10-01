3d41f677443f8d8d99d09121c67afae9ba6024c8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-10-01 09:06:21 +0000

    Allow now lists in get_tasks query

--- branches/groupoffice-3.5/www/classes/base/config.class.inc.php
+++ branches/groupoffice-3.5/www/classes/base/config.class.inc.php
@@ -643,7 +643,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.5.28';
+	var $version = '3.5.29';
 
 
 	/* The permissions mode to use when creating files
@@ -666,7 +666,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20100927';
+	var $mtime = '20101001';
 
 	#group configuration
 	/**
