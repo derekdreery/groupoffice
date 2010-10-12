70bf5dd4f844f44434cc6665b4b8509400d23560
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-10-12 07:59:12 +0000

    Created tag 3.5.33

---
+++ tags/groupoffice-com-3.5.33/modules/files/language/fr.inc.php
@@ -0,0 +1,44 @@
+<?php
+//French Translation v1.0
+//Author : Lionel JULLIEN
+//Date : September, 04 2008
+
+// Update for 3.02-stable-10
+// Author : Cyril DUCHENOY
+// Date : July, 21 2009
+
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('files'));
+
+$lang['files']['name'] = 'Fichiers';
+$lang['files']['description'] = 'Module gestion des fichiers. Module pour partager des fichiers entre utilisateurs de Group-Office.';
+
+$lang['link_type'][6]='Fichier';
+
+$lang['files']['fileNotFound'] = 'Fichier introuvable';
+$lang['files']['folderExists'] = 'Le dossier existe déjà';
+$lang['files']['filenameExists'] = 'Le nom de fichier existe déjà';
+$lang['files']['uploadedSucces'] = 'Fichier envoyé avec succès';
+
+$lang['files']['ootextdoc']='Document texte Open-Office';
+$lang['files']['wordtextdoc']='Document Microsoft Word';
+
+$lang['files']['personal']='Personnel';
+$lang['files']['shared']='Partagé';
+$lang['files']['general']='Général';
+$lang['files']['folder_modified_subject']='Changer en dossier Group-Office';
+$lang['files']['folder_modified_body']='Vous avez demandé à être avisé lorsque des changements sont apportés à :
+
+%s
+
+Les changements suivants ont été effectués par %s :
+
+%s
+';
+
+$lang['files']['modified']='Modifié';
+$lang['files']['new']='Nouveau';
+$lang['files']['deleted']='Supprimé';
+
+$lang['files']['file']='Fichier';
+$lang['files']['folder']='Dossier';
