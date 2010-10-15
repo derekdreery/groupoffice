d6dd8cbec5125b72af9e22c59044e847b03f2fd3
sjmeut <sjmeut@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-10-15 14:38:45 +0000

    Emailportet: language update

--- www/modules/emailportlet/language/en.inc.php
+++ www/modules/emailportlet/language/en.inc.php
@@ -3,3 +3,4 @@
 //require($GO_LANGUAGE->get_fallback_language_file('emailportlet'));
 
 $lang['emailportlet']['name']='Email Portlet';
+$lang['emailportlet']['description']='Plugin to show email folders on the summary page.';
