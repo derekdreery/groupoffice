043b568290b2be9b7d490a278effa4e7043067a4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-10-19 07:33:27 +0000

    Check if part exists

--- www/modules/email/mimepart.php
+++ www/modules/email/mimepart.php
@@ -97,7 +97,10 @@ $part = Mail_mimeDecode::decode($params);
 
 $parts_arr = explode('.',$_REQUEST['part_imap_id']);
 for($i=0;$i<count($parts_arr);$i++) {
-	$part = $part->parts[$parts_arr[$i]];
+	if(isset($part->parts[$parts_arr[$i]]))
+		$part = $part->parts[$parts_arr[$i]];
+	else
+		die('Part not found');
 }
 
 
