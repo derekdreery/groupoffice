dc81a6bd6638aadc4206f37c075a8ae172811d6b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-10-19 07:19:21 +0000

    Created tag 3.5.36

--- www/modules/dav/language/en.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-//require($GO_LANGUAGE->get_fallback_language_file('dav'));
-$lang['dav']['name']='DAV support';
-$lang['dav']['description']='Access your files and calendars from your desktop with the CalDAV and WebDAV server. The DAV services are built using the SabreDAV library.';
