f6a5eb86e0c7f9087f2ee51025120f8fdab1afb6
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-10-25 14:15:50 +0000

    beta version mediawiki module

--- trunk/www/modules/mediawiki/MainPanel.js
+++ trunk/www/modules/mediawiki/MainPanel.js
@@ -1,5 +1,3 @@
-Ext.namespace('GO.mediawiki');
-
 GO.mediawiki.MainPanel = function(config){
 
 	if(!config)
@@ -7,43 +5,70 @@ GO.mediawiki.MainPanel = function(config){
 		config = {};
 	}
 
-	this.buildForm();
-
-	config.tbar= [new Ext.Button({
-			iconCls: 'btn-save',
+	config.tbar= new Ext.Toolbar({
+		cls: 'go-head-tb',
+		items:[new Ext.Button({
+			iconCls: 'btn-refresh',
 			cls: 'x-btn-text-icon',
-			text: GO.lang['cmdOk'],
+			text: GO.lang['cmdRefresh'],
 			handler:function(){
-				Ext.Ajax.request({
-					url: GO.settings.modules.mediawiki.url + 'json.php',
-					params: {
-						task: 'login'
-					},
-					scope: this
-				})
+				GO.mediawiki.iFrameComponent.setUrl(GO.mediawiki.settings.externalUrl);
 			},
 			scope: this
-		})];
+		}),
+		'-',
+		this.settingsButton = new Ext.Button({
+			iconCls: 'btn-settings',
+			text: GO.lang.administration,
+			cls: 'x-btn-text-icon',
+			handler: function(){
+				if(!this.settingsDialog)
+				{
+					this.settingsDialog = new GO.mediawiki.SettingsDialog();
+				}
+				this.settingsDialog.show();
+			},
+			scope: this
+		})
+		]
+		});
 
-	config.items = [new GO.panel.SecureIFrameComponent({
-			//url:'http://localhost/mediawiki/index.php?wpName=testpersoon',
-			//width: '100%',
-			//height: '100%'
-		})];
+	GO.mediawiki.iFrameComponent = new GO.panel.IFrameComponent({
+		url: GO.mediawiki.settings.externalUrl
+	});
+
+	config.layout='fit';
+	config.items = [GO.mediawiki.iFrameComponent];
+
+	config.title = GO.mediawiki.settings.title;
 
 	GO.mediawiki.MainPanel.superclass.constructor.call(this, config);
 
 }
 
 Ext.extend(GO.mediawiki.MainPanel, Ext.Panel,{
-	afterRender : function()
-	{
-		GO.mediawiki.MainPanel.superclass.afterRender.call(this);
-	},
 
-	buildForm : function() {
-		
+	beforeRender : function() {
+		Ext.Ajax.request({
+			url: GO.settings.modules.mediawiki.url + 'json.php',
+			params: {
+				task: 'load_settings'
+			},
+			scope: this,
+			success: function(response,options) {
+				var responseParams = Ext.decode(response.responseText);
+				if (responseParams.success) {
+					GO.mediawiki.settings.externalUrl = responseParams.data.external_url;
+					GO.mediawiki.iFrameComponent.setUrl(GO.mediawiki.settings.external_url);
+					GO.mediawiki.settings.title = responseParams.data.title;
+					this.title = responseParams.title;
+				} else {
+					Ext.Msg.alert(GO.lang['strError'], responseParams.feedback);
+				}
+			}
+		})
 	}
+
 });
 
 GO.moduleManager.addModule('mediawiki', GO.mediawiki.MainPanel, {
--- trunk/www/modules/mediawiki/json.php
+++ trunk/www/modules/mediawiki/json.php
@@ -28,112 +28,14 @@ try{
 	switch($task)
 	{
 
-		case 'login':
-/*
-			header("Content-type: text/plain;");
+		case 'load_settings':
 
-			$name = 'testpersoon';//urlencode( "spi{$user_id}" );
-			$password = 'q1w2e3';//urlencode( $password );
-			$ch = curl_init( "http://localhost/mediawiki/api.php" );
-			curl_setopt( $ch, CURLOPT_POST, true );
-			curl_setopt( $ch, CURLOPT_POSTFIELDS, "action=login&lgname={$name}&lgpassword={$password}&format=xml" );
-			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-			$str = curl_exec( $ch );
-
-			$xml = simplexml_load_string( $str );
-			$token = $xml->login->attributes()->token;
-			$cookieprefix = $xml->login->attributes()->cookieprefix;
-			$sessionid = $xml->login->attributes()->sessionid;
-
-			//some versions of mediawiki are buggy and give a blank cookie prefix
-			//so detect that and find the real prefix
-			if( $cookieprefix == '' ) {
-					curl_setopt( $ch, CURLOPT_HEADER, true );
-					$str = curl_exec( $ch );
-					curl_setopt( $ch, CURLOPT_HEADER, false );
-					preg_match( '/ (.*?)_session=/', $str, $m );
-					$cookieprefix = $m[1];
-			}
-
-			$cookie = "{$cookieprefix}_session={$sessionid}";
-
-			curl_setopt( $ch, CURLOPT_COOKIE, $cookie );
-			curl_setopt( $ch, CURLOPT_POSTFIELDS, "action=login&lgname={$name}&lgpassword={$password}&lgtoken={$token}&format=json" );
-			$response = json_decode(curl_exec( $ch ));
-			curl_close( $ch );
-*/
-			/*
-			require_once($GO_MODULES->modules['mediawiki']['path'].'snoopy/Snoopy.class.php');
-
-			$snoopy = new Snoopy();
-
-			$snoopy = new Snoopy;
-			$snoopy->curl_path="/usr/bin/curl";
-			$wikiroot = "http://localhost/mediawiki/";
-			$api_url = $wikiroot . "api.php";
-
-			# Login via api.php
-			$login_vars = array(
-				'action' => 'login',
-				'lgname' => 'testpersoon',
-				'lgpassword' => 'q1w2e3',
-				'format' => 'php'
-						);
-			
-			## First part
-			$snoopy->submit($api_url,$login_vars);
-			$response = unserialize($snoopy->results);
-			$login_vars['lgtoken'] = $response['login']['token'];
-			$snoopy->cookies[$response['login']['cookieprefix'].'_session'] = $response['login']['sessionid']; // You may have to change 'wiki_session' to something else on your Wiki
-			## Second part, now that we have the token
-			$login_vars['format'] = 'json';
-			$snoopy->submit($api_url,$login_vars);
-			$response = json_decode($snoopy->results);
-			*/
-
-			/*
-			# Fetch the page
-			$login_vars['action'] = "render";
-			$urlpart='Main_Page';
-			$snoopy->submit($wikiroot . "index.php?title=" . $urlpart, $login_vars);
-			print($snoopy->results);
-			*/
-
-			$_data = array(
-				'action' => 'login',
-				'lgname' => 'Testpersoon',
-				'lgpassword' => 'q1w2e3',
-				'format' => 'json'
-						);
-
-			list($header,$response) = mediawiki::postRequest('http://localhost/mediawiki/api.php', 'http://localhost/', $_data);
-
-			$response = json_decode($response);
-			$header = array("Set-Cookie: ".$response->login->cookieprefix."_session=".$response->login->sessionid.'; domain='.'localhost; '.' path=/; HttpOnly');
-
-			if ($response->login->result=='NeedToken') {
-				$_data = array(
-					'action' => 'login',
-					'lgname' => 'testpersoon',
-					'lgpassword' => 'q1w2e3',
-					'lgtoken' => $response->login->token,
-					'format' => 'json'
-				);
-				list($header,$response) = mediawiki::postRequest('http://localhost/mediawiki/api.php', 'http://localhost/', $_data,$header);
-
-				$response = json_decode($response);
-			}
-
-			$response->url = 'http://localhost/mediawiki/index.php';
-
-			/*
-			global $GO_USERS, $GO_SECURITY;
-			$user = $GO_USERS->get_user($GO_SECURITY->user_id);
-			//$_SESSION['GO_SESSION']['wsUserID'] = $GO_SECURITY->user_id;
-			$response['url'] = 'http://localhost/mediawiki/index.php?wsUserID='.$GO_SECURITY->user_id;//?title=Special:UserLogin&action=submitlogin&type=login&wpName='.$user['username'].'&wpPassword='.$user['password'].'&wpLoginAttempt=Log+in';
-			//$response['url'] = 'http://localhost/mediawiki/index.php?wpName='.$user['username'].'&wpPassword='.$user['password'].'&wpLoginAttempt=Log+in';
+			$response['data'] = array();
+			$response['data']['title'] = $GO_CONFIG->get_setting('mediawiki_title');
+				if (empty($response['data']['title'])) $response['data']['title'] = 'Mediawiki';
+			$response['data']['external_url'] = $GO_CONFIG->get_setting('mediawiki_external_url');
+				if (empty($response['data']['external_url'])) $response['data']['external_url'] = '';
 			$response['success'] = true;
-*/
 
 			break;
 
