61dd707fe968f81b85ec00cb570c7e5191f6bb95
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-10-26 16:02:47 +0000

    Cleaned up code

--- www/modules/mediawiki/MainPanel.js
+++ www/modules/mediawiki/MainPanel.js
@@ -1,3 +1,16 @@
+/**
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @version $Id:
+ * @copyright Copyright Intermesh
+ * @author Wilmar van Beusekom <wilmar@intermesh.nl>
+ */
+
 GO.mediawiki.MainPanel = function(config){
 
 	if(!config)
