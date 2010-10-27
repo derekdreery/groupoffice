a1ea78cf9681e286f81ad517abc11bf4418790c4
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-10-27 11:59:45 +0000

    configurable title

---
+++ trunk/www/modules/mediawiki/language/nl.inc.php
@@ -0,0 +1,4 @@
+<?php
+require($GO_LANGUAGE->get_fallback_language_file('mediawiki'));
+$lang['mediawiki']['name']='Mediawiki interface';
+$lang['mediawiki']['description']='This module integrates Mediawiki with Group-Office.';
