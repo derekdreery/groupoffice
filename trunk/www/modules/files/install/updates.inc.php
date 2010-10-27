39eafe876545fc3595f89ba4a000438d84147c95
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-10-27 12:37:39 +0000

    remove memory_limit

--- trunk/www/modules/files/install/updates.inc.php
+++ trunk/www/modules/files/install/updates.inc.php
@@ -1,6 +1,4 @@
 <?php
-ini_set('memory_limit','500M');
-
 $updates[] = "script:1.inc.php";
 //do it twice because it went wrong the first time on some installations
 $updates[] = "script:1.inc.php";
