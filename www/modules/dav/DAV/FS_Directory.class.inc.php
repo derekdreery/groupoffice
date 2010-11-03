13fa62a3f4791b46390378e0e0badb29fd4cd8e8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-11-03 11:03:48 +0000

    Delete wasn't working with only write permission

--- www/modules/dav/DAV/FS_Directory.class.inc.php
+++ www/modules/dav/DAV/FS_Directory.class.inc.php
@@ -205,7 +205,7 @@ class GO_DAV_FS_Directory extends Sabre_DAV_FS_Node implements Sabre_DAV_ICollec
 
 		global $GO_SECURITY;
 
-		if(!$this->files->has_delete_permission($GO_SECURITY->user_id, $this->folder))
+		if(!$this->files->has_write_permission($GO_SECURITY->user_id, $this->folder))
 			throw new Sabre_DAV_Exception_Forbidden();
 
 
