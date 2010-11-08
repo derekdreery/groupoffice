fef68d738ba502fc2a74b8a1c401a9d07b0c0b82
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-11-08 14:30:15 +0000

    thoroughly tested

--- trunk/www/themes/Default/layout.inc.php
+++ trunk/www/themes/Default/layout.inc.php
@@ -41,7 +41,7 @@ if($GO_SECURITY->logged_in() && trim($_SESSION['GO_SESSION']['name']) != '' && !
 			<img id="reminder-icon" src="<?php echo $GO_CONFIG->host; ?>themes/Default/images/16x16/reminders.png" style="border:0;vertical-align:middle;cursor:pointer" />
 			<!-- <img id="checker-icon" src="<?php echo $GO_CONFIG->host; ?>ext/resources/images/default/grid/loading.gif" style="border:0;vertical-align:middle" /> -->
 			
-			<?php if (isset($GO_MODULES->modules['search'])) {
+			<?php if (isset($GO_MODULES->modules['search']) && $GO_MODULES->modules['search']['read_permission']) {
 			echo '<img src="'.$GO_CONFIG->host.'themes/Default/images/16x16/icon-search.png" style="border:0px;margin-left:10px;margin-right:1px;vertical-align:middle" />';
 			}
 			?>
