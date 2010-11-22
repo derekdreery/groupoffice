85ddca580717ae7bf1151f3dce262bd9ac2c6bef
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-11-22 09:50:26 +0000

    Created tag 3.6.1

---
+++ tags/groupoffice-com-3.6.1/modules/wordpress/language/en.js
@@ -0,0 +1,14 @@
+/**
+ * Don't copy the next lines into a translation
+ */
+Ext.namespace('GO.wordpress');
+
+GO.wordpress.lang={};
+
+/**
+ * Copy everything below for translations
+ */
+
+GO.wordpress.lang.wordpress='Website';
+GO.wordpress.lang.wordpressAdmin='Admin panel';
+GO.wordpress.lang.publishToWebsite='Publish to website';
---
+++ tags/groupoffice-com-3.6.1/sync.php
@@ -0,0 +1,20 @@
+<?php
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id$
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
+if(!file_exists('modules/sync/sync.php'))
+{
+	die('Sorry, the sync module is not available in the community version.');
+}
+require('modules/sync/sync.php');
+?>
