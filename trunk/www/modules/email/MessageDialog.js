f123e6342f89883366d1d38ccb8a66eeeaa7e856
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-11-25 14:45:10 +0000

    added tbar to messagepanel on startpage

--- trunk/www/modules/email/MessageDialog.js
+++ trunk/www/modules/email/MessageDialog.js
@@ -8,6 +8,64 @@ GO.email.MessageDialog = function(config){
 	this.messagePanel = new GO.email.MessagePanel({
 		autoScroll:true
 	});
+
+	this.toolbar =[
+	this.replyButton=new Ext.Button({
+		disabled:false,
+		iconCls: 'btn-reply',
+		text: GO.email.lang.reply,
+		cls: 'x-btn-text-icon',
+		handler: function(){
+
+			GO.email.showComposer({
+				uid: this.messagePanel.uid,
+				task: 'reply',
+				mailbox: this.messagePanel.mailbox,
+				account_id: this.account_id
+			});
+		},
+		scope: this
+	}),this.replyAllButton=new Ext.Button({
+		disabled:false,
+		iconCls: 'btn-reply-all',
+		text: GO.email.lang.replyAll,
+		cls: 'x-btn-text-icon',
+		handler: function(){
+			GO.email.showComposer({
+				uid: this.messagePanel.uid,
+				task: 'reply_all',
+				mailbox: this.messagePanel.mailbox,
+				account_id: this.account_id
+
+			});
+		},
+		scope: this
+	}),this.forwardButton=new Ext.Button({
+		disabled:false,
+		iconCls: 'btn-forward',
+		text: GO.email.lang.forward,
+		cls: 'x-btn-text-icon',
+		handler: function(){
+			GO.email.showComposer({
+				uid: this.messagePanel.uid,
+				task: 'forward',
+				mailbox: this.messagePanel.mailbox,
+				account_id: this.account_id
+			});
+		},
+		scope: this
+	}),
+
+	this.printButton = new Ext.Button({
+		disabled: false,
+		iconCls: 'btn-print',
+		text: GO.lang.cmdPrint,
+		cls: 'x-btn-text-icon',
+		handler: function(){
+			this.messagePanel.body.print();
+		},
+		scope: this
+	})];
 	
 	config.layout='fit';
 	config.title=GO.email.lang.message;
@@ -20,6 +78,7 @@ GO.email.MessageDialog = function(config){
 	config.minizable=true;
 	config.closeAction='hide';	
 	config.items=this.messagePanel;
+	config.tbar=this.toolbar;
 	config.buttons=[{	
 		text: GO.lang['cmdClose'],
 		handler: function()
