c1f48effbd639a1c5654dcfd603ebf3b531ffda7
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-12-07 11:17:49 +0000

    3 instead of 5 login failures

--- www/modules/blacklist/language/en.inc.php
+++ www/modules/blacklist/language/en.inc.php
@@ -8,4 +8,4 @@ $lang['blacklist']['ips']='IP addresses';
 
 $lang['blacklist']['blacklisted']='Your IP address %s is being blocked because there were 3 consecutive login failures from this IP address. Contact the system administrator and supply your IP address to unblock it.';
 $lang['blacklist']['captchaIncorrect']='The security code you entered is incorrect, please try again.';
-$lang['blacklist']['captchaActivated']='There have been five login failures detected. In order to login you need to type the security code.';
+$lang['blacklist']['captchaActivated']='There have been three login failures detected. In order to login you need to type the security code.';
