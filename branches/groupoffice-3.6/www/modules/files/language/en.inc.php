ece02971843e6cdc495397cd3421aa1bc08ed1af
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-12-08 14:02:03 +0000

    Use body tag for download link

--- branches/groupoffice-3.6/www/modules/files/language/en.inc.php
+++ branches/groupoffice-3.6/www/modules/files/language/en.inc.php
@@ -43,6 +43,6 @@ $lang['files']['emptyFile']='Empty file';
 
 $lang['files']['downloadLink'] = 'Download link';
 $lang['files']['clickHereToDownload'] = 'Click here to download the file with a secure link';
-$lang['files']['copyPasteToDownload'] = 'Copy this link to your browser\'s address bar to download the file with a secure connection';
+$lang['files']['copyPasteToDownload'] = 'Click the secured link below or copy it to your browser\'s address bar to download the file.';
 $lang['files']['expireTime'] = 'Download link expire time';
 $lang['files']['possibleUntil'] = 'possible until';
