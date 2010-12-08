a7dbb3ae209121c79bad65583b9d11ea65305ea7
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-12-08 14:55:05 +0000

    Created tag 3.6.9

---
+++ tags/groupoffice-com-3.6.9/modules/ab2users/language/et.inc.php
@@ -0,0 +1,5 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('ab2users'));
+$lang['ab2users']['name']='Addressbook2Users';
+$lang['ab2users']['description']='Lisab nupu, mille abil saad kiirelt kasutaja luua kontakti või ettevõtte alt.';
