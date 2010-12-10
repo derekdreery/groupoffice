a33746c0b3c9b4649ced51cd572d99839f9ab21f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-12-10 10:39:57 +0000

    Created tag 3.6.11

---
+++ tags/groupoffice-com-3.6.11/modules/mailings/language/et.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('mailings'));
+$lang['mailings']['name'] = 'E-kirja kujundused ja aadressilistid';
+$lang['mailings']['description'] = 'Lisab e-kirja kujundused ja aadressilistid saatmiseks aadressiraamatusse.';
+
+$lang['mailings']['templateAlreadyExists'] = 'Kujundus, mida proovid luua, on juba olemas';
+$lang['mailings']['mailingAlreadyExists'] = 'Kiri, mida proovid luua, on juba olemas';
+
+$lang['mailings']['greet']='Lugupidamisega';
+
+$lang['mailings']['unsubscribe']='Lahku listist';
+$lang['mailings']['unsubscription']='Listist lahkumiseks vajuta siia';
+$lang['mailings']['r_u_sure'] = 'Oled kindel, et soovid listist lahkuda?';
+$lang['mailings']['delete_success'] = 'Listist eemaldamine õnnestus';
+$lang['mailings']['setCurrentTemplateAsDefault']='Määra praegune kujundus vaikimisi kujunduseks';
