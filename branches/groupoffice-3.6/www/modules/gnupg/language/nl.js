5ded17ff87c3b7380ac9597cac3ed2cb6a26942c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-12-19 16:08:41 +0000

    Generate key in the background

--- branches/groupoffice-3.6/www/modules/gnupg/language/nl.js
+++ branches/groupoffice-3.6/www/modules/gnupg/language/nl.js
@@ -16,3 +16,6 @@ GO.gnupg.lang.decryptPasswordRequired='Een wachtwoord is vereist om dit bericht 
 GO.gnupg.lang.sendPublicKey='Verstuur publieke sleutel';
 
 GO.gnupg.lang.importPublicKeyAttachment='Dit is een publieke sleutel om versleutelde berichten naar de afzender te vesturen. Importeer deze alleen wanneer u er zeker van bent dat deze afkomstig is van de afzender. Wilt u de sleutel importeren?';
+
+GO.gnupg.lang.generateKeyStarted='Uw sleutelpaar wordt aangemaakt.';
+GO.gnupg.lang.generateKeyStartedText='Uw sleutelpaar wordt aangemaakt. Dit kan tot 10 minuten duren. Controleer later of de sleutel gemaakt is.';
