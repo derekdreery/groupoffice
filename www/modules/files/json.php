11ceabfe518419096727afe0c95d1557d61f8195
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-12-20 10:46:49 +0000

    Add one day to the download expire time

--- www/modules/files/json.php
+++ www/modules/files/json.php
@@ -36,7 +36,7 @@ try {
 
 			$file = $files->get_file($_POST['file_id']);
 			$up_file['id'] = $file['id'];
-			$expire_time = $up_file['expire_time'] = isset($_POST['expire_time']) ? $_POST['expire_time'] : $file['expire_time'];
+			$expire_time = $up_file['expire_time'] = isset($_POST['expire_time']) ? Date::date_add($_POST['expire_time'],1) : $file['expire_time'];
 			$random_code = $up_file['random_code'] = isset($file['random_code']) ? $file['random_code'] : String::random_password('a-z,1-9','i,o',11);
 			$files->update_file($up_file);
 
