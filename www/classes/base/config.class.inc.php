cafa1d9d6407b4ce75e3d86c84b7ac8fa58d2ff2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2010-12-28 16:04:53 +0000

    minified

--- www/classes/base/config.class.inc.php
+++ www/classes/base/config.class.inc.php
@@ -658,7 +658,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.6.13';
+	var $version = '3.6.14';
 
 
 	/* The permissions mode to use when creating files
@@ -688,7 +688,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20101222';
+	var $mtime = '20101228';
 
 	#group configuration
 	/**
