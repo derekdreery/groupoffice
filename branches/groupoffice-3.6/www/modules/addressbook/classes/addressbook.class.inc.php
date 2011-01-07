515531b3df39e1fbf9f13c7d518aea8cc3e688f8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-01-07 16:07:48 +0000

    Creating folders on invalid level bug

--- branches/groupoffice-3.6/www/modules/addressbook/classes/addressbook.class.inc.php
+++ branches/groupoffice-3.6/www/modules/addressbook/classes/addressbook.class.inc.php
@@ -22,7 +22,7 @@ class addressbook extends db {
 		$events->add_listener('check_database', __FILE__, 'addressbook', 'check_database');
 	}
 	public static function check_database() {
-		global $GO_CONFIG, $GO_MODULES, $GO_LANGUAGE;
+		global $GO_CONFIG, $GO_MODULES, $GO_LANGUAGE, $GO_SECURITY;
 
 		$line_break=php_sapi_name() != 'cli' ? '<br />' : "\n";
 
@@ -48,8 +48,10 @@ class addressbook extends db {
 			}
 			flush();
 
-			$files->check_share('contacts', 1, $GO_MODULES->modules['addressbook']['acl_id'], false);
-			$files->check_share('companies', 1, $GO_MODULES->modules['addressbook']['acl_id'], false);
+			$acl_id=$GO_SECURITY->get_global_read_only_acl();
+
+			$files->check_share('contacts', 1,$acl_id, false);
+			$files->check_share('companies', 1, $acl_id, false);
 
 			$db->query("SELECT c.*,a.name AS addressbook_name,a.acl_id FROM ab_contacts c INNER JOIN ab_addressbooks a ON a.id=c.addressbook_id");
 			while($contact = $db->next_record()) {
