01960d832d2f4b2ac4075a318c355d49859d5e7a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-01-17 12:40:54 +0000

    Created tag 3.6.19

---
+++ tags/groupoffice-com-3.6.19/modules/currencies/language/ru.js
@@ -0,0 +1,16 @@
+/**
+ * Russian translation
+ * By Valery Yanchenko (utf-8 encoding)
+ * vajanchenko@hotmail.com
+ * 10 December 2008
+*/
+Ext.namespace('GO.currencies');
+GO.currencies.lang={};
+GO.currencies.lang.currencies='currencies';
+
+/* table: cu_currencies */
+GO.currencies.lang.currency="Валюта";
+GO.currencies.lang.currencies="Валюты";
+GO.currencies.lang.code="Код";
+GO.currencies.lang.symbol="Символ";
+GO.currencies.lang.value="Значание курса";
---
+++ tags/groupoffice-com-3.6.19/modules/imapauth/language/ru.inc.php
@@ -0,0 +1,4 @@
+<?php
+//require($GO_LANGUAGE->get_fallback_language_file('imapauth'));
+$lang['imapauth']['name']='IMAP аутентификация';
+$lang['imapauth']['description']='Это модуль аутентифицируется на IMAP сервере и автоматически создает нового пользователя и почтовый ящик в Group-Office. Очень полезен когда у Вас уже есть почтовый сервер.';
