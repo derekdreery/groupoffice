cb38582c22a5ce38be347efa294d1933130be0a2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-01-17 10:02:06 +0000

    Created tag 3.6.18

---
+++ tags/groupoffice-com-3.6.18/modules/reminders/language/nb.inc.php
@@ -0,0 +1,6 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('reminders'));
+$lang['reminders']['name']='Gruppekunngjøringer';
+$lang['reminders']['description']='En modul for å legge inn sprettopp kunngjøringer til brukere eller brukergrupper.';
+
