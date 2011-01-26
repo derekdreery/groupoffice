d749133bbe0f54ef696005ab3e903d1ad5394013
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-01-26 21:52:58 +0000

    import script

---
+++ scripts/test/importldapusers.php
@@ -0,0 +1,45 @@
+<?php
+if(!isset($argv[1]))
+{
+	die('No config! :: The first argument is empty!');
+}
+
+define('CONFIG_FILE', $argv[1]);
+require($argv[1]);
+
+require_once($config['root_path']."Group-Office.php");
+
+require_once($GO_MODULES->modules['ldapauth']['class_path'].'ldapauth.class.inc.php');
+
+require_once($GO_CONFIG->class_path.'base/users.class.inc.php');
+$GO_USERS = new GO_USERS();
+
+$la = new ldapauth();
+
+$ldap = $la->connect();
+
+$search_id=$ldap->search('uid=*', $ldap->PeopleDN);
+
+$count=0;
+for ($entryID=ldap_first_entry($ldap->Link_ID,$search_id);
+            $entryID!=false;
+            $entryID=ldap_next_entry($ldap->Link_ID,$entryID))
+{
+	$entry = ldap_get_attributes ($ldap->Link_ID,$entryID);
+	//var_dump($entry);
+
+	//$user = $la->convert_ldap_entry_to_groupoffice_record($entry);
+
+	//var_dump($entry);
+
+	//$gouser = $GO_USERS->get_user_by_username($username);
+
+	ldapauth::before_login();
+
+	
+
+	$count++;
+
+	if($count==10)
+		break;
+}
