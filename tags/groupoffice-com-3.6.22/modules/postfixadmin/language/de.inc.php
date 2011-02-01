4e4f93c103481c9b50e93abffb45bc54888e9efb
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-02-01 14:52:42 +0000

    Created tag 3.6.22

---
+++ tags/groupoffice-com-3.6.22/modules/postfixadmin/language/de.inc.php
@@ -0,0 +1,14 @@
+<?php
+require($GO_LANGUAGE->get_fallback_language_file('postfixadmin'));
+$lang['postfixadmin']['name']='PostfixVerwaltung';
+$lang['postfixadmin']['description']='Modul zum Verwalten von Postfix-Benutzern in der GroupOffice-Datenbank';
+$lang['postfixadmin']['alias']='Alias';
+$lang['postfixadmin']['aliases']='Aliase';
+$lang['postfixadmin']['domain']='Domäne';
+$lang['postfixadmin']['domains']='Domänen';
+$lang['postfixadmin']['fetchmail_config']='Fetchmail (config)';
+$lang['postfixadmin']['fetchmail_configs']='Fetchmail (configs)';
+$lang['postfixadmin']['mailbox']='Mailbox';
+$lang['postfixadmin']['mailboxes']='Mailboxen';
+$lang['postfixadmin']['vacation_config']='Abwesenheit (config)';
+$lang['postfixadmin']['vacation_configs']='Abwesenheit (configs)';
