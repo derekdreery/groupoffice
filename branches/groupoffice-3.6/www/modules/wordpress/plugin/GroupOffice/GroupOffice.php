a1ca6ba6039e80ab414b11c190078462344c2b50
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-02-03 14:55:58 +0000

    Don't enable display_errors

--- branches/groupoffice-3.6/www/modules/wordpress/plugin/GroupOffice/GroupOffice.php
+++ branches/groupoffice-3.6/www/modules/wordpress/plugin/GroupOffice/GroupOffice.php
@@ -23,8 +23,8 @@ if(isset($go_config['config_file'])){
 	require($config['root_path'].'Group-Office.php');
 	//ini_set('display_errors', 0);
 }
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL & ~E_NOTICE);
+//ini_set('display_errors', 1);
+//ini_set('error_reporting', E_ALL & ~E_NOTICE);
 
 class groupoffice_connector {
 
