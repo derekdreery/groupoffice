f84bfe32787dd451e79fabc3beef999dfe6dbb25
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-02-11 09:33:49 +0000

    Version update

--- branches/groupoffice-3.6/www/classes/base/config.class.inc.php
+++ branches/groupoffice-3.6/www/classes/base/config.class.inc.php
@@ -658,7 +658,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.6.22';
+	var $version = '3.6.23';
 
 
 	/* The permissions mode to use when creating files
@@ -688,7 +688,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20110201';
+	var $mtime = '20110211';
 
 	#group configuration
 	/**
