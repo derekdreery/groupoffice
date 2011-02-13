811e58a54c58c9e91301c24d5a70a2303680da3e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-02-13 12:49:35 +0000

    Search for e-mail accounts

--- trunk/www/modules/email/AccountsGrid.js
+++ trunk/www/modules/email/AccountsGrid.js
@@ -66,16 +66,70 @@ GO.email.AccountsGrid = function(config){
 		forceFit: true,
 		emptyText: GO.lang['strNoItems']		
 	});
+
+	this.searchField = new GO.form.SearchField({
+		store: config.store,
+		width:320
+	});
+	config.tbar = [{
+		iconCls: 'btn-add',
+		text: GO.lang.cmdAdd,
+		cls: 'x-btn-text-icon',
+		handler: function(){
+			
+			this.showAccountDialog();
+		},
+		scope: this,
+		disabled: !GO.settings.modules.email.write_permission
+	},{
+		iconCls: 'btn-delete',
+		text: GO.lang.cmdDelete,
+		cls: 'x-btn-text-icon',
+		handler: function(){
+			this.deleteSelected({
+				callback: function(){
+					if(GO.email.aliasesStore.loaded)
+					{
+						GO.email.aliasesStore.reload();
+					}
+					this.fireEvent('delete', this);
+				},
+				scope: this
+			});
+		},
+		scope:this,
+		disabled: !GO.settings.modules.email.write_permission
+	},'-',GO.lang['strSearch'] + ':', this.searchField];
 	
 	config.sm=new Ext.grid.RowSelectionModel();
-	config.loadMask=true;			
+	config.loadMask=true;
+
+	this.accountDialog = new GO.email.AccountDialog();
+	this.accountDialog.on('save', function(){
+	this.store.reload();
+		if(GO.email.aliasesStore.loaded)
+		{
+			GO.email.aliasesStore.reload();
+		}
+	}, this);
 	
 	GO.email.AccountsGrid.superclass.constructor.call(this, config);	
 
 	this.addEvents({'delete':true});
 
+	this.on('rowdblclick', function(grid, rowIndex){
+		var record = grid.getStore().getAt(rowIndex);
+	
+		this.showAccountDialog(record.data.id);
+
+	}, this);
+
 };
 
 Ext.extend(GO.email.AccountsGrid, GO.grid.GridPanel,{
+	showAccountDialog : function(account_id){
 
+		
+		this.accountDialog.show(account_id);
+	}
 });
