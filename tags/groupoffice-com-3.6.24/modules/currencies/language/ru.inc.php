4f7fa365e1ed7e14c9054fa19475773b67f04d7e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-02-22 08:55:41 +0000

    Created tag 3.6.24

---
+++ tags/groupoffice-com-3.6.24/modules/currencies/language/ru.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Russian translation
+ * By Valery Yanchenko (utf-8 encoding)
+ * vajanchenko@hotmail.com
+ * 10 December 2008
+*/
+////Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('currencies'));
+$lang['currencies']['name']='Валюты';
+$lang['currencies']['description']='Валюты';
+$lang['currencies']['currency']='Валюта';
+$lang['currencies']['currencies']='Валюты';
