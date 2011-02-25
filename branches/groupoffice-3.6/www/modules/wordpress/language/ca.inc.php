e3ef144bfe02a6f267d0baf4d0f3e81fc64225b0
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-02-25 08:32:11 +0000

    Added Catalan translation

---
+++ branches/groupoffice-3.6/www/modules/wordpress/language/ca.inc.php
@@ -0,0 +1,7 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('wordpress'));
+
+$lang['wordpress']['name']='Wordpress CMS';
+$lang['wordpress']['description']='Un pont pel CMS Wordpress per gestionar un lloc Web. El plugin de GroupOffice necessita ser instal·lat dins el sistema Wordpress.';
+?>
