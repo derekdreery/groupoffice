58c2053ea0f1eead546e35de585bc01f7ad23048
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-02-28 16:24:37 +0000

    Allow free busy checks for all users by default

--- www/modules/calendar/json.php
+++ www/modules/calendar/json.php
@@ -1185,8 +1185,15 @@ try {
 
 				if($user) {
 					//Only show availability if user has access to the default calendar
-					$default_calendar = $cal->get_default_calendar($user['id']);
-					if($GO_SECURITY->has_permission($GO_SECURITY->user_id, $default_calendar['acl_id'])){
+					if(!empty($GO_CONFIG->require_calendar_access_for_freebusy)){
+						$default_calendar = $cal2->get_default_calendar($user['id']);
+						$permission = $GO_SECURITY->has_permission($GO_SECURITY->user_id, $default_calendar['acl_id']);
+					}else
+					{
+						$permission=true;
+					}
+
+					if($permission){
 						$response[$email]=$cal->is_available($user['id'], $_REQUEST['start_time'], $_REQUEST['end_time'], $event) ? '1' : '0';
 					}
 				}
