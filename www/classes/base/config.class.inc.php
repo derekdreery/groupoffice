f0c53171476747cef6df6b89f66d889fc49be87e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-03-02 18:32:05 +0000

    Ready for 3.6.27 tag

--- www/classes/base/config.class.inc.php
+++ www/classes/base/config.class.inc.php
@@ -658,7 +658,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.6.26';
+	var $version = '3.6.27';
 
 
 	/* The permissions mode to use when creating files
@@ -688,7 +688,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20110228';
+	var $mtime = '20110302';
 
 	#group configuration
 	/**
