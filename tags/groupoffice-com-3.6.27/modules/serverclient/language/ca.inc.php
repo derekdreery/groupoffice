9cb85833ae93c180f14931f7805733581f0eff86
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-03-02 18:32:34 +0000

    Created tag 3.6.27

---
+++ tags/groupoffice-com-3.6.27/modules/serverclient/language/ca.inc.php
@@ -0,0 +1,7 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('serverclient'));
+
+$lang['serverclient']['name']='Serverclient';
+$lang['serverclient']['description']='Crear automàticament bústies de correu dins una instal·lació de Group-Office que gestiona el servidor d\'e-mail amb el mòdul postfixadmin';
+$lang['serverclient']['connect_error']='No s\'ha pogut connectar a %s';
