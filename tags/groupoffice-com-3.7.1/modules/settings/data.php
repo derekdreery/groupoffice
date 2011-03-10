1c35e7a1307007639676050869d9ae2f6769b8a3
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-03-10 12:38:10 +0000

    Created tag 3.7.1

---
+++ tags/groupoffice-com-3.7.1/modules/settings/data.php
@@ -0,0 +1,41 @@
+<?php
+
+require('../../Group-Office.php');
+
+session_write_close();
+
+ini_set('max_execution_time',360);
+
+switch($_REQUEST['task']){
+
+	case 'load_settings':
+
+		// Triggered when saving
+		if(isset($_REQUEST['save']) && $_REQUEST['save'] == 'true')
+		{
+			$GO_EVENTS->fire_event('save_global_settings', array(&$response));
+			$response['saved'] = true;
+		}
+
+		$response['data']=array();
+
+		$GO_EVENTS->fire_event('load_global_settings',array(&$response));
+
+		$response['success']=true;
+		break;
+
+
+/*
+	case 'save_settings':
+
+		$response=array();
+
+		$GO_EVENTS->fire_event('save_global_settings', array(&$response));
+
+		$response['success']=true;
+
+		break; 
+ */
+} 
+
+echo json_encode($response);
