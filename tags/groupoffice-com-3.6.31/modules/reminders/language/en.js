6197e38816adc95c9d79699db454246e7f7bc3b3
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-03-23 13:57:03 +0000

    Created tag 3.6.31

---
+++ tags/groupoffice-com-3.6.31/modules/reminders/language/en.js
@@ -0,0 +1,11 @@
+Ext.namespace('GO.reminders');
+GO.reminders.lang={};
+GO.reminders.lang.reminders='Popup reminders';
+
+/* table: go_reminders */
+GO.reminders.lang.reminder="Popup reminder";
+GO.reminders.lang.time="Time";
+GO.reminders.lang.snoozeTime="Snooze time";
+GO.reminders.lang.text='Text';
+GO.reminders.lang.addUsers='Add users';
+GO.reminders.lang.addUserGroups='Add user groups';
