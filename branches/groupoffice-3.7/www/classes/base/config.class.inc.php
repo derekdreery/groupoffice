f12af50c593ced3b37890d9e37792566f332ba34
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-03-29 06:47:28 +0000

    minified

--- branches/groupoffice-3.7/www/classes/base/config.class.inc.php
+++ branches/groupoffice-3.7/www/classes/base/config.class.inc.php
@@ -687,7 +687,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.7.4';
+	var $version = '3.7.5';
 
 
 	/* The permissions mode to use when creating files
@@ -717,7 +717,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20110325';
+	var $mtime = '20110329';
 
 	#group configuration
 	/**
