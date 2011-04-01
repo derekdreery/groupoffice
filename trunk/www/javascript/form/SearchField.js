c6f7a7fd3420b5755fda1c94c2e21cd45c0a6137
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-04-01 09:30:45 +0000

    Added clear and reset event

--- trunk/www/javascript/form/SearchField.js
+++ trunk/www/javascript/form/SearchField.js
@@ -11,7 +11,7 @@
  * @author Merijn Schering <mschering@intermesh.nl>
  */
 
- /**
+/**
  * @class GO.form.SearchField
  * @extends Ext.form.TriggerField
  * Search text field that will add a query parameter to a Datastore automatically
@@ -23,62 +23,65 @@ GO.form.SearchField = Ext.extend(Ext.form.TwinTriggerField, {
 	/**
 	 * @cfg {Number} store The data store to add the query too
 	 */
-		store : false,
-    initComponent : function(){
-        GO.form.SearchField.superclass.initComponent.call(this);
-        this.on('specialkey', function(f, e){
-            if(e.getKey() == e.ENTER){
-                this.onTrigger2Click();
-            }
-        }, this);
+	store : false,
+	initComponent : function(){
+		GO.form.SearchField.superclass.initComponent.call(this);
+		this.on('specialkey', function(f, e){
+			if(e.getKey() == e.ENTER){
+				this.onTrigger2Click();
+			}
+		}, this);
         
-        this.on('focus', function(){this.focus(true);}, this);
-    },
+		this.on('focus', function(){
+			this.focus(true);
+		}, this);
 
-    validationEvent:false,
-    validateOnBlur:false,
-    trigger1Class:'x-form-clear-trigger',
-    trigger2Class:'x-form-search-trigger',
-    //hideTrigger1:true,
-    width:180,
-    hasSearch : false,
-    paramName : 'query',
+		this.addEvents({reset:true,search:true});
+	},
+
+	validationEvent:false,
+	validateOnBlur:false,
+	trigger1Class:'x-form-clear-trigger',
+	trigger2Class:'x-form-search-trigger',
+	//hideTrigger1:true,
+	width:180,
+	hasSearch : false,
+	paramName : 'query',
 	emptyText: GO.lang.strSearch,
 
-    onTrigger1Click : function(){
-        if(this.hasSearch){
-            //this.store.baseParams['start']=0;
-            this.store.baseParams[this.paramName]='';
-            this.store.load();
-            this.el.dom.value = '';
-            //this.triggers[0].hide();
-            this.hasSearch = false;
-        }
-    },
+	onTrigger1Click : function(){
 
-    onTrigger2Click : function(){
-        var v = this.getRawValue();
-        if(v.length < 1){
-            this.onTrigger1Click();
-            return;
-        }
-        //this.store.baseParams['start']=0;
-        this.store.baseParams[this.paramName]=v;
-        this.store.load();
-        this.hasSearch = true;
-        //this.triggers[0].show();
-    },afterRender:function(){
-			GO.form.SearchField.superclass.afterRender.call(this);
-			if(Ext.isIE8)this.el.setTop(0);
-		},setValue : function(v){
-			GO.form.SearchField.superclass.setValue.call(this, v);
-			if(v!='')
-			{					
-				this.hasSearch=true;
-				if(this.rendered)
-				{
-					//this.triggers[0].show();
-				}
-			}
+		this.fireEvent('reset', this);
+		
+		if(this.hasSearch){
+			this.store.baseParams[this.paramName]='';
+			this.store.load();
+			this.el.dom.value = '';
+			this.hasSearch = false;
+		}		
+	},
+
+	onTrigger2Click : function(){
+		this.fireEvent('search', this);
+
+		var v = this.getRawValue();
+		if(v.length < 1){
+			this.onTrigger1Click();
+			return;
 		}
+		this.store.baseParams[this.paramName]=v;
+		this.store.load();
+		this.hasSearch = true;
+	},
+	afterRender:function(){
+		GO.form.SearchField.superclass.afterRender.call(this);
+		if(Ext.isIE8)this.el.setTop(0);
+	},
+	setValue : function(v){
+		GO.form.SearchField.superclass.setValue.call(this, v);
+		if(v!='')
+		{
+			this.hasSearch=true;
+		}
+	}
 });
