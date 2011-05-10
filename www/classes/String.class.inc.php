9c3321ca3e85617514078024ada62bed121417a6
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-05-10 11:31:58 +0000

    Allow + sign in e-mail adress mailbox part

--- www/classes/String.class.inc.php
+++ www/classes/String.class.inc.php
@@ -512,7 +512,7 @@ class String {
 	 * @return	string
 	 */
 	public static function get_email_validation_regex() {
-		return "/^[a-z0-9\._\-]+@[a-z0-9\.\-_]+\.[a-z]{2,6}$/i";
+		return "/^[a-z0-9\._\-+]+@[a-z0-9\.\-_]+\.[a-z]{2,6}$/i";
 	}
 
 
