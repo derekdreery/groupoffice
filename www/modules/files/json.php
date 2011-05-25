0e34162ffed0becb79c52cbeea88ca684ef8d7c1
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-05-25 06:43:02 +0000

    I've found a bug for the functions Email Download link. Sometimes the random code isn't showed. it seems that sometimes $file['random_code'] is there but empty
    Changed /modules/files/json.php, line 40 solved this
    
    $random_code = $up_file['random_code'] = isset($file['random_code']) ? $file['random_code'] : String::random_password('a-z,1-9','i,o',11);
    
    to
    
    $random_code = $up_file['random_code'] = !empty($file['random_code']) ? $file['random_code'] : String::random_password('a-z,1-9','i,o',11);

--- www/modules/files/json.php
+++ www/modules/files/json.php
@@ -37,7 +37,7 @@ try {
 			$file = $files->get_file($_POST['file_id']);
 			$up_file['id'] = $file['id'];
 			$expire_time = isset($_POST['expire_time']) ? $_POST['expire_time'] : $file['expire_time'];
-			$random_code = $up_file['random_code'] = isset($file['random_code']) ? $file['random_code'] : String::random_password('a-z,1-9','i,o',11);
+			$random_code = $up_file['random_code'] = !empty($file['random_code']) ? $file['random_code'] : String::random_password('a-z,1-9','i,o',11);
 
 			$up_file['expire_time']=Date::date_add($expire_time, 1);
 			$files->update_file($up_file);
