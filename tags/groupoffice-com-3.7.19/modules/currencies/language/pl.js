7d3c59907be002163ad597273f2d9fe30f2f206f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-05-25 14:24:22 +0000

    Created tag 3.7.19

---
+++ tags/groupoffice-com-3.7.19/modules/currencies/language/pl.js
@@ -0,0 +1,9 @@
+
+GO.currencies.lang.currencies='Waluty';
+
+/* table: cu_currencies */
+GO.currencies.lang.currency="Waluta";
+GO.currencies.lang.currencies="Waluty";
+GO.currencies.lang.code="Kod";
+GO.currencies.lang.symbol="Symbol";
+GO.currencies.lang.value="Wartość";
