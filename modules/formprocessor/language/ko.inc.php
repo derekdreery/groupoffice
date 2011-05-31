bde41148190deee69400879c9b9b4538753cc8b8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-05-31 07:05:24 +0000

    Created tag 3.7.21

---
+++ modules/formprocessor/language/ko.inc.php
@@ -0,0 +1,5 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('formprocessor'));
+$lang['formprocessor']['name']='Formprocessor';
+$lang['formprocessor']['description']='Can be used to create contact forms, newsletter subscriptions and adding contacts from a website';
