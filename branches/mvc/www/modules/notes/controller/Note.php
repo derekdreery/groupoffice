2ba644eaeb6bf555e3d1275c1e45b657443b056c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-06-08 15:25:35 +0000

    Setup structure for base classes and implemented PDO in ActiveRecord

--- branches/mvc/www/modules/notes/controller/Note.php
+++ branches/mvc/www/modules/notes/controller/Note.php
@@ -1,17 +1,29 @@
 <?php
-class GO_Controller_Note extends GO_BaseController{
-	function init(){
+class GO_Controller_Note extends GO_Base_Controller_AbstractController{
+	function init($output){
+		parent::init($output);
+		
 		$this->addPermissionCheck(GO::modules()->modules['notes']['acl_id'], GO_SECURITY::WRITE_PERMISSION);
 		
 		$this->addPermissionCheck(GO::modules()->modules['notes']['acl_id'], GO_SECURITY::DELETE_PERMISSION,'delete');
 		
 	}
 	
-	public function actionGet($note_id){
+	public function actionTest($note_id=0){
 		
 		$note = new GO_Notes_Model_Note($note_id);
+	
+		$note->name='Test';
+		$note->content=date('c');
 		
-		var_dump($note);
+		$response['success'] = $note->save();
+		
+		$this->output($response);
+		
+		//GO::output($response);
+		
+		//var_dump($ret);
+	
 		
 //		$note->setAttributes($_POST);
 //		$note->date = $note->formatdate($_POST['date']);
