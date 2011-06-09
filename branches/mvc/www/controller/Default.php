96ba49b52270856b688bc9f7de159cf382c5063f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-06-09 15:07:25 +0000

    Moved javascript dir to theme

--- branches/mvc/www/controller/Default.php
+++ branches/mvc/www/controller/Default.php
@@ -7,19 +7,83 @@ class GO_Controller_Default extends GO_Base_Controller_AbstractController{
 		$this->render('init');
 	}
 	
-	protected function registerClientScript($url){
+	
+	protected function actionCompress(){
 		
 	}
 	
-	/**
-	 *
-	 * @param type $text 
-	 */
-	protected function registerInlineScript($text){
-		
+	
+	private $clientScripts=array();
+	
+	protected function registerClientScript($url, $type='url'){
+		$this->clientScripts[]=array($type, $url);
 	}
 	
-	protected function registerCssFile(){
 		
+	private function replaceUrl($css, $baseurl) {
+		return preg_replace('/url[\s]*\(([^\)]*)\)/ieU', "self::replaceUrlCallback('$1', \$baseurl)", $css);
+	}
+
+	public static function replaceUrlCallback($url, $baseurl) {
+		return 'url(' . $baseurl . trim(stripslashes($url), '\'" ') . ')';
+	}
+
+	protected function registerCssFile($url) {
+
+		//echo '<!-- '.$path.' -->'."\n";
+
+		go_debug('Adding stylesheet: ' . $url);
+
+		$this->stylesheets[] = $url;
+	}
+
+//	function loadModuleStylesheets($derrived_theme=false) {
+//		global $GO_MODULES;
+//
+//		foreach (GO::modules()->modules as $module) {
+//			if (file_exists($module['path'] . 'themes/Default/style.css')) {
+//				$this->add_stylesheet($module['path'] . 'themes/Default/style.css');
+//			}
+//
+//			if ($this->theme != 'Default') {
+//				if ($derrived_theme && file_exists($module['path'] . 'themes/' . $derrived_theme . '/style.css')) {
+//					$this->add_stylesheet($module['path'] . 'themes/' . $derrived_theme . '/style.css');
+//				}
+//				if (file_exists($module['path'] . 'themes/' . $this->theme . '/style.css')) {
+//					$this->add_stylesheet($module['path'] . 'themes/' . $this->theme . '/style.css');
+//				}
+//			}
+//		}
+//	}
+
+	public function getCachedCss() {
+		global $GO_CONFIG, $GO_SECURITY, $GO_MODULES;
+
+		$mods = '';
+		foreach (GO::modules()->modules as $module) {
+			$mods.=$module['id'];
+		}
+
+		$hash = md5(GO::config()->file_storage_path . GO::config()->host . GO::config()->mtime . $mods);
+
+		$relpath = 'cache/' . $hash . '-' . GO::view() . '-style.css';
+		$cssfile = GO::config()->file_storage_path . $relpath;
+
+		if (!file_exists($cssfile) || GO::config()->debug) {
+
+			File::mkdir(GO::config()->file_storage_path . 'cache');
+
+			$fp = fopen($cssfile, 'w+');
+			foreach ($this->stylesheets as $s) {
+
+				$baseurl = GO::config()->host.dirname($s) . '/';
+
+				fputs($fp, $this->replaceUrl(file_get_contents(GO::config()->root_path.$s), $baseurl));
+			}
+			fclose($fp);
+		}
+
+		$cssurl = GO::config()->host . 'compress.php?file=' . basename($relpath);
+		echo '<link href="' . $cssurl . '" type="text/css" rel="stylesheet" />';
 	}
 }
