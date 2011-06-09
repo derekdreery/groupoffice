fc0730b80066f3ccd34285d126a2099c000213aa
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-06-09 07:21:32 +0000

    New module collection

--- branches/mvc/www/GO/Base/Controller/Main.php
+++ branches/mvc/www/GO/Base/Controller/Main.php
@@ -3,4 +3,16 @@ class GO_Base_Controller_Main extends GO_Base_Controller_AbstractController{
 	protected function actionIndex(){
 		$this->render('index');
 	}
+	
+	protected function registerClientScript($url){
+		
+	}
+	
+	/**
+	 *
+	 * @param type $text 
+	 */
+	protected function registerInlineScript($text){
+		
+	}
 }
