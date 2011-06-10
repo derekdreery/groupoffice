2e75276cb47ccfc14155f001f9c6ed4d610191ee
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-06-10 13:20:58 +0000

    Session write close

--- trunk/www/modules/tools/checkmodule.php
+++ trunk/www/modules/tools/checkmodule.php
@@ -5,6 +5,8 @@ define('NOLOG', true);
 //event firing will cause problems with Ioncube
 define('NO_EVENTS',true);
 
+ini_set('max_execution_time', 3600);
+
 if(isset($argv[2]))
 {
 	define('CONFIG_FILE', $argv[2]);
@@ -13,7 +15,7 @@ if(isset($argv[2]))
 chdir(dirname(__FILE__));
 
 require_once("../../Group-Office.php");
-
+session_write_close();
 if(php_sapi_name()!='cli')
 {
 	$GO_SECURITY->html_authenticate('tools');
