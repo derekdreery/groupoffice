56b64d722bf5f08a2e2f5675c81d3da7426447cd
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-06-22 09:30:58 +0000

    Made a checkbox to enable the login screen text at the settings module

--- branches/groupoffice-3.7/www/modules/settings/language/en.js
+++ branches/groupoffice-3.7/www/modules/settings/language/en.js
@@ -13,3 +13,5 @@ GO.settingsmodule.lang.mainTitle = 'Settings';
 GO.settingsmodule.lang.loginScreenText='Login screen text';
 GO.settingsmodule.lang.text='Text';
 GO.settingsmodule.lang.title='Title';
+
+GO.settingsmodule.lang.loginTextEnabled='Enable login screen text';
