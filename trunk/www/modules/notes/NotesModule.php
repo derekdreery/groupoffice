57c1736d583787841cddcace1e081b365c78ac10
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-06-23 14:58:35 +0000

    Join custom fields in find function

--- trunk/www/modules/notes/NotesModule.php
+++ trunk/www/modules/notes/NotesModule.php
@@ -4,4 +4,15 @@ class GO_Notes_NotesModule extends GO_Base_Model_Module{
 	public function initListeners(){
 		
 	}	
+	
+	/**
+	 * 
+	 * When a user is created, updated or logs in this function will be called.
+	 * The function can check if the default calendar, addressbook, notebook etc.
+	 * is created for this user.
+	 * 
+	 */
+	public function initUser($userId){
+		
+	}
 }
