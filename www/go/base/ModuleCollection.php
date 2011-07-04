d56138e1a727b5ed5a438ab620d60ba96663f0ce
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-07-04 11:56:07 +0000

    removed redundant global $GO_CONFIG

--- www/go/base/ModuleCollection.php
+++ www/go/base/ModuleCollection.php
@@ -22,13 +22,13 @@ class GO_Base_ModuleCollection extends GO_Base_Model_ModelCollection{
 				unset($this->_models[$module->id]);
 			}else
 			{
-			
+
 				$this->modules[$module->id]=$module->getAttributes();
 				$this->modules[$module->id]['path']=$module->path;
 				$this->modules[$module->id]['class_path']=$module->class_path;
 				$this->modules[$module->id]['url']=$module->url;
 				$this->modules[$module->id]['full_url']=$module->full_url;
-				
+
 				$this->modules[$module->id]['humanName']=$module->humanName;
 				$this->modules[$module->id]['description']=$module->description;
 				
@@ -60,7 +60,6 @@ class GO_Base_ModuleCollection extends GO_Base_Model_ModelCollection{
 
 		if(!$this->allowed_modules)
 		{
-			global $GO_CONFIG;
 			$this->allowed_modules=empty(GO::config()->allowed_modules) ? array() : explode(',', GO::config()->allowed_modules);
 		}
 		return !count($this->allowed_modules) || in_array($module, $this->allowed_modules);
@@ -72,8 +71,8 @@ class GO_Base_ModuleCollection extends GO_Base_Model_ModelCollection{
 	public function load_modules(){
 		
 	}
-	
-	
+
+
 	/**
 	 * Checks if the currently active user is permissioned for a module
 	 *
