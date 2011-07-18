a0c527ee46419be04cbe5073d932f33721310d74
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-07-18 09:14:29 +0000

    get XML site map

--- www/modules/cms/action.php
+++ www/modules/cms/action.php
@@ -299,6 +299,13 @@ try {
 			$folder = $cms->get_folder($folder_id);
 			$site = $cms->get_site($folder['site_id']);
 
+			$file['permalink']=$cms->build_path($folder['id']).'/'.$file['name'];
+			$file['permalink']=substr($file['permalink'],5);
+			$file['permalink']=str_replace('^',' ',$file['permalink']);
+			$file['permalink']=preg_replace('/[^0-9^a-z^A-Z^\s^\/]/','',$file['permalink']);
+			$file['permalink']=preg_replace('/[\s]+/',' ',$file['permalink']);
+			$file['permalink']=strtolower(str_replace(' ','-',$file['permalink']));
+
 			if(!GO::security()->has_permission(GO::security()->user_id, $site['acl_write'])) {
 				throw new AccessDeniedException();
 			}
