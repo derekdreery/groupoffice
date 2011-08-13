c9d05e71527002fd2d3fce1925ac03c06d916261
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-08-13 14:02:58 +0000

    use call_user_func for php5.2 compatibility

--- www/modules/users/model/CustomFieldsRecord.php
+++ www/modules/users/model/CustomFieldsRecord.php
@@ -1,4 +1,15 @@
 <?php
 class GO_Users_Model_CustomFieldsRecord extends GO_Customfields_Model_AbstractCustomFieldsRecord{
+	
+	/**
+	 * Returns a static model of itself
+	 * 
+	 * @param String $className
+	 * @return GO_Users_Model_CustomFieldsRecord 
+	 */
+	public static function model($className=__CLASS__)
+	{	
+		return parent::model($className);
+	}
 	const linkType = 8;
 }
