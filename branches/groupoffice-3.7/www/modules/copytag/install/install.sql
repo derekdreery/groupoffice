3c1d5da411ad24ad11a1487026a373b50722b312
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-08-17 07:41:12 +0000

    invalid location

--- branches/groupoffice-3.7/www/modules/copytag/install/install.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `ct_tags`;
-CREATE TABLE `ct_tags` (
-`user_id` INT NOT NULL ,
-`tag` VARCHAR( 5 ) NOT NULL ,
-PRIMARY KEY ( `user_id` ) ,
-UNIQUE (
-`tag`
-)
-) ENGINE = MYISAM ;
--- branches/groupoffice-3.7/www/modules/copytag/language/en.js
+++
@@ -1,6 +0,0 @@
-Ext.namespace("GO.copytag.lang");
-
-GO.copytag.lang.copyTag='CopyTag';
-GO.copytag.lang.userTags='User tags';
-GO.copytag.lang.user='User';
-GO.copytag.lang.tag='Tag';
