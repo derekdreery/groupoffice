5dcb59f07b0b4e8d080cc2a0b94adf626166aaaf
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-09-05 07:34:22 +0000

    LAnguage update

--- www/modules/smime/language/de.js
+++ www/modules/smime/language/de.js
@@ -1,10 +1,3 @@
-/**
- * Don't copy the next lines into a translation
- */
-Ext.namespace('GO.smime');
-
-GO.smime.lang = {};
-
 GO.smime.lang.enterPassword="Geben Sie bitte das Kennwort für Ihren SMIME-Schlüssel ein.";
 GO.smime.lang.messageEncrypted="Diese E-Mail wurde Ihnen verschlüsselt geschickt.";
 GO.smime.lang.messageSigned="Diese E-Mail ist kryptographisch signiert. Klicken Sie hier, um die Signatur zu überprüfen und das Zertifikat zu importieren.";
