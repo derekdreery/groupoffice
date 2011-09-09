19bdd994ee1dd347e788482202d3f9ba20ccea88
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-09-09 08:53:58 +0000

    Created a Rrule object and changed the task module to use this Rrule object. Also created the task module mvc structure to let the customfields module work with this module.

--- trunk/www/modules/tasks/controller/TaskController.php
+++ trunk/www/modules/tasks/controller/TaskController.php
@@ -3,22 +3,7 @@ class GO_Tasks_Controller_Task extends GO_Base_Controller_AbstractModelControlle
 	
 	protected $model = 'GO_Tasks_Model_Task';
 	
-	protected function getGridParams($params) {
-		
-		switch($params['filter']){
-			case 'active':
-				$findParams = array(
-						'where'=>'start_time>:start_time',
-						'bindParams'=>array(':start_time'=>time())
-				);
-				break;
-		}
-		
-		return $findParams;
-	}
-	
 	protected function afterDisplay(&$response, &$model,&$params) {
-		
 		$response['data']['user_name']=$model->user->name;
 		$response['data']['tasklist_name']=$model->tasklist->name;
 		$response['data']['status_text']=GO::t($model->status,'tasks');
@@ -28,18 +13,41 @@ class GO_Tasks_Controller_Task extends GO_Base_Controller_AbstractModelControlle
 	
 	protected function afterLoad(&$response, &$model, &$params) {
 		$rRule = new GO_Base_Util_Icalendar_Rrule();
-		$rRule->parseRrule($model->rrule);
+		$rRule->readRruleString($model->start_time, $model->rrule);
+		$createdRule = $rRule->createOutputArray();
 		
-		$createdRule = $rRule->createResponseArray();
-		array_merge($response['data'],$createdRule);
-		
+		$response['data'] = array_merge($response['data'],$createdRule);
+
 		return parent::afterLoad($response, $model, $params);
 	}
 	
-//	protected function remoteComboFields(){
-//		return array('category_id'=>'$model->category->name');
+	protected function beforeSubmit(&$response, &$model, &$params) {
+		$rRule = new GO_Base_Util_Icalendar_Rrule();
+		$rRule->readInputArray($model->start_time, $params);
+		$model->rrule = $rRule->createRrule();
+
+		return parent::beforeSubmit($response, $model, $params);
+	}
+	
+//	protected function getGridParams($params) {
+//		
+//		switch($params['filter']){
+//			case 'active':
+//				$findParams = array(
+//						'where'=>'start_time>:start_time',
+//						'bindParams'=>array(':start_time'=>time())
+//				);
+//				break;
+//		}
+//		
+//		return $findParams;
 //	}
-	
-	
+
+	protected function remoteComboFields(){
+		return array(
+				'category_id'=>'$model->category->name',
+				'tasklist_id'=>'$model->tasklist->name'
+				);
+	}
 }
 	
