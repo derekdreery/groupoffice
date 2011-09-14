2cb6840d7e2efd3a58114f3370a6a018fb36fc9c
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-09-14 08:11:39 +0000

    Updated the generator for the new MVC Models and Controllers Added the create Controller functionality

--- scripts/mvc/createCFModel.php
+++ scripts/mvc/createCFModel.php
@@ -6,8 +6,9 @@ if(isset($_POST['createCFModelForm']))
 	$extendclassName = $_POST['modelExtendName'];
 	$moduleName = $_POST['moduleName'];
 	
-	include "generateCFModel.php";
-	
+	// TODO: Check for module exists
+	// TODO: Check for extended model exists
+		
 	generateCFModel($extendclassName, $moduleName);
 	
 }
@@ -22,5 +23,49 @@ else
 </form>
 
 <?php
+}
+
+function generateCFModel($extendclassName, $moduleName){
+	$date = date('Y-m-d H:i:s');
+	$className = 'GO_'.$moduleName.'_Model_'.$extendclassName.'CustomFieldsRecord';
+	$extendclassName = 'GO_'.$moduleName.'_Model_'.$extendclassName;
+
+	echo '<pre>'.htmlspecialchars('<?php
+	/**
+	 * Copyright Intermesh
+	 *
+	 * This file is part of Group-Office. You should have received a copy of the
+	 * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+	 *
+	 * If you have questions write an e-mail to info@intermesh.nl
+	 *
+	 * @version $Id: '.$className.'.php 7607 '.$date.'Z <<USERNAME>> $
+	 * @copyright Copyright Intermesh
+	 * @author <<FIRST_NAME>> <<LAST_NAME>> <<EMAIL>>@intermesh.nl
+	 */  
+
+	/**
+	 * The '.$className.' model
+	 *
+	 */
+
+	class '.$className.' extends GO_Customfields_Model_AbstractCustomFieldsRecord{
+
+		/**
+		 * Returns a static model of itself
+		 * 
+		 * @param String $className
+		 * @return GO_Notes_Model_CustomFieldsRecord 
+		 */
+		public static function model($className=__CLASS__)
+		{	
+			return parent::model($className);
+		}
+
+		public function extendsModel(){
+			return "'.$extendclassName.'";
+		}
+	}
+	').'</pre>';
 }
 ?>
--- scripts/mvc/createModel.php
+++ scripts/mvc/createModel.php
@@ -7,7 +7,9 @@ if(isset($_POST['createModelForm']))
 	$className = $_POST['modelName'];
 	$moduleName = $_POST['moduleName'];
 	
-	include "generateModel.php";
+	// TODO: Check for module exists
+	// TODO: Check for table exists
+	// TODO: Check for model is not empty
 	
 	generateModel($className, $table, $moduleName);
 	
@@ -24,5 +26,115 @@ else
 </form>
 
 <?php
+}
+
+
+function generateModel($className, $table, $moduleName){
+	require('../../www/Group-Office.php');
+	$conn = GO::getDbConnection();
+	
+	$sql = "SHOW FIELDS FROM `".$table."`;";
+
+	$stmt = $conn->query($sql);
+
+	$props='';
+
+	while($field = $stmt->fetch()){
+		preg_match('/([a-zA-Z].*)\(([1-9].*)\)/',$field['Type'], $matches);
+		if($matches){
+			$length = $matches[2];
+			$type = $matches[1];
+		}else
+		{
+			$type = $field['Type'];
+			$length=0;
+		}
+
+		$pdoType = $type;
+
+		switch($type){
+			case 'int':
+			case 'tinyint':
+			case 'bigint':
+				$pdoType = "int";
+			break;	
+
+			case 'varchar':
+			case 'char':
+			case 'text':
+				$pdoType='String';
+			break;
+
+			case 'enum(\'0\',\'1\')';
+				$pdoType='boolean';
+			break;
+		}	
+
+		$props .= " * @property ".$pdoType." $".$field['Field'];
+		$props .= "\n";
+	}
+	$props .= " */";
+	rtrim($props,',');
+
+	$date = date('Y-m-d H:i:s');
+	$className = 'GO_'.$moduleName.'_Model_'.$className;
+
+
+	echo '<pre>'.htmlspecialchars('<?php
+	/**
+	 * Copyright Intermesh
+	 *
+	 * This file is part of Group-Office. You should have received a copy of the
+	 * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+	 *
+	 * If you have questions write an e-mail to info@intermesh.nl
+	 *
+	 * @version $Id: '.$className.'.php 7607 '.$date.'Z <<USERNAME>> $
+	 * @copyright Copyright Intermesh
+	 * @author <<FIRST_NAME>> <<LAST_NAME>> <<EMAIL>>@intermesh.nl
+	 */  
+
+	/**
+	 * The '.$className.' model
+	 * 
+	'.$props.'
+
+	class '.$className.' extends GO_Base_Db_ActiveRecord{
+
+		/**
+		 * Returns a static model of itself
+		 * 
+		 * @param String $className
+		 * @return '.$className.'
+		 */
+		public static function model($className=__CLASS__)
+		{	
+			return parent::model($className);
+		}
+
+
+		/**
+		 * Enable this function if you want this model to check the acl\'s automatically.
+		 */
+		// public function aclField(){
+		//	 return \'acl_id\';	
+		// }
+
+		/**
+		 * Returns the table name
+		 */
+		 public function tableName() {
+			 return \''.$table.'\';
+		 }
+
+		/**
+		 * Here you can define the relations of this model with other models.
+		 * See the parent class for a more detailed description of the relations.
+		 */
+		 public function relations() {
+			 return array();
+		 }
+	}
+	').'</pre>';
 }
 ?>
