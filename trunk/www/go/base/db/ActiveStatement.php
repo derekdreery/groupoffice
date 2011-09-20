cf21ee2b72b9f847df9fd16b7ce4941f5e7e40e2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-09-20 07:48:19 +0000

    PHPDoc tags

--- trunk/www/go/base/db/ActiveStatement.php
+++ trunk/www/go/base/db/ActiveStatement.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * 
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @version $Id: File.class.inc.php 7607 2011-06-15 09:17:42Z mschering $
+ * @copyright Copyright Intermesh
+ * @author Merijn Schering <mschering@intermesh.nl> 
+ * @package GO.base.db
+ */
+
+/**
+ * An extended version of the PDOStatement PHP class that provides extra
+ * functionality.
+ *
+ * @package GO.base.db
+ */
 
 class GO_Base_Db_ActiveStatement extends PDOStatement {
 
