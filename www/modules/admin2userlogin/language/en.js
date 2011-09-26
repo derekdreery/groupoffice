2213deedf0abd59fcf5f675a7e29bf56fa42560f
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-09-26 15:02:41 +0000

    Added username column to the grid

--- www/modules/admin2userlogin/language/en.js
+++ www/modules/admin2userlogin/language/en.js
@@ -10,6 +10,7 @@ GO.admin2userlogin.lang.admin2userlogin='Switch user';
  */
 GO.admin2userlogin.lang.userId="ID";
 GO.admin2userlogin.lang.name="Name";
+GO.admin2userlogin.lang.username="Username";
 GO.admin2userlogin.lang.lastlogin="Last login attempt";
 GO.admin2userlogin.lang.registrationtime="Time Registered";
 
