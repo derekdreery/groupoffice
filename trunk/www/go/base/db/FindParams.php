54a99ede0d7c81d525713a63cb98b63b9390c8df
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-09-30 10:02:40 +0000

    New find params object

--- trunk/www/go/base/db/FindParams.php
+++ trunk/www/go/base/db/FindParams.php
@@ -1,4 +1,28 @@
 <?php
+/**
+ * 
+ * Copyright Intermesh BV.
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @package GO.base.db
+ * @version $Id: File.class.inc.php 7607 2011-06-15 09:17:42Z mschering $
+ * @copyright Copyright Intermesh BV.
+ * @author Merijn Schering <mschering@intermesh.nl> 
+ */
+
+/**
+ * The parameters for GO_Base_Db_ActiveRecord::find() can be constructed with this class
+ *
+ * @package GO.base.db
+ * @version $Id: File.class.inc.php 7607 2011-06-15 09:17:42Z mschering $
+ * @copyright Copyright Intermesh BV.
+ * @author Merijn Schering <mschering@intermesh.nl>  * 
+ */
+
 class GO_Base_Db_FindParams{
 	
 	private $_params=array();
