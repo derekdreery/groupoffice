cdf0f9b4c5f13dbe71445134d5b4ae08bc469533
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-01 12:37:41 +0000

    Lang errors

--- trunk/www/index.php
+++ trunk/www/index.php
@@ -1,12 +1,23 @@
 <?php
+
+$root = dirname(__FILE__).'/';
+
 if(PHP_SAPI=='cli'){	
 	//on the command line you can pass -c=/path/to/config.php to set the config file.
-	if(preg_match('/-c=([^-]+)/', $argv[1], $matches)){
-		define("GO_CONFIG_FILE", trim($matches[1]));
+	
+	require_once($root.'go/base/util/Cli.php');
+	
+	$args = GO_Base_Util_Cli::parseArgs();
+	
+	var_dump($args);
+	exit();
+	
+	if(isset($args['c'])){
+		define("GO_CONFIG_FILE", $args['c']);
 	}
 }
 
-$root = dirname(__FILE__).'/';
+
 require_once($root.'go/GO.php');
 GO::init();
 
