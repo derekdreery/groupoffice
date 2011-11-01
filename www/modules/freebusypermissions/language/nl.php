b6ec576a08eaf38119d056651c5e7267c2c7a48c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-01 12:27:05 +0000

    Lang errors

--- www/modules/freebusypermissions/language/nl.php
+++ www/modules/freebusypermissions/language/nl.php
@@ -1,5 +1,3 @@
 <?php
-
-
 $l['name']='Freebusy toegangsrechten';
-$l['description']='Alle gebruikers moeten andere gebruikers authoriseren om hun beschikbaarheidsinformatie te publiceren. Een knop wordt toegevoegd bij Instellingen -> Agenda.';GO.freebusypermission.lang.freebusyPermissions='Toegangsrechten voor beschikbaarheid';
+$l['description']='Alle gebruikers moeten andere gebruikers authoriseren om hun beschikbaarheidsinformatie te publiceren. Een knop wordt toegevoegd bij Instellingen -> Agenda.';
