36bb4009618df6cd0ad2e2df5bd3e83a5624cd7d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-02 09:05:30 +0000

    Set invalid resource_event_id's to 0

--- trunk/www/modules/calendar/install/updates.inc.php
+++ trunk/www/modules/calendar/install/updates.inc.php
@@ -93,3 +93,5 @@ $updates["201110281025"][]="ALTER TABLE `cal_settings` CHANGE `calendar_id` `cal
 
 $updates["201110281025"][]="ALTER TABLE `cal_participants` CHANGE `last_modified` `last_modified` VARCHAR( 20 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''";
 $updates["201110281025"][]="ALTER TABLE `cal_participants` CHANGE `role` `role` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''";
+
+$updates["201111021004"][]="update `cal_events` set resource_event_id=0 WHERE id=resource_event_id;";
