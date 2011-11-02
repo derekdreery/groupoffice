d126bd6d9a35908b1144474552145467a5238ecc
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-02 08:55:43 +0000

    Added new en.php in languages

---
+++ www/modules/admin2userlogin/language/en.php
@@ -0,0 +1,7 @@
+<?php
+$l['admin2userlogin']='Switch user';
+$l['userId']="ID";
+$l['name']="Name";
+$l['username']="Username";
+$l['lastlogin']="Last login attempt";
+$l['registrationtime']="Time Registered";
