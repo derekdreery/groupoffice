084416a43f947fc3c6497f92dc3495bbdc593c0f
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-04 10:05:44 +0000

    Added vertical tabpanel component + style Added an ignoreButtonParams to the addButton and deleteButton components

--- www/views/Extjs3/javascript/buttons/AddButton.js
+++ www/views/Extjs3/javascript/buttons/AddButton.js
@@ -3,21 +3,22 @@ Ext.ns('GO.buttons');
 GO.buttons.AddButton = Ext.extend(Ext.Button,{
 	
 	buttonParams : null,
+	ignoreButtonParams : false,
 	
 	initComponent : function(){
 		
 		Ext.applyIf(this,{
 			iconCls: 'btn-add',
 			itemId:'add',
-			disabled:true,
+			disabled:!this.ignoreButtonParams,
 			text: GO.lang.cmdAdd,
 			cls: 'x-btn-text-icon'
 		});
 		
-		if(this.grid){
+		if(this.grid && !this.ignoreButtonParams){
 			this.grid.store.on('load', function(){
 				this.buttonParams = this.grid.store.reader.jsonData.buttonParams;
-
+				
 				this.setDisabled(!this.buttonParams);
 
 			}, this);
