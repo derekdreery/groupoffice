852d12b10a9312333a4535d9fab2c8e11e7dac35
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-10 13:35:37 +0000

    Folder creation

--- trunk/www/modules/files/controller/FolderController.php
+++ trunk/www/modules/files/controller/FolderController.php
@@ -454,7 +454,7 @@ class GO_Files_Controller_Folder extends GO_Base_Controller_AbstractModelControl
 		if(!$folder->fsFolder->exists())
 		{
 			$folder->delete();
-			if($mustExist)
+			if($mustExist || $model->acl_id)
 				return $this->createNewModelFolder($model);
 			else
 				return 0;
