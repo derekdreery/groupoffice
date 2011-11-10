5e795db780e2bbf6518ad29ff91c3a4507f2c67b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-10 20:15:30 +0000

    Sort comments descending

--- www/go/base/controller/AbstractModelController.php
+++ www/go/base/controller/AbstractModelController.php
@@ -490,6 +490,7 @@ class GO_Base_Controller_AbstractModelController extends GO_Base_Controller_Abst
 
 			$stmt = GO_Comments_Model_Comment::model()->find(GO_Base_Db_FindParams::newInstance()
 							->limit(5)
+							->order('id','DESC')
 							->criteria(GO_Base_Db_FindCriteria::newInstance()
 							        ->addModel(GO_Comments_Model_Comment::model())
 											->addCondition('model_id', $model->id)
