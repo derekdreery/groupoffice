aae1fdc3ba0b585ce1f57f5a7ac11de75689cd2d
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-11 10:15:36 +0000

    generic Email Editor Panel: HtmlEditor with Image Attachment Plugin for inside a FormPanel

--- www/go/base/mail/Message.php
+++ www/go/base/mail/Message.php
@@ -295,35 +295,37 @@ class GO_Base_Mail_Message extends Swift_Message{
 	 * This method can be used in Models and Controllers. It puts the email body
 	 * and inline (image) attachments from the client in the message, which can
 	 * then be used for storage in the database or sending emails.
-	 * @param String $body The email body text as given by the client.
-	 * @param String $inlineAttachments The JSON encoded array of inline
-	 * (image) attachments (therefore a string)
+	 * @param Array $params Must contain elements: body (string) and
+	 * inlineAttachments (string).
 	 */
-	public function handleEmailFormInput($body,$inlineAttachments){
-		$this->setHtmlAlternateBody($params['body']);
-		if(!empty($inlineAttachments)) {
-			$inlineAttachments = json_decode($inlineAttachments);
-			/**
-			 * TODO foreach inline attachment: if its url is not in an image in the
-			 * body, remove it from the inlineAttachments
+	public function handleEmailFormInput($params){
+		
+		//inlineAttachments is an array(array('url'=>'',tmp_file=>'relative/path/');
+		if(!empty($params['inlineAttachments'])){
+			$inlineAttachments = json_decode($params['inlineAttachments'], true);
+
+			/* inline attachments must of course exist as a file, and also be used in
+			 * the message body
 			 */
 			foreach ($inlineAttachments as $ia) {
-				var_dump($ia);
-//				if(file_exists($tmp_name)) {
-//					//Browsers reformat URL's so a pattern match
-//					$just_filename = utf8_basename($ia['url']);
-//					if(preg_match('/="([^"]*'.preg_quote($just_filename).')"/',$body,$matches)){
-//						//go_debug($matches);
-//						$img = Swift_EmbeddedFile::fromPath($tmp_name);
-//						$img->setContentType(File::get_mime($tmp_name));
-//						$src_id = $swift->message->embed($img);
-//
-//						//Browsers reformat URL's so a pattern match
-//						$body = str_replace($matches[1], $src_id, $body);
-//					}
-//				}
+
+				$tmpFile = new GO_Base_Fs_File(GO::config()->tmpdir.$ia['tmp_file']);
+
+				if ($tmpFile->exists()) {
+					//Different browsers reformat URL's to absolute or relative. So a pattern match on the filename.
+					$filename = urlencode($tmpFile->name());
+					$result = preg_match('/="([^"]*'.preg_quote($filename).'[^"]*)"/',$params['body'],$matches);
+					if($result){
+						$img = Swift_EmbeddedFile::fromPath($tmpFile->path());
+						$img->setContentType($tmpFile->mimeType());
+						$contentId = $this->embed($img);
+
+						$params['body'] = str_replace($matches[1], $contentId, $params['body']);
+					}
+				}
 			}
-			exit();
 		}
+		$this->setHtmlAlternateBody($params['body']);
 	}
+	
 }
