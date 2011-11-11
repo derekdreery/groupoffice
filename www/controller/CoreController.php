056e8e46bc94641710b3301ea087b54363049aea
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-11 13:21:49 +0000

    bug fix htmleditor upload

--- www/controller/CoreController.php
+++ www/controller/CoreController.php
@@ -456,7 +456,7 @@ class GO_Core_Controller_Core extends GO_Base_Controller_AbstractController {
 	public function actionUpload($params) {
 
 		$tmpFolder = new GO_Base_Fs_Folder(GO::config()->tmpdir . 'uploadqueue');
-		$tmpFolder->delete();
+//		$tmpFolder->delete();
 		$tmpFolder->create();
 
 		$files = GO_Base_Fs_File::moveUploadedFiles($_FILES['attachments'], $tmpFolder);
