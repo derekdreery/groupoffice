c07323a9d06d2c8533bc34c7e4004a061b019769
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-17 15:50:23 +0000

    Fixed customfields for batchedit

--- www/modules/addressbook/scripts.inc.php
+++ www/modules/addressbook/scripts.inc.php
@@ -2,14 +2,14 @@
 require($GLOBALS['GO_LANGUAGE']->get_language_file('addressbook'));
 $GO_SCRIPTS_JS .= 'GO.addressbook.lang.defaultSalutationExpression="'.String::escape_javascript($lang['addressbook']['defaultSalutation']).'";';
 
-if(isset($GLOBALS['GO_MODULES']->modules['customfields']))
-{
-	
-	require_once($GLOBALS['GO_MODULES']->modules['customfields']['class_path'].'customfields.class.inc.php');
-	$cf = new customfields();
-	$GO_SCRIPTS_JS .= $cf->get_javascript(3, $lang['addressbook']['companies']);
-	$GO_SCRIPTS_JS .= $cf->get_javascript(2, $lang['addressbook']['contacts']);
-}
+//if(isset($GLOBALS['GO_MODULES']->modules['customfields']))
+//{
+//	
+//	require_once($GLOBALS['GO_MODULES']->modules['customfields']['class_path'].'customfields.class.inc.php');
+//	$cf = new customfields();
+//	$GO_SCRIPTS_JS .= $cf->get_javascript(3, $lang['addressbook']['companies']);
+//	$GO_SCRIPTS_JS .= $cf->get_javascript(2, $lang['addressbook']['contacts']);
+//}
 
 
 $export_acl_id = $GLOBALS['GO_CONFIG']->get_setting('go_addressbook_export', 0);
