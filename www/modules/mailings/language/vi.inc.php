131b854055a442108ab42002a96f436629d8030e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-17 07:34:44 +0000

    Added vietnamese

---
+++ www/modules/mailings/language/vi.inc.php
@@ -0,0 +1,27 @@
+<?php
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id: en.js 7708 2011-07-06 14:13:04Z wilmar1980 $
+ * @author Dat Pham <datpx@fab.vn> +84907382345
+ */
+require($GO_LANGUAGE->get_fallback_language_file('mailings'));
+$lang['mailings']['name'] = 'Danh sách mẫu và địa chỉ E-mail';
+$lang['mailings']['description'] = 'Thêm mẫu email và danh sách địa chỉ cho gửi thư tới danh bạ.';
+
+$lang['mailings']['templateAlreadyExists'] = 'Mẫu bạn đang tạo đã tồn tại';
+$lang['mailings']['mailingAlreadyExists'] = 'Email bạn đang tạo đã có';
+
+$lang['mailings']['greet']='Thân ái';
+
+$lang['mailings']['unsubscribe']='Chưa duyệt';
+$lang['mailings']['unsubscription']='Nhấn vào đây để không duyệt từ danh sách.';
+$lang['mailings']['r_u_sure'] = 'Bạn có chắc bỏ duyệt?';
+$lang['mailings']['delete_success'] = 'Bạn đã thành công bỏ duyệt.';
+$lang['mailings']['setCurrentTemplateAsDefault']='thiết lập mẫu hiện thời làm ngầm định';
