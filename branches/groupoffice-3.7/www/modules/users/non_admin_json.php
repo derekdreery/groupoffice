894f48274fec0ba6d66a15368045b824f0d8589f
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-18 09:20:58 +0000

    Added a limit on the user grids when a config variable "limit_usersearch" is set in the Group-Office config file

--- branches/groupoffice-3.7/www/modules/users/non_admin_json.php
+++ branches/groupoffice-3.7/www/modules/users/non_admin_json.php
@@ -62,35 +62,43 @@ switch($task)
 		$search_field = isset($_REQUEST['search_field']) ? ($_REQUEST['search_field']) : null;
 		//$user_id = isset($_REQUEST['user_id']) ? ($_REQUEST['user_id']) : null;
 
-		$user_id = (!$GO_MODULES->modules['users']['read_permission']) ? $GO_SECURITY->user_id : 0;
-
 		$response['results']=array();
 		$response['total']=0;
-		//if($user_id==0 || !empty($query)){
-			
-		$response['total'] = $GO_USERS->search($query, $search_field, $user_id, $start, $limit, $sort,$dir);
+		
+		// Check for the value "limit_usersearch" in the group-office config file and then add the limit.
+		if(!empty($GO_CONFIG->limit_usersearch) && !$GO_SECURITY->has_admin_permission($GO_SECURITY->user_id))
+			$limit = $GO_CONFIG->limit_usersearch;
 
-		if($GO_MODULES->has_module('customfields')) {
-			require_once($GO_MODULES->modules['customfields']['class_path'].'customfields.class.inc.php');
-			$cf = new customfields();
-		}
-
-
-
-		while($user=$GO_USERS->next_record())
+		if(!empty($GO_CONFIG->limit_usersearch) && !empty($_REQUEST['query']) || empty($GO_CONFIG->limit_usersearch) || $GO_SECURITY->has_admin_permission($GO_SECURITY->user_id))
 		{
-			$user['name'] = String::format_name($GO_USERS->f('last_name'),$GO_USERS->f('first_name'),$GO_USERS->f('middle_name'));
-			$user['lastlogin']=Date::get_timestamp($user['lastlogin']);
-			$user['registration_time']=Date::get_timestamp($user['registration_time']);
-			$user['cf']=$user['id'].':'.$user['name'];//special field used by custom fields. They need an id an value in one.
+			$user_id = (!$GO_MODULES->modules['users']['read_permission']) ? $GO_SECURITY->user_id : 0;
 
-			if(isset($cf)){
-				$cf->format_record($user, 8, true);
+			//if($user_id==0 || !empty($query)){
+
+			$response['total'] = $GO_USERS->search($query, $search_field, $user_id, $start, $limit, $sort,$dir);
+
+			if($GO_MODULES->has_module('customfields')) {
+				require_once($GO_MODULES->modules['customfields']['class_path'].'customfields.class.inc.php');
+				$cf = new customfields();
 			}
 
-			$response['results'][]=$user;
-		}
+
+
+			while($user=$GO_USERS->next_record())
+			{
+				$user['name'] = String::format_name($GO_USERS->f('last_name'),$GO_USERS->f('first_name'),$GO_USERS->f('middle_name'));
+				$user['lastlogin']=Date::get_timestamp($user['lastlogin']);
+				$user['registration_time']=Date::get_timestamp($user['registration_time']);
+				$user['cf']=$user['id'].':'.$user['name'];//special field used by custom fields. They need an id an value in one.
+
+				if(isset($cf)){
+					$cf->format_record($user, 8, true);
+				}
+
+				$response['results'][]=$user;
+			}
 		//}
+		}
 
 		echo json_encode($response);
 		break;
