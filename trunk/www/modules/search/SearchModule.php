a43f77fb263df1982baf4df0815a79ea76075271
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-20 14:12:09 +0000

    Share with internal group

--- trunk/www/modules/search/SearchModule.php
+++ trunk/www/modules/search/SearchModule.php
@@ -4,8 +4,4 @@ class GO_Search_SearchModule extends GO_Base_Module{
 	public function autoInstall() {
 		return true;
 	}
-	
-	public function install() {		
-		GO::modules()->search->acl->addGroup(GO::config()->group_everyone);
-	}
 }
