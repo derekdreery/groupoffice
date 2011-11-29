93a66216ce595cba5ccd8d0e3c464f85c5532e78
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-11-29 08:50:07 +0000

    one function for sending emails

--- www/modules/addressbook/sendmailing.php
+++ www/modules/addressbook/sendmailing.php
@@ -18,8 +18,8 @@ GO::session()->setCurrentUser($mailing->user_id);
 
 $addresslist = GO_Addressbook_Model_Addresslist::model()->findByPk($mailing->addresslist_id);
 $mimeData = file_get_contents($mailing->message_path);
-$message = GO_Base_Mail_Message::newInstance();
-$message->loadMimeMessage($mimeData);
+$message = GO_Base_Mail_Message::newInstance()
+				->loadMimeMessage($mimeData);
 
 if($test)
 	echo "Running in test mode\n";
@@ -32,17 +32,23 @@ if(isset($argv[3]) && $argv[3]=='restart') {
 }
 
 
-$joinCriteria = GO_Base_Db_FindCriteria::newInstance()
-						->addRawCondition('t.id', 'a.account_id');
-		
-			$findParams = GO_Base_Db_FindParams::newInstance()
-							->single()
-							->debugSql()
-							->join(GO_Email_Model_Alias::model()->tableName(), $joinCriteria,'a')
-							->criteria(GO_Base_Db_FindCriteria::newInstance()->addCondition('id', $mailing->alias_id,'=','a'));
+$joinCriteria = GO_Base_Db_FindCriteria::newInstance()->addRawCondition('t.id', 'a.account_id');		
+$findParams = GO_Base_Db_FindParams::newInstance()
+	->single()
+	->debugSql()
+	->join(GO_Email_Model_Alias::model()->tableName(), $joinCriteria,'a')
+	->criteria(GO_Base_Db_FindCriteria::newInstance()->addCondition('id', $mailing->alias_id,'=','a')
+);
 $account = GO_Email_Model_Account::model()->find($findParams);
 
-$mailer = GO_Email_Transport::newGoInstance($account);
+$mailer = GO_Base_Mail_Mailer::newGoInstance(GO_Email_Transport::newGoInstance($account));
+
+echo "Will send emails from ".$account->username.".\n";
+
+//$message->setTo(GO::config()->webmaster_email,"Wilmar van Beusekom");
+//
+//$mailer = GO_Base_Mail_Mailer::newGoInstance();
+
 if(!$test){
 	//Rate limit to 100 emails per-minute
 	$mailer->registerPlugin(new Swift_Plugins_ThrottlerPlugin(GO::config()->mailing_messages_per_minute, Swift_Plugins_ThrottlerPlugin::MESSAGES_PER_MINUTE));
@@ -50,37 +56,47 @@ if(!$test){
 
 echo 'Sending a maximum of '.GO::config()->mailing_messages_per_minute.' messages per minute'."\n";
 
-foreach ($addresslist->contacts as $contact) {
-	$current_message = clone $message;
-	$current_message->setTo($contact->email,$contact->name);
-	$current_message->setBody(GO_Addressbook_Model_Template::model()->replaceModelTags($current_message->getBody(),$contact));
-	echo "Sending to contact id: ".$contact->id." email: ".$contact->email."\n";
+$failedRecipients = array();
+
+$bodyWithTags = $message->getBody();
+
+function sendmail($typestring,$message,$model,$mailer,$addresslist,$mailing) {
+	echo "Sending to ".$typestring." id: ".$model->id." email: ".$model->email."\n";
+	
 	try {
-		$mailer->send($current_message);
-	} catch(Exception $e ) {
+		$mailer->send($message);
+	} catch(Exception $e) {
 		$status=$e->getMessage();
 	}
 	if(!empty($status)) {
-
 		echo "---------\n";
-
 		echo "Failed!\n";
 		echo $status."\n";
-
 		echo "---------\n";
 		
 		$error = 1;
 		$sent = 0;
+		unset($status);
 	} else {
 		$error = 0;
 		$sent = 1;
 	}
-	$stmt = GO_Addressbook_Model_SendmailingContact::model()->findByAttributes(
-		array(
-			"addresslist_id" => $addresslist->id,
-			"contact_id" => $contact->id
-		)
-	);
+	
+	if ($typestring=='contact') {
+		$stmt = GO_Addressbook_Model_SendmailingContact::model()->findByAttributes(
+			array(
+				"addresslist_id" => $addresslist->id,
+				"contact_id" => $model->id
+			)
+		);
+	} else {
+		$stmt = GO_Addressbook_Model_SendmailingCompany::model()->findByAttributes(
+			array(
+				"addresslist_id" => $addresslist->id,
+				"company_id" => $model->id
+			)
+		);
+	}
 	if ($coupling = $stmt->fetch())
 		$coupling->delete();
 
@@ -91,40 +107,16 @@ foreach ($addresslist->contacts as $contact) {
 	$mailing->save();
 }
 
+foreach ($addresslist->contacts as $contact) {
+	$message->setTo($contact->email,$contact->name);	
+	$message->setBody(GO_Addressbook_Model_Template::model()->replaceModelTags($bodyWithTags,$contact));
+	sendmail('contact',$message,$contact,$mailer,$addresslist,$mailing);
+}
+
 foreach ($addresslist->companies as $company) {
-	$current_message = clone $message;
-	$current_message->setTo($company->email,$company->name);
-	$current_message->setBody(GO_Addressbook_Model_Template::model()->replaceModelTags($current_message->getBody(),$company));
-	echo "Sending to company id: ".$company->id." email: ".$company->email."\n";
-	try {
-		$mailer->send($current_message);
-	} catch(Exception $e ) {
-		$status=$e->getMessage();
-	}
-	if(!empty($status)) {
-
-		echo "---------\n";
-
-		echo "Failed!\n";
-		echo $status."\n";
-
-		echo "---------\n";
-	}
-	$stmt = GO_Addressbook_Model_SendmailingCompany::model()->findByAttributes(
-		array(
-			"addresslist_id" => $addresslist->id,
-			"company_id" => $company->id
-		)
-	);
-
-	if ($coupling = $stmt->fetch())
-		$coupling->delete();
-
-	$mailing->setAttributes(array(
-			"sent" => $sent+$mailing->sent,
-			"errors" => $error+$mailing->errors
-		));
-	$mailing->save();
+	$message->setTo($company->email,$company->name);
+	$message->setBody(GO_Addressbook_Model_Template::model()->replaceModelTags($bodyWithTags,$company));
+	sendmail('company',$message,$company,$mailer,$addresslist,$mailing);
 }
 
 echo "Mailing finished\n";
