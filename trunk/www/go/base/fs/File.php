282ed0bf08fc105b56ef3c424abe6f57c01e74a7
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-12-06 14:11:51 +0000

    Added humansize function

--- trunk/www/go/base/fs/File.php
+++ trunk/www/go/base/fs/File.php
@@ -29,6 +29,41 @@ class GO_Base_Fs_File extends GO_Base_Fs_Base{
 	}
 	
 	/**
+	 * Get the size formatted nicely like 1.5 MB
+	 * 
+	 * @param int $decimals
+	 * @return string 
+	 */
+	public function humanSize($decimals = 1) {
+		$size = $this->size();
+		if($size==0)
+			return 0;
+		
+		switch ($size) {
+			case ($size > 1073741824) :
+				$size = GO_Base_Util_Number::localize($size / 1073741824, $decimals);
+				$size .= " GB";
+				break;
+
+			case ($size > 1048576) :
+				$size = GO_Base_Util_Number::localize($size / 1048576, $decimals);
+				$size .= " MB";
+				break;
+
+			case ($size > 1024) :
+				$size = GO_Base_Util_Number::localize($size / 1024, $decimals);
+				$size .= " KB";
+				break;
+
+			default :
+				$size = GO_Base_Util_Number::localize($size, $decimals);
+				$size .= " bytes";
+				break;
+		}
+		return $size;
+	}
+	
+	/**
 	 * Delete the file
 	 * 
 	 * @return boolean 
