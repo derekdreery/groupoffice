3e551be6f8da10ec9710b21581b7ee3dd6754439
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-12-13 13:32:35 +0000

    Created getHtmlTable for cart

--- trunk/www/modules/sites/templates/Example/cart.php
+++ trunk/www/modules/sites/templates/Example/cart.php
@@ -8,46 +8,8 @@ require('header.php');
 <!--								<h1>Shopping cart</h1>-->
 								
 								<?php $cart = new GO_Webshop_Util_Cart(); ?>
-
-								<?php if(!$cart->hasProducts()):?>	
-									<p>There are no products in your shopping cart</p>
-								<?php else : ?>
-									<?php GO_Base_Html_Form::renderBegin('webshop/cart/updatePrices','cart',true); ?>
-									<table class="cart">
-										<tr>
-										<th>Amount</th><th>Name</th><th>Price</th>
-										</tr>
-									<?php	$cart = $cart->getCart(); ?>
-
-									
-									<?php foreach ($cart['products'] as $p): ?>
-										<?php $language = $p['product']->getLanguage(); ?>
-										<tr>
-											<td>
-												<?php 
-													GO_Base_Html_Input::render(array(
-														"required" => true,
-														"label" => "",
-														"name" => "product[".$p['product']->id."]",
-														"value" => $p['amount'],
-														"renderContainer" => false,
-														"class" => "amount"
-													));
-												?>
-											</td>
-											<td><?php echo $language->name; ?></td>
-											<td align="left">€&nbsp;<?php echo $p['product']->list_price*$p['amount']; ?></td>
-										</tr>
-										<?php endforeach;?>
-								<?php	endif; ?>
-									<tr><td colspan="3" class="minicart_total" style="text-align:right;font-weight:bold;">Total: € <?php echo number_format($cart['subtotal']); ?> <a target="_blank" href="http://finance.yahoo.com/currency/convert?amt=<?php echo $cart['subtotal']; ?>&amp;from=EUR&amp;to=USD&amp;submit=Convert">(Convert currency)</a></td></tr>
-									</table>
-								<div onmouseout="this.className='button-green';" onmouseover="this.className='button-green-hover';" class="button-green">
-								<div class="button-green-right">
-									<a class="button-green-center" onclick="document.forms['cart'].submit();" href="#"> 
-									Update amounts
-									</a>
-								</div>
+								
+								<?php echo $cart->getCartTable(false,true,true,true);?>								
 							</div>
 							<?php GO_Base_Html_Form::renderEnd(); ?>
 							</div>
