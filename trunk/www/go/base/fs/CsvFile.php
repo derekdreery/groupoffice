a56c6583dec2f1a69972db64d75de90a51a07e8b
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-12-23 12:48:52 +0000

    GO_Base_Fs_CsvFile, a GO_Base_Fs_File for either writing or reading from CSVs

--- trunk/www/go/base/fs/CsvFile.php
+++ trunk/www/go/base/fs/CsvFile.php
@@ -37,7 +37,7 @@ class GO_Base_Fs_CsvFile extends GO_Base_Fs_File{
 	private function _setCSV($mode){
 		if(!isset($this->_csv)){
 			if ($mode='w') {
-				$this->_csv = new GO_Base_Csv_Writer();
+				$this->_csv = new GO_Base_Csv_Writer($this->name());
 			} elseif ($mode='r') {
 				$this->_csv = new GO_Base_Csv_Reader($this->name());
 			}
