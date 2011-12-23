e55108962f19473ab070ac7e70e2e8295bddd45f
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-12-23 08:10:11 +0000

    Created HTTP utils file

--- trunk/www/go/base/util/Common.php
+++ trunk/www/go/base/util/Common.php
@@ -17,50 +17,6 @@
  * @package GO.base.util 
  */
 class GO_Base_Util_Common {
-
-	/**
-	 * Get information about the browser currently using Group-Office.
-	 * 
-	 * @return array('name','version')
-	 */
-	public static function getBrowser() {
-		if(!isset($_SERVER['HTTP_USER_AGENT']))
-			return array('version'=>0, 'name'=>'OTHER');
-		
-		if (preg_match("'msie ([0-9].[0-9]{1,2})'i", $_SERVER['HTTP_USER_AGENT'], $log_version)) {
-			$browser['version'] = $log_version[1];
-			$browser['name'] = 'MSIE';
-		} elseif (preg_match("'opera/([0-9].[0-9]{1,2})'i", $_SERVER['HTTP_USER_AGENT'], $log_version)) {
-			$browser['version'] = $log_version[1];
-			$browser['name'] = 'OPERA';
-		} elseif (preg_match("'mozilla/([0-9].[0-9]{1,2}).*gecko/([0-9]+)'i", $_SERVER['HTTP_USER_AGENT'], $log_version)) {
-			$browser['version'] = $log_version[1];
-			$browser['name'] = 'MOZILLA';
-			$browser['subversion'] = $log_version[2];
-		} elseif (preg_match("'netscape/([0-9].[0-9]{1,2})'i", $_SERVER['HTTP_USER_AGENT'], $log_version)) {
-			$browser['version'] = $log_version[1];
-			$browser['name'] = 'NETSCAPE';
-		} elseif (preg_match("'safari/([0-9]+.[0-9]+)'i", $_SERVER['HTTP_USER_AGENT'], $log_version)) {
-			$browser['version'] = $log_version[1];
-			$browser['name'] = 'SAFARI';
-		} else {
-			$browser['version'] = 0;
-			$browser['name'] = 'OTHER';
-		}
-		return $browser;
-	}
-
-	/**
-	 * Check if the current user is using internet explorer
-	 * 
-	 * @return boolean 
-	 */
-	public static function isInternetExplorer() {
-		$b = self::getBrowser();
-
-		return $b['name'] == 'MSIE';
-	}
-	
 	/**
 	 * Check if this is a windows server
 	 * 
@@ -123,50 +79,6 @@ class GO_Base_Util_Common {
 		$format= str_replace('{country}', GO::t($isoCountry,'base','countries'), $format);
 
 		return preg_replace("/(\r\n)+|(\n|\r)+/", "\n", $format);
-	}
-	
-	
-	
-	public static function outputDownloadHeaders(GO_Base_Fs_File $file, $inline=false, $cache=false) {
-		if($file->exists()){
-			header('Content-Length: ' . $file->size());
-			header("Last-Modified: " . gmdate("D, d M Y H:i:s", $file->mtime())." GMT");
-			header("ETag: " . md5_file($file->path()));
-		}
-		header('Content-Transfer-Encoding: binary');		
-		
-		$disposition = $inline ? 'inline' : 'attachment';
-
-		if ($cache) {
-			header("Expires: " . date("D, j M Y G:i:s ", time() + 86400) . 'GMT'); //expires in 1 day
-			header('Cache-Control: cache');
-			header('Pragma: cache');
-		}
-		if (GO_Base_Util_Common::isInternetExplorer()) {
-			header('Content-Type: application/download');
-			header('Content-Disposition: '.$disposition.'; filename="' . $file->name() . '"');
-
-			if (!$cache) {
-				header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-				header('Pragma: public');
-			}
-		} else {
-			header('Content-Type: ' .$file->mimeType());
-			header('Content-Disposition: '.$disposition.'; filename="' . $file->name() . '"');
-
-			if (!$cache) {
-				header('Pragma: no-cache');
-			}
-		}
-	}
-	
-	/**
-	 * Check if the request was made with ajax.
-	 * 
-	 * @return boolean 
-	 */
-	public static function isAjaxRequest(){
-		return isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && $_SERVER["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest";  
 	}
 
 	
