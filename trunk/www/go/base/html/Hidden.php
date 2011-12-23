02b643b9c7b0f99fdb0d2c02d0a9bcb3cbabc020
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2011-12-23 13:41:30 +0000

    Added a hidden field

---
+++ trunk/www/go/base/html/Hidden.php
@@ -0,0 +1,19 @@
+<?php
+
+class GO_Base_Html_Hidden extends GO_Base_Html_Input {
+	
+	public static function render($attributes,$echo=true) {
+		$i = new self($attributes);
+		if($echo)
+			echo $i->getHtml();
+		else
+			return $i->getHtml();
+	}
+	
+	protected function init() {
+		$this->attributes['type']='hidden';		
+		$this->attributes['class'].=' hidden';
+	}
+
+
+}
