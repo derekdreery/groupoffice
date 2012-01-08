df2257747c4ff3878bd7a7edf2301e9ae1ae7460
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-08 15:33:57 +0000

    Upgraded SabreDAV 1.5.6

--- www/go/vendor/SabreDAV/lib/Sabre/DAV/Server.php
+++ www/go/vendor/SabreDAV/lib/Sabre/DAV/Server.php
@@ -5,7 +5,7 @@
  * 
  * @package Sabre
  * @subpackage DAV
- * @copyright Copyright (C) 2007-2011 Rooftop Solutions. All rights reserved.
+ * @copyright Copyright (C) 2007-2012 Rooftop Solutions. All rights reserved.
  * @author Evert Pot (http://www.rooftopsolutions.nl/) 
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
  */
@@ -127,7 +127,7 @@ class Sabre_DAV_Server {
      *
      * @var bool 
      */
-    public $debugExceptions = true;
+    public $debugExceptions = false;
 
     /**
      * This property allows you to automatically add the 'resourcetype' value 
@@ -237,9 +237,6 @@ class Sabre_DAV_Server {
             $this->httpResponse->sendStatus($httpCode);
             $this->httpResponse->setHeaders($headers);
             $this->httpResponse->sendBody($DOM->saveXML());
-						
-						GO::debug($e->getMessage());
-						GO::debug($e->getTraceAsString());
 
         }
 
