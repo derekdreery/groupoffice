ee96652fbe8c7ded0f57442e78175b9a58316253
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-09 14:49:09 +0000

    Fixed for mod_rewrite

--- www/modules/sites/install/updates.inc.php
+++ www/modules/sites/install/updates.inc.php
@@ -1,3 +1,8 @@
 <?php
 $updates["201112081020"][]="ALTER TABLE `si_sites` ADD `login_page` VARCHAR( 255 ) NOT NULL DEFAULT 'login'";
 $updates["201112081020"][]="ALTER TABLE `si_pages` ADD `login_required` BOOLEAN NOT NULL DEFAULT '0'";
+
+
+$updates["201112081020"][]="ALTER TABLE `si_sites` ADD `ssl` BOOLEAN NOT NULL DEFAULT '0',
+ADD `mod_rewrite` BOOLEAN NOT NULL DEFAULT '0',
+ADD `mod_rewrite_base_path` VARCHAR( 50 ) NOT NULL DEFAULT '/'";
