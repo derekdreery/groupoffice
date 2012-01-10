23319db01c8c3415dbfff3bc63c423ec12c54bd4
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-10 08:32:20 +0000

    include GO.php instead of Group-Office.php

--- scripts/mvc/createModel.php
+++ scripts/mvc/createModel.php
@@ -30,7 +30,7 @@ else
 
 
 function generateModel($className, $table, $moduleName){
-	require('../../www/Group-Office.php');
+	require('../../www/GO.php');
 	$conn = GO::getDbConnection();
 	
 	$sql = "SHOW FIELDS FROM `".$table."`;";
@@ -81,13 +81,18 @@ function generateModel($className, $table, $moduleName){
 
 
 	echo '<pre>'.htmlspecialchars('<?php
-/*
+/**
  * Copyright Intermesh BV.
  *
  * This file is part of Group-Office. You should have received a copy of the
  * Group-Office license along with Group-Office. See the file /LICENSE.TXT
  *
  * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @package GO.modules.'.$moduleName.'
+ * @version $Id: '.$className.'.php 7607 '.$date.'Z <<USERNAME>> $
+ * @copyright Copyright Intermesh BV.
+ * @author <<FIRST_NAME>> <<LAST_NAME>> <<EMAIL>>@intermesh.nl
  */
  
 /**
