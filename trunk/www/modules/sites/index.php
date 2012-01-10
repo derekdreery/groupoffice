1576d29b5f555d25cc68d22954debee9abffc683
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-10 14:27:15 +0000

    Fixed for mod_rewrite and added mod_rewrite example

--- trunk/www/modules/sites/index.php
+++ trunk/www/modules/sites/index.php
@@ -5,7 +5,7 @@ $site_id=isset($_REQUEST['site_id']) ? $_REQUEST['site_id'] : '1';
 
 $site = GO_Sites_Model_Site::model()->findByPk($site_id);
 
-$path = ltrim($_SERVER['REDIRECT_URL'],'/');
+$path = isset($_REQUEST['path']) ? $_REQUEST['path'] : '';
 
 $page = GO_Sites_Model_Page::model()->findSingleByAttributes(array('site_id'=>$site_id, 'path'=>$path));
 
@@ -19,3 +19,27 @@ $controller = new $page->controller($site, $page);
 $action = 'action'.$page->controller_action;
 
 $controller->$action($_REQUEST);
+
+/**
+ * EXAMPLE MOD_REWRITE RULE FOR A SITE
+
+
+<VirtualHost *:80>
+ServerName group-office.com
+ServerAlias testshop.group-office.com
+DocumentRoot /var/www/testshop.group-office.com/html
+RewriteLog "/var/log/apache2/rewrite.log"
+RewriteLogLevel 3
+<Directory /var/www/testshop.group-office.com/html>
+RewriteEngine On
+RewriteBase /
+
+RewriteRule ^index.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteRule (.*) /groupoffice/modules/sites/index.php?site_id=1&path=$1&%{QUERY_STRING} [L]
+
+</Directory>
+</VirtualHost>
+
+
+*/
