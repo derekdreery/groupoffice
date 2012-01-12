4872c04a7cacd850a046dba6b799d121fdcc4bae
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-12 13:41:37 +0000

    Fixed

--- www/modules/tasks/install/updates.inc.php
+++ www/modules/tasks/install/updates.inc.php
@@ -66,3 +66,4 @@ $updates["201112221547"][]="CREATE TABLE IF NOT EXISTS `cf_ta_tasks` (
 
 $updates["201112221547"][]="ALTER TABLE `ta_tasks` ADD `percentage_complete` TINYINT NOT NULL DEFAULT '0'";
 $updates["201112221547"][]="ALTER TABLE `ta_tasks` ADD `project_id` INT NOT NULL DEFAULT '0'";
+$updates["201201121400"][]="ALTER TABLE `ta_tasklists` DROP `acl_write`";
