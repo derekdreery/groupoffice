4ee870c46c5d972b931b22d48c92c7a16e41e085
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-12 15:26:55 +0000

    Added function descriptions and fixed the accessdenied message

--- www/modules/sites/controller/SiteController.php
+++ www/modules/sites/controller/SiteController.php
@@ -12,9 +12,9 @@
  * The GO_Tasks_Controller_Category controller
  *
  * @package GO.modules.Tasks
- * @version $Id: GO_Tasks_Controller_Category.php 7607 2011-09-20 10:07:50Z <<USERNAME>> $
+ * @version $Id: GO_Tasks_Controller_Category.php 7607 2011-09-20 10:07:50Z wsmits $
  * @copyright Copyright Intermesh BV.
- * @author <<FIRST_NAME>> <<LAST_NAME>> <<EMAIL>>@intermesh.nl
+ * @author Wesley Smits wsmits@intermesh.nl
  */
 
 class GO_Sites_Controller_Site extends GO_Base_Controller_AbstractController{
@@ -33,13 +33,43 @@ class GO_Sites_Controller_Site extends GO_Base_Controller_AbstractController{
 	 */
 	private $page;
 	
+	/**
+	 * The path to the template folder in the sites module.
+	 *
+	 * @var string 
+	 */
 	protected $rootTemplatePath;
+	
+	/**
+	 * The url to the template folder in the sites module.
+	 * 
+	 * @var string 
+	 */
 	protected $rootTemplateUrl;
+	
+	/**
+	 * The url to the template folder of the current module
+	 * 
+	 * @var string 
+	 */
 	protected $templateUrl;
+	
+	/**
+	 * The path of the template folder in the current module
+	 * 
+	 * @var string 
+	 */
 	protected $templateFolder;
 		
 //	protected $notification;
 	
+	/**
+	 * Construct this object.
+	 * Sets the private variables site and page.
+	 * 
+	 * @param GO_Sites_Model_Site $site
+	 * @param GO_Sites_Model_Page $page 
+	 */
 	public function __construct($site, $page) {	
 		
 		$this->site=$site;
@@ -48,18 +78,39 @@ class GO_Sites_Controller_Site extends GO_Base_Controller_AbstractController{
 		parent::__construct();
 	}
 	
+	/**
+	 * Returns the current site object
+	 * 
+	 * @return GO_Sites_Model_Site The current site
+	 */
 	public function getSite(){
 		return $this->site;
 	}
 	
+	/**
+	 * Returns the current page object
+	 * 
+	 * @return GO_Sites_Model_Page The current page 
+	 */
 	public function getPage(){
 		return $this->page;
 	}
 	
+	/**
+	 * Sets the access permissions for guests
+	 * Defaults to '*' which means that all functions can be accessed by guests.
+	 * 
+	 * @return array List of all functions that can be accessed by guests 
+	 */
 	protected function allowGuests() {
-		return array('index');
+		return array('*');
 	}
 	
+	/**
+	 * Initialize this object and sets the protected variables
+	 * 
+	 * @return mixed 
+	 */
 	protected function init() {
 		
 		$this->_checkSessionVars();
@@ -74,14 +125,29 @@ class GO_Sites_Controller_Site extends GO_Base_Controller_AbstractController{
 		return parent::init();
 	}
 	
+	/**
+	 * Get the path of the template folder in the sites module.
+	 * 
+	 * @return string The path to the template folder in the sites module. 
+	 */
 	public function getRootTemplatePath(){
 		return $this->rootTemplatePath;
 	}
 	
+	/**
+	 * Get the url of the template folder in the sites module.
+	 * 
+	 * @return string The url to the template folder in the sites module.
+	 */
 	public function getRootTemplateUrl(){
 		return $this->rootTemplateUrl;
 	}
 		
+	/**
+	 * Private function to get the template folder path of the current module.
+	 * 
+	 * @return string The path to the template folder in the current module. 
+	 */
 	private function _getTemplateFolderPath(){
 		$path = '';
 		$moduleName = $this->getModule()->id;
@@ -93,6 +159,11 @@ class GO_Sites_Controller_Site extends GO_Base_Controller_AbstractController{
 		return $path;
 	}
 	
+	/**
+	 * Private function to get the template folder url of the current module.
+	 * 
+	 * @return string The url to the template folder in the current module. 
+	 */
 	private function _getTemplateFolderUrl(){
 		$url = '';
 		$moduleName = $this->getModule()->id;
@@ -104,19 +175,36 @@ class GO_Sites_Controller_Site extends GO_Base_Controller_AbstractController{
 		return $url;
 	}
 	
-	
-	
 	/**
+	 * The default index action
 	 * This default action should be overrriden
+	 * 
+	 * @param array $params The parameters that need to be passed through to 
+	 * the page.
 	 */
 	protected function actionIndex($params){		
 		$this->renderPage($params);		
 	}
 	
+	/**
+	 * Function that you can override.
+	 * This function will be called right before the renderpage function.
+	 * 
+	 * @param array $params The parameters that need to be passed through to 
+	 * the page.
+	 */
 	protected function beforeRenderPage($params){
 		
 	}
 
+	/**
+	 * Render the page 
+	 * 
+	 * @todo Make the header and footer dynamic
+	 * 
+	 * @param array $params The parameters that need to be passed through to 
+	 * the page.
+	 */
 	protected function renderPage($params=array()){
 		
 		$this->beforeRenderPage($params);
@@ -130,6 +218,10 @@ class GO_Sites_Controller_Site extends GO_Base_Controller_AbstractController{
 		
 	}
 
+	/**
+	 * Checks if the last path needs to be changed and sets the right last path 
+	 * in the session. 
+	 */
 	private function _checkSessionVars(){
 		
 		if(!isset(GO::session()->values['sites']))
@@ -141,49 +233,30 @@ class GO_Sites_Controller_Site extends GO_Base_Controller_AbstractController{
 		if($this->page->path == $this->site->getLoginPath()){
 			if($this->site->getLastPath() == $this->site->getLoginPath())
 				$this->site->setLastPath($this->site->getHomePagePath());
-		} else {
+		} else if($this->page->path == $this->site->getRegisterPath()){
+			if($this->site->getLastPath() == $this->site->getRegisterPath())
+				$this->site->setLastPath($this->site->getHomePagePath());
+		}else {
 			GO::session()->values['sites']['lastPath'] = $this->page->path;
 		}
 	}
 	
+	/**
+	 * Check if the page needs that a user is logged in and checks if the user is 
+	 * logged in also. 
+	 */
 	private function _checkAuth() {
 		if($this->page->login_required && !GO::user()){
 			$this->pageRedirect($this->site->getLoginPath());
 		}
 	}
-	
-//	private function _handleForm($params){
-//		if(isset($params['formRoute'])){
-//			$params['r']=$params['formRoute'];
-//			if(GO_Base_Html_Input::checkRequired()){
-//				$router = new GO_Base_Router();
-//				$router->runController($params);
-//			}
-//		}
-//	}
-	
+
+	/**
+	 * Will be displayed when a page is not found 
+	 */
 	protected function notFound(){
 		echo '<h1>Not found</h1>';
 	}
-	
-//	/**
-//	 * Generate a URL to a page.
-//	 * 
-//	 * @param string $path
-//	 * @param array $params
-//	 * @param boolean $relative
-//	 * @return string 
-//	 */
-//	public static function pageUrl($path, $params=array(), $relative=true){
-//		$params['p']=$path;	
-//		
-//		$page = GO_Sites_Model_Page::model()->findSingleByAttribute('path', $path);
-//		
-//		if(!$page)
-//			throw new Exception('NOT FOUND');
-//		return $page->getUrl($params, $relative);
-//	}
-	
 	
 	/**
 	 * Generate a controller URL.
@@ -200,9 +273,11 @@ class GO_Sites_Controller_Site extends GO_Base_Controller_AbstractController{
 	
 	
 	/**
-	 * Redirect to a page.
+	 * Redirect to the given page.
 	 * 
-	 * @param string $path 
+	 * @param string $path The path of the redirect page
+	 * @param array $params The parameters that need to be passed through the 
+	 * redirect page
 	 */
 	protected function pageRedirect($path = '', $params=array()) {
 		header('Location: ' .$this->site->pageUrl($path, $params, true, false));
