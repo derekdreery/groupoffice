1cbe88ffc0f10534c9fc83fc58559ba5e43b050e
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-12 13:04:19 +0000

    Fixed Exception

--- www/views/Extjs3/Exception.php
+++ www/views/Extjs3/Exception.php
@@ -4,11 +4,11 @@ if(GO_Base_Util_Http::isAjaxRequest()){
 }elseif(PHP_SAPI=='cli'){
 	echo "ERROR: ".trim($data['feedback'])."\n\n";
 	if(GO::config()->debug)
-		echo $data['trace']."\n\n";
+		echo $data['exception']."\n\n";
 }else
 {
 	echo '<h1>Error</h1>';
 	echo '<p style="color:red">'.$data['feedback'].'</p>';
 	if(GO::config()->debug)
-		echo '<pre>'.$data['trace'].'</pre>';
+		echo '<pre>'.$data['exception'].'</pre>';
 }
