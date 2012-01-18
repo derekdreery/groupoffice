0bf81b01e1b3f1f6bbceabfde9199c7b3b2d7cd7
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-18 13:53:20 +0000

    Fixed forgot password for Groupoffice GUI

--- trunk/www/modules/sites/language/en.php
+++ trunk/www/modules/sites/language/en.php
@@ -2,3 +2,7 @@
 
 $l['name']='CMS module';
 $l['description']='A module to maintain websites';
+
+$l['noemail']='No email given!';
+$l['invalidusertoken']='The provided usertoken is not valid!';
+$l['invaliduser']='No user found with the given email address!';
