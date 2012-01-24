ca7dce3a36cb4fb710841cc65fdf60ad82f9cac6
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-24 15:26:39 +0000

    Fixed cart

--- trunk/www/modules/sites/templates/Example/sidebar.php
+++ trunk/www/modules/sites/templates/Example/sidebar.php
@@ -1,23 +1,13 @@
 <h1>Shopping cart</h1>
-<?php
 
-$cart = GO_Webshop_Model_Webshop::getByController($this)->getCart();
+<?php 
+$webshop = GO_Webshop_Model_Webshop::getByController($this);
+echo $webshop->getWebshopOrder()->getCart()->getSmallTable(); ?>
 
-
-if (!$cart->hasProducts()) {
-	?>	
-	<p>There are no products in your shopping cart</p>
-	<?php
-} else {
-	echo $cart->getCartTable();
-	?>
-	<div class="button-green-side" onmouseover="this.className='button-green-side-hover';"  onmouseout="this.className='button-green-side';">
-		<div class="button-green-side-right">
-			<a href="<?php echo $this->pageUrl('cart'); ?>" class="button-green-side-center"> 
-				Checkout
-			</a>
-		</div>
+<div class="button-green-side" onmouseover="this.className='button-green-side-hover';"  onmouseout="this.className='button-green-side';">
+	<div class="button-green-side-right">
+		<a href="<?php echo $this->pageUrl($webshop->getCartPath()); ?>" class="button-green-side-center"> 
+			Checkout
+		</a>
 	</div>
-		<?php
-	}
-	?>
+</div>
