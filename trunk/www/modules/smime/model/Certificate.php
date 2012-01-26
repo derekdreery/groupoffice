f296e7787e7a3d70191b912b77286b28cdce02bb
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-26 12:49:29 +0000

    Generated docblocks

--- trunk/www/modules/smime/model/Certificate.php
+++ trunk/www/modules/smime/model/Certificate.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * Group-Office
+ * 
+ * Copyright Intermesh BV. 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @license AGPL/Proprietary http://www.group-office.com/LICENSE.TXT
+ * @link http://www.group-office.com
+ * @package GO.modules.smime.model
+ * @version $Id: example.php 7607 20120101Z <<USERNAME>> $
+ * @copyright Copyright Intermesh BV.
+ * @author <<FIRST_NAME>> <<LAST_NAME>> <<EMAIL>>@intermesh.nl
+ */
+ 
+/**
+ * The GO_Smime_Model_Certificate model
+ *
+ * @package GO.modules.smime.model
+ * @property int $account_id
+ * @property string $cert
+ * @property boolean $always_sign
+ */
+
 class GO_Smime_Model_Certificate extends GO_Base_Db_ActiveRecord {
 
 	/**
