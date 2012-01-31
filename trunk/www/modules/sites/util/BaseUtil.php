6553bbfb04a56f0f2b3ab5602e6526a383b1e19c
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-01-31 11:40:54 +0000

    Added discount

--- trunk/www/modules/sites/util/BaseUtil.php
+++ trunk/www/modules/sites/util/BaseUtil.php
@@ -7,4 +7,8 @@ class GO_Sites_Util_BaseUtil {
 	public function __construct($site_id){
 		$this->site_id = $site_id;
 	}
+	
+	public function getWebshop(){
+		return GO_Webshop_Model_Webshop::model()->findSingleByAttribute('site_id', $this->site_id);
+	}
 }
