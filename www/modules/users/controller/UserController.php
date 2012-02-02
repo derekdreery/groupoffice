4c9c050edc103f08be36a8032f53d4a35a90b0dc
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-02-02 10:23:09 +0000

    Ignore acl ojn user controller

--- www/modules/users/controller/UserController.php
+++ www/modules/users/controller/UserController.php
@@ -4,9 +4,9 @@ class GO_Users_Controller_User extends GO_Base_Controller_AbstractModelControlle
 
 	protected $model = 'GO_Base_Model_User';
 
-//	protected function ignoreAclPermissions() {
-//		return array('*');
-//	}
+	protected function ignoreAclPermissions() {
+		return array('*');
+	}
 	
 	protected function afterDisplay(&$response, &$model, &$params) {
 		
