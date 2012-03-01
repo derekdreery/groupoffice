b4778e911d14f0143fc7c11e8b45e2c64f00efb9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-03-01 12:25:50 +0000

    Fixed bookmarks upgrade

--- www/modules/bookmarks/controller/CategoryController.php
+++ www/modules/bookmarks/controller/CategoryController.php
@@ -3,11 +3,10 @@ class GO_Bookmarks_Controller_Category extends GO_Base_Controller_AbstractModelC
 
 	protected $model ='GO_Bookmarks_Model_Category';
 	
-	protected function actionTest($params)
-	{
-		echo 'test';
+	protected function formatColumns(GO_Base_Data_ColumnModel $columnModel) {
+		$columnModel->formatColumn('user_name', '$model->user->name');
+		return parent::formatColumns($columnModel);
 	}
-	
 	protected function getStoreParams($params) {
 		return array(
 				'order' => 'name',
