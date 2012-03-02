c8cfbf18ca251ed0a9ac475be9dfb6adfce4a120
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-03-02 09:18:30 +0000

    custom logo and custom product_name

--- trunk/www/modules/smime/language/nb.inc.php
+++ trunk/www/modules/smime/language/nb.inc.php
@@ -7,7 +7,7 @@ $lang['smime']['description']='Utvide e-postmodulen med støtte for SMIME signer
 $lang['smime']['noPublicCertForEncrypt']="Kan ikke kryptere meldingen fordi du ikke har det offentlige sertifiktatet for %s. Du kan åpne en signert melding fra mottageren og kontrollere signaturen for å importere den offentlige nøkkelen.";
 $lang['smime']['noPrivateKeyForDecrypt']="Denne meldingen er kryptert, men du har ikke den private nøkkelen som kreves for å dekryptere den.";
 
-$lang['smime']['badGoLogin']="Group-Office passordet er feil.";
+$lang['smime']['badGoLogin']="{product_name} passordet er feil.";
 $lang['smime']['smime_pass_matches_go']="Passordet til SMIME nøkkelen er det samme som GroupOffice passordet. Av sikkerhetsårsaker er dette ikke tillatt!";
 $lang['smime']['smime_pass_empty']="Denne SMIME nøkkelen har ikke noe passord. Av sikkerhetsårsaker er dette ikke tillatt!";
 
