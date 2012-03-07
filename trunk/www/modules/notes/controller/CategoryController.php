f16998e157065667a7cc75ea1fef30360ea6ab75
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-03-07 12:55:17 +0000

    New MultiSelectGrid Component

--- trunk/www/modules/notes/controller/CategoryController.php
+++ trunk/www/modules/notes/controller/CategoryController.php
@@ -3,16 +3,6 @@ class GO_Notes_Controller_Category extends GO_Base_Controller_AbstractModelContr
 	
 	protected $model = 'GO_Notes_Model_Category';
 	
-	
-//	protected function getStoreMultiSelectProperties(){
-//		return array('requestParam'=>'no-multiselect');
-//	}
-//	
-//	protected function getStoreMultiSelectDefault(){
-//		$category = GO_Notes_NotesModule::getDefaultNoteCategory(GO::user()->id);
-//		return $category->id;
-//	}	
-	
 	protected function formatColumns(GO_Base_Data_ColumnModel $columnModel) {
 		$columnModel->formatColumn('user_name','$model->user ? $model->user->name : 0');
 		return parent::formatColumns($columnModel);
