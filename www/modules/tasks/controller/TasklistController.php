dcedb66d6e9d9529fd164bceec6d1ad8cce95d45
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-03-08 08:18:05 +0000

    import ics for tasklist

--- www/modules/tasks/controller/TasklistController.php
+++ www/modules/tasks/controller/TasklistController.php
@@ -42,4 +42,24 @@ class GO_Tasks_Controller_Tasklist extends GO_Base_Controller_AbstractModelContr
 				'user_name'=>'$model->user->name'
 				);
 	}
+	
+	public function actionImportIcs($params) {
+		$response = array( 'success' => true );
+		$count = 0;
+		if (!file_exists($_FILES['ical_file']['tmp_name'][0])) {
+			throw new Exception($lang['common']['noFileUploaded']);
+		}else {
+			$file = new GO_Base_Fs_File($_FILES['ical_file']['tmp_name'][0]);
+			$file->convertToUtf8();
+			$contents = $file->getContents();
+			$vcal = GO_Base_VObject_Reader::read($contents);
+			foreach($vcal->vtodo as $vtask) {
+				$event = new GO_Tasks_Model_Task();
+				$event->importVObject( $vtask, array('tasklist_id'=>$params['tasklist_id']) );
+				$count++;
+			}
+		}
+		$response['feedback'] = sprintf(GO::t('import_success','tasklist'), $count);
+		return $response;
+	}
 }
