573449db2b1fd64a6b6ac0113a861245a571272b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-03-12 15:04:39 +0000

    More info

--- www/modules/files/controller/FolderController.php
+++ www/modules/files/controller/FolderController.php
@@ -598,7 +598,7 @@ class GO_Files_Controller_Folder extends GO_Base_Controller_AbstractModelControl
 //			$fsFolder->appendNumberToNameIfExists();
 			
 			if(($existingFolder = $destinationFolder->hasFolder($fsFolder->name()))){
-				GO::debug("Merging into existing folder.".$folder->path.' -> '.$existingFolder->path);
+				GO::debug("Merging into existing folder.".$folder->path.' ('.$folder->id.') -> '.$existingFolder->path).' ('.$existingFolder->id.')';
 				if (!empty($model->acl_id))
 					$existingFolder->acl_id = $model->acl_id;
 	
@@ -607,22 +607,6 @@ class GO_Files_Controller_Folder extends GO_Base_Controller_AbstractModelControl
 				$existingFolder->save();
 				
 				$existingFolder->moveContentsFrom($folder);
-				
-//				$folder->syncFileSystem();				
-//				
-//				$stmt = $folder->folders();
-//				while($subfolder = $stmt->fetch()){
-//					$subfolder->parent_id=$existingFolder->id;
-//					$subfolder->appendNumberToNameIfExists();
-//					$subfolder->save();
-//				}
-//				
-//				$stmt = $folder->files();
-//				while($file = $stmt->fetch()){
-//					$file->folder_id=$existingFolder->id;
-//					$file->appendNumberToNameIfExists();
-//					$file->save();
-//				}
 				
 				//delete empty folder.
 				$folder->delete();				
