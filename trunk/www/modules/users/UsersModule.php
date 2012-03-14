4f673cebd5515fb0516cc98d81f159e49612c73b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-03-14 09:19:03 +0000

    Load start module in settings

--- trunk/www/modules/users/UsersModule.php
+++ trunk/www/modules/users/UsersModule.php
@@ -14,4 +14,10 @@ class GO_Users_UsersModule extends GO_Base_Module{
 	public function adminModule() {
 		return true;
 	}
+	public static function loadSettings(&$settingsController, &$params, &$response, $user) {
+		$startModule = GO_Base_Model_Module::model()->findByPk($user->start_module);
+		$response['data']['start_module_name']=$startModule ? $startModule->moduleManager->name() : '';
+		
+		return parent::loadSettings($settingsController, $params, $response, $user);
+	}
 }
