152900a0a8386e6cea0cf85c33cc7b26509bf5b6
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-03-15 11:36:47 +0000

    Fixed this getDefaultpages function

--- trunk/www/modules/sites/controller/SiteModuleController.php
+++ trunk/www/modules/sites/controller/SiteModuleController.php
@@ -2,14 +2,14 @@
 
 class GO_Sites_Controller_SiteModule extends GO_Sites_Controller_AbstractSiteModule {
 		
-	protected function getDefaultPages($site_id=0){		
+	protected function getDefaultPages(){		
 		
 		return array(
-			array('site_id'=>$site_id,'page'=>'login','controller'=>'GO_Sites_Controller_User','template'=>'login','action'=>'login','title'=>'Login','name'=>'Login'),
-			array('site_id'=>$site_id,'page'=>'logout','controller'=>'GO_Sites_Controller_User','template'=>'logout','action'=>'logout','title'=>'Logout','name'=>'Logout'),
-			array('site_id'=>$site_id,'page'=>'register','controller'=>'GO_Sites_Controller_User','template'=>'register','action'=>'register','title'=>'Register','name'=>'Register'),
-			array('site_id'=>$site_id,'page'=>'resetpassword','controller'=>'GO_Sites_Controller_User','template'=>'resetpassword','action'=>'resetpassword','title'=>'Reset Password','name'=>'Reset Password'),
-			array('site_id'=>$site_id,'page'=>'lostpassword','controller'=>'GO_Sites_Controller_User','template'=>'lostpassword','action'=>'recover','title'=>'Lost Password','name'=>'Lost Password')
+			array('site_id'=>$this->site_id,'path'=>'login','controller'=>'GO_Sites_Controller_User','template'=>'login','action'=>'login','title'=>'Login','name'=>'Login'),
+			array('site_id'=>$this->site_id,'path'=>'logout','controller'=>'GO_Sites_Controller_User','template'=>'logout','action'=>'logout','title'=>'Logout','name'=>'Logout'),
+			array('site_id'=>$this->site_id,'path'=>'register','controller'=>'GO_Sites_Controller_User','template'=>'register','action'=>'register','title'=>'Register','name'=>'Register'),
+			array('site_id'=>$this->site_id,'path'=>'resetpassword','controller'=>'GO_Sites_Controller_User','template'=>'resetpassword','action'=>'resetpassword','title'=>'Reset Password','name'=>'Reset Password'),
+			array('site_id'=>$this->site_id,'path'=>'lostpassword','controller'=>'GO_Sites_Controller_User','template'=>'lostpassword','action'=>'recover','title'=>'Lost Password','name'=>'Lost Password')
 		);
 	}	
 }
