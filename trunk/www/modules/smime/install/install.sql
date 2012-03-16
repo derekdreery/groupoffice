a89d24a75c83b9bff6e7bd3c2ccd0f8b6c521288
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-03-16 08:46:41 +0000

    Auto increment

--- trunk/www/modules/smime/install/install.sql
+++ trunk/www/modules/smime/install/install.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS `smi_certs` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `cert` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT;
 
 DROP TABLE IF EXISTS `smi_pkcs12`;
 CREATE TABLE IF NOT EXISTS `smi_pkcs12` (
