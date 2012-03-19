6c1d83b61456889dbc0cad535518ec7eb597c679
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-03-19 10:59:34 +0000

    External footer and header views

--- trunk/www/modules/calendar/views/Extjs3/invitation.php
+++ trunk/www/modules/calendar/views/Extjs3/invitation.php
@@ -11,50 +11,28 @@
  * @version $Id: invitation.php 7752 2011-07-26 13:48:43Z mschering $
  * @author Merijn Schering <mschering@intermesh.nl>
  */
-
 //require_once(GO::config()->root_path."Group-Office.php");
 
 extract($data);
 
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-	<head>
-		<link href="<?php echo GO::config()->host; ?>views/Extjs3/themes/Default/external.css" type="text/css" rel="stylesheet" />
-		<?php
-		
-		$theme = GO::user() ? GO::user()->theme : GO::config()->theme;
-		
-		if($theme!='Default'){
-			?>
-			<link href="<?php echo GO::config()->host; ?>views/Extjs3/themes/<?php echo $theme; ?>/external.css" type="text/css" rel="stylesheet" />
-			<?php
-		}
-		if(!empty($GLOBALS['GO_CONFIG']->custom_css_url))
-			echo '<link href="'.$GLOBALS['GO_CONFIG']->custom_css_url.'" type="text/css" rel="stylesheet" />';
-		?>
-		<title><?php echo GO::config()->title; ?></title>
-	</head>
-<body>
-	<div id="container">
+
+$this->render('externalHeader');
+
+if ($participant->status == GO_Calendar_Model_Participant::STATUS_ACCEPTED) {
+	?>
+	<p><?php echo GO::t('eventAccepted', 'calendar'); ?></p>		
 	<?php
-	
-	if($participant->status==GO_Calendar_Model_Participant::STATUS_ACCEPTED){	
-		?>
-		<p><?php echo GO::t('eventAccepted','calendar'); ?></p>		
 	}else
 	{
-		?>
-		<p><?php echo GO::t('eventDeclined','calendar'); ?></p>
-		<?php
-	}
-	
-	if($event){
-		?>
-		<p><?php echo sprintf(GO::t('eventScheduledIn','calendar'),$event->calendar->name, $participant->statusName); ?></p>
-		<?php
-	}
 	?>
-	</div>
-</body>
-</html>
+	<p><?php echo GO::t('eventDeclined', 'calendar'); ?></p>
+	<?php
+}
+
+if ($event) {
+	?>
+	<p><?php echo sprintf(GO::t('eventScheduledIn', 'calendar'), $event->calendar->name, $participant->statusName); ?></p>
+	<?php
+}
+$this->render('externalFooter');
+
