cf475f384fbe10ac8906b4fd09528d13adf05dfa
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-03-23 08:34:57 +0000

    Added profile page to default pages

--- www/modules/sites/controller/SiteModuleController.php
+++ www/modules/sites/controller/SiteModuleController.php
@@ -9,7 +9,8 @@ class GO_Sites_Controller_SiteModule extends GO_Sites_Controller_AbstractSiteMod
 			array('site_id'=>$this->site_id,'path'=>'logout','controller'=>'GO_Sites_Controller_User','template'=>'logout','action'=>'logout','title'=>'Logout','name'=>'Logout'),
 			array('site_id'=>$this->site_id,'path'=>'register','controller'=>'GO_Sites_Controller_User','template'=>'register','action'=>'register','title'=>'Register','name'=>'Register'),
 			array('site_id'=>$this->site_id,'path'=>'resetpassword','controller'=>'GO_Sites_Controller_User','template'=>'resetpassword','action'=>'resetpassword','title'=>'Reset Password','name'=>'Reset Password'),
-			array('site_id'=>$this->site_id,'path'=>'lostpassword','controller'=>'GO_Sites_Controller_User','template'=>'lostpassword','action'=>'recover','title'=>'Lost Password','name'=>'Lost Password')
+			array('site_id'=>$this->site_id,'path'=>'lostpassword','controller'=>'GO_Sites_Controller_User','template'=>'lostpassword','action'=>'recover','title'=>'Lost Password','name'=>'Lost Password'),
+			array('site_id'=>$this->site_id,'path'=>'profile','controller'=>'GO_Sites_Controller_User','template'=>'profile','action'=>'profile','title'=>'Your account','name'=>'Your account')
 		);
 	}	
 }
