7bd8a54a96aca2c5087c1b15c4719f008fb0c77e
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-04-03 08:50:51 +0000

    Added settingsmodule file and fixed some translation errors

--- trunk/www/modules/settings/language/en.php
+++ trunk/www/modules/settings/language/en.php
@@ -9,3 +9,8 @@
 //GO.settingsmodule.lang.loginTextEnabled='Enable login screen text';
 $l['name']='Settings';
 $l['description']='With this module you can manage various settings of groupoffice and itm modules';
+$l['mainTitle'] = 'Settings';
+$l['loginScreenText']='Text for login screen';
+$l['text']='Text';
+$l['title']='Title';
+$l['loginTextEnabled']='Enable login screen text';
