98deb9d54d5e171642c9d35fd580cefb4bcaaeec
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-04-05 13:32:56 +0000

    Started with Debian packages

--- trunk/debian-groupoffice-servermanager/usr/share/groupoffice-servermanager/configure.php
+++ trunk/debian-groupoffice-servermanager/usr/share/groupoffice-servermanager/configure.php
@@ -31,12 +31,6 @@ function set_value($file, $str) {
 
 echo 'Configuring apache'."\n";
 
-if(!file_exists('/etc/groupoffice/servermanager.inc.php'))
-	create_file('/etc/groupoffice/servermanager.inc.php', 'tpl/etc/groupoffice/servermanager.inc.php', $replacements);
-
-chgrp('/etc/groupoffice/servermanager.inc.php', 'www-data');
-chmod('/etc/groupoffice/servermanager.inc.php', 0640);
-
 if(!file_exists('/etc/apache2/sites-enabled/000-groupoffice'))
 	create_file('/etc/apache2/sites-enabled/000-groupoffice', 'tpl/etc/apache2/sites-enabled/000-groupoffice', $replacements);
 
@@ -44,7 +38,7 @@ if(file_exists('/etc/apache2/sites-enabled/000-default'))
 	unlink('/etc/apache2/sites-enabled/000-default');
 
 echo "Configuring sudo\n";
-set_value('/etc/sudoers','www-data ALL=NOPASSWD:/usr/share/groupoffice/modules/servermanager/sudo.php');
+set_value('/etc/sudoers','www-data ALL=NOPASSWD:/usr/share/groupoffice/groupofficecli.php');
 
 echo "Done!\n\n";
 ?>
