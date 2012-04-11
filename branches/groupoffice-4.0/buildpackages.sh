40af27c970a6279350731188e81c34cf21feb554
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-04-11 15:27:57 +0000

    Updated changelogs

--- branches/groupoffice-4.0/buildpackages.sh
+++ branches/groupoffice-4.0/buildpackages.sh
@@ -3,12 +3,12 @@ php ./createchangelogs.php
 
 svn commit -m 'Updated changelogs'
 
-#./debian-groupoffice-servermanager/builddeb.sh send
-#./debian-groupoffice-mailserver/builddeb.sh send
-#./debian-groupoffice-pro/builddeb.sh send
-#./debian-groupoffice-com/builddeb.sh real send
+./debian-groupoffice-servermanager/builddeb.sh send
+./debian-groupoffice-mailserver/builddeb.sh send
+./debian-groupoffice-pro/builddeb.sh send
+./debian-groupoffice-com/builddeb.sh real send
 
-./debian-groupoffice-servermanager/builddeb.sh 
-./debian-groupoffice-mailserver/builddeb.sh 
-./debian-groupoffice-pro/builddeb.sh 
-./debian-groupoffice-com/builddeb.sh real 
+#./debian-groupoffice-servermanager/builddeb.sh 
+#./debian-groupoffice-mailserver/builddeb.sh 
+#./debian-groupoffice-pro/builddeb.sh 
+#./debian-groupoffice-com/builddeb.sh real 
