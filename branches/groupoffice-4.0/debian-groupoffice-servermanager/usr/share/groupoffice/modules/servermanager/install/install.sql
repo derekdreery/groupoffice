f8a7c1a07a8bee88a0dc3d4235b573e168010424
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-04-11 14:21:39 +0000

    Updated install sql for servermanager

--- branches/groupoffice-4.0/debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/install/install.sql
+++ branches/groupoffice-4.0/debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/install/install.sql
@@ -1,113 +1,66 @@
 -- phpMyAdmin SQL Dump
--- version 2.6.0-pl2
+-- version 3.4.5deb1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generatie Tijd: 02 Aug 2009 om 18:23
--- Server versie: 5.0.32
--- PHP Versie: 5.2.0-8+etch15
+-- Machine: localhost
+-- Genereertijd: 11 apr 2012 om 16:20
+-- Serverversie: 5.1.61
+-- PHP-Versie: 5.3.6-13ubuntu3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 --
--- Database: `servermanager`
+-- Database: `groupofficecomoud`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabel structuur voor tabel `sm_installations`
+-- Tabelstructuur voor tabel `sm_installations`
 --
 
 DROP TABLE IF EXISTS `sm_installations`;
 CREATE TABLE IF NOT EXISTS `sm_installations` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `ctime` int(11) NOT NULL,
   `mtime` int(11) NOT NULL,
   `max_users` int(11) NOT NULL,
-  `count_users` int(11) NOT NULL,
-  `install_time` int(11) NOT NULL,
-  `lastlogin` int(11) NOT NULL,
-  `total_logins` int(11) NOT NULL,
-  `database_usage` int(11) NOT NULL,
-  `file_storage_usage` int(11) NOT NULL,
+  `count_users` int(11) NOT NULL DEFAULT '0',
+  `install_time` int(11) NOT NULL DEFAULT '0',
+  `lastlogin` int(11) NOT NULL DEFAULT '0',
+  `total_logins` int(11) NOT NULL DEFAULT '0',
+  `database_usage` int(11) NOT NULL DEFAULT '0',
+  `file_storage_usage` int(11) NOT NULL DEFAULT '0',
   `mailbox_usage` int(11) DEFAULT NULL,
-  `report_ctime` int(11) NOT NULL,
+  `report_ctime` int(11) NOT NULL DEFAULT '0',
   `comment` text,
   `features` varchar(255) DEFAULT NULL,
   `mail_domains` varchar(255) DEFAULT NULL,
   `admin_email` varchar(100) DEFAULT NULL,
   `admin_name` varchar(100) DEFAULT NULL,
   `admin_salutation` varchar(100) DEFAULT NULL,
-  `admin_country` char(2) NOT NULL,
+  `admin_country` char(2) NOT NULL DEFAULT '',
   `date_format` varchar(20) DEFAULT NULL,
-  `thousands_separator` char(1) NOT NULL,
+  `thousands_separator` char(1) DEFAULT NULL,
   `decimal_separator` char(1) DEFAULT NULL,
-  `billing` tinyint(1) NOT NULL,
-  `professional` tinyint(1) NOT NULL,
+  `billing` tinyint(1) NOT NULL DEFAULT '0',
+  `professional` tinyint(1) NOT NULL DEFAULT '0',
   `status` varchar(50) NOT NULL,
-  `status_change_time` int(11) NOT NULL,
-  `config_file` varchar(255) NOT NULL,
-	`token` VARCHAR( 100 ) NOT NULL,
+  `status_change_time` int(11) NOT NULL DEFAULT '0',
+  `config_file` varchar(255) NOT NULL DEFAULT '',
+  `token` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=36 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabel structuur voor tabel `sm_new_trials`
+-- Tabelstructuur voor tabel `sm_installation_users`
 --
 
-DROP TABLE IF EXISTS `sm_new_trials`;
-CREATE TABLE `sm_new_trials` (
-  `name` varchar(50) NOT NULL default '',
-  `title` varchar(100) default NULL,
-  `first_name` varchar(50) default NULL,
-  `last_name` varchar(50) default NULL,
-  `email` varchar(100) default NULL,
-  `password` varchar(20) default NULL,
-  `key` varchar(100) default NULL,
-  `ctime` int(11) NOT NULL,
-  PRIMARY KEY  (`name`),
-  KEY `key` (`key`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tabel structuur voor tabel `sm_reports`
---
-
-DROP TABLE IF EXISTS `sm_reports`;
-CREATE TABLE `sm_reports` (
-  `name` varchar(100) NOT NULL default '',
-  `count_users` int(11) NOT NULL,
-  `install_time` int(11) NOT NULL,
-  `lastlogin` int(11) NOT NULL,
-  `total_logins` int(11) NOT NULL,
-  `database_usage` int(11) NOT NULL,
-  `file_storage_usage` int(11) NOT NULL,
-  `mailbox_usage` int(11) default NULL,
-  `ctime` int(11) NOT NULL,
-  `comment` text,
-  `features` varchar(255) default NULL,
-  `mail_domains` varchar(255) default NULL,
-  `admin_email` varchar(100) default NULL,
-  `admin_name` varchar(100) default NULL,
-  `admin_salutation` varchar(100) default NULL,
-  `admin_country` char(2) NOT NULL,
-  `date_format` varchar(20) default NULL,
-  `thousands_separator` char(1) NOT NULL,
-  `decimal_separator` char(1) default NULL,
-  `billing` tinyint(1) NOT NULL,
-  `professional` tinyint(1) NOT NULL,
-  `max_users` int(11) NOT NULL,
-  PRIMARY KEY  (`name`),
-  KEY `ctime` (`ctime`),
-  KEY `professional` (`professional`),
-  KEY `billing` (`billing`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-        
-
-
+DROP TABLE IF EXISTS `sm_installation_users`;
 CREATE TABLE IF NOT EXISTS `sm_installation_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `installation_id` int(11) NOT NULL,
@@ -129,8 +82,29 @@ CREATE TABLE IF NOT EXISTS `sm_installation_users` (
 -- Tabelstructuur voor tabel `sm_installation_user_modules`
 --
 
+DROP TABLE IF EXISTS `sm_installation_user_modules`;
 CREATE TABLE IF NOT EXISTS `sm_installation_user_modules` (
   `user_id` int(11) NOT NULL,
   `module_id` varchar(100) NOT NULL,
   PRIMARY KEY (`user_id`,`module_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `sm_new_trials`
+--
+
+DROP TABLE IF EXISTS `sm_new_trials`;
+CREATE TABLE IF NOT EXISTS `sm_new_trials` (
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `title` varchar(100) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `key` varchar(100) DEFAULT NULL,
+  `ctime` int(11) NOT NULL,
+  PRIMARY KEY (`name`),
+  KEY `key` (`key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
