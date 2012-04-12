3e7bf679898baebc2ac4b6a1563242ed53f870d8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-04-12 08:50:28 +0000

    Faster delete cache

--- www/modules/email/install/install.sql
+++ www/modules/email/install/install.sql
@@ -150,7 +150,8 @@ CREATE TABLE IF NOT EXISTS `em_messages_cache` (
   `to` varchar(255) DEFAULT NULL,
   `serialized_message_object` mediumtext NOT NULL,
   PRIMARY KEY (`folder_id`,`uid`),
-  KEY `account_id` (`account_id`)
+  KEY `account_id` (`account_id`),
+	KEY `account_id` (`udate`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
