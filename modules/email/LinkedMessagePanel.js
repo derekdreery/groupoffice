1c85d077db6e00867e5a1be196885d90b60a830e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-04-19 09:58:50 +0000

    Created tag 4.0.5

---
+++ modules/email/LinkedMessagePanel.js
@@ -0,0 +1,170 @@
+GO.email.LinkedMessagePanel = Ext.extend(GO.email.MessagePanel,{
+	initComponent : function(){
+		this.tbar=[{
+					iconCls: 'btn-print',
+					text: GO.lang.cmdPrint,
+					cls: 'x-btn-text-icon',
+					handler: function(){
+						this.body.print();
+					},
+					scope: this
+				},
+				'-',
+				{
+					iconCls: 'btn-reply',
+					text: GO.email.lang.reply,
+					cls: 'x-btn-text-icon',
+					handler: function(){
+						GO.email.showComposer({
+							task:'reply',
+							loadParams : {
+								path:this.data.path
+							}
+						});
+					},
+					scope: this
+				},
+				{
+					iconCls: 'btn-reply-all',
+					text: GO.email.lang.replyAll,
+					cls: 'x-btn-text-icon',
+					handler: function(){
+						GO.email.showComposer({
+							task:'reply_all',
+							loadParams : {
+								path:this.data.path
+							}
+						});
+					},
+					scope: this
+				},
+				{
+					iconCls: 'btn-forward',
+					text: GO.email.lang.forward,
+					cls: 'x-btn-text-icon',
+					handler: function(){						
+						GO.email.showComposer({
+							task:'forward',
+							loadParams : {
+								path:this.data.path
+							}
+						});
+					},
+					scope: this
+				},{
+					iconCls: 'btn-edit',
+					text: GO.lang.cmdEdit,
+					handler: function(){
+						var composer = GO.email.showComposer({
+							task:'opendraft',
+							loadParams : {
+								path:this.data.path
+							},
+							saveToPath:this.data.path
+						});
+						
+						composer.on('hide', this.reload, this, {single:true});
+					},
+					scope: this
+				}];
+
+		GO.email.LinkedMessagePanel.superclass.initComponent.call(this);
+	},
+	border:false,
+	autoScroll:true,
+	editHandler : function(){
+		//needed because it needs to be compatible with javascript/DisplayPanel.js
+	},
+	loadUrl: '',
+	reload : function (){
+		this.load(this.lastId, this.lastConfig);	
+	},
+	load : function(id, config){
+
+	 config = config || {};
+	 
+	 this.lastConfig=config;
+	 this.lastId=id;
+	 
+		this.el.mask(GO.lang.strWaitMsgLoad);
+
+		if(!this.remoteMessage)
+			this.remoteMessage={};
+
+
+		this.messageId=id;		
+		this.remoteMessage.id=this.messageId;
+
+		this.loadUrl = '';
+		switch(config.action){
+			
+			case 'path':
+				this.loadUrl=GO.url("savemailas/linkedEmail/loadPath");
+			break;
+			
+			case 'attachment':
+				this.loadUrl = GO.url("email/message/messageAttachment");
+				break;
+				
+			case 'file':
+				this.loadUrl=GO.url("savemailas/linkedEmail/loadFile");
+				break;
+				
+			default:
+				this.loadUrl=GO.url("savemailas/linkedEmail/loadLink");
+				
+				break;
+			
+		}
+
+		Ext.Ajax.request({
+			url: this.loadUrl,
+			params: this.remoteMessage,
+			scope: this,
+			callback: function(options, success, response)
+			{
+				var data = Ext.decode(response.responseText);
+				this.setMessage(data);
+				this.el.unmask();
+			}
+		});
+	},
+	listeners:{
+		scope:this,
+		linkClicked: function(href){
+			var win = window.open(href);
+			win.focus();
+		},
+		attachmentClicked: function(attachment, panel){
+			if(attachment.mime=='message/rfc822')
+			{
+				GO.email.showMessageAttachment(0, {
+					action:'path',
+					path:attachment.tmp_file,
+					isTempFile:true
+				});
+			}else
+			{
+				window.open(attachment.url);
+				
+//				if(panel.data.path)
+//				{
+//					document.location.href=GO.settings.modules.email.url+
+//					'mimepart.php?path='+
+//					encodeURIComponent(panel.data.path)+'&part_imap_id='+attachment.imap_id;
+//				}else
+//				{
+//					document.location.href=GO.settings.modules.email.url+
+//					'mimepart.php?uid='+panel.uid+'' +
+//					'&account_id='+panel.remoteMessage.account_id+'' +
+//					'&encoding='+panel.remoteMessage.encoding+'' +
+//					'&mailbox='+encodeURIComponent(panel.remoteMessage.mailbox)+'' +
+//					'&imap_id='+panel.remoteMessage.imap_id+'' +
+//					'&part_imap_id='+attachment.imap_id;
+//				}
+			}
+		}
+	}
+
+});
+
