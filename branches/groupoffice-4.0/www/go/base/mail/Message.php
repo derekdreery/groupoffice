93b8a7de59d2d858c3b222e356b64d7483ba7bfb
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-04-19 08:09:27 +0000

    Token not found

--- branches/groupoffice-4.0/www/go/base/mail/Message.php
+++ branches/groupoffice-4.0/www/go/base/mail/Message.php
@@ -411,9 +411,8 @@ class GO_Base_Mail_Message extends Swift_Message{
 								$img->setContentType($tmpFile->mimeType());
 								$contentId = $this->embed($img);
 
-								//$tmpFile->delete();
-
-								$params['htmlbody'] = str_replace($matches[1], $contentId, $params['htmlbody']);
+								//$tmpFile->delete();								
+								$params['htmlbody'] = GO_Base_Util_String::replaceOnce($matches[1], $contentId, $params['htmlbody']);
 							}else
 							{
 								throw new Exception("Error: inline attachment could not be found in text: ".$ia->token);
