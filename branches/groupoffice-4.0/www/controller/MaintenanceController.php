bdbf710a4af4119fa28ad62a4d96fb8cd72d98b1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-04-24 07:03:31 +0000

    Don't remove duplicate orders

--- branches/groupoffice-4.0/www/controller/MaintenanceController.php
+++ branches/groupoffice-4.0/www/controller/MaintenanceController.php
@@ -38,7 +38,7 @@ class GO_Core_Controller_Maintenance extends GO_Base_Controller_AbstractControll
 				"GO_Calendar_Model_Event"=>array('name', 'start_time', 'end_time', 'calendar_id', 'rrule', 'user_id'),
 				"GO_Tasks_Model_Task"=>array('name', 'start_time', 'due_time', 'tasklist_id', 'rrule', 'user_id'),
 				"GO_Addressbook_Model_Contact"=>array('first_name', 'middle_name', 'last_name', 'addressbook_id', 'company_id', 'email'),
-				"GO_Billing_Model_Order"=>array('order_id','book_id','btime')
+				//"GO_Billing_Model_Order"=>array('order_id','book_id','btime')
 			);
 		
 		foreach($checkModels as $modelName=>$checkFields){
