f1615c054d374cd7cdeddba09f52156799e84a67
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-04-25 14:42:35 +0000

    Created tag 4.0.6

---
+++ go/base/Router.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Group-Office
+ * 
+ * Copyright Intermesh BV. 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @license AGPL/Proprietary http://www.group-office.com/LICENSE.TXT
+ * @link http://www.group-office.com
+ * @copyright Copyright Intermesh BV
+ * @version $Id: Number.php 7962 2011-08-24 14:48:45Z mschering $
+ * @author Merijn Schering <mschering@intermesh.nl>
+ * @package GO.base
+ */
+
+
+/**
+ * Router
+ * 
+ * The Router class which looks up the controller by the request URL. 
+ * URL Should be like index.php?r=module/controller/method&param=value
+ * 
+ * @author Merijn Schering <mschering@intermesh.nl>
+ * @version $Id: config.class.inc.php 7687 2011-06-23 12:00:34Z mschering $
+ * @copyright Copyright Intermesh BV.
+ * @package GO.base 
+ */
+class GO_Base_Router{
+	
+	/**
+	 * Analyzes the request URL and finds the controller.
+	 * 
+	 * URL Should be like index.php?r=module/controller/method&param=value
+	 * 
+	 * If a controller consist of two words then the second word should start with
+	 * a capital letter.
+	 * 
+	 */
+	
+	private $_controller;
+	
+	private $_action;
+	
+	private $_r;
+	
+	/**
+	 * Get the controller route. eg. email/message/view
+	 * 
+	 * @return string 
+	 */
+	public function getControllerRoute(){
+		return $this->_r;
+	}
+	
+	/**
+	 * Get the currently active controller for this request.
+	 * 
+	 * @return GO_Base_Controller_AbstractController 
+	 */
+	public function getController(){
+		return $this->_controller;
+	}
+	
+	/**
+	 * Get the currently processing controller action in lowercase.
+	 * 
+	 * @return string
+	 */
+	public function getControllerAction(){
+		return $this->_action;
+	}	
+	
+	/**
+	 * Runs a controller action with the given params
+	 * 
+	 * @param array $params 
+	 */
+	public function runController($params=false){
+		
+		if(!$params){
+			if(PHP_SAPI=='cli'){
+				$params = GO_Base_Util_Cli::parseArgs();
+			}else
+			{
+				$params=$_REQUEST;				
+			}
+		}
+				
+		$r = !empty($params['r']) ?  explode('/', $params['r']): array();		
+		$this->_r=isset($params['r']) ? $params['r'] : "";
+		
+		$first = isset($r[0]) ? ucfirst($r[0]) : 'Auth';
+		
+		if(file_exists(GO::config()->root_path.'controller/'.$first.'Controller.php')){
+			//this is a controller name that belongs to the Group-Office framework
+			$module='Core';
+			$controller=$first;
+			$action = isset($r[1]) ? $r[1] : '';
+			
+		}else
+		{
+			//it must be pointing to a module
+			$module=strtolower($r[0]);
+			$controller=isset($r[1]) ? ucfirst($r[1]) : 'Default';
+			$action = isset($r[2]) ? $r[2] : '';
+		}
+		
+		$action = strtolower($action);
+				
+		$controllerClass='GO_';
+		
+		if(!empty($module))
+			$controllerClass.=ucfirst($module).'_';
+		
+		$controllerClass.='Controller_'.$controller;
+		
+		$this->_action=$action;		
+		$this->_controller = new $controllerClass;
+		$this->_controller->run($action, $params);		
+	}
+}
