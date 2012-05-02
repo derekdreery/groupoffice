0620c348accaee049c3ecacf04eef00126f2f754
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-05-02 09:40:16 +0000

    Created tag 4.0.9

---
+++ tags/groupoffice-com-4.0.9/modules/notes/language/vi.inc.php
@@ -0,0 +1,27 @@
+<?php
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @copyright Copyright Intermesh
+ * @version $Id: en.js 7708 2011-07-06 14:13:04Z wilmar1980 $
+ * @author Dat Pham <datpx@fab.vn> +84907382345
+ */
+require($GO_LANGUAGE->get_fallback_language_file('notes'));
+
+$lang['notes']['name']='Ghi chú';
+$lang['notes']['description']='Module tạo ra ghi chú nhanh';
+
+$lang['notes']['category']='Sổ tay';
+$lang['notes']['categories']='Sổ tay';
+$lang['link_type'][4]=$lang['notes']['note']='Ghi chú';
+$lang['notes']['notes']='Ghi chú';
+
+$lang['notes']['general'] = 'Chung';
+$lang['notes']['incomplete_delete']='Bạn không có quyền xóa những ghi chú đang chọn';
+
+$lang['notes']['multipleCategoriesSelected']='Đã chọn nhiều ghi chú';
