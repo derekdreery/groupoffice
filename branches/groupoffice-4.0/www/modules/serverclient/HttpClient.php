79c5a7ac3c4515a31b4e680c6062891b134594e4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-05-03 14:57:56 +0000

    Calculate mailbox usage

--- branches/groupoffice-4.0/www/modules/serverclient/HttpClient.php
+++ branches/groupoffice-4.0/www/modules/serverclient/HttpClient.php
@@ -5,13 +5,13 @@ class GO_Serverclient_HttpClient extends GO_Base_Util_HttpClient{
 		return $this->groupofficeLogin(GO::config()->serverclient_server_url, GO::config()->serverclient_username, GO::config()->serverclient_password);		
 	}
 	
-	public function postfixRequest($params){
-		$this->postfixLogin();		
-		
-		$url = GO::config()->serverclient_server_url.'modules/postfixadmin/json.php';
-		
-		return $this->request($url, $params);
-	}	
+//	public function postfixRequest($params){
+//		$this->postfixLogin();		
+//		
+//		$url = GO::config()->serverclient_server_url.'modules/postfixadmin/json.php';
+//		
+//		return $this->request($url, $params);
+//	}	
 	
 	
 }
