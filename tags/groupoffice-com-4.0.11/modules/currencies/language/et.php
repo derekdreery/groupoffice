d88924efac8315721835f18795b819a97959c312
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-05-03 13:58:59 +0000

    Created tag 4.0.11

---
+++ tags/groupoffice-com-4.0.11/modules/currencies/language/et.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$l['name']='rahaühikud';
+$l['description']='Lisa siia kirjeldus';
+$l['currency']='Rahaühik';
+$l['currencies']='Rahaühikud';
+
+$l["currencies"]='currencies';
+$l["currency"]="Rahaühik";
+$l["currencies"]="Rahaühikud";
+$l["code"]="Kood";
+$l["symbol"]="Sümbol";
+$l["value"]="Väärtus";
---
+++ tags/groupoffice-com-4.0.11/modules/email/zip_attachments.php
@@ -0,0 +1,55 @@
+<?php
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * @version $Id$
+ * @copyright Copyright Intermesh
+ * @author Merijn Schering <mschering@intermesh.nl>
+ * @since Group-Office 1.0
+ */
+
+
+require_once("../../Group-Office.php");
+$GLOBALS['GO_SECURITY']->authenticate();
+$GLOBALS['GO_MODULES']->authenticate('email');
+
+
+//close writing to session so other concurrent requests won't be locked out.
+session_write_close();
+
+require_once($GLOBALS['GO_CONFIG']->class_path."mail/imap.class.inc");
+require_once($GLOBALS['GO_MODULES']->class_path."email.class.inc.php");
+$mail = new imap();
+$email = new email();
+
+require_once ($GLOBALS['GO_LANGUAGE']->get_language_file('email'));
+
+$filename = File::strip_invalid_chars($_REQUEST['filename']);
+if(empty($filename))
+	$filename=$lang['email']['attachments'];
+
+
+$browser = detect_browser();
+//header('Content-Length: '.strlen($file));
+header('Expires: '.gmdate('D, d M Y H:i:s') . ' GMT');
+header('Content-Disposition: attachment; filename="'.$filename.'.zip"');
+if ($browser['name'] == 'MSIE')
+{
+	header('Content-Type: application/download');
+	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+	header('Pragma: public');
+}else
+{
+	header('Content-Type: application/x-zip');
+	header('Pragma: no-cache');
+}
+header('Content-Transfer-Encoding: binary');
+$zip_file=$email->get_zip_of_attachments($_REQUEST['account_id'],$_REQUEST['uid'], $_REQUEST['mailbox']);
+
+readfile($zip_file);
+
+unlink($zip_file);
