463bb54d6744f453bb6dfa17e332533b346580f1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-05-07 07:36:33 +0000

    Syntax error in SQL

--- branches/groupoffice-4.0/www/modules/postfixadmin/install/install.sql
+++ branches/groupoffice-4.0/www/modules/postfixadmin/install/install.sql
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `pa_domains` (
   `description` varchar(255) default NULL,
   `max_aliases` int(10) NOT NULL default '0',
   `max_mailboxes` int(10) NOT NULL default '0',
-  `quota` bigint(20) NOT NULL default '0',
+  `total_quota` bigint(20) NOT NULL default '0',
   `default_quota` bigint(20) NOT NULL default '0',
   `transport` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'virtual',
   `backupmx` tinyint(1) NOT NULL default '0',
