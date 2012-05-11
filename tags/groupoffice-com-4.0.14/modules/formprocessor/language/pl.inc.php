7b6819baa1d39d5d35890d18321a51fedb3e8f69
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-05-11 12:17:18 +0000

    Created tag 4.0.14

---
+++ tags/groupoffice-com-4.0.14/modules/formprocessor/language/pl.inc.php
@@ -0,0 +1,8 @@
+<?php
+//Polish Translation v1.1
+//Author : Paweł Dmitruk pawel.dmitruk@gmail.com
+//Date : September, 05 2010
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('formprocessor'));
+$lang['formprocessor']['name']='Formprocessor';
+$lang['formprocessor']['description']='Może być wykorzystywany do tworzenia formularzy kontaktowych, subskrypcji newslettera i dodawania kontaktów ze strony internetowej';
