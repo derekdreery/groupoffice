a6e8f54e84b8f6ba5c5dfcb2eed6776c981ebba5
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-05-13 19:33:39 +0000

    Invalid sql in installs.sql for email

--- branches/groupoffice-3.7/www/classes/base/config.class.inc.php
+++ branches/groupoffice-3.7/www/classes/base/config.class.inc.php
@@ -735,7 +735,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '3.7.50';
+	var $version = '3.7.51';
 
 
 	/* The permissions mode to use when creating files
@@ -765,7 +765,7 @@ class GO_CONFIG {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20120513';
+	var $mtime = '20120513-1';
 
 	#group configuration
 	/**
