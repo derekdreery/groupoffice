e453efd330ce7eb8594470d6f937afba8cff03f9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-05-25 12:16:05 +0000

    Dependencies for module

--- branches/groupoffice-4.0/www/go/base/controller/AbstractModelController.php
+++ branches/groupoffice-4.0/www/go/base/controller/AbstractModelController.php
@@ -589,7 +589,7 @@ class GO_Base_Controller_AbstractModelController extends GO_Base_Controller_Abst
 		if (isset(GO::modules()->files) && $model->hasFiles() && $response['data']['files_folder_id']>0) {
 
 			$fc = new GO_Files_Controller_Folder();
-			$listResponse = $fc->run("list",array('folder_id'=>$response['data']['files_folder_id']),false);
+			$listResponse = $fc->run("list",array('folder_id'=>$response['data']['files_folder_id'], "limit"=>20),false);
 			$response['data']['files'] = $listResponse['results'];
 		} else {
 			$response['data']['files'] = array();
