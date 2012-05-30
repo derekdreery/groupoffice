40c4758007221d8a6bb7fc844a6a3af48640719d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-05-30 15:39:46 +0000

    Created tag 4.0.19

---
+++ tags/groupoffice-com-4.0.19/modules/currencies/language/en.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$l["currencies"]='currencies';
+$l["currency"]="Currency";
+$l["currencies"]="Currencies";
+$l["code"]="Code";
+$l["symbol"]="Symbol";
+$l["value"]="Value";
+
+$l['name']='Currencies';
+$l['description']='Select different currency types in the project expenses and income dialogs';
+$l['currency']='Currency';
+$l['currencies']='Currencies';
