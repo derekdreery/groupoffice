100de346ffcb0134e570bb40914743a9f65c56a1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-05-30 16:12:23 +0000

    Bug in language generation

--- branches/groupoffice-4.0/www/go/base/Config.php
+++ branches/groupoffice-4.0/www/go/base/Config.php
@@ -677,7 +677,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '4.0.19';
+	var $version = '4.0.20';
 
 
 	/* The permissions mode to use when creating files
@@ -707,7 +707,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20120530';
+	var $mtime = '20120530-1';
 
 	#group configuration
 	/**
