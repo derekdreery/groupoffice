669b16fbf6e09b375ff919df1ff3849a0d44e6c9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-05-31 16:05:38 +0000

    Created tag 4.0.21

---
+++ tags/groupoffice-com-4.0.21/modules/links/language/nb.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$l['name']='Lenkebeskrivelser';
+$l['description']='Behandle standard lenkebeskrivelser';
+$l['link_description']='Lenkebeskrivelse';
+$l['link_descriptions']='Lenkebeskrivelser';
+$l["links"]='Lenker';
+$l["linkDescription"]="Lenkebeskrivelse";
+$l["linkDescriptions"]="Lenkebeskrivelser";
+$l["defaultLinkFolderText"]='Mappene angitt her vil bli opprettet automatisk når et nytt element opprettes. Legg inn sti/navn på separate linjer, for eksempel: <br /><br />Mappe/Under<br />Mappe 2/Under 2<br /><br />';
+$l["linkFolders"]='Standard lenkemapper';
+$l["link"]='Lenke';
