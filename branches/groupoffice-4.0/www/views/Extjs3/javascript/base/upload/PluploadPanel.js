687009cc58bdb5eceb1a52ff98b22bac8d73cbdc
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-01 08:13:32 +0000

    Removed silverlight runtime from uploadpanel

--- branches/groupoffice-4.0/www/views/Extjs3/javascript/base/upload/PluploadPanel.js
+++ branches/groupoffice-4.0/www/views/Extjs3/javascript/base/upload/PluploadPanel.js
@@ -11,7 +11,7 @@ GO.base.upload.PluploadPanel = function(config){
 		//the only required parameter
 
 		//runtimes: 'html5,gears,flash,silverlight,browserplus,html4',
-		runtimes: 'html5,gears,silverlight,flash,html4',
+		runtimes: 'html5,gears,flash,html4',
 		//runtimes: 'html4',
 		// first available runtime will be used
 
@@ -34,7 +34,7 @@ GO.base.upload.PluploadPanel = function(config){
 //		},
 
 		flash_swf_url: BaseHref+'views/Extjs3/javascript/plupload/plupload/js/plupload.flash.swf',
-		silverlight_xap_url: BaseHref+'views/Extjs3/javascript/plupload/plupload/js/plupload.silverlight.xap',
+//		silverlight_xap_url: BaseHref+'views/Extjs3/javascript/plupload/plupload/js/plupload.silverlight.xap',
 		// urls must be set properly or absent, otherwise uploader fail to initialize
 
 		//			filters: [  {
