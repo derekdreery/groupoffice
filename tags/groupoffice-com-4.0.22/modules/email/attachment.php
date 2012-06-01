67b3621309688f7dd4694aa1ff6e1bf4e38f93d4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-01 10:47:43 +0000

    Created tag 4.0.22

---
+++ tags/groupoffice-com-4.0.22/modules/email/attachment.php
@@ -0,0 +1,175 @@
+<?php
+/** 
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ * @version $Id$
+ * @copyright Copyright Intermesh
+ * @author Merijn Schering <mschering@intermesh.nl>
+ * @since Group-Office 1.0
+ */
+
+
+require_once("../../Group-Office.php");
+$GLOBALS['GO_SECURITY']->authenticate();
+$GLOBALS['GO_MODULES']->authenticate('email');
+
+//close writing to session so other concurrent requests won't be locked out.
+session_write_close();
+
+require_once($GLOBALS['GO_MODULES']->modules['email']['class_path']."cached_imap.class.inc.php");
+require_once($GLOBALS['GO_MODULES']->modules['email']['class_path']."email.class.inc.php");
+require_once($GLOBALS['GO_CONFIG']->class_path."mail/mimeDecode.class.inc");
+$imap = new cached_imap();
+$email = new email();
+
+
+
+
+if(!empty($_REQUEST['filepath'])){
+	//message is cached on disk
+	$path = $GLOBALS['GO_CONFIG']->file_storage_path.$_REQUEST['filepath'];
+
+	if(File::path_leads_to_parent($path) || !file_exists($path)){
+		die('Invalid request');
+	}
+	$params['input'] = file_get_contents($path);
+	$params['include_bodies'] = true;
+	$params['decode_bodies'] = true;
+	$params['decode_headers'] = false;
+
+	$part = Mail_mimeDecode::decode($params);
+
+	$parts_arr = explode('.',$_REQUEST['imap_id']);
+	for($i=0;$i<count($parts_arr);$i++) {
+		if(isset($part->parts[$parts_arr[$i]])){
+			$part = $part->parts[$parts_arr[$i]];
+		}else{
+			go_debug('Mime part not found!');
+			go_debug($_REQUEST);
+			die('Part not found');
+		}
+	}	
+	$data = $part->body;
+	unset($part);
+
+}else
+{
+	
+	if(File::path_leads_to_parent($_REQUEST['filename']))
+			die("Invalid request");
+
+	$account = $imap->open_account($_REQUEST['account_id'], $_REQUEST['mailbox']);
+
+	if(!$GLOBALS['GO_SECURITY']->has_permission($GLOBALS['GO_SECURITY']->user_id,$account['acl_id'])) {
+		die($lang['common']['accessDenied']);
+	}
+
+
+	if(!empty($_REQUEST['uuencoded_partnumber']) && $_REQUEST['uuencoded_partnumber']!='undefined') {
+		$data = $imap->get_message_part_decoded($_REQUEST['uid'], $_REQUEST['imap_id'], $_REQUEST['encoding'], false);
+
+		$attachments = $imap->extract_uuencoded_attachments($data);
+
+		$data = convert_uudecode($attachments[$_REQUEST['uuencoded_partnumber']-1]['data']);
+		$size = strlen($data);
+	}
+
+	$extension = File::get_extension($_REQUEST['filename']);
+
+	if($GLOBALS['GO_MODULES']->has_module('gnupg') && ($extension=='pgp' || $extension=='gpg')) {
+		require_once ($GLOBALS['GO_MODULES']->modules['gnupg']['class_path'].'gnupg.class.inc.php');
+		$gnupg = new gnupg();
+
+		$tmpfile = $GLOBALS['GO_CONFIG']->tmpdir.$_REQUEST['filename'];
+
+		$_REQUEST['filename']=File::strip_extension($_REQUEST['filename']);
+		
+		
+		
+		$file=$GLOBALS['GO_CONFIG']->tmpdir.$_REQUEST['filename'];
+		$fp = fopen($tmpfile, 'w+');
+
+		if(!$fp)
+			die('Could not write to temp file');
+
+		$imap->get_message_part_start($_REQUEST['uid'], $_REQUEST['imap_id']);
+
+		while($line = $imap->get_message_part_line()){
+
+			switch(strtolower($_REQUEST['encoding'])) {
+				case 'base64':
+					$line=base64_decode($line);
+					break;
+				case 'quoted-printable':
+					$line= quoted_printable_decode($line);
+					break;
+			}
+
+			if(!fputs($fp, $line))
+				die('Could not write to temp file');
+		}
+
+		fclose($fp);
+
+		$passphrase=isset($_SESSION['GO_SESSION']['gnupg']['passwords'][$_REQUEST['sender']]) ? $_SESSION['GO_SESSION']['gnupg']['passwords'][$_REQUEST['sender']] : '';
+
+		$gnupg->decode_file($tmpfile, $file, $passphrase);
+		unlink($tmpfile);
+
+
+		//$file = file_get_contents($outfile);
+	}else
+	{
+		$size = $imap->get_message_part_start($_REQUEST['uid'], $_REQUEST['imap_id']);
+		//exit($size);
+	}
+}
+
+$browser = detect_browser();
+
+header("Expires: " . date("D, j M Y G:i:s ", time()+(86400*14)) . 'GMT');//expires in 2 weeks
+header('Cache-Control: cache');
+header('Pragma: cache');	
+if ($browser['name'] == 'MSIE') {
+	header('Content-Type: application/download');
+	header('Content-Disposition: attachment; filename="'.rawurlencode($_REQUEST['filename']).'";');
+}else {
+	$mime = File::get_mime($_REQUEST['filename']);
+
+	header('Content-Type: '.$mime);
+	header('Content-Disposition: attachment; filename="'.$_REQUEST['filename'].'"');
+}
+header('Content-Transfer-Encoding: binary');
+
+//unfortunately we don't know the size because file is not decoded yet
+//header('Content-Length: '.$size);
+
+
+if(isset($file)){
+	//tmp file from gnupg
+	readfile($file);
+	unlink($file);
+}elseif(isset($data)){
+	echo $data;
+}else
+{
+	//read from IMAP server
+	while($line = $imap->get_message_part_line()){
+		switch(strtolower($_REQUEST['encoding'])) {
+			case 'base64':
+				echo base64_decode($line);
+				break;
+			case 'quoted-printable':
+				echo quoted_printable_decode($line);
+				break;
+			default:
+				echo $line;
+				break;
+		}
+	}
+}
+$imap->disconnect();
---
+++ tags/groupoffice-com-4.0.22/modules/links/language/pt_BR.inc.php
@@ -0,0 +1,7 @@
+<?php
+//Uncomment this line in new translations!
+require($GLOBALS['GO_LANGUAGE']->get_fallback_language_file('links'));
+$lang['links']['name']='Descrições de links';
+$lang['links']['description']='Gerenciar descrições padrão de links';
+$lang['links']['link_description']='Descrição de link';
+$lang['links']['link_descriptions']='Descrições de links';
