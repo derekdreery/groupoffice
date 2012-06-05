c1484f574df5a6928bddc139cd6fd0adc454af5f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-05 15:09:47 +0000

    Updated changelogs

--- branches/groupoffice-4.0/www/modules/calendar/EventDialog.js
+++ branches/groupoffice-4.0/www/modules/calendar/EventDialog.js
@@ -822,7 +822,7 @@ Ext.extend(GO.calendar.EventDialog, Ext.util.Observable, {
 			}
 		});
 
-		this.allDayCB = new Ext.form.Checkbox({
+		this.allDayCB = new Ext.ux.form.XCheckbox({
 			boxLabel : GO.calendar.lang.allDay,
 			name : 'all_day_event',
 			checked : false,
@@ -1051,7 +1051,7 @@ Ext.extend(GO.calendar.EventDialog, Ext.util.Observable, {
 
 		this.cb = [];
 		for (var day = 0; day < 7; day++) {
-			this.cb[day] = new Ext.form.Checkbox({
+			this.cb[day] = new Ext.ux.form.XCheckbox({
 				boxLabel : GO.lang.shortDays[day],
 				name : days[day],
 				disabled : true,
@@ -1076,7 +1076,7 @@ Ext.extend(GO.calendar.EventDialog, Ext.util.Observable, {
 			}
 		});
 
-		this.repeatForever = new Ext.form.Checkbox({
+		this.repeatForever = new Ext.ux.form.XCheckbox({
 			boxLabel : GO.calendar.lang.repeatForever,
 			name : 'repeat_forever',
 			checked : true,
@@ -1167,7 +1167,7 @@ Ext.extend(GO.calendar.EventDialog, Ext.util.Observable, {
 		this.participantsPanel = new GO.calendar.ParticipantsPanel(this);
 
 
-		this.privateCB = new Ext.form.Checkbox({
+		this.privateCB = new Ext.ux.form.XCheckbox({
 			boxLabel : GO.calendar.lang.privateEvent,
 			name : 'private',
 			checked : false,
