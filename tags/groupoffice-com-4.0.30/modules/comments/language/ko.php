abc4d401da75be923d57233dc6ed740972f5ca50
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-08 12:42:30 +0000

    Created tag 4.0.30

---
+++ tags/groupoffice-com-4.0.30/modules/comments/language/ko.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$l["comment"]="코멘트";
+$l["comments"]="코멘트";
+$l["recentComments"]='최근 코멘트';
+$l["browseComments"]= 'Browse comments';
+$l['name']='코멘트';
+$l['description']='Adds comments functionality to other modules';
+$l['comment']='코멘트';
+$l['comments']='코멘트';
