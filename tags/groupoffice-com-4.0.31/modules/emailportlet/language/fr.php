b1a1aedf5ad1e0d8024496b2bcf5507239547580
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-08 13:23:57 +0000

    Created tag 4.0.31

---
+++ tags/groupoffice-com-4.0.31/modules/emailportlet/language/fr.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$l["showOnSummary"]='Afficher sur la page de sommaire';
+$l["email"]='Email';
+$l["noEmailFolders"]='Aucun répertoire n\'a encore été ajouté.';
+$l["folderAdded"]='Le répertoire a été ajouté a la page sommaire.';
+
+$l['name']='Greffon email';
+$l['description']='Greffon pour afficher des répertoires de votre boite email sur la page de sommaire.';
