5ddf58b0c36488a45b356eba17d7fff924c58768
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-13 13:16:54 +0000

    no default addressbook, means no default addressbooks can be renamed

--- branches/groupoffice-4.0/www/modules/addressbook/GlobalSettings.js
+++ branches/groupoffice-4.0/www/modules/addressbook/GlobalSettings.js
@@ -8,18 +8,20 @@ GO.moduleManager.on('moduleconstructed',function(mm,moduleName,panel){
 				name:'addressbook_name_template',
 				fieldLabel:GO.addressbook.lang.globalsettings_templatelabel,
 				width: 300
-			},{
-				xtype:'checkbox',
-				name:'change_all_addressbook_names',
-				fieldLabel:GO.addressbook.lang.globalsettings_allchangelabel,
-				listeners: {
-	  			 "check": function(cb, isenabled) {
-						if(isenabled && !confirm(GO.addressbook.lang.globalsettings_renameall))
-							cb.setValue(false);
-					},
-					scope:this
-				}
-			}]
+			}
+//			,{
+//				xtype:'checkbox',
+//				name:'change_all_addressbook_names',
+//				fieldLabel:GO.addressbook.lang.globalsettings_allchangelabel,
+//				listeners: {
+//	  			 "check": function(cb, isenabled) {
+//						if(isenabled && !confirm(GO.addressbook.lang.globalsettings_renameall))
+//							cb.setValue(false);
+//					},
+//					scope:this
+//				}
+//			}
+		]
 		});
 		
 		panel.add(fieldset);
