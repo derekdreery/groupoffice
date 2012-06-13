b132529be2c851ab1262aad8c6f902f9ae16d7e1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-13 15:02:25 +0000

    Also use name field for searching

--- branches/groupoffice-4.0/www/controller/SearchController.php
+++ branches/groupoffice-4.0/www/controller/SearchController.php
@@ -84,8 +84,8 @@ class GO_Core_Controller_Search extends GO_Base_Controller_AbstractModelControll
 		
 		$subCriteria = GO_Base_Db_FindCriteria::newInstance()
 						//->addMatchCondition(array('name','keywords'), $params['match'],'t',true,'NATURAL');
-						->addMatchCondition(array('name','keywords'), $str);
-						//->addCondition('name', preg_replace('/[\s]+/','%',$params['match']).'%', 'LIKE','t',false);
+						->addMatchCondition(array('name','keywords'), $str)
+						->addCondition('name', preg_replace('/[\s*]+/','%',$params['match']).'%', 'LIKE','t',false);
 		
 		$storeParams->getCriteria()->mergeWith($subCriteria);
 		
