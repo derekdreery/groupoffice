a32361fd6854339160f4d837f9cff497878d3ba1
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-18 07:41:58 +0000

    Changed the category dialog to a tabbedform dialog.

--- branches/groupoffice-4.0/www/modules/bookmarks/CategoryDialog.js
+++ branches/groupoffice-4.0/www/modules/bookmarks/CategoryDialog.js
@@ -1,208 +1,58 @@
-/**
+/** 
  * Copyright Intermesh
- *
+ * 
  * This file is part of Group-Office. You should have received a copy of the
  * Group-Office license along with Group-Office. See the file /LICENSE.TXT
- *
+ * 
  * If you have questions write an e-mail to info@intermesh.nl
- *
+ * 
  * @version $Id$
  * @copyright Copyright Intermesh
- * @author Twan Verhofstad
+ * @author Merijn Schering <mschering@intermesh.nl>
  */
-
-
-GO.bookmarks.CategoryDialog = function(config){
-	
-	
-	if(!config)
-	{
-		config={};
-	}
-	
-	
-	this.buildForm();
-	
-	var focusFirstField = function(){
-		this.propertiesPanel.items.items[0].focus();
-	};
-	
-	
-	config.maximizable=true;
-	config.layout='fit';
-	config.modal=false;
-	config.resizable=false;
-	config.width=500;
-	config.height=440;
-	config.closeAction='hide';
-	config.title= GO.bookmarks.lang.category;
-	config.items= this.formPanel;
-	config.focus= focusFirstField.createDelegate(this);
-	config.buttons=[{
-		text: GO.lang['cmdOk'],
-		handler: function(){
-			this.submitForm(true);
-		},
-		scope: this
-	},{
-		text: GO.lang['cmdApply'],
-		handler: function(){
-			this.submitForm();
-		},
-		scope:this
-	},{
-		text: GO.lang['cmdClose'],
-		handler: function(){
-			this.hide();
-		},
-		scope:this
-	}
-	];
-
-	
-	GO.bookmarks.CategoryDialog.superclass.constructor.call(this, config);
-	
-	this.addEvents({
-		'save' : true
-	});
-}
-
-Ext.extend(GO.bookmarks.CategoryDialog, Ext.Window,{
-	
-	show : function (category_id) {
-
-		if(!this.rendered)
-			this.render(Ext.getBody());
-
+ 
+GO.bookmarks.CategoryDialog = Ext.extend(GO.dialog.TabbedFormDialog , {
+	initComponent : function(){
 		
-		this.tabPanel.setActiveTab(0);
+		Ext.apply(this, {
+			titleField:'name',
+			title:GO.bookmarks.lang.category,
+			formControllerUrl: 'bookmarks/category',
+			height:440,
+			width:500
+		});
 		
-		
-		
-		if(!category_id)
-		{
-			category_id=0;			
-		}
-			
-		this.setCategoryId(category_id);
-
-	//	console.log(category_id);
-
-		if(this.category_id>0)
-		{
-			this.formPanel.load({
-				url : GO.url('bookmarks/category/load'),
-
-				success:function(form, action)
-				{					
-					//	this.setWritePermission(action.result.data.write_permission);
-					this.readPermissionsTab.setAcl(action.result.data.acl_id);
-					
-					GO.bookmarks.CategoryDialog.superclass.show.call(this);
-				},
-
-				failure:function(form, action)
-				{
-					Ext.Msg.alert(GO.lang['strError'], action.result.feedback)
-				},
-				scope: this
-				
-			});
-		}else 
-		{			
-			this.formPanel.form.reset();
-			this.setWritePermission(true);
-			
-			GO.bookmarks.CategoryDialog.superclass.show.call(this);
-		}
+		GO.bookmarks.CategoryDialog.superclass.initComponent.call(this);	
 	},
-	
-	
-	setWritePermission : function(writePermission)
-	{
-		this.buttons[0].setDisabled(!writePermission);
-		this.buttons[1].setDisabled(!writePermission);
-	},
-
-	setCategoryId : function(category_id)
-	{
-		this.formPanel.form.baseParams['id']=category_id;
-		this.category_id=category_id;
-		
-	},
-	
-	submitForm : function(hide){
-		this.formPanel.form.submit(
-		{
-			url : GO.url('bookmarks/category/submit'),
-			waitMsg:GO.lang['waitMsgSave'],
-			success:function(form, action){				
-				this.fireEvent('save', this);				
-				if(hide)
-				{
-					this.hide();	
-				}else
-				{				
-					if(action.result.id)					
-						this.setCategoryId(action.result.id);
-
-					if(typeof(action.result.acl_id)!='undefined')
-						this.readPermissionsTab.setAcl(action.result.acl_id);
-				}	
-			},		
-			failure: function(form, action) {
-				if(action.failureType == 'client')
-				{					
-					Ext.MessageBox.alert(GO.lang['strError'], GO.lang['strErrorsInForm']);			
-				} else {
-					Ext.MessageBox.alert(GO.lang['strError'], action.result.feedback);
-				}
-			},
-			scope: this
-		});		
-	},	
-	
 	buildForm : function () {
 
-		var items=[{
-			xtype: 'textfield',
-			name: 'name',
-			anchor: '100%',
-			allowBlank:false,
-			fieldLabel: GO.lang.strName
-		}
-		]
-
 		this.propertiesPanel = new Ext.Panel({
-			border: false,			
-			title:GO.lang['strProperties'],			
-			cls:'go-form-panel',
-			waitMsgTarget:true,
-			layout:'form',
-			autoScroll:true,			
-			items: items				
-		});
-
-		var items  = [this.propertiesPanel];
-		
-		this.readPermissionsTab = new GO.grid.PermissionsPanel({
-			});
-
-		items.push(this.readPermissionsTab);
- 
-		this.tabPanel = new Ext.TabPanel({
-			activeTab: 0,
-			deferredRender: false,
+			url: GO.settings.modules.notes.url+'action.php',
 			border: false,
-			items: items,
-			anchor: '100% 100%'
-		}) ;    
-    
-		this.formPanel = new Ext.form.FormPanel({
-			waitMsgTarget:true,
-			url : GO.url('bookmarks/category/category'),
-			baseParams: {},
-			items:this.tabPanel				
+			baseParams: {task: 'category'},			
+			title:GO.lang['strProperties'],			
+			cls:'go-form-panel',waitMsgTarget:true,			
+			layout:'form',
+			autoScroll:true,
+			items:[{
+				xtype: 'textfield',
+			  name: 'name',
+				anchor: '100%',
+			  allowBlank:false,
+			  fieldLabel: GO.lang.strName
+			}
+//			,this.selectUser = new GO.form.SelectUser({
+//				fieldLabel: GO.lang['strUser'],
+//				disabled : !GO.settings.has_admin_permission,
+//				value: GO.settings.user_id,
+//				anchor: '100%'
+//			})
+		]
+				
 		});
+
+		this.addPanel(this.propertiesPanel);	
+ 
+    this.addPermissionsPanel(new GO.grid.PermissionsPanel());    
 	}
 });
