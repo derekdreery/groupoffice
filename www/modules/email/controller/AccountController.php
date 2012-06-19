cc27702c32d680de3b642c93b3075834ffcbea98
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-19 08:21:51 +0000

    checkunseen new style

--- www/modules/email/controller/AccountController.php
+++ www/modules/email/controller/AccountController.php
@@ -61,38 +61,27 @@ class GO_Email_Controller_Account extends GO_Base_Controller_AbstractModelContro
 
 	protected function actionCheckUnseen($params) {
 
-		require_once(GO::config()->root_path . 'Group-Office.php');
-		global $GO_SECURITY, $GO_MODULES;
-
+		$response=array("success"=>true);
+		$response['email_status']['unseen']=0;
+		
 		GO::session()->closeWriting();
+		
+		$findParams = GO_Base_Db_FindParams::newInstance()						
+						->ignoreAdminGroup();
 
+		$stmt = GO_Email_Model_Account::model()->find($findParams);
 
-		require_once ($GLOBALS['GO_MODULES']->modules['email']['class_path'] . "email.class.inc.php");
-		require_once ($GLOBALS['GO_MODULES']->modules['email']['class_path'] . "cached_imap.class.inc.php");
-
-		$imap = new cached_imap();
-		$email = new email();
-		$email2 = new email();
-
-
-		$response['success'] = true;
-		$count = $email->get_accounts($GLOBALS['GO_SECURITY']->user_id);
-		$response['email_status'] = array();
-		while ($email->next_record()) {
+		while ($account = $stmt->fetch()) {
 			try {
-				$account = $imap->open_account($email->f('id'), 'INBOX', false);
+				$imap = $account->openImapConnection();
+				
+				$unseen = $imap->get_unseen();
 
-				if ($account) {
-					$inbox = $email2->get_folder($email->f('id'), 'INBOX');
-
-					$unseen = $imap->get_unseen();
-
-					//$response['email_status'][$inbox['id']]=$account;
-					$response['email_status'][$inbox['id']]['unseen'] = $unseen['count'];
-					$response['email_status'][$inbox['id']]['messages'] = $imap->selected_mailbox['messages'];
-				}
+				//$response['email_status'][$inbox['id']]=$account;
+				$response['email_status']['unseen'] += $unseen['count'];
+				
 			} catch (Exception $e) {
-				go_debug($e->getMessage());
+				GO::debug($e->getMessage());
 			}
 			$imap->disconnect();
 		}
