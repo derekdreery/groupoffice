71ffb93ae455a9802198a57423e8e84e51b7bfb2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-26 13:37:54 +0000

    Version 4.0.49

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -723,7 +723,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '4.0.48';
+	var $version = '4.0.49';
 
 
 	/* The permissions mode to use when creating files
@@ -753,7 +753,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20120623';
+	var $mtime = '20120626';
 
 	#group configuration
 	/**
