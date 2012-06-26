fbb7750eb5627e9168346e0c286c0060713e71d4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-26 15:51:34 +0000

    undefined index

--- branches/groupoffice-4.0/www/Group-Office.php
+++ branches/groupoffice-4.0/www/Group-Office.php
@@ -112,8 +112,8 @@ if(!is_int($_SESSION['GO_SESSION']['timezone']))
 	date_default_timezone_set($_SESSION['GO_SESSION']['timezone']);
 }
 //after date_default_timezone otherwise date function might raise an error
-if(!class_exists('GO'))
-	go_debug('['.date('Y-m-d G:i').'] Start of new request: '.$_SERVER['REQUEST_URI']);
+//if(!class_exists('GO'))
+//	go_debug('['.date('Y-m-d G:i').'] Start of new request: '.$_SERVER['REQUEST_URI']);
 //go_debug($_POST);
 if($GO_CONFIG->debug){
 	function groupoffice_error_handler($errno, $errstr, $errfile, $errline, $errcontext) {
