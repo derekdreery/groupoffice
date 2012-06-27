67a350703fa635f2290fa4774312463a333c793c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-27 12:01:21 +0000

    Smarter search

--- www/go/base/db/ActiveRecord.php
+++ www/go/base/db/ActiveRecord.php
@@ -1398,7 +1398,7 @@ abstract class GO_Base_Db_ActiveRecord extends GO_Base_Model{
 		//throw new Exception('Error: you supplied a searchQuery parameter to find but getFindSearchQueryParamFields() should be overriden in '.$this->className());
 		$fields = array();
 		foreach($this->columns as $field=>$attributes){
-			if(isset($attributes['gotype']) && ($attributes['gotype']=='textfield' || $attributes['gotype']=='textarea' || $attributes['gotype']=='customfield'))
+			if(isset($attributes['gotype']) && ($attributes['gotype']=='textfield' || $attributes['gotype']=='textarea' || ($attributes['gotype']=='customfield' && $attributes['customfield']->customfieldtype->includeInSearches())))
 				$fields[]='`'.$prefixTable.'`.`'.$field.'`';
 		}
 		
