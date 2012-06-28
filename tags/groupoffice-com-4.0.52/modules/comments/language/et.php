158814a4d8d5131ec53922e873d36de6f0a3dd44
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-28 15:26:34 +0000

    Created tag 4.0.52

---
+++ tags/groupoffice-com-4.0.52/modules/comments/language/et.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$l['name']='Kommentaarid';
+$l['description']='Lisab teistele moodulitele kommenteerimise võimaluse';
+$l['comment']='Kommenteeri';
+$l['comments']='Kommentaarid';
+
+$l["comment"]="Kommentaar";
+$l["comments"]="Kommentaarid";
+$l["recentComments"]='Viimased kommentaarid';
+$l["browseComments"]= 'Sirvi kommentaare';
