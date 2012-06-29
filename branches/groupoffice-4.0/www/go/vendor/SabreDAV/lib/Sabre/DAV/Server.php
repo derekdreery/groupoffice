1ae2427c3291a3fbcc3b4e47199ce19f97053d4b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-06-29 14:47:18 +0000

    Debug info

--- branches/groupoffice-4.0/www/go/vendor/SabreDAV/lib/Sabre/DAV/Server.php
+++ branches/groupoffice-4.0/www/go/vendor/SabreDAV/lib/Sabre/DAV/Server.php
@@ -244,6 +244,9 @@ class Sabre_DAV_Server {
             $this->httpResponse->sendStatus($httpCode);
             $this->httpResponse->setHeaders($headers);
             $this->httpResponse->sendBody($DOM->saveXML());
+						
+						GO::debug("SabreDAV Exception: ");
+						GO::debug((string) $e);
 
         }
 
