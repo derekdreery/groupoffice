aebbeb3b4765e4dca897fa40d6fb54344d87161c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-02 13:17:57 +0000

    Created tag 4.0.56

---
+++ tags/groupoffice-com-4.0.56/modules/tools/language/es.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$l["tools"]='Herramientas de administración';
+$l["description"]='Haga clic en una de las secuencia de comandos para ejecutarlo.';
+$l["scripts"]='Script';
+$l["scriptOutput"]='Script de salida';
+$l['name']='Herramientas';
+$l['description']='Módulo para realizar tareas administrativas.';
+$l['dbcheck']='Comprobar la base de datos';
+$l['rm_duplicates']='Eliminar los contactos y eventos duplicados';
+$l['backupdb']='Copia de seguridad de base de datos';
+$l['index_files']='Indexar todos los archivos';
+$l['buildsearchcache']='Crear indice de búsqueda';
+$l['checkmodules']='Verificar módulos';
