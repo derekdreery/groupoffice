b2d93bb45d0894161a385e99773bbb92e943aef9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-02 07:34:29 +0000

    Bug in announcement formatting

--- www/modules/summary/model/Announcement.php
+++ www/modules/summary/model/Announcement.php
@@ -25,4 +25,10 @@ class GO_Summary_Model_Announcement extends GO_Base_Db_ActiveRecord {
 		return 'su_announcements';
 	}
 	
+	protected function init() {
+		$this->columns['content']['gotype']='html';
+		$this->columns['due_time']['gotype']='unixdate';
+		return parent::init();
+	}
+	
 }
