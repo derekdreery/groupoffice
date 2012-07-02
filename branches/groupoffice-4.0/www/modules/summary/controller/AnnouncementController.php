331b806cb5e0573497d19231c73286014b927ee0
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-02 07:34:29 +0000

    Bug in announcement formatting

--- branches/groupoffice-4.0/www/modules/summary/controller/AnnouncementController.php
+++ branches/groupoffice-4.0/www/modules/summary/controller/AnnouncementController.php
@@ -12,21 +12,6 @@
 class GO_Summary_Controller_Announcement extends GO_Base_Controller_AbstractModelController{
 	
 	protected $model = 'GO_Summary_Model_Announcement';
-
-	protected function beforeSubmit(&$response, &$model, &$params) {
-		$model->due_time = $params['due_time'] = GO_Base_Util_Date::to_unixtime($params['due_time']);
-	}
-	
-	protected function beforeLoad(&$response, &$model, &$params) {
-		$model->due_time = $response['due_time'] = GO_Base_Util_Date::format($model->due_time);
-		return $response;
-	}
-
-	public function formatStoreRecord($record, $model, $store) {
-		$record['due_time'] = date($_SESSION['GO_SESSION']['date_format'],$record['due_time']);
-		$record['content'] = htmlspecialchars_decode($record['content']);
-		return parent::formatStoreRecord($record, $model, $store);
-	}
 	
 	protected function getStoreParams($params) {
 		if (!empty($params['active']))
