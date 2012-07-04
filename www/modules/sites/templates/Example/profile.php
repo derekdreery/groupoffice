e7c3470197a4c84e4ce9b17e2a207cd1e737c7c1
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-04 07:48:31 +0000

    debug info

--- www/modules/sites/templates/Example/profile.php
+++ www/modules/sites/templates/Example/profile.php
@@ -1,3 +1,5 @@
+					<?php $this->_page->attachHeaderInclude('js',$this->_controller->getRootTemplateUrl().'js/jquery-1.7.2.min.js'); ?>
+
 					<div class="subkader-small-top">
 						<div class="subkader-small-bottom">
 							<div class="subkader-small-center">						
