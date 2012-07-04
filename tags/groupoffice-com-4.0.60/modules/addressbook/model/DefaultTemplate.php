31fcef63a19aeff8aff5c3cc48db07c2a6cf06fd
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-04 12:17:15 +0000

    Created tag 4.0.60

---
+++ tags/groupoffice-com-4.0.60/modules/addressbook/model/DefaultTemplate.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @copyright Copyright Intermesh
+ * @author Merijn Schering <mschering@intermesh.nl>
+ * @author Wilmar van Beusekom <wilmar@intermesh.nl>
+ * @property int $template_id
+ * @property int $user_id
+ */
+
+class GO_Addressbook_Model_DefaultTemplate extends GO_Base_Db_ActiveRecord {
+	
+	/**
+	 * Returns a static model of itself
+	 * 
+	 * @param String $className
+	 * @return GO_Addressbook_Model_Company 
+	 */
+	public static function model($className=__CLASS__)
+	{	
+		return parent::model($className);
+	}
+	
+	public function tableName() {
+		return 'ab_default_email_templates';
+	}
+	
+	public function primaryKey() {
+		return 'user_id';
+	}
+}
