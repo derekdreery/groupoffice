27e6b7c6675b4975cc9335c7309fac8f962d5c35
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-05 06:51:59 +0000

    Don't add trial users to addressbook

--- branches/groupoffice-4.0/debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/controller/SiteController.php
+++ branches/groupoffice-4.0/debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/controller/SiteController.php
@@ -104,49 +104,51 @@ class GO_Servermanager_Controller_Site extends GO_Sites_Controller_Site{
 	}
 	
 	private function _writeAddressbookEntries($params) {
-		$companyModel = new GO_Addressbook_Model_Company();
-		$companyEmptyAttributes = $companyModel->getAttributes('raw');
-		$contactModel = new GO_Addressbook_Model_Contact();
-		$contactEmptyAttributes = $contactModel->getAttributes('raw');
-			
-		foreach ($params as $k => $v) {
-			$paramKeyStringArr = explode('_',$k);
-			if ($paramKeyStringArr[0] == 'addressbook') {
-				array_shift($paramKeyStringArr);
-				switch ($paramKeyStringArr[0]) {
-					case 'company' :
-						array_shift($paramKeyStringArr);
-						$attributeName = implode('_',$paramKeyStringArr);
-						if (array_key_exists($attributeName,$companyEmptyAttributes))
-							$companyModel->$attributeName = $v;
-						break;
-					case 'contact' :
-						array_shift($paramKeyStringArr);
-					default:
-						$attributeName = implode('_',$paramKeyStringArr);
-						if (array_key_exists($attributeName,$contactEmptyAttributes))
-							$contactModel->$attributeName = $v;
-						break;
-				}
-			} else {
-				if (array_key_exists($k,$contactEmptyAttributes))
-					$contactModel->$k = $v;
-			}
-		}
+		if(!empty(GO::config()->servermanager_trials_addressbook_server_url)){
+			$companyModel = new GO_Addressbook_Model_Company();
+			$companyEmptyAttributes = $companyModel->getAttributes('raw');
+			$contactModel = new GO_Addressbook_Model_Contact();
+			$contactEmptyAttributes = $contactModel->getAttributes('raw');
 
-		$serverClient = new GO_Serverclient_HttpClient();
-		$serverClient->groupofficeLogin(GO::config()->servermanager_trials_addressbook_server_url, GO::config()->servermanager_trials_addressbook_login_username, GO::config()->servermanager_trials_addressbook_login_password);
-		
-		if ($companyModel->isModified()) {
-			$companyModel->addressbook_id = GO::config()->servermanager_trials_addressbook_id;			
-			$serverClient->request(GO::config()->servermanager_trials_addressbook_server_url.'?r=addressbook/company/submit', $companyModel->getAttributes());
-		}
-		
-		if ($contactModel->isModified()) {
-			$contactModel->addressbook_id = GO::config()->servermanager_trials_addressbook_id;
-			if ($companyModel->id > 0)
-				$contactModel->company_id = $companyModel->id;
-			$serverClient->request(GO::config()->servermanager_trials_addressbook_server_url.'?r=addressbook/contact/submit', $contactModel->getAttributes());
+			foreach ($params as $k => $v) {
+				$paramKeyStringArr = explode('_',$k);
+				if ($paramKeyStringArr[0] == 'addressbook') {
+					array_shift($paramKeyStringArr);
+					switch ($paramKeyStringArr[0]) {
+						case 'company' :
+							array_shift($paramKeyStringArr);
+							$attributeName = implode('_',$paramKeyStringArr);
+							if (array_key_exists($attributeName,$companyEmptyAttributes))
+								$companyModel->$attributeName = $v;
+							break;
+						case 'contact' :
+							array_shift($paramKeyStringArr);
+						default:
+							$attributeName = implode('_',$paramKeyStringArr);
+							if (array_key_exists($attributeName,$contactEmptyAttributes))
+								$contactModel->$attributeName = $v;
+							break;
+					}
+				} else {
+					if (array_key_exists($k,$contactEmptyAttributes))
+						$contactModel->$k = $v;
+				}
+			}
+
+			$serverClient = new GO_Serverclient_HttpClient();
+			$serverClient->groupofficeLogin(GO::config()->servermanager_trials_addressbook_server_url, GO::config()->servermanager_trials_addressbook_login_username, GO::config()->servermanager_trials_addressbook_login_password);
+
+			if ($companyModel->isModified()) {
+				$companyModel->addressbook_id = GO::config()->servermanager_trials_addressbook_id;			
+				$serverClient->request(GO::config()->servermanager_trials_addressbook_server_url.'?r=addressbook/company/submit', $companyModel->getAttributes());
+			}
+
+			if ($contactModel->isModified()) {
+				$contactModel->addressbook_id = GO::config()->servermanager_trials_addressbook_id;
+				if ($companyModel->id > 0)
+					$contactModel->company_id = $companyModel->id;
+				$serverClient->request(GO::config()->servermanager_trials_addressbook_server_url.'?r=addressbook/contact/submit', $contactModel->getAttributes());
+			}
 		}
 	}
 }
