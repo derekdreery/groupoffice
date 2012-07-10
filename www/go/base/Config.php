8f5d2b7ddf492a3766757dece254506195428b32
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-10 12:50:13 +0000

    Removed focus.js

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -730,7 +730,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '4.0.67';
+	var $version = '4.0.68';
 
 
 	/* The permissions mode to use when creating files
