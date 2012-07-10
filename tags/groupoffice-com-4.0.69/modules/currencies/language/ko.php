604ee362277cbbaa4d45e4ffd1a05ef06d1dfce1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-10 14:24:45 +0000

    Created tag 4.0.69

---
+++ tags/groupoffice-com-4.0.69/modules/currencies/language/ko.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$l["currency"]="Currency";
+$l["currencies"]="Currencies";
+$l["code"]="Code";
+$l["symbol"]="Symbol";
+$l["value"]="Value";
+
+$l['name']='Currencies';
+$l['description']='Select different currency types in the project expenses and income dialogs';
+$l['currency']='Currency';
+$l['currencies']='Currencies';
---
+++ tags/groupoffice-com-4.0.69/modules/tools/language/hu.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$l["tools"]='Adminisztrációs eszközök';
+$l["description"]='Kattints az alábbi szkriptek egyikére annak végrehajtásához.';
+$l["scripts"]='Szkriptek';
+$l["scriptOutput"]='Végeredmény';
+$l['name']='Eszközök';
+$l['description']='A module to perform administrative tasks.';
+$l['dbcheck']='Adatbázis ellenőrzés';
+$l['rm_duplicates']='Duplikált kapcsolatok és események eltávolítása';
+$l['backupdb']='Adatbázis mentése';
+$l['index_files']='Az összes fájl újraindexelése';
+$l['buildsearchcache']='Keresési index létrehozása';
+$l['checkmodules']='Modulok ellenőrzése';
