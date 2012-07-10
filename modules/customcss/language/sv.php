2741153da0a60d466093453c565a4292477fc662
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-10 12:45:51 +0000

    Created tag 4.0.67

---
+++ modules/customcss/language/sv.php
@@ -0,0 +1,6 @@
+<?php
+
+$l["customcss"]='Egna CSS-filer';
+$l["selectFile"]='Välj fil';
+$l['name']='Egna CSS-stilar och skript';
+$l['description']='Åsidosätter standardstilen för ett {product_name}-tema för att till exempel lägga till din egen logotyp.';
--- www/language/holidays/en.php
+++
@@ -1,8 +0,0 @@
-<?php
-$input_holidays['fix']['01-01'] = 'New Years Day';
-$input_holidays['fix']['12-25'] = 'Christmas Day';
-$input_holidays['fix']['12-26'] = 'Boxing Day';
-
-$input_holidays['var']['-2'] = 'Good Friday';
-$input_holidays['var']['0'] = 'Easter Sunday';
-$input_holidays['var']['1'] = 'Easter Monday';
