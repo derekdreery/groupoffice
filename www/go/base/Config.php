866c14e0da8a78020a69ff3712b4cbce5dd9d4d3
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-12 13:02:09 +0000

    return acl_book

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -722,7 +722,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '4.0.69';
+	var $version = '4.0.70';
 
 
 	/* The permissions mode to use when creating files
@@ -752,7 +752,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20120710-1';
+	var $mtime = '20120712';
 
 	#group configuration
 	/**
