9dc19090187cb2e65f4c95ad2f5dce6aafc06efe
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-13 06:39:06 +0000

    Created tag 4.0.71

---
+++ tags/groupoffice-com-4.0.71/modules/postfixadmin/language/ca.inc.php
@@ -0,0 +1,15 @@
+<?php
+//Uncomment this line in new translations!
+require($GLOBALS['GO_LANGUAGE']->get_fallback_language_file('postfixadmin'));
+$lang['postfixadmin']['name']='Postfix admin';
+$lang['postfixadmin']['description']='Un mòdul per gestionar usuaris virtuals de postfix dins la base de dades de {product_name}';
+$lang['postfixadmin']['alias']='Àlias';
+$lang['postfixadmin']['aliases']='Àlias';
+$lang['postfixadmin']['domain']='Domini';
+$lang['postfixadmin']['domains']='Dominis';
+$lang['postfixadmin']['fetchmail_config']='Configuració Fetchmail';
+$lang['postfixadmin']['fetchmail_configs']='Configuracions Fetchmail';
+$lang['postfixadmin']['mailbox']='Bústia de correu';
+$lang['postfixadmin']['mailboxes']='Bústies de correu';
+$lang['postfixadmin']['vacation_config']='Configuració de vacances';
+$lang['postfixadmin']['vacation_configs']='Configuracions de vacances';
