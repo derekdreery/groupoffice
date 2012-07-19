18ead178b1fa5d9d82f2ce11022e26cae8853fd2
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-19 11:36:20 +0000

    Fixed printing(PDF) for events and added description to calendar views

--- trunk/www/modules/calendar/model/LocalEvent.php
+++ trunk/www/modules/calendar/model/LocalEvent.php
@@ -138,6 +138,9 @@ class GO_Calendar_Model_LocalEvent extends GO_Base_Model {
 		$response['private'] = $this->isPrivate();
 		$response['status_color'] = $this->_event->getStatusColor();
 		
+		if(!empty($this->_event->description))
+			$response['description'] = $this->_event->description;
+		
 		$response['repeats'] = $this->isRepeating();
 		$response['all_day_event'] = $this->isAllDay();
 		$response['day'] = $dayString[date('w', ($this->_event->start_time))].' '.GO_Base_Util_Date::get_timestamp($this->_event->start_time,false);  // date(implode(GO::user()->date_separator,str_split(GO::user()->date_format,1)), ($eventModel->start_time));
