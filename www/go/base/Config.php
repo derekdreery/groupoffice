cd61e9a34c9045c33a6d9f611ee320456f0fd462
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-26 08:47:06 +0000

    VCF import fix

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -722,7 +722,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '4.0.74';
+	var $version = '4.0.75';
 
 
 	/* The permissions mode to use when creating files
@@ -752,7 +752,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20120725-1';
+	var $mtime = '20120726';
 
 	#group configuration
 	/**
