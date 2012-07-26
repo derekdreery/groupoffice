6f41143f80eb98a5dd3badd12424ca6fba9bbf64
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-26 09:34:08 +0000

    Created tag 4.0.75

---
+++ tags/groupoffice-com-4.0.75/modules/groups/language/cs.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$l['name'] = 'Skupiny';
+$l['description'] = 'Umožňuje upravovat skupiny.';
+$l['noDeleteAdmins'] = 'Nemůžete smazat skupinu Administrátorů';
+$l['noDeleteEveryone'] = 'Nemůžete smazat skupinu Všichni';
+$l['groupNameAlreadyExists'] = 'Zkoušíte vytvořit skupinu, která již existuje';
+$l['dontRemoveAdmin']='Nemůžete smazat hlavního Administrátora ze skupiny Administrátorů';
+$l["group"]= 'Skupina';
+$l["groups"]= 'Skupiny';
+$l["owner"]= 'Vlastník';
+$l["lblNew"]= 'Název';
+$l["newGroupName"]= 'Zadejte název nové skupiny';
+$l["searchAllUsers"]= 'Hledat ve všech uživatelích';
