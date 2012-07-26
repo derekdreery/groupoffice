3f62869f92248ace9cfad211def6837a8fb27a7b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-26 07:42:15 +0000

    Added search field

--- branches/groupoffice-4.0/www/modules/postfixadmin/MailboxesGrid.js
+++ branches/groupoffice-4.0/www/modules/postfixadmin/MailboxesGrid.js
@@ -79,6 +79,10 @@ GO.postfixadmin.MailboxesGrid = function(config){
 			this.store.reload();	    			    			
 		}, this);
 	
+	this.searchField = new GO.form.SearchField({
+		store: config.store,
+		width:320
+  });
 	
 	config.tbar=[{
 			iconCls: 'btn-add',							
@@ -96,7 +100,7 @@ GO.postfixadmin.MailboxesGrid = function(config){
 				this.deleteSelected();
 			},
 			scope: this
-		}];
+		},GO.lang['strSearch']+': ', ' ',this.searchField];
 
 	config.listeners={
 		show:function(){
