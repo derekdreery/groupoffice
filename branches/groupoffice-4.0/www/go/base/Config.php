3444c088f72249f8c03c15482a68f0039315845c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-30 13:05:58 +0000

    Don't exit automatically

--- branches/groupoffice-4.0/www/go/base/Config.php
+++ branches/groupoffice-4.0/www/go/base/Config.php
@@ -722,7 +722,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '4.0.78';
+	var $version = '4.0.79';
 
 
 	/* The permissions mode to use when creating files
@@ -752,7 +752,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20120730';
+	var $mtime = '20120730-1';
 
 	#group configuration
 	/**
