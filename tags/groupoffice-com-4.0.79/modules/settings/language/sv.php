8b7c8331f55973900b124db8e97e623982bfc4f1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-07-30 13:06:28 +0000

    Created tag 4.0.79

---
+++ tags/groupoffice-com-4.0.79/modules/settings/language/sv.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$l['name']='Inställningar';
+$l['description']='Med denna modul kan du hantera många inställningar i GroupOffice och dess moduler';
+$l['mainTitle'] = 'Inställningar';
+$l['loginScreenText']='Text för inloggningsskärmen';
+$l['text']='Text';
+$l['title']='Titel';
+$l['loginTextEnabled']='Aktivera text på inloggningsskärmen';
