f2f97d083278cbad174a7c9723dd531ae860491c
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-08-01 07:07:16 +0000

    Max length of event name changed from 100 to 150 in the db

--- branches/groupoffice-4.0/www/modules/calendar/install/updates.php
+++ branches/groupoffice-4.0/www/modules/calendar/install/updates.php
@@ -150,3 +150,5 @@ $updates["201205031333"][]="delete FROM `cal_participants` WHERE event_id=0;";
 $updates["201206131104"][]="ALTER TABLE `cal_categories` CHANGE `color` `color` CHAR( 6 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'EBF1E2'";
 
 $updates["201207090855"][]="ALTER TABLE `cal_calendars` CHANGE `show_bdays` `show_bdays` TINYINT( 1 ) NOT NULL DEFAULT '0'";
+
+$updates["201208010855"][]="ALTER TABLE `cal_events` CHANGE `name` `name` VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL"; 
