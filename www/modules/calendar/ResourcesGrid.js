0e04e25a83cd6cd02c17234e54dd8914cba2ea19
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-08-01 11:16:32 +0000

    Resources available for regular users

--- www/modules/calendar/ResourcesGrid.js
+++ www/modules/calendar/ResourcesGrid.js
@@ -37,7 +37,7 @@ GO.calendar.ResourcesGrid = function(config){
     config.tbar=[{
 	iconCls: 'btn-add',
 	text: GO.lang.cmdAdd,
-	disabled: !GO.settings.modules.calendar.write_permission,
+//	disabled: !GO.settings.modules.calendar.write_permission,
 	cls: 'x-btn-text-icon',
 	handler: function(){
 	    GO.calendar.calendarDialog.show(0, true);
@@ -45,7 +45,7 @@ GO.calendar.ResourcesGrid = function(config){
 	scope: this
     },{
 	iconCls: 'btn-delete',
-	disabled: !GO.settings.modules.calendar.write_permission,
+//	disabled: !GO.settings.modules.calendar.write_permission,
 	text: GO.lang.cmdDelete,
 	cls: 'x-btn-text-icon',
 	handler: function(){
