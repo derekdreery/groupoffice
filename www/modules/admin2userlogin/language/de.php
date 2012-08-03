87159838214b01125edaa2d1993d2a3540566435
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-08-03 13:54:31 +0000

    Created tag 4.0.88

--- www/modules/admin2userlogin/language/de.php
+++
@@ -1,8 +0,0 @@
-<?php
-$l['admin2userlogin']='Nutzer wechseln';
-$l['userId']="ID";
-$l['name']="Nutzer wechseln";
-$l['description']="Der Administrator kann sich ohne Kenntnis des Nutzerkennwortes als dieser Nutzer einloggen.";
-$l['username']="Benutzer";
-$l['lastlogin']="Letzter Loginversuch";
-$l['registrationtime']="Registrierungszeit";
