2f3442930f214ff69c6a4114718f3aa389a8ce38
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-08-09 13:00:07 +0000

    Added italian language

---
+++ branches/groupoffice-4.0/www/modules/emailportlet/language/it.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$l["showOnSummary"]='Mostra nella pagina Riepilogo';
+$l["email"]='Email';
+$l["noEmailFolders"]='Non è stata aggiunta alcuna cartella.';
+$l["folderAdded"]='La cartella è stata aggiunta alla pagina Riepilogo.';
+
+$l['name']='Portlet Email';
+$l['description']='Plugin per mostrare le cartelle di posta nella pagina di riepilogo.';
