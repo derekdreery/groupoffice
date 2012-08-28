670947c4a94d85c21c06580f22df72a9caf1855c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-08-28 12:24:04 +0000

    Created tag 4.0.90

---
+++ tags/groupoffice-com-4.0.90/modules/admin2userlogin/language/fr.inc.php
@@ -0,0 +1,26 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright Intermesh
+// 
+// This file is part of Group-Office. You should have received a copy of the
+// Group-Office license along with Group-Office. See the file /LICENSE.TXT
+// 
+// If you have questions write an e-mail to info@intermesh.nl
+//
+// @copyright Copyright Intermesh
+// @version $Id: fr.inc.php 6616 2011-01-13 09:36:17Z mschering $
+// @author Merijn Schering <mschering@intermesh.nl>
+//
+// French Translation
+// Version : 3.7.29 
+// Author : Lionel JULLIEN
+// Date : September, 27 2011
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('admin2userlogin'));
+
+$lang['admin2userlogin']['name']='Changer d\'utilisateur';
+$lang['admin2userlogin']['description']='Permet à l\'administrateur de se connecter avec l\'identifiant d\'un utilisateur sans avoir à saisir son mot de passe.';
