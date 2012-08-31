bcf45e984121c5bc4a7899a5a01c32b64c78bfa8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-08-31 13:04:11 +0000

    Created tag 4.0.93

---
+++ tags/groupoffice-com-4.0.93/modules/currencies/language/ru.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$l['name']='Валюты';
+$l['description']='Валюты';
+$l['currency']='Валюта';
+$l['currencies']='Валюты';
+
+$l["currencies"]='currencies';
+$l["currency"]="Валюта";
+$l["currencies"]="Валюты";
+$l["code"]="Код";
+$l["symbol"]="Символ";
+$l["value"]="Значание курса";
---
+++ tags/groupoffice-com-4.0.93/modules/emailportlet/language/en.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$l["showOnSummary"]='Show on Summary page';
+$l["email"]='Email';
+$l["noEmailFolders"]='No folders have been added.';
+$l["folderAdded"]='Folder is added to the Summary page.';
+
+$l['name']='Email Portlet';
+$l['description']='Plugin to show email folders on the summary page.';
