c3ffbe8d22450395277458ecf2b9c73eb3b53f86
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-08-31 15:02:13 +0000

    Bug in sending mail with some inline attachments

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -722,7 +722,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '4.0.93';
+	var $version = '4.0.94';
 
 
 	/* The permissions mode to use when creating files
@@ -752,7 +752,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20120831-1';
+	var $mtime = '20120831-2';
 
 	#group configuration
 	/**
