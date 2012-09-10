5f43588e06f690f9ddb95f49126325e4132638de
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-09-10 06:54:26 +0000

    4.0.96

--- www/modules/email/model/Account.php
+++ www/modules/email/model/Account.php
@@ -284,7 +284,8 @@ class GO_Email_Model_Account extends GO_Base_Db_ActiveRecord {
 	 */
 	public function getAutoCheckMailboxes(){
 		$checkMailboxArray = empty($this->check_mailboxes) ? array() : explode(',',$this->check_mailboxes);
-		$checkMailboxArray[]="INBOX";
+		if(!in_array("INBOX", $checkMailboxArray))
+			$checkMailboxArray[]="INBOX";
 		return $checkMailboxArray;
 	}
 
