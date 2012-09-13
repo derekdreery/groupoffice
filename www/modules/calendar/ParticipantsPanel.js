532de388e3fd6a90466a2d61472457f74e8c883c
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-09-13 09:05:23 +0000

    fixed bug where attempt is made to create duplicate participants without an event id (used to happen during creation of exception event)

--- www/modules/calendar/ParticipantsPanel.js
+++ www/modules/calendar/ParticipantsPanel.js
@@ -255,23 +255,23 @@ Ext.extend(GO.calendar.ParticipantsPanel, Ext.Panel, {
 		//this.inviteCheckbox.setValue(false);
 		this.importCheckbox.setValue(false);
 
-		if(this.isVisible()){
-			this.store.reload();
-		}
+//		if(this.isVisible()){
+//			this.store.reload();
+//		}
 	},
 	
-	onShow : function() {
-		if (!this.store.loaded) {
-			if(this.store.baseParams.event_id > 0)
-			{
-				this.store.load();
-			}else
-			{
-				this.addDefaultParticipant();
-			}			
-		}
-		GO.calendar.ParticipantsPanel.superclass.onShow.call(this);
-	},
+//	onShow : function() {
+//		if (!this.store.loaded) {
+//			if(this.store.baseParams.event_id > 0)
+//			{
+//				this.store.load();
+//			}else
+//			{
+//				this.addDefaultParticipant();
+//			}			
+//		}
+//		GO.calendar.ParticipantsPanel.superclass.onShow.call(this);
+//	},
 	
 	invitationRequired : function(){
 		//invitation is required if there's a participant that is not the current user.
