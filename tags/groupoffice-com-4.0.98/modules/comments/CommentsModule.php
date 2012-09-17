3c719affd4e12eaacca69a6a4f27d1b2e926e0ac
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-09-17 13:27:08 +0000

    Removed invalid tag 4.0.98

--- tags/groupoffice-com-4.0.98/modules/comments/CommentsModule.php
+++
@@ -1,6 +0,0 @@
-<?php
-class GO_Comments_CommentsModule extends GO_Base_Module{
-	public function autoInstall() {
-		return true;
-	}
-}
--- tags/groupoffice-com-4.0.98/modules/customcss/language/nb.js
+++
@@ -1,4 +0,0 @@
-
-GO.customcss.lang.customcss='Egendefinert CSS & Javascript';
-
-GO.customcss.lang.selectFile='Velg fil';
