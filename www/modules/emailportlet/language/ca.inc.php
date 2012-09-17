b8708093bbb8dc88130433312e9b8fa5bcd9a084
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-09-17 13:31:39 +0000

    Created tag 4.0.98

--- www/modules/emailportlet/language/ca.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Uncomment this line in new translations!
-require($GLOBALS['GO_LANGUAGE']->get_fallback_language_file('emailportlet'));
-
-$lang['emailportlet']['name']='Portlet d\'Email';
-$lang['emailportlet']['description']='Plugin per mostrar les carpetes d\'e-mail dins la pàgina de sumari.';
