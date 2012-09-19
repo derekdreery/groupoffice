b97dfab9b15d732ff376e7afd7b5d738fb382944
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-09-19 14:00:25 +0000

    Created tag 4.0.100

--- www/modules/emailportlet/language/et.js
+++
@@ -1,4 +0,0 @@
-GO.emailportlet.lang.showOnSummary='Näita ülevaadete lehel';
-GO.emailportlet.lang.email='E-post';
-GO.emailportlet.lang.noEmailFolders='Ühtegi kausta ei ole lisatud';
-GO.emailportlet.lang.folderAdded='Kaust lisati ülevaadete lehele';
