ea4c95b4b7ab38a6951613b5acdc6230f0d6d604
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-09-19 14:00:25 +0000

    Created tag 4.0.100

---
+++ tags/groupoffice-com-4.0.100/index.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @copyright Copyright Intermesh
+ * @version $Id: json.php 8246 2011-10-05 13:55:38Z mschering $
+ * @author Merijn Schering <mschering@intermesh.nl>
+ */
+
+$root = dirname(__FILE__).'/';
+
+//initialize autoloading of library
+require_once($root.'go/GO.php');
+GO::init();
+
+if(!GO::user())
+	GO::session()->loginWithCookies();	
+
+//check if GO is installed
+if(empty($_REQUEST['r']) && PHP_SAPI!='cli'){	
+	
+	if(GO::user() && isset($_SESSION['GO_SESSION']['after_login_url'])){
+		$url = GO::session()->values['after_login_url'];
+		unset(GO::session()->values['after_login_url']);
+		header('Location: '.$url);
+		exit();
+	}
+	
+	$installed=true;
+	if(!GO::config()->get_config_file() || empty(GO::config()->db_user)){			
+		$installed=false;
+	}else
+	{
+		$stmt = GO::getDbConnection()->query("SHOW TABLES");
+		if(!$stmt->rowCount())
+			$installed=false;
+	}
+	if(!$installed){
+		header('Location: '.GO::config()->host.'install/');				
+		exit();
+	}
+
+	//check for database upgrades
+	$mtime = GO::config()->get_setting('upgrade_mtime');
+
+	if($mtime!=GO::config()->mtime)
+	{
+		GO::infolog("Running system update");
+		header('Location: '.GO::url('maintenance/upgrade'));
+		exit();
+	}
+}
+
+GO::router()->runController();
