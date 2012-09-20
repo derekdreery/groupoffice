b29a923d19abc29c4a23ca16eddfd87edf85a80c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-09-20 08:55:51 +0000

    4.0.103

--- branches/groupoffice-4.0/www/go/base/Config.php
+++ branches/groupoffice-4.0/www/go/base/Config.php
@@ -722,7 +722,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '4.0.102';
+	var $version = '4.0.103';
 
 
 	/* The permissions mode to use when creating files
@@ -752,7 +752,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20120920';
+	var $mtime = '20120920-1';
 
 	#group configuration
 	/**
