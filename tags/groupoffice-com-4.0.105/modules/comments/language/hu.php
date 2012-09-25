9b2fbb0a4fe850952e80d1ededb85cb9ae2b5887
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-09-25 14:38:14 +0000

    Created tag 4.0.105

---
+++ tags/groupoffice-com-4.0.105/modules/comments/language/hu.php
@@ -0,0 +1,11 @@
+<?php
+
+;
+$l["comment"]="Hozzászólás";
+$l["comments"]="Hozzászólások";
+$l["fiveLatestComments"]='5 utolsó hozzászólás';
+$l["browseComments"]= 'Tallózás a hozzászólásokban';
+$l['name']='Hozzászólások';
+$l['description']='Hozzászólások a többimodulhoz';
+$l['comment']='Hozzászólás';
+$l['comments']='Hozzászólások';
