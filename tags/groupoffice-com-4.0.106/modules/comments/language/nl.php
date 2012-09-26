b935c17b6aeae79348c6d6ca9c9ad7311e3cca38
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-09-26 07:29:50 +0000

    Created tag 4.0.106

---
+++ tags/groupoffice-com-4.0.106/modules/comments/language/nl.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$l['name']='Opmerkingen';
+$l['description']='Voegt opmerkingen functionaliteit toe aan andere modules';
+$l['comment']='Opmerking';
+$l['comments']='Opmerkingen';
+$l["comment"]="Opmerking";
+$l["comments"]="Opmerkingen";
+$l["recentComments"]='Meest recente opmerkingen';
+$l["browseComments"]= 'Blader door opmerkingen';
+
+$l['manageCategories']='Categorieën beheren';
+$l['category']='Categorie';
+$l['noCategory']='Geen categorie';
+
+$l['newCommentText']='Voer hier nieuwe commentaar in ...';
+$l['newComment']='Nieuw commentaar';
