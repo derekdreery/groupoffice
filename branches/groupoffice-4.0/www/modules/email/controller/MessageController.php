d0169f637d760bf0f4db0d216b59f4d47e738468
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-09-29 13:50:15 +0000

    Throw not found exception in email

--- branches/groupoffice-4.0/www/modules/email/controller/MessageController.php
+++ branches/groupoffice-4.0/www/modules/email/controller/MessageController.php
@@ -923,6 +923,9 @@ class GO_Email_Controller_Message extends GO_Base_Controller_AbstractController 
 		$account = GO_Email_Model_Account::model()->findByPk($params['account_id']);
 		$imapMessage = GO_Email_Model_ImapMessage::model()->findByUid($account, $params['mailbox'], $params['uid']);
 		
+		if(!$imapMessage)
+			throw new GO_Base_Exception_NotFound();
+		
 		//workaround for gmail. It doesn't flag messages as seen automatically.
 //		if (!$imapMessage->seen && stripos($account->host, 'gmail') !== false)
 //			$imapMessage->getImapConnection()->set_message_flag(array($imapMessage->uid), "\Seen");
