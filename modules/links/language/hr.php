c53b16bfcdd346080a8cd64521611cc03859e078
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-10-01 06:55:04 +0000

    Created tag 4.0.107

---
+++ modules/links/language/hr.php
@@ -0,0 +1,11 @@
+<?php
+$l['name']='Opisi poveznica';
+$l['description']='Upravljaj zadanim opisima poveznica';
+$l['link_description']='Opis poveznice';
+$l['link_descriptions']='Opisi poveznica';
+$l["links"]='Poveznice';
+$l["linkDescription"]="Opis poveznice";
+$l["linkDescriptions"]="Opisi poveznica";
+$l["defaultLinkFolderText"]='Direktoriji navedeni ovdje će biti stvoreni automatski kada stavka bude stvorena. Unesite putanje direktorija svaku u poseban red, ovako: <br /><br />Direktorij/Poddirektorij<br />Direktorij 2/Poddirektorij 2<br /><br />';
+$l["linkFolders"]='Zadana poveznica za direktorije';
+$l["link"]='Poveznica';
