46cb86c87e96a16127a835c40fce6c732fb2188f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-10-01 12:33:40 +0000

    Created tag 4.0.108

--- www/modules/emailportlet/language/nl.js
+++
@@ -1,5 +0,0 @@
-GO.emailportlet.lang.showOnSummary='Toon op Startpagina';
-GO.emailportlet.lang.email='Email';
-GO.emailportlet.lang.noEmailFolders='Er zijn geen mappen toegevoegd.';
-GO.emailportlet.lang.folderAdded='Map wordt getoond op de startpagina.';
-//GO.emailportlet.lang.folderAdded='Map wordt getoond op de startpagina';
