072cad582df1ddd3cdc0f8f1bb0cb5650497fd5b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-10-02 07:50:10 +0000

    Updated German

--- www/modules/comments/language/de.php
+++ www/modules/comments/language/de.php
@@ -9,3 +9,9 @@ $l['name']='Kommentare';
 $l['description']='Modul zur Aktivierung von Kommentaren in anderen Modulen';
 $l['comment']='Kommentar';
 $l['comments']='Kommentare';
+
+$l['manageCategories']='Kategorien verwalten';
+$l['category']='Kategorie';
+$l['noCategory']='Keine Kategorie';
+$l['newCommentText']='Hier einen neuen Kommentar eingeben ...';
+$l['newComment']='Neuer Kommentar';
