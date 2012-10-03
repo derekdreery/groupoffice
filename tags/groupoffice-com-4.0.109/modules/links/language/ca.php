5192b449e6e81047eb1299cde0b67e0d5df535f2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-10-03 13:26:34 +0000

    Created tag 4.0.109

---
+++ tags/groupoffice-com-4.0.109/modules/links/language/ca.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$l['name']='Descripcions d\'enllaç';
+$l['description']='Administrar descripció d\'enllaços per defecte';
+$l['link_description']='Descripció d\'enllaç';
+$l['link_descriptions']='Descripcions d\'enllaç';
+$l["link"]='Enllaç';
+$l["links"]='Enllaços';
+$l["linkDescription"]='Descripció d\'enllaç';
+$l["linkDescriptions"]='Descripcions d\'enllaç';
+$l["defaultLinkFolderText"]='Les carpetes llistades aquí es crearan automàticament quan es creï un ítem. Introduïu la ruta de cada carpeta en una nova línia com: <br /><br />Carpeta/Subcarpeta<br />Carpeta 2/Subcarpeta 2<br /><br />';
+$l["linkFolders"]='Carpetes d\'enllaços per defecte';
