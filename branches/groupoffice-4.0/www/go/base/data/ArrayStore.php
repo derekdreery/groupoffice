7f4dfc2aeaacd0a5f5bc495f40fbc0f32683d196
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-10-03 08:15:47 +0000

    Functional array store

--- branches/groupoffice-4.0/www/go/base/data/ArrayStore.php
+++ branches/groupoffice-4.0/www/go/base/data/ArrayStore.php
@@ -15,10 +15,22 @@
  * @version $Id: ArrayStore.php 7607 2011-08-04 13:41:42Z wsmits $
  * @copyright Copyright Intermesh BV.
  * @author Wesley Smits <wsmits@intermesh.nl>
+ * @author Merijn Schering <mschering@intermesh.nl>
  * @package GO.base.data
  */
 class GO_Base_Data_ArrayStore extends GO_Base_Data_AbstractStore {
 
 	
+	public function getData() {
+		$this->response['total']=$this->getTotal();
+		return $this->response;
+	}
 	
+	public function getTotal() {
+		return count($this->response['results']);
+	}
+	
+	public function nextRecord() {
+		return next($this->response['results']);
+	}
 }
