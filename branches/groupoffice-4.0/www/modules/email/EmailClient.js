27ae0da83149a519fda3fe82616fdf70169ec461
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-10-09 08:13:03 +0000

    Mute e-mail sound alone didn't work

--- branches/groupoffice-4.0/www/modules/email/EmailClient.js
+++ branches/groupoffice-4.0/www/modules/email/EmailClient.js
@@ -1385,8 +1385,11 @@ GO.mainLayout.onReady(function(){
 
 //					if(!ep || !ep.isVisible()){
 						GO.email.notificationEl.setDisplayed(true);
+						
 						data.popup=true;
-						data.alarm=true;
+						
+						if(!GO.settings.mute_new_mail_sound)
+							data.alarm=true;
 //					}
 
 					GO.email.notificationEl.update(result.email_status.total_unseen);
