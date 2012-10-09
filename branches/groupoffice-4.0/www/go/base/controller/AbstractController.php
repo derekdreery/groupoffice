20d096d2124423b603de28a8ba8b964981bdbe79
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-10-09 12:47:48 +0000

    Log info about access denied

--- branches/groupoffice-4.0/www/go/base/controller/AbstractController.php
+++ branches/groupoffice-4.0/www/go/base/controller/AbstractController.php
@@ -340,6 +340,14 @@ abstract class GO_Base_Controller_AbstractController extends GO_Base_Observable 
 				//doesn't work well with extjs
 //				header("HTTP/1.1 403 Forbidden");
 				
+				$report = 
+								"Access denied\n".
+								"controller: ".get_class($this)." action: ".$action."\n".
+								"params: ".var_export($params, true)."\n".
+								(string) $e;
+				
+				trigger_error($report, E_USER_WARNING);
+				
 				$response['redirectToLogin']=empty(GO::session()->values['user_id']);
 			}
 			
