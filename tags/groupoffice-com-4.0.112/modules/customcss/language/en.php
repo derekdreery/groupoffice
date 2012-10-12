11ec59a061ba142bbbd6d180e040147c8961deed
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-10-12 10:04:12 +0000

    Created tag 4.0.112

---
+++ tags/groupoffice-com-4.0.112/modules/customcss/language/en.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$l["customcss"]='Custom CSS and Javascript';
+$l["selectFile"]='Select file';
+
+$l['name']='Custom CSS styles and scripts';
+$l['description']='Override the default style of a {product_name} theme to add your own logo for example.';
---
+++ tags/groupoffice-com-4.0.112/modules/tools/language/da.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$l["tools"]='Admin værktøjer';
+$l["description"]='Klik på et script nedenfor for at udføre det.';
+$l["scripts"]='Scripts';
+$l["scriptOutput"]='Script resultat';
+$l['name']='Værktøjer';
+$l['description']='Modul til at udføre administrator opgaver.';
+$l['dbcheck']='Database check';
+$l['rm_duplicates']='Fjern dobbelte kontakter og hændelser';
+$l['backupdb']='Backup database';
