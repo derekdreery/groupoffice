77632225747451ad59066d896c91f3954f5f20b2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-10-16 13:35:20 +0000

    Utility to download files from the shop

--- www/controller/MaintenanceController.php
+++ www/controller/MaintenanceController.php
@@ -7,7 +7,7 @@
 class GO_Core_Controller_Maintenance extends GO_Base_Controller_AbstractController {
 	
 	protected function allowGuests() {
-		return array('upgrade','checkdatabase','servermanagerreport','test');
+		return array('upgrade','checkdatabase','servermanagerreport','test','getlatest');
 	}
 	
 	//don't check token in this controller
@@ -20,6 +20,55 @@ class GO_Core_Controller_Maintenance extends GO_Base_Controller_AbstractControll
 		ini_set('display_errors','on');
 		error_reporting(E_ALL);
 		
+		
+	}
+	
+	
+	protected function actionGetLatest($params){
+		
+		$this->requireCli();
+		
+		$this->checkRequiredParameters(array('shopuser','shoppass'), $params);
+		
+		$shopUrl = 'https://shop.group-office.com/groupoffice/';
+		
+		$packages = array('documents-4.0', 'billing-4.0', 'groupoffice-pro-4.0');
+		
+		foreach($packages as $package_name){
+			
+			echo "\nGetting latest ".$package_name."\n";
+		
+			$packageUrl = 'https://shop.group-office.com/groupoffice/?r=licenses/package/downloadPackageFile&package_name='.$package_name;
+			
+
+			$c = new GO_Base_Util_HttpClient();
+			if(!$c->groupofficeLogin($shopUrl, $params['shopuser'],$params['shoppass']))
+				exit("Bad user name or password for shop");
+			else
+				echo "Shop login successful\n";
+
+			$tmpDir = new GO_Base_Fs_Folder(getcwd());
+			if(!$tmpDir->isWritable())
+				exit("Error: ".$tmpDir->path ()." is not writable!\n");
+
+			$file = $tmpDir->createChild($package_name.'.tar.gz');
+			echo "Downloading file from shop...\n";
+			if(!$c->downloadFile($packageUrl, $file))		
+				exit("Error: Failed to download file");
+			
+			$file->rename($c->getLastDownloadedFilename());
+			
+			//echo "Filename: ".$c->getLastDownloadedFilename()."\n";
+
+			echo "File saved in ".$file->path()."\n";
+//			chdir($tmpDir->path());
+
+//			echo "Unpacking ".$file->name()."\n";
+//			system('tar zxf '.$file->name());
+
+		}
+		
+		echo "All done\n";
 		
 	}
 	
