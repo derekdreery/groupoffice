09474a1e7b50ab704f678d14124dce460f6c45b2
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-10-16 10:53:08 +0000

    Created tag 4.0.113

---
+++ tags/groupoffice-com-4.0.113/modules/links/language/es.js
@@ -0,0 +1,5 @@
+GO.links.lang.links='Verknüpfungen';
+GO.links.lang.linkDescription='Beschreibung';
+GO.links.lang.linkDescriptions='Bschreibungen';
+GO.links.lang.defaultLinkFolderText='Hier eingetragene Ordner werden automatisch angelegt, wenn ein Eintrag erstellt wird. Geben Sie jeden Ordnerpfad in einer eigenen Zeile wie folgt an: <br /><br />Ordner/Unterordner<br />Ordner 2/Unterordner 2<br /><br />';
+GO.links.lang.linkFolders='Standard Verknüpfungsordner';
