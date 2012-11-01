dda41100a0edd5728ba34017c0af98070814e1ae
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-11-01 12:59:48 +0000

    content types can be defined with uppercase characters, GO can now handle this

--- www/modules/email/model/SavedMessage.php
+++ www/modules/email/model/SavedMessage.php
@@ -148,9 +148,14 @@ class GO_Email_Model_SavedMessage extends GO_Email_Model_ComposerMessage {
 
 	private function _getParts($structure, $part_number_prefix='') {
 		if (isset($structure->parts)) {
+			$structure->ctype_primary = strtolower($structure->ctype_primary);
+			$structure->ctype_secondary = strtolower($structure->ctype_secondary);
 			//$part_number=0;
 			foreach ($structure->parts as $part_number => $part) {
-
+				
+				$part->ctype_primary = strtolower($part->ctype_primary);
+				$part->ctype_secondary = strtolower($part->ctype_secondary);
+				
 				//text part and no attachment so it must be the body
 				if ($structure->ctype_primary == 'multipart' && $structure->ctype_secondary == 'alternative' &&
 								$part->ctype_primary == 'text' && $part->ctype_secondary == 'plain') {
