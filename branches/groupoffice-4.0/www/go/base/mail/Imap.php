d5fce62dded5986701afc3288056497a15e59e26
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-11-02 11:24:01 +0000

    Don't log imap commands

--- branches/groupoffice-4.0/www/go/base/mail/Imap.php
+++ branches/groupoffice-4.0/www/go/base/mail/Imap.php
@@ -101,9 +101,9 @@ class GO_Base_Mail_Imap extends GO_Base_Mail_ImapBodyStruct {
 			$this->send_command($command);
 			$this->state = 'disconnected';
 			$result = $this->get_response();
-			GO::debug($this->commands);
-
-			//GO::debug($this->responses);
+//			GO::debug($this->commands);
+//
+//			GO::debug($this->responses);
 
 			fclose($this->handle);
 
