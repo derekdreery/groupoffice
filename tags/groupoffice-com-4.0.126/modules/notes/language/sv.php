03ae6ed8e861421588d7c16103fef484acbd3f94
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-11-12 15:02:25 +0000

    Created tag 4.0.126

---
+++ tags/groupoffice-com-4.0.126/modules/notes/language/sv.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$l['name']= 'Anteckningar';
+$l['description']= 'Modul för enklare anteckningar.';
+$l['category']= 'Kategori';
+$l['categories']= 'Kategorier';
+$lang['link_type'][4]=$l['note']= 'Anteckning';
+$l['notes']= 'Anteckningar';
+$l['general'] = 'Allmänt';
+$l['incomplete_delete']='Du har inte behörighet att radera alla valda anteckningar';
+$l['multipleCategoriesSelected']='Flera anteckningsböcker valda';
+
+$l["category"]= 'Kategori';
+$l["categories"]= 'Kategorier';
+$l["note"]= 'Anteckning';
+$l["notes"]= 'Anteckningar';
+$l["category_id"]= 'Kategori-ID';
+$l["content"]= 'Innehåll';
+$l["manageCategories"]= 'Hantera kategorier';
