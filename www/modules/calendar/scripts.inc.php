397f779d4a633cb16d6c8b4b93ec60328383f7cc
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-11-12 15:02:25 +0000

    Created tag 4.0.126

--- www/modules/calendar/scripts.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-$calendar = GO_Calendar_Model_Calendar::model()->getDefault(GO::user());
-
-if($calendar)
-	$GO_SCRIPTS_JS .= 'GO.calendar.defaultCalendar = '.json_encode($calendar->getAttributes()).';';
