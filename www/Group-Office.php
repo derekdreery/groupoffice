4f248556f79e9b4f7fc370256d0c7e3a12c3cdca
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-11-14 12:40:42 +0000

    4.0.127

--- www/Group-Office.php
+++ www/Group-Office.php
@@ -223,8 +223,8 @@ umask(0);
 
 //todo this would be the new code:
 
-if(!class_exists('GO')){
-	$root = dirname(__FILE__).'/';
-	require_once($root.'go/GO.php');
-	GO::init();
-}
+//if(!class_exists('GO')){
+//	$root = dirname(__FILE__).'/';
+//	require_once($root.'go/GO.php');
+//	GO::init();
+//}
