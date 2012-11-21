aef8679c73b808250815200b35444333b124c236
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-11-21 08:01:42 +0000

    New CMS

--- trunk/www/modules/sites/templates/intermeshshop/views/webshop/paymentnotification.php
+++ trunk/www/modules/sites/templates/intermeshshop/views/webshop/paymentnotification.php
@@ -3,4 +3,6 @@
 <?php
 echo $order->replaceTemplateTags($order->status->getLanguage($order->language_id)->screen_template);
 ?>
+<p>
 <a href="https://shop.group-office.com">Return to shop.group-office.com</a>
+</p>
