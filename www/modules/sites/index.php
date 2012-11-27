2941c7f6a9f236a635cf40920c81c5dcc004d054
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-11-27 19:44:14 +0000

    Added deploy instructions

--- www/modules/sites/index.php
+++ www/modules/sites/index.php
@@ -1,26 +1,6 @@
 <?php
 //Copy this file to the root of your website
-$path_to_gos = '/var/www/trunk/www/modules/sites/components/GOS.php';
+//adjust the $path_to_gos file to the location of groupoffice/modules/sites/components/GOS.php
+$path_to_gos = dirname(__FILE__).'/components/GOS.php';
 require($path_to_gos);
 GOS::launch()->run();
-
-/**
- * EXAMPLE MOD_REWRITE RULE FOR A SITE
-
-<VirtualHost *:80>
-	ServerName group-office.com
-	ServerAlias website.group-office.com
-	DocumentRoot /var/www/website.group-office.com/html
-
-	<Directory /var/www/website.group-office.com/html>
-		RewriteEngine On
-		RewriteBase /
-
-		RewriteCond %{REQUEST_FILENAME} !-f
-		RewriteCond %{REQUEST_FILENAME} !-d
-		RewriteRule ^(.*)\?*$ index.php/$1 [L,QSA]
-
-	</Directory>
-</VirtualHost>
-
-*/
