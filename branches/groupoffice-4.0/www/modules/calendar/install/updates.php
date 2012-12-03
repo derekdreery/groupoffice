a06679b62d2d1ff73de02c46cc4e2995e8da866c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-12-03 15:48:31 +0000

    Remove erroneous rrules

--- branches/groupoffice-4.0/www/modules/calendar/install/updates.php
+++ branches/groupoffice-4.0/www/modules/calendar/install/updates.php
@@ -167,3 +167,5 @@ $updates['201210161843'][]="insert ignore into cal_groups (id,name, user_id) val
 
 $updates['201210300847'][]="ALTER TABLE `cal_events` CHANGE `location` `location` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''";
 $updates['201210300847'][]="ALTER TABLE `cal_events` CHANGE `name` `name` VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ";
+
+$updates['201212031644'][]="update cal_events set rrule='FREQ=YEARLY' WHERE rrule='RRULE:YM1 #0';";
