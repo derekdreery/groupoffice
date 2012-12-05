23b3d1e13c55bcc027017fd04a0f3c3580d26f2f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-12-05 16:02:44 +0000

    Created tag 4.0.140

---
+++ tags/groupoffice-com-4.0.140/modules/notes/language/pt_BR.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$l["category_id"]="Identif categoria";
+$l["content"]="Conteúdo";
+$l["manageCategories"]="Administrar categorias";
+
+$l['name']='Notas';
+$l['description']='Um módulo para criar notas';
+$l['category']='Categoria';
+$l['categories']='Categorias';
+$lang['link_type'][4]=$l['note']='Nota';
+$l['notes']='Notas';
+$l['general'] = 'Geral';
