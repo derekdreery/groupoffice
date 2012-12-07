88218eec9f7d57ef3a71dc90e4ea7fc8c232bd39
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-12-07 10:28:50 +0000

    Don't log all access denied messages

--- branches/groupoffice-4.0/www/go/base/controller/AbstractController.php
+++ branches/groupoffice-4.0/www/go/base/controller/AbstractController.php
@@ -396,13 +396,13 @@ abstract class GO_Base_Controller_AbstractController extends GO_Base_Observable 
 				//doesn't work well with extjs
 //				header("HTTP/1.1 403 Forbidden");
 				
-				$report = 
-								"Access denied\n".								
-								"controller: ".get_class($this)." action: ".$action."\n".
-								"params: ".var_export($params, true)."\n".
-								(string) $e;
-				if(!GO::config()->debug)
-					trigger_error($report, E_USER_WARNING);
+//				$report = 
+//								"Access denied\n".								
+//								"controller: ".get_class($this)." action: ".$action."\n".
+//								"params: ".var_export($params, true)."\n".
+//								(string) $e;
+//				if(!GO::config()->debug)
+//					trigger_error($report, E_USER_WARNING);
 				
 				$response['redirectToLogin']=empty(GO::session()->values['user_id']);
 			}
