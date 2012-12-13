7d55dc3932ca64000f7bbf576033df48e0e28fa8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-12-13 11:38:52 +0000

    Created tag 4.0.143

---
+++ tags/groupoffice-com-4.0.143/modules/admin2userlogin/language/de.js
@@ -0,0 +1,6 @@
+GO.admin2userlogin.lang.userId="ID";
+GO.admin2userlogin.lang.name="Name";
+GO.admin2userlogin.lang.username="Benutzer";
+GO.admin2userlogin.lang.lastlogin="Letzter Loginversuch";
+GO.admin2userlogin.lang.registrationtime="Registrierungszeit";
+GO.admin2userlogin.lang.admin2userlogin='Nutzer wechseln';
