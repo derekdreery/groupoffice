d03dead5a3af95e550e83a614be4d9cfefed6421
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-12-16 17:46:25 +0000

    setMaxExecutionTime function

--- branches/groupoffice-4.1/www/controller/MaintenanceController.php
+++ branches/groupoffice-4.1/www/controller/MaintenanceController.php
@@ -15,7 +15,7 @@ class GO_Core_Controller_Maintenance extends GO_Base_Controller_AbstractControll
 	
 	protected function init() {
 		GO::$disableModelCache=true; //for less memory usage
-		ini_set('max_execution_time', '0'); //allow long runs		
+		GO::setMaxExecutionTime(0); //allow long runs		
 		ini_set('memory_limit','512M');
 		ini_set('display_errors','on');
 	}
