0a6e70b1663f22c45c09b81d919d1689b69d0ada
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-12-18 09:32:48 +0000

    Created tag 4.0.144

---
+++ tags/groupoffice-com-4.0.144/modules/admin2userlogin/language/nl.inc.php
@@ -0,0 +1,6 @@
+<?php
+//Uncomment this line in new translations!
+require($GO_LANGUAGE->get_fallback_language_file('admin2userlogin'));
+
+$lang['admin2userlogin']['name']='Inloggen als gebruiker';
+$lang['admin2userlogin']['description']='Geeft de administrator de mogelijkheid om in te kunnen loggen als een normale gebruiker zonder een wachtwoord in te vullen.';
