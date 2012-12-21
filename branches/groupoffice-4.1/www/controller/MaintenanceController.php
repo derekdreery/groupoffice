2dd3aaa16be3223c02a798b07f8e7a642d1412fa
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-12-21 08:04:59 +0000

    Don't compare user_id for event duplicates

--- branches/groupoffice-4.1/www/controller/MaintenanceController.php
+++ branches/groupoffice-4.1/www/controller/MaintenanceController.php
@@ -170,7 +170,7 @@ class GO_Core_Controller_Maintenance extends GO_Base_Controller_AbstractControll
 		GO::session()->closeWriting(); //close writing otherwise concurrent requests are blocked.
 		
 		$checkModels = array(
-				"GO_Calendar_Model_Event"=>array('name', 'start_time', 'end_time', 'calendar_id', 'rrule', 'user_id'),
+				"GO_Calendar_Model_Event"=>array('name', 'start_time', 'end_time', 'calendar_id', 'rrule'),
 				"GO_Tasks_Model_Task"=>array('name', 'start_time', 'due_time', 'tasklist_id', 'rrule', 'user_id'),
 				"GO_Addressbook_Model_Contact"=>array('first_name', 'middle_name', 'last_name', 'addressbook_id', 'company_id', 'email'),
 				//"GO_Billing_Model_Order"=>array('order_id','book_id','btime')
