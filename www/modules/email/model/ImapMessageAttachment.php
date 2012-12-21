04cd53a7104113e1fbc6fe05dfc34d66c194ef85
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2012-12-21 12:30:41 +0000

    forward email with same-name attachments

--- www/modules/email/model/ImapMessageAttachment.php
+++ www/modules/email/model/ImapMessageAttachment.php
@@ -58,7 +58,8 @@ class GO_Email_Model_ImapMessageAttachment extends GO_Email_Model_MessageAttachm
 	public function createTempFile() {
 		
 		if(!$this->hasTempFile()){
-			$tmpFile = new GO_Base_Fs_File($this->getTempDir().GO_Base_Fs_File::stripInvalidChars($this->name));				
+			$file = new GO_Base_Fs_File($this->name);
+			$tmpFile = new GO_Base_Fs_File($this->getTempDir().uniqid(time()).'.'.$file->extension());
 			if(!$tmpFile->exists()){
 				$imap = $this->account->openImapConnection($this->mailbox);
 				$imap->save_to_file($this->uid, $tmpFile->path(),  $this->number, $this->encoding, true);
