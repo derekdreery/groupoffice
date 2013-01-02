44f42c06758f01db8382a332ac87e641c76d0f55
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-02 12:20:07 +0000

    Created tag 4.1.10

---
+++ tags/groupoffice-com-4.1.10/modules/links/language/sv.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$l['name']='Länkbeskrivningar';
+$l['description']='Hantera standardlänkbeskrivningar.';
+$l['link_description']='Länkbeskrivning';
+$l['link_descriptions']='Länkbeskrivningar';
+$l["links"]='Länkar';
+$l["linkDescription"]="Länkbeskrivning";
+$l["linkDescriptions"]="Länkbeskrivningar";
+$l["defaultLinkFolderText"]='Mappar som listas här kommer skapas automatiskt när ett object skapas. Ange sökvägar, en på varje rad. Ex: <br /><br />Mapp/Undermapp<br />Mapp 2/Undermapp 2<br /><br />';
+$l["linkFolders"]='Standard länkmappar';
+$l["link"]='Länk';
---
+++ tags/groupoffice-com-4.1.10/modules/sites/templates/Plain/language/en.php
@@ -0,0 +1,61 @@
+<?php
+
+$l['compareEmailError']='The email fields do not compare';
+$l['formSubmitNotification']='Thank you for submitting this form';
+
+$l['resetPassword']='Reset password';
+$l['resetPasswordText']='Fill in the form below to reset your password.';
+$l['resetPasswordSuccessTitle']='Reset password was successful.';
+$l['resetPasswordSuccess']='Your password is successfully reset.';
+$l['recoverPassword']='Recover password';
+$l['recoverPasswordInstructions']='Enter your e-mail address. If a valid user account with that e-mail address is found, your username and a new password will be sent to your e-mail address.';
+
+$l['male']='Male';
+$l['female']='Female';
+
+$l['firstName']='First name';
+$l['middleName']='Middle name';
+$l['lastName']='Last name';
+$l['gender']='Gender';
+$l['address']='Address';
+$l['housenumber']='Number of house';
+$l['zip']='ZIP/Postal code';
+$l['city']='City';
+$l['state']='State/Province';
+$l['country']='Country';
+$l['phone']='Phone';
+$l['mobile']='Mobile';
+
+$l['company']='Company';
+$l['department']='Department';
+$l['function']='Function';
+$l['vat']='EU VAT No.';
+
+$l['password']='Password';
+$l['confirm']='Confirm';
+$l['email']='Email';
+$l['username']='Username';
+
+$l['login']='Login';
+$l['logout']='Logout';
+$l['register']='Register';
+
+$l['firstTime']='Are you here for the first time?';
+$l['registeredLogin']='Login if you are already registered';
+$l['registerOnceClick']='Click here to register once';
+$l['registerTitle']='Register at Intermesh Group-Office';
+$l['registerText']='Fill out this form and click on \'Ok\' to register. The fields marked with a * are required.';
+$l['lostPassword']='Lost password';
+
+
+
+
+
+$l['pager_page']='Page';
+$l['pager_of']='of';
+$l['pager_totalItems']='Total items';
+
+$l['uploader_noFlash']='You browser doesn\'t have Adobe Flash installed.';
+
+$l['webshop_checkout']='Checkout';
+
