fa50fb43d8d111a61ce20577af5ec73382b3bd5b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-02 12:13:37 +0000

    Created tag 4.1.9

--- www/modules/admin2userlogin/language/nb.js
+++
@@ -1,6 +0,0 @@
-GO.admin2userlogin.lang.admin2userlogin='Bytt bruker';
-
-GO.admin2userlogin.lang.userId="ID";
-GO.admin2userlogin.lang.name="Navn";
-GO.admin2userlogin.lang.lastlogin="Siste innloggingsforsøk";
-GO.admin2userlogin.lang.registrationtime="Registrert den";
