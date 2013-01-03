fc3a641d53a15757d18e10db8feeb357f3d5666f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-03 16:58:55 +0000

    Created tag 4.1.12

---
+++ tags/groupoffice-com-4.1.12/modules/notes/language/cs.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$l['name']='Poznámky';
+$l['description']='Slouží k uchování rychlých zpráv.';
+$lang['link_type'][4]=$l['general'] = 'Obecné';
+$l['incomplete_delete']='Nemáte oprávnění pro smazání všech vybraných poznámek';
+$l['multipleCategoriesSelected']='Vybráno více kategorií';
+
+$l["category"]="Kategorie";
+$l["categories"]="Kategorie";
+$l["note"]="Poznámka";
+$l["notes"]="Poznámky";
+$l["category_id"]="Kategorie";
+$l["content"]="Obsah";
+$l["manageCategories"]="Správa kategorií";
