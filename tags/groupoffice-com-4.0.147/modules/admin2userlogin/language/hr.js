120b5ba0c6617be1432ab9f895fce6e55d261786
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-11 15:37:59 +0000

    Created tag 4.0.147

---
+++ tags/groupoffice-com-4.0.147/modules/admin2userlogin/language/hr.js
@@ -0,0 +1,10 @@
+/** 
+ * @copyright Copyright Boso d.o.o.
+ * @author Mihovil Stanić <mihovil.stanic@boso.hr>
+ */
+
+GO.admin2userlogin.lang.userId="ID";
+GO.admin2userlogin.lang.name="Ime";
+GO.admin2userlogin.lang.lastlogin="Zadnji pokušaj prijave";
+GO.admin2userlogin.lang.registrationtime="Vrijeme registracije";
+
