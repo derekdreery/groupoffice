82c0fb59b923240b98f3974769b1292f29703481
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-14 15:36:16 +0000

    refresh grid after copy event action

--- branches/groupoffice-4.1/www/modules/calendar/MonthGrid.js
+++ branches/groupoffice-4.1/www/modules/calendar/MonthGrid.js
@@ -565,23 +565,23 @@ GO.grid.MonthGrid = Ext.extend(Ext.Panel, {
 				{
 					if(calendar_visible)
 					{
-						if(event.repeats)
-						{
+//						if(event.repeats)
+//						{
 							this.store.reload();
-						}else
-						{
-							var newEvent = GO.util.clone(event);
+//						}else
+//						{
+//							var newEvent = GO.util.clone(event);
 
-							delete(newEvent.id);
-
-							newEvent.event_id = new_event_id;
-							newEvent.startDate = Date.parseDate(newEvent.start_time, this.dateTimeFormat).add(Date.DAY, obj.offset);
-							newEvent.endDate = Date.parseDate(newEvent.end_time, this.dateTimeFormat).add(Date.DAY, obj.offset);
-							newEvent.start_time=newEvent.startDate.format(this.dateTimeFormat);
-							newEvent.end_time=newEvent.endDate.format(this.dateTimeFormat);
-
-							this.addMonthGridEvent(newEvent);
-						}
+//							delete(newEvent.id);
+//
+//							newEvent.event_id = new_event_id;
+//							newEvent.startDate = Date.parseDate(newEvent.start_time, this.dateTimeFormat).add(Date.DAY, obj.offset);
+//							newEvent.endDate = Date.parseDate(newEvent.end_time, this.dateTimeFormat).add(Date.DAY, obj.offset);
+//							newEvent.start_time=newEvent.startDate.format(this.dateTimeFormat);
+//							newEvent.end_time=newEvent.endDate.format(this.dateTimeFormat);
+//
+//							this.addMonthGridEvent(newEvent);
+//						}
 					}
 				}else
 				{
