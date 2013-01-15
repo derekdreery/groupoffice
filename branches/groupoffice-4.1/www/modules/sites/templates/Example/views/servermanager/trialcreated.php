d842cd7b89d8a52b3ac26480733cb152914e3265
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-15 12:45:00 +0000

    Trial creation works

--- branches/groupoffice-4.1/www/modules/sites/templates/Example/views/servermanager/trialcreated.php
+++ branches/groupoffice-4.1/www/modules/sites/templates/Example/views/servermanager/trialcreated.php
@@ -1,3 +1,9 @@
-<h1>Thank you</h1>
+<div class="subkader-small-top">
+		<div class="subkader-small-bottom">
+			<div class="subkader-small-center">						
+				<h1>Thank you</h1>
 
-<p>An e-mail with instructions to complete your trial installation has been sent to <?php echo $this->newTrial->email; ?>.</p>
+				<p>An e-mail with instructions to complete your trial installation has been sent to <?php echo $model->email; ?>.</p>
+			</div>
+		</div>
+</div>
