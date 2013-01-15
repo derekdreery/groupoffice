c2ef44d647a22c0b4eebcfdb9daafece171445cc
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-15 14:06:49 +0000

    remove

--- tags/groupoffice-com-4.1.17/modules/ldapauth/language/fr.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-
-$l['name']='Authentification LDAP';
-$l['description']='Ce module permet d\'utiliser un serveur LDAP pour authentifier et automatiquement configurer les nouveaux utilisateurs dans {product_name}.';
