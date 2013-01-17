a0ab702a0685a28283ccf8684d3e123269733228
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-17 07:57:14 +0000

    Remove z-push from go_modules if present

--- branches/groupoffice-4.1/www/install/updates.php
+++ branches/groupoffice-4.1/www/install/updates.php
@@ -269,3 +269,4 @@ $updates["201210021548"][]="ALTER TABLE `go_holidays` CHANGE `region` `region` V
 
 $updates["201201150948"][]="ALTER TABLE `go_holidays` CHANGE `id` `id` INT( 11 ) NOT NULL AUTO_INCREMENT ";
 $updates["201201150948"][]="ALTER TABLE `go_holidays` CHANGE `region` `region` VARCHAR( 10 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''";
+$updates["201301170847"][]="DELETE FROM go_modules WHERE id='z-push';";
