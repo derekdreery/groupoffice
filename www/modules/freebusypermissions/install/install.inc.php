91fed8c5639410883f5858045cd34fb5bc8939aa
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-18 16:41:15 +0000

    Created tag 4.1.19

--- www/modules/freebusypermissions/install/install.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-global $GO_SECURITY, $GO_CONFIG, $GO_MODULES;
-$GLOBALS['GO_SECURITY']->add_group_to_acl($GLOBALS['GO_CONFIG']->group_everyone, $GLOBALS['GO_MODULES']->modules['freebusypermissions']['acl_id']);
