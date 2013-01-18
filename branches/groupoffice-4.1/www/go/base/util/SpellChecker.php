4363ba146548f2ca00b6768d50b4cc149b04facb
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-18 09:57:54 +0000

    Error check on pspell

--- branches/groupoffice-4.1/www/go/base/util/SpellChecker.php
+++ branches/groupoffice-4.1/www/go/base/util/SpellChecker.php
@@ -13,6 +13,9 @@ class GO_Base_Util_SpellChecker {
     public static function check($text, $language) {
 
         self::$_pLink = pspell_new($language, "", "", "utf-8", PSPELL_FAST);
+				
+				if(!self::$_pLink)
+					throw new Exception("Could not initialize pspell for language ".$language);
 
         $words = self::_getWords($text);
         $checkspelling = self::_checkWords($words);
