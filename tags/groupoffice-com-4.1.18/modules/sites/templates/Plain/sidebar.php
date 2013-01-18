a674ed559693efef1e87d9e5f7b9015d78a76a75
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-18 12:13:29 +0000

    Created tag 4.1.18

---
+++ tags/groupoffice-com-4.1.18/modules/sites/templates/Plain/sidebar.php
@@ -0,0 +1,13 @@
+<h1>Shopping cart</h1>
+
+<?php 
+$webshop = GO_Webshop_Model_Webshop::getByController($this);
+echo $webshop->getWebshopOrder()->getCart()->getSmallTable(); ?>
+
+<div class="button-green-side" onmouseover="this.className='button-green-side-hover';"  onmouseout="this.className='button-green-side';">
+	<div class="button-green-side-right">
+		<a href="<?php echo $this->pageUrl($webshop->getCartPath()); ?>" class="button-green-side-center"> 
+			<?php echo GOS::t('webshop_checkout'); ?>
+		</a>
+	</div>
+</div>
