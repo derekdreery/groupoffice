4bf81d4037bb6fd247e09494c482fdcd8402be64
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-21 11:13:46 +0000

    Created tag 4.1.20

---
+++ language/holidays/de.php
@@ -0,0 +1,10 @@
+<?php
+// holidays with fixed date
+$input_holidays['fix']['01-01'] = 'Neujahr';
+$input_holidays['fix']['12-25'] = '1. Weihnachtsfeiertag';
+$input_holidays['fix']['12-26'] = '2. Weihnachtsfeiertag';
+
+// holidays with variable date (christian holidays computation is based on the date of easter day)
+$input_holidays['var']['-2'] = 'Karfreitag';
+$input_holidays['var']['0'] = 'Ostersonntag';
+$input_holidays['var']['1'] = 'Ostermontag';
