045f4ba1077fed85ab081d5614bc0a7b2c7848d3
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-22 10:40:04 +0000

    Created tag 4.1.24

--- www/modules/admin2userlogin/language/de.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Uncomment this line in new translations!
-require($GO_LANGUAGE->get_fallback_language_file('admin2userlogin'));
-
-$lang['admin2userlogin']['name']='Nutzer wechseln';
-$lang['admin2userlogin']['description']='Der Administrator kann sich ohne Kenntnis des Nutzerkennwortes als dieser Nutzer einloggen.';
