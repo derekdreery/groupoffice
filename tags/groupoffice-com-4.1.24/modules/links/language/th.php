e655ce466ed40f4e97cc5876113e5c1fc04f6ecb
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-22 10:40:04 +0000

    Created tag 4.1.24

---
+++ tags/groupoffice-com-4.1.24/modules/links/language/th.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$l['name']='เชื่อมโยงคำอธิบาย';//Link descriptions;
+$l['description']='การจัดการค่าเริ่มต้นการเชื่อมโยงคำอธิบาย';//Manage default link descriptions;
+$l['link_description']='เชื่อมโยงคำอธิบาย';//Link description;
+$l['link_descriptions']='เชื่อมโยงคำอธิบาย';//Link descriptions;
+$l["links"]='เชื่อมโยง';//Links;
+$l["linkDescription"]="เชื่อมโยงคำอธิบาย";//Link description;
+$l["linkDescriptions"]="เชื่อมโยงคำอธิบาย";//Link descriptions;
+$l["defaultLinkFolderText"]='รายการโฟลเดอร์นี้จะถูกสร้างขึ้นโดยอัตโนมัติเมื่อสร้างรายการ ตัวอย่างการระบุตำแหน่งของโฟลเดอร์ : <br /><br />โฟลเดอร์/โฟลเดอร์ย่อย<br />โฟลเดอร์ 2/โฟลเดอร์ย่อย 2<br /><br />';
+$l["linkFolders"]='ค่าเริ่มต้นการเชื่อมโยงโฟลเดอร์';
