38b12bcb77da0bffcb06069dcaf22090a2f266af
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-22 09:53:32 +0000

    temp file problem

--- branches/groupoffice-4.1/www/go/GO.php
+++ branches/groupoffice-4.1/www/go/GO.php
@@ -565,12 +565,6 @@ class GO{
 	 */
 	public static function shutdown(){
 		
-		//clear temp files etc.
-		if(PHP_SAPI=="CLI" && GO::user()){
-			GO::session()->logout();
-		}
-		
-		
 		$error = error_get_last();		
 		if($error){			
 			//z-push uses a lot of ugly @fputs etc to suppresss errors. We don't want to log those.
