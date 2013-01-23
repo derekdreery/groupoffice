ab905c98ba0fc8eb9d61e330c94aa5b3eb820508
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-23 16:13:08 +0000

    Created tag 4.1.25

---
+++ tags/groupoffice-com-4.1.25/modules/links/language/th.inc.php
@@ -0,0 +1,7 @@
+<?php
+//Uncomment this line in new translations!
+require($GLOBALS['GO_LANGUAGE']->get_fallback_language_file('links'));
+$lang['links']['name']='เชื่อมโยงคำอธิบาย';//Link descriptions
+$lang['links']['description']='การจัดการค่าเริ่มต้นการเชื่อมโยงคำอธิบาย';//Manage default link descriptions
+$lang['links']['link_description']='เชื่อมโยงคำอธิบาย';//Link description
+$lang['links']['link_descriptions']='เชื่อมโยงคำอธิบาย';//Link descriptions
