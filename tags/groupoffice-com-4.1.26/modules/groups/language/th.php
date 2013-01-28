6ee5c327b5ab481450c28d3c91d7d2f8293acd91
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-28 09:14:06 +0000

    Created tag 4.1.26

---
+++ tags/groupoffice-com-4.1.26/modules/groups/language/th.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$l['name'] = 'กลุ่มผู้ใช้งาน';
+$l['description'] = 'สำหรับการจัดการกลุ่มผู้ใช้งาน.';
+$l['noDeleteAdmins'] = 'ไม่ได้รับสิทธิ์ิให้ทำการลบกลุ่มผู้ดูแลระบบ';//You can\'t delete the group Admins;
+$l['noDeleteEveryone'] = 'ไม่ได้รับสิทธิ์ิให้ทำการลบกลุ่มบุคคล';//You can\'t delete the group Everyone;
+$l['groupNameAlreadyExists'] = 'มีการสร้างกลุ่มนี้แล้ว.เกิดความซ้ำซ้อน';//The group you are trying to create already exists;
+$l['dontRemoveAdmin']='ไม่ได้รับสิทธิ์ให้ลบผู้ดูแลระบบหลักจากกลุ่มผู้ดูแลระบบ';//You can\'t remove the primary administrator from the administrator group;
+
+$l["group"]= 'กลุ่มผู้ใช้งาน';
+$l["groups"]= 'กลุ่มผู้ใช้งาน';
+$l["owner"]= 'เจ้าของ';
+$l["lblNew"]= 'สร้างกลุ่มผู้ใช้งาน';
+$l["newGroupName"]= 'ชื่อกลุ่มผู้ใช้งาน';
+$l["searchAllUsers"]= 'ค้นหาทจากกลุ่มผู้ใช้งาน ทั้งหมด';
