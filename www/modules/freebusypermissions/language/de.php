d02db050b8c537b949e8ae0a2aed0e0afd0bbf63
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-01-29 14:56:25 +0000

    Created tag 4.1.27

--- www/modules/freebusypermissions/language/de.php
+++
@@ -1,4 +0,0 @@
-<?php
-$l['name']='Berechtigungen für die Verfügbarkeit';
-$l['description']='Alle Nutzer müssen andere Nutzer autorisieren, deren terminliche Verfügbarkeit anzuzeigen, wenn dieses Modul aktiviert ist. Ein Button wird unter Einstellungen -> Kalender verfügbar.';
-$l['FreeBusyaccessrights']= 'Berechtigungen für die Verfügbarkeit';
