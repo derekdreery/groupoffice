2ae2a11f03bdaf38dd0a76ed7be44cdf294cf9a3
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-02-08 10:22:03 +0000

    new trunk based on 4.1

---
+++ trunk/communitymodules/cms/CmsModule.php
@@ -0,0 +1,16 @@
+<?php
+class GO_Cms_CmsModule extends GO_Base_Module{
+	
+	public function autoInstall() {
+		return true;
+	}
+	
+	public function author() {
+		return 'Merijn Schering';
+	}
+	
+	public function authorEmail() {
+		return 'mschering@intermesh.nl';
+	}
+}
+?>
