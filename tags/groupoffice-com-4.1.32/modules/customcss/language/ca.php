ebff0bc150926c9f4842eddb2cd1820bd9b509b1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-02-12 20:43:37 +0000

    Removed invalid tag 4.1.32

--- tags/groupoffice-com-4.1.32/modules/customcss/language/ca.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-$l['name']='Estils CSS i scripts personalitzats';
-$l['description']='Substituïr l\'estil per defecte d\'un tema de {product_name} per afegir el vostre propi logo per exemple.';
-
-$l["customcss"]='CSS i Javascript personalitzat';
-$l["selectFile"]='Seleccionar arxiu';
--- tags/groupoffice-com-4.1.32/modules/links/language/de.js
+++
@@ -1,6 +0,0 @@
-GO.links.lang.links='Beschreibungen zu Verknüpfungen';
-GO.links.lang.linkDescription='Beschreibung';
-GO.links.lang.linkDescriptions='Beschreibungen';
-GO.links.lang.defaultLinkFolderText='Hier eingetragene Ordner werden automatisch angelegt, wenn ein neuer Eintrag verknüpft wird. Geben Sie jeden Ordnerpfad in einer eigenen Zeile wie folgt an: <br /><br />Ordner/Unterordner<br />Ordner 2/Unterordner 2<br /><br />'
-GO.links.lang.linkFolders='Standard Ordnerverknüpfungen';
-GO.links.lang.link='Verknüpfung';
