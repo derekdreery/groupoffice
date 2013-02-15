2f67fa321ad2259b160d7b8547c78455384d53fb
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-02-15 07:59:51 +0000

    Created tag 4.1.34

--- www/modules/files/filehandler/Interface.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface GO_Files_Filehandler_Interface{
-	public function supportedExtensions();
-	public function getName();
-	
-	public function fileIsSupported(GO_Files_Model_File $file);
-	
-	public function getHandler(GO_Files_Model_File $file);
-}
