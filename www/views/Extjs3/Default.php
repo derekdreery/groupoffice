b18a60b5557ce8daf28e4fe563820cff3eeed13a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-02-15 07:57:11 +0000

    Send file handler straight away to avoid browser security measures

--- www/views/Extjs3/Default.php
+++ www/views/Extjs3/Default.php
@@ -5,7 +5,7 @@ if(strpos($string,'startjs:')!==false){
 	preg_match_all('/"startjs:(.*?):endjs"/usi', $string, $matches, PREG_SET_ORDER);
 
 	for($i=0;$i<count($matches);$i++){
-		$string = str_replace($matches[$i][0], stripslashes($matches[$i][1]), $string);
+		$string = str_replace($matches[$i][0], stripslashes(str_replace(array('\t','\n'),'',$matches[$i][1])), $string);
 	}
 }
 
