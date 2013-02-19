1cfeeaac2f4883edd2ea6241595df1ca50f863cd
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-02-19 08:46:45 +0000

    Sabredav upgrade branch

---
+++ branches/sabredav/debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/language/en.inc.php
@@ -0,0 +1,13 @@
+<?php
+//Uncomment this line in new translations!
+//require($GLOBALS['GO_LANGUAGE']->get_fallback_language_file('servermanager'));
+$lang['servermanager']['name']='Servermanager';
+$lang['servermanager']['description']='Manage multiple Group-Office installations on a Debian or Ubuntu server.';
+$lang['servermanager']['installation']='Installation';
+$lang['servermanager']['installations']='Installations';
+$lang['servermanager']['invalidEmail']='Please enter a valid e-mail address';
+$lang['servermanager']['noHost']='Please enter a hostname';
+$lang['servermanager']['invalidHost']='The hostname may only contain the charachters a to z';
+$lang['servermanager']['duplicateHost']='Sorry, the hostname already exists';
+
+$lang['servermanager']['new_trial_subject']='Complete your Group-Office trial installation';
