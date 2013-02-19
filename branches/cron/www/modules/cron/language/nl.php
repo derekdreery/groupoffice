465f3719217d6388e73d2c1b7401e2031a5f59cd
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-02-19 14:42:27 +0000

    Added cron module to Group-Office BETA VERSION This is the view for the base cron functions of Group-Office

---
+++ branches/cron/www/modules/cron/language/nl.php
@@ -0,0 +1,24 @@
+<?php
+$l['name'] = 'Cron beheren';
+$l['description'] = 'Module om cronjobs in Group-Office te beheren.';
+
+$l['cronManager'] = 'Cron beheren';
+
+$l['name'] = 'Naam';
+$l['minutes'] = 'Minuten';
+$l['hours'] = 'Uren';
+$l['monthdays'] = 'Dagen in maand';
+$l['months'] = 'Maanden';
+$l['weekdays'] = 'Dagen in week';
+$l['years'] = 'jaren';
+$l['active'] = 'Actief';
+$l['runonce'] = 'Eenmalig uitvoeren';
+$l['job'] = 'Taak';
+
+$l['minutesExample'] = '(0-59)';
+$l['hoursExample'] = '(0-23)';
+$l['monthdaysExample'] = '(1-31)';
+$l['monthsExample'] = '(1-12)';
+$l['weekdaysExample'] = '(0-6)';
+
+$l['timeFieldSetTitle'] = 'Tijd';
---
+++ branches/cron/www/modules/cron/views/Extjs3/CronDialog.js
@@ -0,0 +1,158 @@
+GO.cron.CronDialog = Ext.extend(GO.dialog.TabbedFormDialog , {
+
+	initComponent : function(){
+		
+		Ext.apply(this, {
+			goDialogId:'cronjob',
+			title:GO.cron.lang.job,
+			formControllerUrl: 'core/cron',
+			updateAction : 'update',
+			createAction : 'create',
+			height:360,
+			width:350,
+			tools: [{
+				id:'help',
+				qtip: GO.cron.lang.exampleFormats+
+					'<table>'+
+					'<tr><td>*</td><td>'+GO.cron.lang.exampleFormat1Explanation+'</td></tr>'+
+					'<tr><td>1</td><td>'+GO.cron.lang.exampleFormat2Explanation+'</td></tr>'+
+					'<tr><td>1-5</td><td>'+GO.cron.lang.exampleFormat3Explanation+'</td></tr>'+
+					'<tr><td>0-23/2</td><td>'+GO.cron.lang.exampleFormat4Explanation+'</td></tr>'+
+					'<tr><td>1,2,3,13,22</td><td>'+GO.cron.lang.exampleFormat5Explanation+'</td></tr>'+
+					'<tr><td>0-4,8-12</td><td>'+GO.cron.lang.exampleFormat6Explanation+'</td></tr>'+
+					'<table>'
+			}]
+		});
+		
+		GO.cron.CronDialog.superclass.initComponent.call(this);	
+	},
+	 	
+	buildForm : function () {
+				
+		this.nameField = new Ext.form.TextField({
+			name: 'name',
+			width:300,
+			anchor: '100%',
+			maxLength: 100,
+			allowBlank:false,
+			fieldLabel: GO.cron.lang.name
+		});
+		
+		this.jobCombo = new GO.form.ComboBox({
+			hiddenName: 'job',
+			fieldLabel: GO.cron.lang.job,
+			store: GO.cron.jobStore,
+			valueField:'class',
+			displayField:'name',
+			mode:'remote',
+			anchor: '100%%',
+			allowBlank: false,
+			triggerAction: 'all'
+		});
+		
+		this.minutesField = new Ext.form.TextField({
+			name: 'minutes',
+			width:300,
+			anchor: '99%',
+			maxLength: 100,
+			allowBlank:false,
+			fieldLabel: GO.cron.lang.minutes +' '+ GO.cron.lang.minutesExample
+		});
+		
+		this.hoursField = new Ext.form.TextField({
+			name: 'hours',
+			width:300,
+			anchor: '99%',
+			maxLength: 100,
+			allowBlank:false,
+			fieldLabel: GO.cron.lang.hours +' '+ GO.cron.lang.hoursExample
+		});
+		
+		this.monthDaysField = new Ext.form.TextField({
+			name: 'monthdays',
+			width:300,
+			anchor: '99%',
+			maxLength: 100,
+			allowBlank:false,
+			fieldLabel: GO.cron.lang.monthdays +' '+ GO.cron.lang.monthdaysExample
+		});
+		
+		this.monthsField = new Ext.form.TextField({
+			name: 'months',
+			width:300,
+			anchor: '99%',
+			maxLength: 100,
+			allowBlank:false,
+			fieldLabel: GO.cron.lang.months +' '+ GO.cron.lang.monthsExample
+		});
+		
+		this.weekdaysField = new Ext.form.TextField({
+			name: 'weekdays',
+			width:300,
+			anchor: '99%',
+			maxLength: 100,
+			allowBlank:false,
+			fieldLabel: GO.cron.lang.weekdays +' '+ GO.cron.lang.weekdaysExample
+		});
+		
+//		this.yearsField = new Ext.form.TextField({
+//			name: 'years',
+//			width:300,
+//			anchor: '99%',
+//			maxLength: 100,
+//			allowBlank:false,
+//			fieldLabel: GO.cron.lang.years +' '+ GO.cron.lang.yearsExample
+//		});
+//		
+		this.activeCheckbox = new Ext.ux.form.XCheckbox({
+			name: 'active',
+			width:300,
+			anchor: '100%',
+			maxLength: 100,
+			allowBlank:false,
+			fieldLabel: GO.cron.lang.active
+		});
+		
+		this.runOnceCheckbox = new Ext.ux.form.XCheckbox({
+			name: 'runonce',
+			width:300,
+			anchor: '100%',
+			maxLength: 100,
+			allowBlank:false,
+			fieldLabel: GO.cron.lang.runonce
+		});
+		
+		this.timeFieldSet = new Ext.form.FieldSet({
+			title: GO.cron.lang.timeFieldSetTitle,
+			labelWidth: 140,
+			autoHeight: true,
+			border: true,
+			collapsed: false,
+			items:[
+				this.minutesField,
+				this.hoursField,
+				this.monthDaysField,
+				this.monthsField,
+				this.weekdaysField
+//				,
+//				this.yearsField
+			]
+		});
+		
+		this.propertiesPanel = new Ext.Panel({
+			title:GO.lang['strProperties'],			
+			cls:'go-form-panel',
+			layout:'form',
+			labelWidth: 90,
+			items:[
+				this.nameField,
+				this.jobCombo,
+				this.timeFieldSet,
+				this.activeCheckbox,
+				this.runOnceCheckbox
+      ]				
+		});
+
+    this.addPanel(this.propertiesPanel);
+	}
+});
