9365fed4f370566fcd570395cbb982e5646e64f5
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-02-28 15:53:14 +0000

    Created tag 4.1.38

---
+++ tags/groupoffice-com-4.1.38/modules/links/language/nb.inc.php
@@ -0,0 +1,7 @@
+<?php
+//Uncomment this line in new translations!
+require($GLOBALS['GO_LANGUAGE']->get_fallback_language_file('links'));
+$lang['links']['name']='Lenkebeskrivelser';
+$lang['links']['description']='Behandle standard lenkebeskrivelser';
+$lang['links']['link_description']='Lenkebeskrivelse';
+$lang['links']['link_descriptions']='Lenkebeskrivelser';
