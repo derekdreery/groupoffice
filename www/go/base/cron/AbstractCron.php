2c5af18865a78f29bb03ba8e636c937c3a68853c
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-01 12:50:37 +0000

    Created cron user and group selection

--- www/go/base/cron/AbstractCron.php
+++ www/go/base/cron/AbstractCron.php
@@ -23,6 +23,16 @@
 abstract class GO_Base_Cron_AbstractCron extends GO_Base_Model{
 	
 	/**
+	 * Return true or false to enable the selection fo users and groups for 
+	 * this cronjob.
+	 * 
+	 * CAUTION: This will give the run() function a different behaviour. 
+	 *					Please see the documentation of the run() function 
+	 *					to see what is different.
+	 */
+	public abstract function enableUserAndGroupSupport();
+	
+	/**
 	 * Get the label of this Cronjob
 	 * 
 	 * @return String
@@ -38,7 +48,16 @@ abstract class GO_Base_Cron_AbstractCron extends GO_Base_Model{
 	
 	/**
 	 * The code that needs to be called when the cron is running
+	 * 
+	 * If $this->enableUserAndGroupSupport() returns TRUE then the run function 
+	 * will be called for each $user. (The $user parameter will be given)
+	 * 
+	 * If $this->enableUserAndGroupSupport() returns FALSE then the 
+	 * $user parameter is null and the run function will be called only once.
+	 * 
+	 * @param GO_Base_Cron_CronJob $cronJob
+	 * @param GO_Base_Model_User $user [OPTIONAL]
 	 */
-	public abstract function run();
+	public abstract function run(GO_Base_Cron_CronJob $cronJob,GO_Base_Model_User $user = null);
 		
 }
