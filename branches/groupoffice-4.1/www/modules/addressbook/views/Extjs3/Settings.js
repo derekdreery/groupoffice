8a7ed86dc2ff9cafeb5cc193bd182cbc0649ff0f
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-08 12:35:18 +0000

    Fixed addresslist selection in the global settings panel

--- branches/groupoffice-4.1/www/modules/addressbook/views/Extjs3/Settings.js
+++ branches/groupoffice-4.1/www/modules/addressbook/views/Extjs3/Settings.js
@@ -1,5 +1,6 @@
-GO.mainLayout.onReady(function(){		
-	GO.moduleManager.addSettingsPanel('addresslists', GO.addressbook.SelectAddresslistsPanel,{},4);
+GO.mainLayout.onReady(function(){
+	
+	GO.moduleManager.addSettingsPanel('addresslists', GO.addressbook.AddresslistsSettingsPanel,{},4);
 	
 	if(GO.customfields && GO.customfields.types["GO_Addressbook_Model_Contact"])
 	{
