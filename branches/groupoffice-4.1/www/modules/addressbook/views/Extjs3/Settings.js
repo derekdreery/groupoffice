b6a432c13675c852d3ac6bf32fe2224204c99b4b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-08 16:31:04 +0000

    Deactivate settings modification

--- branches/groupoffice-4.1/www/modules/addressbook/views/Extjs3/Settings.js
+++ branches/groupoffice-4.1/www/modules/addressbook/views/Extjs3/Settings.js
@@ -1,12 +1,12 @@
-GO.mainLayout.onReady(function(){
-	
-	GO.moduleManager.addSettingsPanel('addresslists', GO.addressbook.AddresslistsSettingsPanel,{},4);
-	
-	if(GO.customfields && GO.customfields.types["GO_Addressbook_Model_Contact"])
-	{
-		for(var i=0;i<GO.customfields.types["GO_Addressbook_Model_Contact"].panels.length;i++)
-		{
-			GO.moduleManager.addSettingsPanel('contact_cf_panel_'+i,GO.customfields.CustomFormPanel, GO.customfields.types["GO_Addressbook_Model_Contact"].panels[i],i+5);
-		}
-	}	
-});
+//GO.mainLayout.onReady(function(){
+//	
+//	GO.moduleManager.addSettingsPanel('addresslists', GO.addressbook.AddresslistsSettingsPanel,{},4);
+//	
+//	if(GO.customfields && GO.customfields.types["GO_Addressbook_Model_Contact"])
+//	{
+//		for(var i=0;i<GO.customfields.types["GO_Addressbook_Model_Contact"].panels.length;i++)
+//		{
+//			GO.moduleManager.addSettingsPanel('contact_cf_panel_'+i,GO.customfields.CustomFormPanel, GO.customfields.types["GO_Addressbook_Model_Contact"].panels[i],i+5);
+//		}
+//	}	
+//});
