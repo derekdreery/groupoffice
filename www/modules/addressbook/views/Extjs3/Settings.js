8112e9592a4d8257f408ebe4ef09d5fd4427948f
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-08 10:18:20 +0000

    Added loading of contact custom fields to the settings dialog.

--- www/modules/addressbook/views/Extjs3/Settings.js
+++ www/modules/addressbook/views/Extjs3/Settings.js
@@ -1,3 +1,11 @@
 GO.mainLayout.onReady(function(){		
 	GO.moduleManager.addSettingsPanel('addresslists', GO.addressbook.SelectAddresslistsPanel);
+	
+	if(GO.customfields && GO.customfields.types["GO_Addressbook_Model_Contact"])
+	{
+		for(var i=0;i<GO.customfields.types["GO_Addressbook_Model_Contact"].panels.length;i++)
+		{
+			GO.moduleManager.addSettingsPanel('contact_cf_panel_'+i,GO.customfields.CustomFormPanel, GO.customfields.types["GO_Addressbook_Model_Contact"].panels[i]);
+		}
+	}	
 });
