ab7f17033276598555177b8efe7d9683ec79fd88
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-08 10:50:55 +0000

    Changed order of the customfield panels and the addresslist panel

--- branches/groupoffice-4.1/www/modules/addressbook/views/Extjs3/Settings.js
+++ branches/groupoffice-4.1/www/modules/addressbook/views/Extjs3/Settings.js
@@ -1,11 +1,11 @@
 GO.mainLayout.onReady(function(){		
-	GO.moduleManager.addSettingsPanel('addresslists', GO.addressbook.SelectAddresslistsPanel);
+	GO.moduleManager.addSettingsPanel('addresslists', GO.addressbook.SelectAddresslistsPanel,{},4);
 	
 	if(GO.customfields && GO.customfields.types["GO_Addressbook_Model_Contact"])
 	{
 		for(var i=0;i<GO.customfields.types["GO_Addressbook_Model_Contact"].panels.length;i++)
 		{
-			GO.moduleManager.addSettingsPanel('contact_cf_panel_'+i,GO.customfields.CustomFormPanel, GO.customfields.types["GO_Addressbook_Model_Contact"].panels[i]);
+			GO.moduleManager.addSettingsPanel('contact_cf_panel_'+i,GO.customfields.CustomFormPanel, GO.customfields.types["GO_Addressbook_Model_Contact"].panels[i],i+5);
 		}
 	}	
 });
