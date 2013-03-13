8a2685bfc75159e98e46c8db3761607ac6b4660e
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-13 08:30:25 +0000

    Added tab selection for contact custom fields for showing in the global settings dialog

--- www/modules/addressbook/views/Extjs3/Settings.js
+++ www/modules/addressbook/views/Extjs3/Settings.js
@@ -1,12 +1,18 @@
-//GO.mainLayout.onReady(function(){
-//	
-//	GO.moduleManager.addSettingsPanel('addresslists', GO.addressbook.AddresslistsSettingsPanel,{},4);
-//	
-//	if(GO.customfields && GO.customfields.types["GO_Addressbook_Model_Contact"])
-//	{
-//		for(var i=0;i<GO.customfields.types["GO_Addressbook_Model_Contact"].panels.length;i++)
-//		{
-//			GO.moduleManager.addSettingsPanel('contact_cf_panel_'+i,GO.customfields.CustomFormPanel, GO.customfields.types["GO_Addressbook_Model_Contact"].panels[i],i+5);
-//		}
-//	}	
-//});
+GO.mainLayout.onReady(function(){
+	
+	if(GO.addressbook && GO.settings.show_addresslist_tab == "1")
+		GO.moduleManager.addSettingsPanel('addresslists', GO.addressbook.AddresslistsSettingsPanel,{},4);
+	
+	
+	if(GO.customfields && GO.customfields.types["GO_Addressbook_Model_Contact"])
+	{
+		for(var i=0;i<GO.customfields.types["GO_Addressbook_Model_Contact"].panels.length;i++)
+		{
+			var id = '';
+			id = GO.customfields.types["GO_Addressbook_Model_Contact"].panels[i].category_id;
+
+			if(GO.settings.show_contact_cf_tabs[id] && GO.settings.show_contact_cf_tabs[id] == true)
+				GO.moduleManager.addSettingsPanel('contact_cf_panel_'+i,GO.customfields.CustomFormPanel, GO.customfields.types["GO_Addressbook_Model_Contact"].panels[i],i+5);
+		}
+	}	
+});
