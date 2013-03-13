d3fd06aa4b50cbf8dd548482ee953857b53f7014
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-13 08:29:20 +0000

    Added tab selection for contact custom fields for showing in the global settings dialog

--- branches/groupoffice-4.1/www/modules/users/controller/SettingsController.php
+++ branches/groupoffice-4.1/www/modules/users/controller/SettingsController.php
@@ -2,7 +2,7 @@
 class GO_Users_Controller_Settings extends GO_Base_Controller_AbstractController{
 	
 	protected function actionLoad($params) {
-		
+		$response = array();
 		$settings =  GO_Users_Model_Settings::load();
 		
 		if(empty($settings->register_email_subject))
@@ -11,24 +11,70 @@ class GO_Users_Controller_Settings extends GO_Base_Controller_AbstractController
 		if(empty($settings->register_email_body))
 			$settings->register_email_body = GO::t('register_email_body','users');
 		
-		// GET ALL CUSTOMFIELDS TABS AND CREATE AN OPTION TO SELECT/DESELECT THEM
+		// Load the custom field categories of the contact model
+		if(GO::modules()->customfields){
+			$tabs = GO_Users_Model_CfSettingTab::model()->find();
+			foreach($tabs as $t)
+				$response['tab_cf_cat_'.$t->cf_category_id]=true;
+		}
 		
-		// GET OPTION TO SHOW OR HIDE THE ADDRESSLIST PANEL
-			
+		$responseData = array_merge($response,$settings->getArray());
+		
 		return array(
 				'success'=>true,
-				'data'=>$settings->getArray()
+				'data'=>$responseData
 		);
 	}
 	
 	protected function actionSubmit($params) {
 		
-		$settings =  GO_Users_Model_Settings::load();
+		if(GO::modules()->addressbook && GO::modules()->customfields){
+			
+			// Remove all existing records from the table
+			$tabs = GO_Users_Model_CfSettingTab::model()->find();
+			foreach($tabs as $t)
+				$t->delete();
+			
+			$contactClassName = GO_Addressbook_Model_Contact::model()->className();
+			$customfieldsCategories = GO_Customfields_Model_Category::model()->findByModel($contactClassName);
 
+			// Add the posted records back to the table (go_cf_setting_tabs)
+			foreach($customfieldsCategories as $cfc){
+				if(isset($params['tab_cf_cat_'.$cfc->id])){
+					$cft = new GO_Users_Model_CfSettingTab();
+					$cft->cf_category_id = $cfc->id;
+					$cft->save();
+				}
+			}
+		}
+	
+		$settings =  GO_Users_Model_Settings::load();
+		
 		return array(
 				'success'=>$settings->saveFromArray($params),
 				'data'=>$settings->getArray()
 		);
 	}
-	
+
+	/**
+	 * Get all customfield categories of the contact model
+	 * 
+	 * @param type $params
+	 */
+	public function actionLoadContactCustomfieldCategories($params){
+		$response = array();
+		$customfieldsCategoriesArray = array();
+		
+		$contactClassName = GO_Addressbook_Model_Contact::model()->className();
+		$customfieldsCategories = GO_Customfields_Model_Category::model()->findByModel($contactClassName);
+
+		foreach($customfieldsCategories as $cfc)
+			$customfieldsCategoriesArray[] = $cfc->getAttributes();
+		
+		$response['results'] = $customfieldsCategoriesArray;
+		$response['total'] = count($customfieldsCategoriesArray);
+		$response['success'] = true;
+		
+		return $response;
+	}	
 }
