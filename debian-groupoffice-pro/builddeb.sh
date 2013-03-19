56e7fa0bf467bd9fe0647e731846d7455ccf5caa
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-19 09:08:36 +0000

    Custom fields open source

--- debian-groupoffice-pro/builddeb.sh
+++ debian-groupoffice-pro/builddeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROMODULES="sync customfields gota caldav documenttemplates savemailas projects professional timeregistration hoursapproval tickets syncml carddav zpushadmin";
+PROMODULES="sync gota caldav documenttemplates savemailas projects professional timeregistration hoursapproval tickets syncml carddav zpushadmin";
 
 # useful: DEBCONF_DEBUG="developer"
 
