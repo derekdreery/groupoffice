e33157806afe071ad173735c34e441866a4fbfcd
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-20 16:49:04 +0000

    Created tag 4.1.47

---
+++ tags/groupoffice-com-4.1.47/go/base/view/Theme.php
@@ -0,0 +1,38 @@
+<?php
+
+class GO_Base_View_Theme{
+	/**
+	 * Get the name of the theme that is selected by the user.
+	 * 
+	 * @return string
+	 */
+	public function getName(){
+		$theme = GO::config()->allow_themes && GO::user() ? GO::user()->theme : GO::config()->theme;
+		
+		if(!file_exists(GO::config()->root_path.'views/'.GO::view().'/themes/'.$theme.'/Layout.php')){
+			return 'Default';
+		}  else {
+			return $theme;
+		}
+	}
+	
+	/**
+	 * Get the full path to the main theme folder with trailing slash.
+	 * 
+	 * @return string
+	 */
+	public function getPath(){
+		return GO::config()->root_path.'views/'.GO::view().'/themes/'.$this->getName().'/';
+	}
+	
+	/**
+	 * Get the full path to the main theme folder with trailing slash.
+	 * 
+	 * @return string
+	 */
+	public function getUrl(){
+		return GO::config()->host.'views/'.GO::view().'/themes/'.$this->getName().'/';
+	}
+	
+	
+}
