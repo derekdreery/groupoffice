152459bc263a64dcf738177fcd440f15d2ace4fc
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-24 18:57:03 +0000

    Ticketsystem hanged in weekends

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -926,7 +926,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '4.1.49';
+	var $version = '4.1.50';
 	
 	/**
 	 * Modification date
@@ -934,7 +934,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20130324';
+	var $mtime = '20130324-1';
 
 	#group configuration
 	/**
