96ef4a2baf5c38d2eebcf33d73215fa58aed0608
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-26 11:22:00 +0000

    Hide disabled custom field categories

--- branches/groupoffice-4.1/www/modules/addressbook/controller/CompanyController.php
+++ branches/groupoffice-4.1/www/modules/addressbook/controller/CompanyController.php
@@ -35,6 +35,19 @@ class GO_Addressbook_Controller_Company extends GO_Base_Controller_AbstractModel
 					'email' => $contact->email
 			);
 		}
+		
+		
+		if(GO::modules()->customfields && isset($response['data']['customfields']) && GO_Customfields_Model_DisableCategories::isEnabled("GO_Addressbook_Model_Company", $model->addresbook_id)){
+			$ids = GO_Customfields_Model_EnabledCategory::model()->getEnabledIds("GO_Addressbook_Model_Company", $model->addresbook_id);
+			
+			$enabled = array();
+			foreach($response['data']['customfields'] as $cat){
+				if(in_array($cat['id'], $ids)){
+					$enabled[]=$cat;
+				}
+			}
+			$response['data']['customfields']=$enabled;
+		}
 
 		return parent::afterDisplay($response, $model, $params);
 	}
