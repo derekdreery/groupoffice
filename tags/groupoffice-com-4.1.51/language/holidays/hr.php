498337d20f2716a0fd783674d0332ce0356c212a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-28 16:13:23 +0000

    Created tag 4.1.51

---
+++ tags/groupoffice-com-4.1.51/language/holidays/hr.php
@@ -0,0 +1,10 @@
+<?php
+// holidays with fixed date
+$input_holidays['fix']['01-01'] = 'Nova godina';
+$input_holidays['fix']['12-25'] = 'Božić';
+$input_holidays['fix']['12-26'] = 'Sveti Stjepan';
+
+// holidays with variable date (christian holidays computation is based on the date of easter day)
+$input_holidays['var']['-2'] = 'Veliki petak';
+$input_holidays['var']['0'] = 'Uskrs';
+$input_holidays['var']['1'] = 'Uskršnji ponedjeljak';
