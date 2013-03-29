6498aa9d13d374698adf85658e3ec1ff97d937d7
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-03-29 15:35:53 +0000

    Created tag 4.1.52

---
+++ tags/groupoffice-com-4.1.52/modules/users/model/CfSettingTab.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * Copyright Intermesh BV.
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ */
+ 
+/**
+ * The GO_Users_Model_CfSettingTab model
+ *
+ * @package GO.modules.Users
+ * @version $Id: CfSettingTab.php 7607 2011-11-03 10:28:32Z wsmits $
+ * @copyright Copyright Intermesh BV.
+ * @author Wesley Smits <wsmits@intermesh.nl>
+ *
+ * @property int $cf_category_id
+ */
+
+class GO_Users_Model_CfSettingTab extends GO_Base_Db_ActiveRecord{
+
+	/**
+	 * Returns a static model of itself
+	 * 
+	 * @param String $className
+	 * @return GO_Ads_Model_Format
+	 */
+	public static function model($className=__CLASS__)
+	{	
+		return parent::model($className);
+	}
+
+
+	public function primaryKey() {
+		return 'cf_category_id';
+	}
+	
+	/**
+	 * Enable this function if you want this model to check the acl's automatically.
+	 */
+	// public function aclField(){
+	//	 return 'acl_id';	
+	// }
+
+	/**
+	 * Returns the table name
+	 */
+	 public function tableName() {
+		 return 'go_cf_setting_tabs';
+	 }
+
+	/**
+	 * Here you can define the relations of this model with other models.
+	 * See the parent class for a more detailed description of the relations.
+	 */
+	 public function relations() {
+		 return array();
+	 }
+}
