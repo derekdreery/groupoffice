77bf6148b18eba570ef15b896a500ace44f21da8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-04-15 09:02:38 +0000

    Check if projects module is installed

--- www/modules/calendar/install/updatescripts/2_fix_project_calendars.php
+++ www/modules/calendar/install/updatescripts/2_fix_project_calendars.php
@@ -2,21 +2,23 @@
 //require_once('../../../../GO.php');
 //GO::session()->runAsRoot();
 
-$fp = GO_Base_Db_FindParams::newInstance()->ignoreAcl();
+if(GO::modules()->isInstalled('projects')){
+	$fp = GO_Base_Db_FindParams::newInstance()->ignoreAcl();
 
-$joinCriteria = GO_Base_Db_FindCriteria::newInstance()->addRawCondition('t.acl_id', 'p.acl_id');
+	$joinCriteria = GO_Base_Db_FindCriteria::newInstance()->addRawCondition('t.acl_id', 'p.acl_id');
 
-$fp->join('pm_types', $joinCriteria,'p');
+	$fp->join('pm_types', $joinCriteria,'p');
 
-$stmt = GO_Calendar_Model_Calendar::model()->find($fp);
+	$stmt = GO_Calendar_Model_Calendar::model()->find($fp);
 
-foreach($stmt as $calendar){
-	
-	echo "Fixing ".$calendar->name."\n";
-	$oldAcl = $calendar->acl;
-	
-	$newAcl = $calendar->setNewAcl();
-	$calendar->save();
-	
-	$oldAcl->copyPermissions($newAcl);
+	foreach($stmt as $calendar){
+
+		echo "Fixing ".$calendar->name."\n";
+		$oldAcl = $calendar->acl;
+
+		$newAcl = $calendar->setNewAcl();
+		$calendar->save();
+
+		$oldAcl->copyPermissions($newAcl);
+	}
 }
