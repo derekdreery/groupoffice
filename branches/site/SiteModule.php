be201dd8349418243742bc43424ff57f367ae87c
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-04-17 12:43:55 +0000

    Added files to dependencies

--- branches/site/SiteModule.php
+++ branches/site/SiteModule.php
@@ -10,6 +10,10 @@ class GO_Site_SiteModule extends GO_Base_Module{
 		return 'Wesley Smits';
 	}
 	
+	public function depends() {
+		return array('files');
+	}
+
 	public function authorEmail() {
 		return 'wsmits@intermesh.nl';
 	}
