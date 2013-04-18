c570e46d138c70aae65bde7d186ebacdd31664f8
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-04-18 13:17:52 +0000

    blocks showing projects / users / contacts / companies in display panel of contacts / companies

--- trunk/www/modules/addressbook/customfieldtype/Contact.php
+++ trunk/www/modules/addressbook/customfieldtype/Contact.php
@@ -5,6 +5,10 @@ class GO_Addressbook_Customfieldtype_Contact extends GO_Customfields_Customfield
 		return 'Contact';
 	}
 	
+	public static function getModelName() {
+		return 'GO_Addressbook_Model_Contact';
+	}
+	
 	public function formatDisplay($key, &$attributes, GO_Customfields_Model_AbstractCustomFieldsRecord $model) {
 		$html="";
 		if(!empty($attributes[$key])) {
