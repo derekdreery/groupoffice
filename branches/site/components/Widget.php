97772fff6e4b576e5fbaee9078d1041124131b7e
michaelhart86 <michaelhart86@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-04-22 11:54:08 +0000

    abstract widget has magic getter and can create id's automatic if not in config

--- branches/site/components/Widget.php
+++ branches/site/components/Widget.php
@@ -1,7 +1,44 @@
 <?php
 abstract class GO_Site_Components_Widget {
 	
+	/**
+	 * @var integer the counter for generating implicit IDs.
+	 */
+	private static $_counter=0;
+	/**
+	 * @var string id of the widget.
+	 */
+	private $_id;
 	
+	public function __get($name) {
+		$getter = 'get'.ucfirst($name);
+			
+		if(method_exists($this,$getter))
+			return $this->$getter();
+		else
+			throw new Exception('Call for unexisting property: '. $name);
+	}
+	
+	/**
+	 * Returns the ID of the widget or generates a new one if requested.
+	 * @param boolean $autoGenerate whether to generate an ID if it is not set previously
+	 * @return string id of the widget.
+	 */
+	public function getId()
+	{
+		if($this->_id!==null)
+			return $this->_id;
+		return $this->_id='go'.self::$_counter++;
+	}
+	
+	/**
+	 * Sets the ID of the widget.
+	 * @param string $value id of the widget.
+	 */
+	public function setId($value)
+	{
+		$this->_id=$value;
+	}
 	
 	/**
 	 * The render function to render this widget 
