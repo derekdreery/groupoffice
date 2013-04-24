f8d06097a91b0a4626f6076beb89e3512cf9491a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-04-24 08:40:52 +0000

    updated changelog

--- branches/groupoffice-4.1/www/modules/addressbook/scripts.inc.php
+++ branches/groupoffice-4.1/www/modules/addressbook/scripts.inc.php
@@ -19,24 +19,25 @@ $acl_level = GO_Base_Model_Acl::getUserPermissionLevel($export_acl_id, GO::user(
 $GO_SCRIPTS_JS .= 'GO.addressbook.exportPermission="'.(($acl_level) ? 1 : 0).'";';
 
 
-if(GO::modules()->customfields)
-$GO_SCRIPTS_JS .=  '
+if(GO::modules()->customfields){
+	$GO_SCRIPTS_JS .=  '
 
-GO.customfields.settingsPanels={
-	name: "'.GO_Addressbook_Model_Contact::model()->localizedName.'",
-	panels: []
-};'."\n";
+	GO.customfields.settingsPanels={
+		name: "'.GO_Addressbook_Model_Contact::model()->localizedName.'",
+		panels: []
+	};'."\n";
 
-$stmt = GO_Users_Model_CfSettingTab::model()->getSettingTabs();
-while($category = $stmt->fetch()){
+	$stmt = GO_Users_Model_CfSettingTab::model()->getSettingTabs();
+	while($category = $stmt->fetch()){
 
-	$fields = array();
-	$fstmt = $category->fields();
-	while($field = $fstmt->fetch()){
-		$fields[]=$field->toJsonArray();
+		$fields = array();
+		$fstmt = $category->fields();
+		while($field = $fstmt->fetch()){
+			$fields[]=$field->toJsonArray();
+		}
+
+		// Makes global, client-side, editable form panels for every customfield category
+		$GO_SCRIPTS_JS .= "\n\n".'GO.customfields.settingsPanels.panels.push({xtype : "customformpanel", itemId:"cf-panel-'.$category->id.'", category_id: '.$category->id.', title : "'.htmlspecialchars($category->name,ENT_QUOTES, 'UTF-8').'", customfields : '.json_encode($fields).'});'."\n";
 	}
-
-	// Makes global, client-side, editable form panels for every customfield category
-	$GO_SCRIPTS_JS .= "\n\n".'GO.customfields.settingsPanels.panels.push({xtype : "customformpanel", itemId:"cf-panel-'.$category->id.'", category_id: '.$category->id.', title : "'.htmlspecialchars($category->name,ENT_QUOTES, 'UTF-8').'", customfields : '.json_encode($fields).'});'."\n";
 }
 					
