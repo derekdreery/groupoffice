77424df1c7e28cd11c846bc004081fa294304331
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-05-12 13:35:24 +0000

    test

--- trunk/www/controller/DeveloperController.php
+++ trunk/www/controller/DeveloperController.php
@@ -1,5 +1,5 @@
 <?php
-class GO_Core_Controller_Developer extends GO_Base_Controller_AbstractController {
+class GO_Core_Controller_Developer extends GO_Base_Controller_AbstractController 
 
 	protected function allowGuests() {
 		return array('testvobject');
