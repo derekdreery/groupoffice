d162be3d83e6889e8bb1795eed010d2d19580e90
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-05-14 14:47:30 +0000

    Update fix old events that have an end_time that is earlier than the start_time.

--- branches/groupoffice-4.1/www/modules/calendar/install/updates.php
+++ branches/groupoffice-4.1/www/modules/calendar/install/updates.php
@@ -210,3 +210,5 @@ ENGINE = InnoDB";
 
 
 $updates['201303191307'][] ="ALTER TABLE `cal_participants` ADD INDEX ( `event_id` , `user_id` ) ;";
+
+$updates["201305141646"][]="update cal_events set mtime=unix_timestamp(), end_time=start_time+3600 where end_time<start_time;";
