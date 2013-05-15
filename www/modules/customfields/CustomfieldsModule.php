6fe094b159fa854795f560797ca9a5950154c9c9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-05-15 14:00:34 +0000

    Function to get types

--- www/modules/customfields/CustomfieldsModule.php
+++ www/modules/customfields/CustomfieldsModule.php
@@ -3,4 +3,30 @@ class GO_Customfields_CustomfieldsModule extends GO_Base_Module{
 	public function autoInstall() {
 		return true;
 	}
+	
+	
+	public static function getCustomfieldTypes() {
+
+		$types = array();
+
+		$modules = GO::modules()->getAllModules();
+			
+		while ($module=array_shift($modules)) {
+			if ($module->moduleManager) {
+				$classes = $module->moduleManager->findClasses('customfieldtype');
+
+				foreach ($classes as $class) {
+
+					if ($class->isSubclassOf('GO_Customfields_Customfieldtype_Abstract') && $class->getName()!='GO_Customfields_Customfieldtype_TreeselectSlave') {
+
+						$className = $class->getName();
+						$t = new $className;
+						$types[] = array('className' => $className, 'type' => $t->name());
+					}
+				}
+			}
+		}
+		return $types;
+	}
+
 }
