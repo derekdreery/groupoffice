89cbdbc2479d67d5825bef8ca024995e31c4cd7a
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-05-23 09:36:38 +0000

    Added query to remove duplicate participants

--- www/modules/calendar/install/updates.php
+++ www/modules/calendar/install/updates.php
@@ -212,3 +212,5 @@ ENGINE = InnoDB";
 $updates['201303191307'][] ="ALTER TABLE `cal_participants` ADD INDEX ( `event_id` , `user_id` ) ;";
 
 $updates["201305141646"][]="update cal_events set mtime=unix_timestamp(), end_time=start_time+3600 where end_time<start_time;";
+
+$updates["201305231136"][]="ALTER IGNORE TABLE cal_participants ADD UNIQUE(event_id,email );";
