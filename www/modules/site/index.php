7c647821a86473b1f1249b51c5db82b916455789
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-05-24 12:09:47 +0000

    Moved Site to trunk

---
+++ www/modules/site/index.php
@@ -0,0 +1,8 @@
+<?php
+//ini_set('display_errors','on');
+define('GO_CONFIG_FILE','/var/www/groupoffice-4.1/config.php'); //TODO!!!
+
+require(dirname(__FILE__).'/../../GO.php');
+require('components/Site.php');
+Site::launch();
+?>
