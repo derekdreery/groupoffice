eb736e26f52b07a936baba60faef222ef4cb60ae
michaelhart86 <michaelhart86@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-05-27 13:07:47 +0000

    files_folder_id of no_categories is default '0'

--- branches/groupoffice-4.1/www/modules/notes/install/updates.php
+++ branches/groupoffice-4.1/www/modules/notes/install/updates.php
@@ -33,3 +33,5 @@ $updates["201112221547"][]="CREATE TABLE IF NOT EXISTS `cf_no_notes` (
 $updates["201207191545"][]="ALTER TABLE `no_notes` ADD `password` varchar(255) DEFAULT '';";
 
 $updates["201301111137"][]="ALTER TABLE `no_categories` CHANGE `name` `name` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ";
+
+$updates["201109230000"][]="ALTER TABLE `no_categories` CHANGE `files_folder_id` INT NOT NULL DEFAULT '0';";
