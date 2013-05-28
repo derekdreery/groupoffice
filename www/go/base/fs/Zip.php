eefe73b6cedb833cf56f8b7c18f0a2bf7f86889a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-05-28 07:09:26 +0000

    Better way of creating ZIP archive of e-mail attachments so that foreign characters will work on Windows

--- www/go/base/fs/Zip.php
+++ www/go/base/fs/Zip.php
@@ -1,7 +1,40 @@
 <?php
 
+/**
+ * Copyright Intermesh BV
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @package GO.base.fs
+ * @version $Id$
+ * @author Merijn Schering <mschering@intermesh.nl>
+ * @copyright Copyright Intermesh BV.
+ */
+
+/**
+ * Static function to create a ZIP archive
+ * 
+ * @package GO.base.fs
+ * @version $Id$
+ * @author Merijn Schering <mschering@intermesh.nl>
+ * @copyright Copyright Intermesh BV.
+ */
+
 class GO_Base_Fs_Zip {
 
+	/**
+	 * Create a ZIP archive encoded in CP850 so that Windows will understand
+	 * foreign characters
+	 * 
+	 * @param GO_Base_Fs_File $archiveFile
+	 * @param GO_Base_Fs_Folder $workingFolder
+	 * @param GO_Base_Fs_Base[] $sources
+	 * @param boolean $utf8 Set to true to use UTF8 encoding. This is not supported by Windows explorer.
+	 * @throws Exception
+	 */
 	public static function create(GO_Base_Fs_File $archiveFile, GO_Base_Fs_Folder $workingFolder, $sources, $utf8=false) {
 	
 		if (class_exists("ZipArchive") && !$utf8) {
