b2ec216a5b7e2d9be3ff811a66893438bba86cf6
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-06-06 12:46:45 +0000

    prefix for settingsCollections

--- www/modules/users/model/Settings.php
+++ www/modules/users/model/Settings.php
@@ -6,4 +6,9 @@ class GO_Users_Model_Settings extends GO_Base_Model_AbstractSettingsCollection{
 	public $register_email_body;
 	
 	public $globalsettings_show_tab_addresslist;	
+	
+	public function myPrefix() {
+		return '';
+	}
+	
 }
