78a92ef8cc438fc5348ea3acb2e7f94eb9f03148
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-06-07 10:12:07 +0000

    Created tag 4.1.74

--- www/modules/site/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-//If the config.php file can't be found add this to the Apache configuration:
-//SetEnv GO_CONFIG /etc/groupoffice/config.php
-require(dirname(__FILE__).'/../../GO.php');
-require(GO::config()->root_path.'modules/site/components/Site.php');
-Site::launch();
-?>
