7dee44f837fa2624d1db375ebfcc4b8c8481b26d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-06-10 08:19:19 +0000

    Some instructions in index.php

--- trunk/www/modules/site/index.php
+++ trunk/www/modules/site/index.php
@@ -1,7 +1,34 @@
 <?php
+/*
+ * Copy this file to the document root 
+ * 
+ * Change require(dirname(__FILE__).'/../../GO.php'); to point to the correct location.
+ * 
+ * Add this to a .htaccess file or to 
+ * the VirtualHost file to enable pretty URL's.
+ * 
+ * 
+RewriteEngine On
+
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule ^(.*)\?*$ index.php/$1 [L,QSA]
+
+ */
 //If the config.php file can't be found add this to the Apache configuration:
 //SetEnv GO_CONFIG /etc/groupoffice/config.php
-require(dirname(__FILE__).'/../../GO.php');
+
+//Or you can use:
+//define('GO_CONFIG_FILE', '/path/to/config.php');
+
+$go = dirname(__FILE__).'/../../GO.php';
+if(file_exists($go))
+	require($go);
+elseif(file_exists('/usr/share/groupoffice/GO.php'))
+	require('/usr/share/groupoffice/GO.php');
+else
+	die("Please change the \$go variable to the correct location of GO.php");
+
 require(GO::config()->root_path.'modules/site/components/Site.php');
 Site::launch();
 ?>
