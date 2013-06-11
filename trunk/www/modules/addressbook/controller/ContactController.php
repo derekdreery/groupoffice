4527da0a17e42515ae40fef243806cce7520f3a9
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-06-11 14:33:09 +0000

    Append to csv instead of truncating

--- trunk/www/modules/addressbook/controller/ContactController.php
+++ trunk/www/modules/addressbook/controller/ContactController.php
@@ -104,8 +104,8 @@ class GO_Addressbook_Controller_Contact extends GO_Base_Controller_AbstractModel
 	
 	
 	protected function actionPhoto($params){
-		
-		$contact = GO_Addressbook_Model_Contact::model()->findByPk($params['id']);
+		//TODO remove permission skip
+		$contact = GO_Addressbook_Model_Contact::model()->findByPk($params['id'],false, true);
 		
 		GO_Base_Util_Http::outputDownloadHeaders($contact->getPhotoFile(), true, false);
 		$contact->getPhotoFile()->output();
