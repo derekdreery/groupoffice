96ed4c2b5011f41089bfafae851f770cc98f7ca3
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-06-17 06:24:18 +0000

    Close session writing

--- www/modules/email/controller/MessageController.php
+++ www/modules/email/controller/MessageController.php
@@ -1427,6 +1427,8 @@ class GO_Email_Controller_Message extends GO_Base_Controller_AbstractController 
 
 	public function actionAttachment($params) {
 		
+		GO::session()->closeWriting();
+		
 		$file = new GO_Base_Fs_File($params['filename']);
 		
 		$account = GO_Email_Model_Account::model()->findByPk($params['account_id']);
