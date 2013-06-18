3bf6de31437cfc7c1df1fa424418f4b2f9941986
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-06-18 07:40:55 +0000

    Created tag 4.1.78

--- www/modules/site/install/uninstall.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `site_sites`;
-DROP TABLE IF EXISTS `site_content`;
-DROP TABLE IF EXISTS `cf_site_sites`;
-DROP TABLE IF EXISTS `cf_site_content`;
-DROP TABLE IF EXISTS `site_multifile_files`;
