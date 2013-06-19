e0c3e02aadbd979c048c2e2228582eba8987eabc
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-06-19 10:36:21 +0000

    Created tag 4.1.79

--- communitymodules/dokuwiki/language/nb.php
+++
@@ -1,5 +0,0 @@
-<?php
-$l['name']='DokuWiki';
-$l['description']='Denne modulen integrerer DokuWiki med Group-Office, og logger automatisk inn Group-Office brukere. Se modules/dokuwiki/INSTALL.TXT for mer informasjon.';
-$l['externalUrl']='DokuWiki URL';
-$l['title']='Tittel';
