19136438fa73482110cd6d8db1f3005f82c6270f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-06-19 10:30:28 +0000

    merge of r13573 through r15135

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -931,7 +931,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '4.2.2';
+	var $version = '4.2.3';
 	
 	/**
 	 * Modification date
@@ -939,7 +939,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20130618';
+	var $mtime = '20130619';
 
 	#group configuration
 	/**
