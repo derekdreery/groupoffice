c304585ccce8eb1c5b3103991686d268eedc08b4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-06-20 10:20:45 +0000

    Smarter runAs

--- branches/groupoffice-4.2/www/go/base/Session.php
+++ branches/groupoffice-4.2/www/go/base/Session.php
@@ -333,13 +333,14 @@ class GO_Base_Session extends GO_Base_Observable{
 	 */
 	public function runAs($id){
 		//Close session writing so that the user won't stay root in browser sessions.
-		
-		$debug = !empty(GO::session()->values['debug']);
-		$debugSql = !empty(GO::session()->values['debugSql']);
-		
-		GO::session()->closeWriting();
-		GO::session()->values=array('debug'=>$debug, 'debugSql'=>$debugSql);
-		GO::session()->setCurrentUser($id);
+		if(!isset($this->values['user_id']) || $id!=$this->values['user_id']){
+			$debug = !empty(GO::session()->values['debug']);
+			$debugSql = !empty(GO::session()->values['debugSql']);
+
+			GO::session()->closeWriting();
+			GO::session()->values=array('debug'=>$debug, 'debugSql'=>$debugSql);
+			GO::session()->setCurrentUser($id);
+		}
 	}
 	
 	/**
