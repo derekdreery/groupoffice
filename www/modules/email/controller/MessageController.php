a24a6f72bc113503caae968e00c75fcc42bf2751
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-06-28 14:25:30 +0000

    Fixed display of contact photo in email when quicklink is disabled

--- www/modules/email/controller/MessageController.php
+++ www/modules/email/controller/MessageController.php
@@ -1153,29 +1153,31 @@ class GO_Email_Controller_Message extends GO_Base_Controller_AbstractController 
 		
 		$useQL = GO::config()->allow_quicklink;
 		$response['allow_quicklink']=$useQL?1:0;
-		if(!$useQL)
-			return $response;
 	
 		$contact = GO_Addressbook_Model_Contact::model()->findSingleByEmail($response['sender']);
 		if(!empty($contact)){
-			$response['sender_contact_id']=$contact->id;
-			$response['contact_name']=$contact->name.' ('.$contact->addressbook->name.')';
 			$response['contact_thumb_url']=$contact->getPhotoThumbURL();
 			
-			$company = $contact->company;
-			if(!empty($company)){
-				$response['sender_company_id']=$company->id;
-				$response['company_name']=$company->name.' ('.$company->addressbook->name.')';
-			}
-			
-			if(GO::modules()->savemailas){
-				$contactLinkedMessage = GO_Savemailas_Model_LinkedEmail::model()->findByImapMessage($imapMessage, $contact);
-				$response['contact_linked_message_id']=$contactLinkedMessage && $contactLinkedMessage->linkExists($contact) ? $contactLinkedMessage->id : 0;
-				
+			if($useQL){
+				$response['sender_contact_id']=$contact->id;
+				$response['contact_name']=$contact->name.' ('.$contact->addressbook->name.')';
+
+
+				$company = $contact->company;
 				if(!empty($company)){
-					$companyLinkedMessage = GO_Savemailas_Model_LinkedEmail::model()->findByImapMessage($imapMessage, $company);
-					$response['company_linked_message_id']=$companyLinkedMessage && $companyLinkedMessage->linkExists($company) ? $companyLinkedMessage->id : 0;
-				}				
+					$response['sender_company_id']=$company->id;
+					$response['company_name']=$company->name.' ('.$company->addressbook->name.')';
+				}
+
+				if(GO::modules()->savemailas){
+					$contactLinkedMessage = GO_Savemailas_Model_LinkedEmail::model()->findByImapMessage($imapMessage, $contact);
+					$response['contact_linked_message_id']=$contactLinkedMessage && $contactLinkedMessage->linkExists($contact) ? $contactLinkedMessage->id : 0;
+
+					if(!empty($company)){
+						$companyLinkedMessage = GO_Savemailas_Model_LinkedEmail::model()->findByImapMessage($imapMessage, $company);
+						$response['company_linked_message_id']=$companyLinkedMessage && $companyLinkedMessage->linkExists($company) ? $companyLinkedMessage->id : 0;
+					}				
+				}
 			}
 		}
 		return $response;
