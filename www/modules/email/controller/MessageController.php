31c8d99180c052ff93ab80a2aa49d23ab422e920
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-07-10 14:48:16 +0000

    Fix for when users have permissions to a sender contact but not the sender company

--- www/modules/email/controller/MessageController.php
+++ www/modules/email/controller/MessageController.php
@@ -1164,7 +1164,7 @@ class GO_Email_Controller_Message extends GO_Base_Controller_AbstractController 
 
 
 				$company = $contact->company;
-				if(!empty($company) && GO_Base_Model_Acl::getUserPermissionLevel($company->addressbook->acl_id)>=GO_Base_Model_Acl::READ_PERMISSION){
+				if(!empty($company) && GO_Base_Model_Acl::getUserPermissionLevel($company->addressbook->acl_id)>=GO_Base_Model_Acl::WRITE_PERMISSION){
 					$response['sender_company_id']=$company->id;
 					$response['company_name']=$company->name.' ('.$company->addressbook->name.')';
 				}
