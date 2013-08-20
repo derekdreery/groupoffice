ab512ee9f7f98fab7ee700b77b6ca40ef3143ed1
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-08-20 08:28:05 +0000

    Removed invalid tag 5.0.1

--- tags/groupoffice-com-5.0.1/modules/comments/language/es.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$l["browseComments"]= 'Personas comentarios';
-$l["recentComments"]='Comentarios recientes';
-
-$l['name']='Comentario';
-$l['description']='Añadir comentario a la funcionalidad de otros módulos';
-$l['comment']='Comentario';
-$l['comments']='Comentarios';
--- tags/groupoffice-com-5.0.1/modules/serverclient/language/ko.js
+++
@@ -1,9 +0,0 @@
-/**
- * Don't copy the next lines into a translation
- */
-/**
- * Copy everything below for translations
- */
-
-GO.serverclient.lang.mailboxes='메일박스';
-GO.serverclient.lang.createMailbox='Create a mailbox for domain';
