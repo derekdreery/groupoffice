99c8f2260aa7222d06452d87d4fbce53e26f3f30
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-09-11 12:22:49 +0000

    user relation: working week

--- www/go/base/model/User.php
+++ www/go/base/model/User.php
@@ -138,7 +138,8 @@ class GO_Base_Model_User extends GO_Base_Db_ActiveRecord {
 		return array(
 			'contact' => array('type' => self::HAS_ONE, 'model' => 'GO_Addressbook_Model_Contact', 'field' => 'go_user_id'),
 			'reminders' => array('type'=>self::MANY_MANY, 'model'=>'GO_Base_Model_Reminder', 'field'=>'user_id', 'linkModel' => 'GO_Base_Model_ReminderUser'),
-			'groups' => array('type'=>self::MANY_MANY, 'model'=>'GO_Base_Model_Group', 'field'=>'user_id', 'linkModel' => 'GO_Base_Model_UserGroup')
+			'groups' => array('type'=>self::MANY_MANY, 'model'=>'GO_Base_Model_Group', 'field'=>'user_id', 'linkModel' => 'GO_Base_Model_UserGroup'),
+			'workingWeek' => array('type' => self::HAS_ONE, 'model' => 'GO_Base_Model_WorkingWeek', 'field' => 'user_id')
 		);
 	}
 	
