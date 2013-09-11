c2f36361de1208abf5e0756c9396e9856e67eb5b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-09-11 11:56:15 +0000

    fireevent

--- branches/groupoffice-4.2/www/go/base/controller/AbstractModelController.php
+++ branches/groupoffice-4.2/www/go/base/controller/AbstractModelController.php
@@ -478,8 +478,7 @@ class GO_Base_Controller_AbstractModelController extends GO_Base_Controller_Abst
 		$this->fireEvent('display', array(
 				&$this,
 				&$response,
-				&$model,
-				&$params
+				&$model
 		));
 
 		return $response;
