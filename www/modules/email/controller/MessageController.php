055787c69fc812e771df2e9999a9a9f1311793be
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-09-13 11:19:39 +0000

    Close writing in email store

--- www/modules/email/controller/MessageController.php
+++ www/modules/email/controller/MessageController.php
@@ -181,7 +181,7 @@ class GO_Email_Controller_Message extends GO_Base_Controller_AbstractController 
 		
 		$this->checkRequiredParameters(array('account_id'), $params);
 		
-//		GO::session()->closeWriting(); //Don't do this because we cache values in the sessions
+		GO::session()->closeWriting();
 		
 		if(!isset($params['start']))
 			$params['start']=0;
