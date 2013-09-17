a3b1aa3f4a9a532bae7964f70bf9a75812e5ddcc
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-09-17 08:28:19 +0000

    Removed invalid tag 5.0.6

--- tags/groupoffice-com-5.0.6/modules/modules/language/de.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-require($GLOBALS['GO_LANGUAGE']->get_fallback_language_file('modules'));
-$lang['modules']['name']='Module';
-$lang['modules']['description']='Modul zum Verwalten aktiver/inaktiver Module';
-$lang['modules']['deleteModule']='Dieses Modul kann nicht gelöscht werden!';
-?>
--- tags/groupoffice-com-5.0.6/modules/sites/install/uninstall.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `si_content`;
-DROP TABLE IF EXISTS `si_sites`;
-DROP TABLE IF EXISTS `cf_si_sites`;
-DROP TABLE IF EXISTS `cf_si_content`;
