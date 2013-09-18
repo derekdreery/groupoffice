235c16e024ed82df479ba229123299443f6ad69b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-09-18 09:22:11 +0000

    Removed invalid tag 4.2.21

--- tags/groupoffice-com-4.2.21/modules/ldapauth/language/ko.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-$l['name']='LDAP 인증';
-$l['description']='This module authenticates to an LDAP server and automatically configures a new user and e-mail account in {product_name}.';
-$l['invalid_email']='Not a valid e-mail. You can only use:';
