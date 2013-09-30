0d30b543b144fda144b54aba45db4fe4670a785b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-09-30 07:43:20 +0000

    workaround activesync problem where 'mailto:' is included in the mail address.

--- www/go/base/mail/Message.php
+++ www/go/base/mail/Message.php
@@ -80,6 +80,12 @@ class GO_Base_Mail_Message extends Swift_Message{
 		$to = isset($structure->headers['to']) && strpos($structure->headers['to'],'undisclosed')===false ? $structure->headers['to'] : '';
 		$cc = isset($structure->headers['cc']) && strpos($structure->headers['cc'],'undisclosed')===false ? $structure->headers['cc'] : '';
 		$bcc = isset($structure->headers['bcc']) && strpos($structure->headers['bcc'],'undisclosed')===false ? $structure->headers['bcc'] : '';
+		
+		//workaround activesync problem where 'mailto:' is included in the mail address.
+		
+		$to = str_replace('mailto:','', $to);
+		$cc = str_replace('mailto:','', $cc);
+		$bcc = str_replace('mailto:','', $bcc);
 	
 		$toList = new GO_Base_Mail_EmailRecipients($to);
 		$to =$toList->getAddresses();
