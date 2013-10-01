3873c6af0073a6c9254d04b71085592c41c52bdd
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-10-01 13:43:25 +0000

    Fixed context menu when searching in e-mail

--- branches/groupoffice-5.0/www/modules/email/EmailClient.js
+++ branches/groupoffice-5.0/www/modules/email/EmailClient.js
@@ -373,9 +373,33 @@ GO.email.EmailClient = function(config){
 		shadow: "frame",
 		minWidth: 180,
 		items: [ 
-		  this.contextMenuSource,
+		  new Ext.menu.Item ({
+		  text: GO.email.lang.viewSource,
+		  handler: function(){
+
+			  var record = this.messagesGrid.selModel.getSelected();
+			  if(record)
+			  {
+				  //var win = window.open(GO.url("email/message/source",{account_id:this.account_id,mailbox:this.mailbox,uid:record.data.uid}));
+				  var win = window.open(GO.url("email/message/source",{account_id:this.account_id,mailbox:record.data.mailbox,uid:record.data.uid}));
+				  win.focus();
+			  }
+
+		  },
+		  scope: this
+	  }),
 		  '-',
-		  this.contextMenuCopyTo
+		 new Ext.menu.Item ({
+		  iconCls: 'btn-copy',
+		  text: GO.email.lang['copyMailTo'],
+		  cls: 'x-btn-text-icon',
+		  handler: function(a,b,c){
+			  var selectedEmails = this.messagesGrid.getSelectionModel().getSelections();
+			  this.showCopyMailToDialog(selectedEmails);
+		  },
+		  scope: this,
+		  multiple:true
+	  })
 		]
 	});
 
