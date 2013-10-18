3cff04aa5bfd918ba4972266996cd22a797b049a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-10-18 06:51:45 +0000

    5.0.12

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -931,7 +931,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '5.0.10';
+	var $version = '5.0.12';
 	
 	/**
 	 * Modification date
@@ -939,7 +939,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20131010';
+	var $mtime = '20131018';
 
 	#group configuration
 	/**
