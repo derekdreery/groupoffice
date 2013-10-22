7d0959bfe0876fdad9d9c86947a10262ac6a7f74
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-10-22 13:25:20 +0000

    Skype icon with tooltip

--- www/modules/addressbook/ReadPanelContact.js
+++ www/modules/addressbook/ReadPanelContact.js
@@ -227,7 +227,7 @@ GO.addressbook.ContactReadPanel = Ext.extend(GO.DisplayPanel,{
 										'<a href="{url_twitter}" target="_blank"><div class="twitter-icon"></div></a>'+
 									'</tpl>'+
 									'<tpl if="!GO.util.empty(skype_name)">'+
-										'<a href="skype:{skype_name}?call" target="_blank">'+GO.addressbook.lang['callOnSkype']+'</a>'+
+										'<a href="skype:{skype_name}?call" target="_blank"><div class="skype-icon" title="'+GO.addressbook.lang['callOnSkype']+'"></div></a>'+
 									'</tpl>'+
 							'</td>'+
 						'</tr>'+
