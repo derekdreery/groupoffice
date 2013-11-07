24de0fc4975b1a0d5c27dee899c93d0f949aaf21
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-11-07 14:22:47 +0000

    merge of r15087 through r16201

--- branches/groupoffice-5.0/www/go/base/Config.php
+++ branches/groupoffice-5.0/www/go/base/Config.php
@@ -931,7 +931,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '5.0.19';
+	var $version = '5.0.20';
 	
 	/**
 	 * Modification date
@@ -939,7 +939,8 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $mtime = '20131101';
+
+	var $mtime = '20131107';
 
 	#group configuration
 	/**
