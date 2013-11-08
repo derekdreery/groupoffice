2531b4319c725d81af788a7aa9c4713b8792ad3b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-11-08 09:40:06 +0000

    updated changelog

--- branches/groupoffice-5.0/www/go/base/Config.php
+++ branches/groupoffice-5.0/www/go/base/Config.php
@@ -931,7 +931,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '5.0.20';
+	var $version = '5.0.21';
 	
 	/**
 	 * Modification date
@@ -940,7 +940,7 @@ class GO_Base_Config {
 	 * @access  public
 	 */
 
-	var $mtime = '20131107';
+	var $mtime = '20131108';
 
 	#group configuration
 	/**
