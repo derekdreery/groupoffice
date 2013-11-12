0d91be488d41f702beb7c7a2d754c07fc13281df
michaelhart86 <michaelhart86@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-11-12 15:53:26 +0000

    the path the the chart.swf file has a starting slash in front of it. Because of this it wouldn't work when groupoffice was installed in a subdirectory

--- branches/groupoffice-5.0/www/views/Extjs3/javascript/MainLayout.js
+++ branches/groupoffice-5.0/www/views/Extjs3/javascript/MainLayout.js
@@ -273,7 +273,7 @@ Ext.extend(GO.MainLayout, Ext.util.Observable, {
 				maxWidth:500
 		});
 		//Ext need to know where this charting swf file is in order to draw charts
-		Ext.chart.Chart.CHART_URL = '/views/Extjs3/ext/resources/charts.swf';
+		Ext.chart.Chart.CHART_URL = 'views/Extjs3/ext/resources/charts.swf';
 
 		var allPanels = GO.moduleManager.getAllPanelConfigs();
 
