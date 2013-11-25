90be92d70789b6de5a99b8ed1de1ffb14d32956d
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-11-25 14:04:05 +0000

    Added check for existing #quick-add-menu div in the page structure

--- www/views/Extjs3/javascript/panels/QuickAddPanel.js
+++ www/views/Extjs3/javascript/panels/QuickAddPanel.js
@@ -50,71 +50,75 @@ GO.panels.QuickAddPanel = Ext.extend(Ext.Container, {
 GO.quickAddPanel = new GO.panels.QuickAddPanel();
 
 GO.mainLayout.on('render', function(){
-	// Render the quickAddPanel to the div so the DOM elements are created
-	GO.quickAddPanel.render("quick-add-menu",0);
 	
-	// Get the dom elements 
-	var menu = Ext.get('quick-add-menu');
-	var toggleButton = Ext.get('quick-add-menu-collapse');
-	
-	// Set the width and the height of the component that contains the buttons
-	menu.setWidth(GO.quickAddPanel.calcWidth());
-	menu.setHeight(GO.quickAddPanel.cmpHeight);
+	if(Ext.getCmp("quick-add-menu")){
 
-	// Declare variable to keep the click state
-	toggleButton.enableClick = true;
+		// Render the quickAddPanel to the div so the DOM elements are created
+		GO.quickAddPanel.render("quick-add-menu",0);
 
-	// Set the click handler on the toggle button
-	toggleButton.on('click',function(){
-		
-		// Check if the button is not clicked before the animation from the previous click is done.
-		if(toggleButton.enableClick === true){
-			
-			// Animation is pending so disable clicking
-			toggleButton.enableClick = false;
-			
-			// Check if the panel is currently hidden or visible
-			var hidden = GO.quickAddPanel.hidden;
+		// Get the dom elements 
+		var menu = Ext.get('quick-add-menu');
+		var toggleButton = Ext.get('quick-add-menu-collapse');
 
-			if(hidden){
-				// Replace the button class so the minus sign is displayed
-				toggleButton.replaceClass('plus-sign','minus-sign');
+		// Set the width and the height of the component that contains the buttons
+		menu.setWidth(GO.quickAddPanel.calcWidth());
+		menu.setHeight(GO.quickAddPanel.cmpHeight);
 
-				// Set the panel to visible
-				GO.quickAddPanel.setVisible(true);
-				menu.setVisible(true);
+		// Declare variable to keep the click state
+		toggleButton.enableClick = true;
 
-				// align the menu to the button
-				menu.alignTo('quick-add-menu-collapse','r-l');
+		// Set the click handler on the toggle button
+		toggleButton.on('click',function(){
 
-				// Animate the menu so it will fade in
-				menu.fadeIn({
-					endOpacity: 1,
-					callback:function(el){
-						
-						// Enable the togglebutton click event again.
-						toggleButton.enableClick = true;
-					},
-					scope:this
-				});
-			}else{
-				// Replace the button class so the plus sign is displayed
-				toggleButton.replaceClass('minus-sign','plus-sign');
+			// Check if the button is not clicked before the animation from the previous click is done.
+			if(toggleButton.enableClick === true){
 
-				// Animate the menu so it will fade out
-				menu.fadeOut({
-					endOpacity: 0,
-					callback:function(el){
-						// Hide the items when the animation is complete
-						GO.quickAddPanel.setVisible(false);
-						menu.setVisible(false);
-						
-						// Enable the togglebutton click event again.
-						toggleButton.enableClick = true;
-					},
-					scope:this
-				});
+				// Animation is pending so disable clicking
+				toggleButton.enableClick = false;
+
+				// Check if the panel is currently hidden or visible
+				var hidden = GO.quickAddPanel.hidden;
+
+				if(hidden){
+					// Replace the button class so the minus sign is displayed
+					toggleButton.replaceClass('plus-sign','minus-sign');
+
+					// Set the panel to visible
+					GO.quickAddPanel.setVisible(true);
+					menu.setVisible(true);
+
+					// align the menu to the button
+					menu.alignTo('quick-add-menu-collapse','r-l');
+
+					// Animate the menu so it will fade in
+					menu.fadeIn({
+						endOpacity: 1,
+						callback:function(el){
+
+							// Enable the togglebutton click event again.
+							toggleButton.enableClick = true;
+						},
+						scope:this
+					});
+				}else{
+					// Replace the button class so the plus sign is displayed
+					toggleButton.replaceClass('minus-sign','plus-sign');
+
+					// Animate the menu so it will fade out
+					menu.fadeOut({
+						endOpacity: 0,
+						callback:function(el){
+							// Hide the items when the animation is complete
+							GO.quickAddPanel.setVisible(false);
+							menu.setVisible(false);
+
+							// Enable the togglebutton click event again.
+							toggleButton.enableClick = true;
+						},
+						scope:this
+					});
+				}
 			}
-		}
-	},this);
+		},this);   
+	} 
 },this);
