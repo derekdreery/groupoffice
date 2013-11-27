66f5523cebfe96df0f2757c5e4fbdb1d187233d7
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-11-27 10:41:41 +0000

    add unique index to go_links tables

--- branches/groupoffice-5.0/www/modules/calendar/install/install.sql
+++ branches/groupoffice-5.0/www/modules/calendar/install/install.sql
@@ -296,7 +296,7 @@ CREATE TABLE IF NOT EXISTS `go_links_cal_events` (
   `model_type_id` int(11) NOT NULL,
   `description` varchar(100) DEFAULT NULL,
   `ctime` int(11) NOT NULL,
-  KEY `link_id` (`model_id`,`model_type_id`),
+  UNIQUE KEY `model_id` (`model_id`,`model_type_id`),
   KEY `id` (`id`,`folder_id`),
   KEY `ctime` (`ctime`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
