11f13fceeda8a6c5403a67690b821048b6e7cd98
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-11-27 17:00:45 +0000

    Removed invalid tag 5.0.25

--- tags/groupoffice-com-5.0.25/modules/sites/templates/Plain/views/sites/404.php
+++
@@ -1,4 +0,0 @@
-<h1>Not found</h1>
-<?php
-	echo '<p>Sorry the page you requested was not found on <a href="'.GOS::site()->getRequest()->getHostInfo().'">'.GOS::site()->getRequest()->getHostInfo().'</a>.</p>';
-?>			
