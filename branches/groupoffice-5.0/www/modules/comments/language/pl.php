02b5af26d37089f13e756211b59a565cc93dd833
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-12-04 12:02:29 +0000

    Updated Polish

--- branches/groupoffice-5.0/www/modules/comments/language/pl.php
+++ branches/groupoffice-5.0/www/modules/comments/language/pl.php
@@ -7,3 +7,11 @@ $l['name']='Komentarze';
 $l['description']='Dodaje funkcjonalność komentowania do innych modułów';
 $l['comment']='Komentarz';
 $l['comments']='Komentarze';
+
+$l['manageCategories']='Zarządzaj kategoriami';
+$l['category']='Kategoria';
+$l['noCategory']='Brak kategorii';
+$l['newCommentText']='Dodaj nowy komentarz ...';
+$l['readMore']="Więcej";
+$l['enableReadMore']="Włącz chowanie komentarzy";
+$l['actionDate']='Data zdarzenia';
