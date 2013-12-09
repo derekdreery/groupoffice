f2e2bc02110c6f76890ece154d5a37b8c484e15f
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-12-09 12:59:40 +0000

    new branch

---
+++ branches/multiview/debian-groupoffice-servermanager/usr/share/groupoffice/modules/servermanager/language/nl.js
@@ -0,0 +1,23 @@
+GO.servermanager.lang.servermanager='Server beheer';
+
+/* table: sm_installations */
+GO.servermanager.lang.installation="Installatie";
+GO.servermanager.lang.installations="Installaties";
+GO.servermanager.lang.webmasterEmail="Webmaster email";
+GO.servermanager.lang.title="Titel";
+GO.servermanager.lang.defaultCountry="Land";
+GO.servermanager.lang.language="Taal";
+GO.servermanager.lang.defaultTimezone="Tijdzone";
+GO.servermanager.lang.defaultCurrency="Valuta";
+GO.servermanager.lang.defaultDateFormat="Datum formaat";
+GO.servermanager.lang.defaultDateSeperator="Datum scheidingsteken";
+GO.servermanager.lang.defaultThousandsSeperator="Duizendtal scheidingsteken";
+GO.servermanager.lang.defaultDecimalSeperator="Decimaal scheidingsteken";
+GO.servermanager.lang.theme="Thema";
+GO.servermanager.lang.allowThemes="Sta thema\'s toe";
+GO.servermanager.lang.allowPasswordChange="Sta wachtwoord veranderen toe";
+GO.servermanager.lang.allowRegistration="Sta registratie toe";
+GO.servermanager.lang.allowDuplicateEmail="Sta dubbele email toe";
+GO.servermanager.lang.autoActivateAccounts="Automatisch accounts activeren";
+GO.servermanager.lang.notifyAdminOfRegistration="Stel beheerder op de hoogte van registratie";
+GO.servermanager.lang.maxUsers="Maximaal aantal gebruikers";
