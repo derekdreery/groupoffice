f239d22642f342973f165eaefec70819581c12f7
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2013-12-11 15:50:51 +0000

    Created tag 5.0.27

--- www/modules/files/install/updatescripts/2_calculate_user_quota.php
+++
@@ -1,6 +0,0 @@
-<?php
-// use /files/file/eecalculateDiskUsage for manual execution or recalculation
-$users = GO_Base_Model_User::model()->find();
-foreach($users as $user) {
-	$user->calculatedDiskUsage()->save();
-}
