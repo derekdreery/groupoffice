592bea9a84cd88f99e56eb60e04366d02e9fc40f
wilmar1980 <wilmar1980@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-01-06 13:30:46 +0000

    Better response to user if logged in user has no access to a calendar of which he wants to accept an invitation.

--- branches/groupoffice-5.0/www/modules/calendar/language/nl.php
+++ branches/groupoffice-5.0/www/modules/calendar/language/nl.php
@@ -249,3 +249,5 @@ $l['sendNotificationToNewParticipants']='Wilt u alleen de zojuist toegevoegde de
 $l['newParticipants']='Nieuwe deelnemers';
 $l['allParticipants']='Alle deelnemers';
 $l['noParticipants']='Geen deelnemers';
+$l['cannotHandleInvitation']='De agenda die hoort bij het email account heet "%s" en u heeft daar geen schrijfrechten toe. Omdat de afspraak in die agenda zit, is haar status nu niet gewijzigd.';
+$l['cannotHandleInvitation2']='Kon de afspraak niet aanpassen, omdat u te weinig toegangsrechten heeft tot de agenda die hoort bij het email account (agenda: "%s"). Omdat de afspraak in die agenda zit, is haar status nu niet gewijzigd.';
