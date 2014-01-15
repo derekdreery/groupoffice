05b5198aa16d653ff3ac46aa5199fe07d789b813
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-01-15 12:26:25 +0000

    5.0.32

--- branches/groupoffice-5.0/www/go/base/Config.php
+++ branches/groupoffice-5.0/www/go/base/Config.php
@@ -945,7 +945,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '5.0.31';
+	var $version = '5.0.32';
 
 	
 	/**
@@ -955,7 +955,7 @@ class GO_Base_Config {
 	 * @access  public
 	 */
 
-	var $mtime = '20140110';
+	var $mtime = '20140115';
 
 	#group configuration
 	/**
