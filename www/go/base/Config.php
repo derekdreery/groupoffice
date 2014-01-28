1841642c7789378532bcd806998a5d50dd1f2534
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-01-28 13:54:54 +0000

    ticket list optimization

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -945,7 +945,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '5.0.36';
+	var $version = '5.0.37';
 
 	
 	/**
@@ -955,7 +955,7 @@ class GO_Base_Config {
 	 * @access  public
 	 */
 
-	var $mtime = '20140124';
+	var $mtime = '20140127';
 
 	#group configuration
 	/**
