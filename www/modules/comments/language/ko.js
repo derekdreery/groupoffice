01e4253a57f006abfbe5f9fec5e9e3d847ba524e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-01-29 16:03:19 +0000

    Removed old language files

--- www/modules/comments/language/ko.js
+++
@@ -1,8 +0,0 @@
-/**
- * Copy everything below for translations
- */
-
-GO.comments.lang.comment="코멘트";
-GO.comments.lang.comments="코멘트";
-GO.comments.lang.recentComments='최근 코멘트';
-GO.comments.lang.browseComments = 'Browse comments';
