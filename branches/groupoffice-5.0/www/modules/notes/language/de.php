5785db82c90b805031d9ac6d93ef761396e2f049
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-01-31 13:02:39 +0000

    updated german

--- branches/groupoffice-5.0/www/modules/notes/language/de.php
+++ branches/groupoffice-5.0/www/modules/notes/language/de.php
@@ -8,7 +8,7 @@ $l['name']='Notizen';
 $l['description']='Modul zum Anlegen von Notizen';
 $l['category']='Notizbuch';
 $l['categories']='Notizbücher';
-$l['note']='Notiz';
+$lang['link_type'][4]=$l['note']='Notiz';
 $l['notes']='Notizen';
 $l['general'] = 'Allgemein';
 $l['incomplete_delete']='Sie haben nicht für alle Notizen die Berechtigung zum Löschen';
