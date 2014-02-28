f0822dab389781919726712a4c60ff9a7036a680
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-02-28 13:24:26 +0000

    removed old sites module

--- trunk/www/modules/sites/templates/intermeshshop/views/sites/404.php
+++
@@ -1,9 +0,0 @@
-<div class="subkader-big-top">
-	<div class="subkader-big-bottom">
-		<div class="subkader-big-center">
-		<?php
-			echo '<p>Sorry the page you requested was not found on <a href="'.GOS::site()->getRequest()->getHostInfo().'">'.GOS::site()->getRequest()->getHostInfo().'</a>.</p>';
-		?>			
-		</div>
-	</div>
-</div>
