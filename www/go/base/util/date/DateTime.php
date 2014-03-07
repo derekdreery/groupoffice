8d2fc250952829186fc65a6c181292a8c70078c8
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-03-07 11:00:17 +0000

    namespaces

--- www/go/base/util/date/DateTime.php
+++ www/go/base/util/date/DateTime.php
@@ -56,7 +56,7 @@ class DateTime extends \DateTime {
 			$base = new \DateTime("$year-03-21");
 			$days = easter_days($year);
 
-			return $base->add(new DateInterval("P{$days}D"));
+			return $base->add(new \DateInterval("P{$days}D"));
 	}
 	
 	/**
