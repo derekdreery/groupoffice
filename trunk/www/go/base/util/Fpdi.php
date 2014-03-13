209c511af11b36b469ca75707eab1ed31850f7f3
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-03-13 14:49:56 +0000

    namespaces

--- trunk/www/go/base/util/Fpdi.php
+++ trunk/www/go/base/util/Fpdi.php
@@ -1,13 +1,12 @@
 <?php
+namespace GO\Base\Util;
+
 require_once(\GO::config()->root_path."go/vendor/tcpdf/tcpdf.php");
 
 //TODO FPDI is not using strict standards
 $oldLvl = error_reporting(E_ALL & ~E_STRICT);
 require_once(\GO::config()->root_path.'go/vendor/fpdi/fpdi.php');
 error_reporting($oldLvl);
-
-
-namespace GO\Base\Util;
 
 
 class Fpdi extends \FPDI {
