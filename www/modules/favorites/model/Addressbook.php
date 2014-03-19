abd489dc825788d37fccc88fb254d0a27e9c7d0d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-03-19 16:07:33 +0000

    Created tag 5.0.47

---
+++ www/modules/favorites/model/Addressbook.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright Intermesh BV.
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @package GO.modules.Favorites
+ * @version $Id$
+ * @copyright Copyright Intermesh BV.
+ * @author Wesley Smits wsmits@intermesh.nl
+ */
+ 
+/**
+ * The GO_Favorites_Model_Addressbook model
+ *
+ * @package GO.modules.Favorites
+ * @version $Id$
+ * @copyright Copyright Intermesh BV.
+ * @author Wesley Smits wsmits@intermesh.nl
+ *
+ * @property int $user_id
+ * @property int $addressbook_id
+ * @property int $sort
+ */
+
+class GO_Favorites_Model_Addressbook extends GO_Base_Db_ActiveRecord{
+
+	/**
+	 * Returns the table name
+	 */
+	 public function tableName() {
+		 return 'fav_addressbook';
+	 }
+	 
+	 public function primaryKey() {
+		 return array('user_id','addressbook_id');
+	 }
+
+	/**
+	 * Here you can define the relations of this model with other models.
+	 * See the parent class for a more detailed description of the relations.
+	 */
+	 public function relations() {
+		 return array(
+				 'addressbook' => array('type' => self::BELONGS_TO, 'model' => 'GO_Addressbook_Model_Addressbook', 'field' => 'addressbook_id', 'delete' => false),
+		 );
+	 }	
+}
