4c1b62c9f175aed2fcc58b6f741a8ca4c9552ff4
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-03-24 13:29:40 +0000

    google tracking

--- trunk/www/modules/manualsite/views/site/layouts/main.php
+++ trunk/www/modules/manualsite/views/site/layouts/main.php
@@ -51,7 +51,7 @@
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			ga('create', '<?php echo Site::model()->{"Google tracking code"}; ?>', '<?php echo $_REQUEST['SERVER_NAME']; ?>');
+			ga('create', '<?php echo Site::model()->{"Google tracking code"}; ?>', '<?php echo $_SERVER['SERVER_NAME']; ?>');
 			ga('send', 'pageview');
 
 		</script>
