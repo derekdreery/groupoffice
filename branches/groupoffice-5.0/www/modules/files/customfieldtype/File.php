fa8c13e8e34a11618471508961d0465b5a7db2a7
michaelhart86 <michaelhart86@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-03-26 08:54:57 +0000

    File browser changes:
    FileBrowser Will accept 0 (default) as valid file_folder_is. This will use the users home folder
    The File custom field will show an edit pencil to open the file with eg. GOTA

--- branches/groupoffice-5.0/www/modules/files/customfieldtype/File.php
+++ branches/groupoffice-5.0/www/modules/files/customfieldtype/File.php
@@ -12,11 +12,13 @@ class GO_Files_Customfieldtype_File extends GO_Customfields_Customfieldtype_Abst
 	public function formatDisplay($key, &$attributes, GO_Customfields_Model_AbstractCustomFieldsRecord $model) {
 		$html="";
 		if(!empty($attributes[$key])) {
+			
+			$file = GO_Files_Model_File::model()->findByPath($attributes[$key]);
 
 			if(!GO_Customfields_Model_AbstractCustomFieldsRecord::$formatForExport){
-				$html='<a href="#" onclick=\'GO.linkHandlers["GO_Files_Model_File"].call(this,"'.
-					$attributes[$key].'");\' title="'.$attributes[$key].'">'.
-						$attributes[$key].'</a>';
+				$html='<a href="#" onclick="GO.linkHandlers[\'GO_Files_Model_File\'].call(this,\''.$attributes[$key].'\');">'.basename($attributes[$key]).'</a>'.
+				'<a href="#" onclick=\''.$file->getDefaultHandler()->getHandler($file).'\' style="display:block;float: right;" class="go-icon btn-edit">&nbsp;</a>';
+				//$html='<a href="#"  title="'.$attributes[$key].'">'.$attributes[$key].'</a>';
 			}else
 			{
 				$html=$attributes[$key];
