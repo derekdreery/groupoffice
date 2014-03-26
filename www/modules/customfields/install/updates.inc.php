a2ff1a6fecd7b2f42dd9a9c7220ad1221ea62505
michaelhart86 <michaelhart86@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-03-26 08:51:29 +0000

    Customfield changes:
    MainPanel has render type function so it can render differend types of customfield in differend ways
    install+update for adding extra_options field to cf_fields t able
    DOC update in Field model (has extra_options)
    FieldDialog.js has showable panel to show the extra fields

--- www/modules/customfields/install/updates.inc.php
+++ www/modules/customfields/install/updates.inc.php
@@ -150,3 +150,5 @@ $updates['201309191000'][]="UPDATE `cf_fields` SET max_length=255;";
 
 $updates['201402110930'][]="ALTER TABLE `cf_fields` ADD `addressbook_ids` VARCHAR(64) NOT NULL DEFAULT '';";
 $updates['201402111315'][]="ALTER TABLE `cf_fields` CHANGE `addressbook_ids` `addressbook_ids` VARCHAR(255) NOT NULL DEFAULT '';";
+
+$updates['201403251524'][]="ALTER TABLE `cf_fields` ADD COLUMN `extra_options` VARCHAR(255) NOT NULL DEFAULT '';";
