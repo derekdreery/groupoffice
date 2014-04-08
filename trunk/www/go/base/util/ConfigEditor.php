7d706e7d82b42bde3d2f45df2f878ed7b9cb5853
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-04-08 08:33:29 +0000

    Only save config parameters if they are different than the default

--- trunk/www/go/base/util/ConfigEditor.php
+++ trunk/www/go/base/util/ConfigEditor.php
@@ -3,21 +3,24 @@
 
 namespace GO\Base\Util;
 
+use GO;
+use ReflectionClass;
 
 class ConfigEditor {
 
 	public static function save(\GO\Base\Fs\File $file, array $config) {
 		$configData = "<?php\n";
+		
+		$configReflection = new ReflectionClass(GO::config());
+		
+		
+		$defaults = $configReflection->getDefaultProperties();
+	
+		
 		foreach ($config as $key => $value) {
-//			if ($value === true) {
-//				$configData .= '$config["' . $key . '"]=true;' . "\n";
-//			} elseif ($value === false) {
-//				$configData .= '$config["' . $key . '"]=false;' . "\n";
-//			} else if(is_array($value)) {
+			if($defaults[$key]!==$value){
 				$configData .= '$config["' . $key . '"]=' . var_export($value,true).';' . "\n";
-//			}else{
-//				$configData .= '$config["' . $key . '"]="' . $value . '";' . "\n";
-//			}
+			}
 		}
 		
 		//make sure directory exists
