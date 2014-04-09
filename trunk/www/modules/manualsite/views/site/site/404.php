0eb868ab9ef17b6a6b982272fbd6751544950baa
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-04-09 09:28:04 +0000

    home url in 404 page

--- trunk/www/modules/manualsite/views/site/site/404.php
+++ trunk/www/modules/manualsite/views/site/site/404.php
@@ -6,5 +6,7 @@ header("Status: 404 Not Found");
 	<div class="wrapper">
 		<h2>404 Not found</h2>
 		<p><?php echo isset($error)?$error:'Niet gevonden'; ?></p>
+		
+		<p><a href="<?php echo \Site::urlManager()->getHomeUrl(); ?>">Go to home page</a></p>
 	</div>
 </div>
