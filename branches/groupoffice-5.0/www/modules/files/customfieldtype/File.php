f78ac50468ffbeed6670fd9948bc17d0b1f91f3e
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-04-14 06:44:39 +0000

    Check if file was found

--- branches/groupoffice-5.0/www/modules/files/customfieldtype/File.php
+++ branches/groupoffice-5.0/www/modules/files/customfieldtype/File.php
@@ -15,13 +15,15 @@ class GO_Files_Customfieldtype_File extends GO_Customfields_Customfieldtype_Abst
 			
 			$file = GO_Files_Model_File::model()->findByPath($attributes[$key]);
 
-			if(!GO_Customfields_Model_AbstractCustomFieldsRecord::$formatForExport){
-				$html='<a href="#" onclick="GO.linkHandlers[\'GO_Files_Model_File\'].call(this,\''.$attributes[$key].'\');">'.basename($attributes[$key]).'</a>'.
-				'<a href="#" onclick=\''.$file->getDefaultHandler()->getHandler($file).'\' style="display:block;float: right;" class="go-icon btn-edit">&nbsp;</a>';
-				//$html='<a href="#"  title="'.$attributes[$key].'">'.$attributes[$key].'</a>';
-			}else
-			{
-				$html=$attributes[$key];
+			if($file){
+				if(!GO_Customfields_Model_AbstractCustomFieldsRecord::$formatForExport){
+					$html='<a href="#" onclick="GO.linkHandlers[\'GO_Files_Model_File\'].call(this,\''.$attributes[$key].'\');">'.basename($attributes[$key]).'</a>'.
+					'<a href="#" onclick=\''.$file->getDefaultHandler()->getHandler($file).'\' style="display:block;float: right;" class="go-icon btn-edit">&nbsp;</a>';
+					//$html='<a href="#"  title="'.$attributes[$key].'">'.$attributes[$key].'</a>';
+				}else
+				{
+					$html=$attributes[$key];
+				}
 			}
 		}
 		return $html;
