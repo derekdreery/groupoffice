57777502bf68800ba8b0385a2069c68cce2b65b5
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-04-16 07:57:48 +0000

    Created tag 5.0.55

---
+++ tags/groupoffice-com-5.0.55/modules/users/UsersGrid.js
@@ -0,0 +1,113 @@
+/** 
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ * 
+ * @version $Id$
+ * @copyright Copyright Intermesh
+ * @author Merijn Schering <mschering@intermesh.nl>
+ * @author Boy Wijnmaalen <bwijnmaalen@intermesh.nl>
+ */
+
+GO.users.UsersGrid = function(config)
+{	
+	if(!config)
+	{
+		config = {};
+	}
+
+	var fields = {
+		fields:['id', 'username', 'name','logins','lastlogin','ctime','address','address_no','zip','city','state','country','home_phone','email',
+	    	'waddress','waddress_no','wzip','wcity','wstate','wcountry','wphone','enabled'],
+		columns:[
+        {header: GO.lang['strUsername'], dataIndex: 'username', width: 200},
+        {header: GO.lang['strName'], dataIndex: 'name', width: 250},
+        {header: GO.users.lang.numberOfLogins, dataIndex: 'logins', width: 100, align:"right"},
+        {header: GO.users.lang['cmdFormLabelLastLogin'], dataIndex: 'lastlogin', width: 110},
+        {header: GO.users.lang['cmdFormLabelRegistrationTime'], dataIndex: 'ctime', width: 110},      
+        {header: GO.lang['strEmail'], dataIndex: 'email',  hidden: false, width: 150},
+				{header: GO.users.lang['cmdBoxLabelEnabled'], dataIndex: 'enabled',  hidden: false, width: 100} 
+    ]
+	};
+
+	if(GO.customfields)
+	{
+		GO.customfields.addColumns("GO_Base_Model_User", fields);
+	}
+
+	config.store = new GO.data.JsonStore({
+	    url: GO.url('users/user/store'),
+	    baseParams: {task: 'users'},
+	    id: 'id',
+	    totalProperty: 'total',
+	    root: 'results',
+	    fields: fields.fields,
+	    remoteSort: true
+	});
+
+	config.loadMask=true;
+						
+	config.store.setDefaultSort('username', 'ASC');
+ 
+	config.view = new Ext.grid.GridView({
+		getRowClass : function(record, rowIndex, p, store){
+			if(record.data.enabled == GO.lang['no']){
+				return 'user-disabled';
+			}
+		}
+	});
+
+	config.deleteConfig={extraWarning:GO.users.lang.deleteWarning+"\n\n"};
+			
+	config.cm = new Ext.grid.ColumnModel({
+		defaults:{
+			sortable:true
+		},
+		columns:fields.columns
+	});	
+    
+   if(GO.settings.config.max_users>0)
+   {
+	   config.bbar = new Ext.PagingToolbar({
+	   			cls: 'go-paging-tb',
+	        store: config.store,
+	        pageSize: parseInt(GO.settings['max_rows_list']),
+	        displayInfo: true,
+	        displayMsg: GO.lang['displayingItems']+'. '+GO.lang.strMax+' '+GO.settings.config.max_users,
+	        emptyMsg: GO.lang['strNoItems']
+	    });
+   }
+
+		config.sm = new Ext.grid.RowSelectionModel();
+		config.paging=true;		
+				
+		GO.users.UsersGrid.superclass.constructor.call(this,config);
+};
+		
+Ext.extend(GO.users.UsersGrid, GO.grid.GridPanel,{
+	
+	afterRender : function(){
+		GO.users.UsersGrid.superclass.afterRender.call(this);
+		
+		this.on("rowdblclick",this.rowDoubleClick, this);			
+		this.store.load();
+
+
+		GO.dialogListeners.add('user',{
+			scope:this,
+			save:function(){
+				this.store.reload();
+			}
+		});
+	},			
+	
+	rowDoubleClick : function (grid, rowIndex, event)
+	{
+		var selectionModel = grid.getSelectionModel();
+		var record = selectionModel.getSelected();
+		GO.users.showUserDialog(record.data['id']);
+	}
+});
