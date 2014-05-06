5ff0ef1f42ba313d9ab7a68a5ff985d28a7aad8d
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-05-06 14:18:59 +0000

    Fixed problem with the reminder of tasks when the task is recurring. The reminder of the recurring item wasn't changed.

--- www/modules/tasks/model/Task.php
+++ www/modules/tasks/model/Task.php
@@ -208,14 +208,23 @@ class GO_Tasks_Model_Task extends GO_Base_Db_ActiveRecord {
 			$nextDueTime = $rrule->getNextRecurrence($this->due_time+1);
 			
 			if($nextDueTime){
-			
-				$this->duplicate(array(
+				
+				$data = array(
 					'completion_time'=>0,
 					'start_time'=>$nextDueTime-$this->due_time+$this->start_time,
 					'due_time'=>$nextDueTime,
 					'status'=>GO_Tasks_Model_Task::STATUS_NEEDS_ACTION,
 					'percentage_complete'=>0
-				));
+				);
+				
+				// If a reminder is set, then calculate the difference between the start dates of the old and the new task.
+				// Then add that difference to the reminder time for the new event. (So the reminder will also move forward)
+				if(!empty($this->reminder)){
+					$diff = $data['start_time'] - $this->start_time;
+					$data['reminder'] = $this->reminder + $diff;
+				}
+
+				$this->duplicate($data);
 			}
 		}
 	}
