011f97ea6916d8b8c76e6473a853a5545e3b8b8d
michaelhart86 <michaelhart86@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-05-07 14:30:14 +0000

    added checkbox for showing and hiding cc/bbc field by default when composer dialog opens

--- branches/groupoffice-5.1/www/modules/email/EmailModule.php
+++ branches/groupoffice-5.1/www/modules/email/EmailModule.php
@@ -58,6 +58,8 @@ class GO_Email_EmailModule extends GO_Base_Module{
 	public static function submitSettings(&$settingsController, &$params, &$response, $user) {
 
 		GO::config()->save_setting('email_use_plain_text_markup', isset($params['use_html_markup']) ? '0' : '1', GO::user()->user_id);
+		GO::config()->save_setting('email_show_cc', isset($params['email_show_cc']) ? 1 : 0, GO::user()->user_id);
+		GO::config()->save_setting('email_show_bcc', isset($params['email_show_bcc']) ? 1 : 0, GO::user()->user_id);
 		GO::config()->save_setting('email_skip_unknown_recipients', isset($params['skip_unknown_recipients']) ? '1' : '0', GO::user()->user_id);
 		GO::config()->save_setting('email_always_request_notification', isset($params['always_request_notification']) ? '1' : '0', GO::user()->user_id);
 		GO::config()->save_setting('email_always_respond_to_notifications', isset($params['always_respond_to_notifications']) ? '1' : '0', GO::user()->user_id);
