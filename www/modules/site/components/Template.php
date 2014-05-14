c77c313bda31d55bdf79d51ec99a4af15f1fcbc0
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-05-14 07:06:37 +0000

    Publish template assets with copy instead of Symlink for windows support

--- www/modules/site/components/Template.php
+++ www/modules/site/components/Template.php
@@ -27,12 +27,30 @@ class GO_Site_Components_Template{
 	private function _checkLink() {
 		
 		$folder = new GO_Base_Fs_Folder(Site::assetManager()->getBasePath());
-		if(!is_link($folder->path().'/template')){
-//		if(!is_link($folder->path().'/template') && !is_dir($folder->path().'/template')){
+				
+//		if(!GO_Base_Util_Common::isWindows()){					
+//			//Symlinks not supported on Windows
+//			if(!is_link($folder->path().'/template')){
+//				if(!symlink($this->getPath().'assets',$folder->path().'/template')){
+//					throw new Exception("Could not publish template assets. Is the \$config['file_storage_path'] path writable?");
+//				}
+//			}
+//		}else
+//		{			
+			$templateFolder = $folder->createChild('template', false);
 			
-			if(!symlink($this->getPath().'assets',$folder->path().'/template')){
-				throw new Exception("Could not publish template assets. Is the \$config['file_storage_path'] path writable?");
+			$mtime = GO::config()->get_setting('site_template_publish_date_'.Site::model()->id);
+			
+			if(GO::config()->debug || $mtime != GO::config()->mtime || $templateFolder->exists()){
+				$templateFolder->delete();
+				
+				$sourceTemplateFolder = new GO_Base_Fs_Folder($this->getPath().'assets');
+				
+				if($sourceTemplateFolder->copy($folder, 'template')){
+					GO::config()->save_setting('site_template_publish_date_'.Site::model()->id, GO::config()->mtime);
+				}
+				
 			}
-		}
+//		}
 	}
 }
