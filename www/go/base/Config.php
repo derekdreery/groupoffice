5a3cae9579dd3aa3e59d856b5653f2a26025726b
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-05-21 10:18:46 +0000

    Wrong if in template check

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -955,7 +955,7 @@ class GO_Base_Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '5.0.59';
+	var $version = '5.0.60';
 
 	
 	/**
@@ -965,7 +965,7 @@ class GO_Base_Config {
 	 * @access  public
 	 */
 
-	var $mtime = '20140521';
+	var $mtime = '20140521-1';
 
 	#group configuration
 	/**
