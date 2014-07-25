911ffc8e8a1e6796804b3ee04ee11f9b8d2f022a
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-07-25 08:02:56 +0000

    Removed invalid tag 6.0.7

--- tags/groupoffice-com-6.0.7/modules/chat/language/en.php
+++
@@ -1,3 +0,0 @@
-<?php
-$l['name']='Chat';
-$l['description']='Connects to an XMPP server that shares the same users as Group-Office. For example Prosody with Group-Office auth or a shared LDAP backend.';
--- tags/groupoffice-com-6.0.7/modules/search/language/sv.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-$l['name']='Sök';
-$l['description']='Sökmätare för att söka genom elementen som har matats in i GO. Mätaren kommer att placeras bredvid Startknappen för alla med läsbehörighet.';
-$l["title"]= 'Sök';
