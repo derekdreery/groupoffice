46fa3194fe38dcce834d574d42d8dea89f084abb
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-08-13 12:54:35 +0000

    Name apps back to modules

--- www/modules/modules/language/de.php
+++ www/modules/modules/language/de.php
@@ -1,16 +1,16 @@
 <?php
 
-$l["modules"]='Apps';
+$l["modules"]='Module';
 $l["cmdSaveChanges"]='Änderungen speichern';
 $l["cmdInstall"]='Hinzufügen';
 $l["cmdUninstall"]='Entfernen';
-$l["cmdAvailableModules"]='Verfügbare Apps';
+$l["cmdAvailableModules"]='Verfügbare Module';
 $l["cmdModuleName"]='Name';
 $l["cmdModuleDescription"]='Beschreibung';
 $l["cmdGridColumnModuleName"]='Name';
-$l["cmdUninstallMessage"]='ACHTUNG! Wenn Sie das App "{0}" deaktivieren, werden alle zugehörigen Daten gelöscht! Möchten Sie es wirklich deaktivieren?';
-$l["cmdModulesCannotBeDeleted"]='Dieses App kann nicht entfernt werden!';
+$l["cmdUninstallMessage"]='ACHTUNG! Wenn Sie das Modul "{0}" deaktivieren, werden alle zugehörigen Daten gelöscht! Möchten Sie es wirklich deaktivieren?';
+$l["cmdModulesCannotBeDeleted"]='Dieses Modul kann nicht entfernt werden!';
 
 $l['name']='Module';
-$l['description']='Modul zum Verwalten aktiver/inaktiver App';
+$l['description']='Modul zum Verwalten aktiver/inaktiver Module';
 $l['deleteModule']='Dieses Modul kann nicht gelöscht werden!';
