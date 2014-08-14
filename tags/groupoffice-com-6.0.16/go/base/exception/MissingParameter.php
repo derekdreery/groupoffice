60c45748a9c716fbb52d2554307e0af427123de6
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-08-14 08:51:30 +0000

    Created tag 6.0.16

---
+++ tags/groupoffice-com-6.0.16/go/base/exception/MissingParameter.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Copyright Intermesh
+ * 
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ * 
+ * If you have questions write an e-mail to info@intermesh.nl
+ */
+
+/**
+ * Thrown when a function has missing parameters
+ * 
+ * @author Wesley Smits <wsmits@intermesh.nl>
+ * @version $Id$
+ * @copyright Copyright Intermesh
+ * @package GO.base.exception
+ * 
+ * @uses Exception
+ */
+
+
+namespace GO\Base\Exception;
+
+
+class MissingParameter extends \Exception
+{
+
+}
