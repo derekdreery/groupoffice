57abc15d3afc628be01c20363d9062c4a929d38d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-09-17 09:16:52 +0000

    Created tag 5.0.80

---
+++ tags/groupoffice-com-5.0.80/modules/settings/language/pl.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$l['name']='Ustawienia';
+$l['description']='Moduł pozwale na zarządzanie różnymi ustawieniami Group-Office i jego modułów';
+$l['mainTitle'] = 'Ustawienia';
+$l['loginScreenText']='Tekst na stronie logowania';
+$l['text']='Tekst';
+$l['title']='Tytuł';
+$l['loginTextEnabled']='Włącz tekst na stronie logowania';
+$l['notRenamedNoUser']='Nie można zmienić, brak przsypisanych użytkowników po zmianie';
+$l['allRenamingSuccess']='Domyślne wzory dla wszystkich użytkowników zostały zmienione';
+$l['GO_Addressbook_Model_Addressbook']='Książka adresowa';
+$l['GO_Tasks_Model_Tasklist']='Zadania';
+$l['GO_Calendar_Model_Calendar']='Kalendarz';
