03257e12fc736cefa71d827395ec513386bded22
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-09-19 12:09:29 +0000

    Created tag 6.0.24

---
+++ tags/groupoffice-com-6.0.24/modules/summary/language/es.php
@@ -0,0 +1,28 @@
+<?php
+
+$l["noItems"]= 'No tienen ningún elemento en su inicio.';
+$l["selectPortlet"]= 'Seleccione portlet';
+$l["enterRssFeed"]= 'Enter URL del feed RSS:';
+$l["notes"]= 'Notas';
+$l["loadingFeed"]= 'Cargando Feed...';
+$l["today"]= 'Hoy';
+$l["dueTime"]="Fecha límite";
+$l["title"]="Título";
+$l["manageAnnouncements"]='Administrar comunicaciones';
+$l["webFeeds"]='Administrar webfeeds';
+$l["type"]='Tipo';
+$l["summaryType"]='Resumen';
+
+$l['description']='Por favor, introduzca una descripción';
+$l['default_rss_url']='http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml';
+$l['announcement']='Comunicación';
+$l['announcements']='Comunicaciones';
+$l['default_rss_title']='Novedades';
+
+$l["selectPortlet"]= 'Selecciona un plugin';
+$l["hotTopics"]= 'Canales RSS';
+$l["noRssFeeds"]='No se ha añadido ningún canal RSS.<br/>Pincha en el botón de configuración en la esquina superior derecha esta ventana para agregar canales RSS';
+$l["rssFeeds"]='Canales RSS';
+$l["summary"]= 'Resumen';
+$l['name']='Resumen';
+$l["goToPost"]= 'Ir a la página';
