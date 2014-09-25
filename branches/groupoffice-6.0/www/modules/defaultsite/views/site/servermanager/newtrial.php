731c01d78045d8d4781d9f4cd1b83c387e6f38f9
michaelhart86 <michaelhart86@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-09-25 14:16:08 +0000

    fixed references to old Sites module (change to Site)

--- branches/groupoffice-6.0/www/modules/defaultsite/views/site/servermanager/newtrial.php
+++ branches/groupoffice-6.0/www/modules/defaultsite/views/site/servermanager/newtrial.php
@@ -6,42 +6,42 @@
 //}
 ?>
 
-<?php echo \GO\Sites\Components\Html::beginForm(); ?>	
+<?php echo \GO\Site\Components\Html::beginForm(); ?>	
 
 <div class="row formrow">					
-	<?php echo \GO\Sites\Components\Html::activeLabelEx($model, 'name'); ?>
-	<?php echo \GO\Sites\Components\Html::activeTextField($model, 'name'); ?>
+	<?php echo \GO\Site\Components\Html::activeLabelEx($model, 'name'); ?>
+	<?php echo \GO\Site\Components\Html::activeTextField($model, 'name'); ?>
 	<span ><?php echo '.'.\GO::config()->servermanager_wildcard_domain; ?></span>
-	<?php echo \GO\Sites\Components\Html::error($model, 'name'); ?>
+	<?php echo \GO\Site\Components\Html::error($model, 'name'); ?>
 </div>
 <div class="row formrow">
-	<?php echo \GO\Sites\Components\Html::activeLabelEx($model, 'title'); ?>
-	<?php echo \GO\Sites\Components\Html::activeTextField($model, 'title'); ?>
-	<?php echo \GO\Sites\Components\Html::error($model, 'title'); ?>
+	<?php echo \GO\Site\Components\Html::activeLabelEx($model, 'title'); ?>
+	<?php echo \GO\Site\Components\Html::activeTextField($model, 'title'); ?>
+	<?php echo \GO\Site\Components\Html::error($model, 'title'); ?>
 </div>		
 
 <div class="row formrow">
-	<?php echo \GO\Sites\Components\Html::activeLabelEx($model, 'first_name'); ?>
-	<?php echo \GO\Sites\Components\Html::activeTextField($model, 'first_name'); ?>
-	<?php echo \GO\Sites\Components\Html::error($model, 'first_name'); ?>
+	<?php echo \GO\Site\Components\Html::activeLabelEx($model, 'first_name'); ?>
+	<?php echo \GO\Site\Components\Html::activeTextField($model, 'first_name'); ?>
+	<?php echo \GO\Site\Components\Html::error($model, 'first_name'); ?>
 </div>	
 
 <div class="row formrow">
-	<?php echo \GO\Sites\Components\Html::activeLabelEx($model, 'last_name'); ?>
-	<?php echo \GO\Sites\Components\Html::activeTextField($model, 'last_name'); ?>
-	<?php echo \GO\Sites\Components\Html::error($model, 'last_name'); ?>
+	<?php echo \GO\Site\Components\Html::activeLabelEx($model, 'last_name'); ?>
+	<?php echo \GO\Site\Components\Html::activeTextField($model, 'last_name'); ?>
+	<?php echo \GO\Site\Components\Html::error($model, 'last_name'); ?>
 </div>	
 
 <div class="row formrow">
-	<?php echo \GO\Sites\Components\Html::activeLabelEx($model, 'email'); ?>
-	<?php echo \GO\Sites\Components\Html::activeTextField($model, 'email'); ?>
-	<?php echo \GO\Sites\Components\Html::error($model, 'email'); ?>
+	<?php echo \GO\Site\Components\Html::activeLabelEx($model, 'email'); ?>
+	<?php echo \GO\Site\Components\Html::activeTextField($model, 'email'); ?>
+	<?php echo \GO\Site\Components\Html::error($model, 'email'); ?>
 </div>	
 
 <div class="row buttons">
-	<?php echo \GO\Sites\Components\Html::submitButton('Create trial!'); ?>
-	<?php echo \GO\Sites\Components\Html::resetButton('Reset'); ?>
+	<?php echo \GO\Site\Components\Html::submitButton('Create trial!'); ?>
+	<?php echo \GO\Site\Components\Html::resetButton('Reset'); ?>
 </div>
 
 <div style="clear:both"></div>
-<?php echo \GO\Sites\Components\Html::endForm(); ?>
+<?php echo \GO\Site\Components\Html::endForm(); ?>
