2bc8b2a75e779df6dfab8351036e024ff624d08d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-10-06 14:34:18 +0000

    Created tag 5.0.82

---
+++ tags/groupoffice-com-5.0.82/modules/favorites/FavoritesModule.php
@@ -0,0 +1,16 @@
+<?php
+
+class GO_Favorites_FavoritesModule extends GO_Base_Module{
+	
+	public function autoInstall() {
+		return false;
+	}
+	
+	public function author() {
+		return 'Wesley Smits';
+	}
+		
+	public function authorEmail() {
+		return 'wsmits@intermesh.nl';
+	}		
+}
