1fa9ebe2ff49dea6c0ec4cff09d42069d2296979
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-10-09 11:42:05 +0000

    php test bug

--- www/install/gotest.php
+++ www/install/gotest.php
@@ -63,9 +63,9 @@ function ini_return_bytes($val) {
     switch($last) {
         // The 'G' modifier is available since PHP 5.1.0
         case 'g':
-            $val *= 1024;
+            $val *= 1024*1024*1024;
         case 'm':
-            $val *= 1024;
+            $val *= 1024*1024;
         case 'k':
             $val *= 1024;
     }
