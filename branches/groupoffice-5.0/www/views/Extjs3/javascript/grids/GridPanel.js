708d3dbf9538422099ba74abfaeadb3c27af4bf9
michaelhart86 <michaelhart86@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-10-16 14:52:15 +0000

    added dirty only feature to editorgrid.getGridData()

--- branches/groupoffice-5.0/www/views/Extjs3/javascript/grids/GridPanel.js
+++ branches/groupoffice-5.0/www/views/Extjs3/javascript/grids/GridPanel.js
@@ -463,14 +463,22 @@ GO.grid.GridPanel =Ext.extend(Ext.grid.GridPanel, {
 		
 //		this.changed=true;
 	},
-
-	getGridData : function(){
+	/**
+	 * Fetch alle the row dat aof the grid's store
+	 * @param {boolean} dirtyOnly fetch only attributes of dirty rows (but all ids)
+	 * @returns {Array}
+	 */
+	getGridData : function(dirtyOnly){
 
 		var data = [];
 		var record;
 
 		for (var i = 0; i < this.store.data.items.length;  i++)
 		{
+			if(dirtyOnly && !this.store.data.items[i].dirty) {
+				data.push({id: this.store.data.items[i].data.id});
+				continue;
+			}
 			var r = this.store.data.items[i].data;
 
 			record={};
