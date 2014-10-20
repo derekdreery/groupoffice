a1dc2f0dfa111b2841bfd2dda12a762a676ce70c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-10-20 12:08:40 +0000

    Created tag 5.0.87

--- communitymodules/dokuwiki/language/ru.php
+++
@@ -1,5 +0,0 @@
-<?php
-$l['name']='Dokuwiki';
-$l['description']='Этот модуль интегрирует Dokuwiki с Group-Office. Дополнительная информация в файле modules/dokuwiki/INSTALL.TXT.';
-$l['externalUrl']='Dokuwiki url';
-$l['title']='Dokuwiki';
