d3f210853fcf26eda580b2dab3d004d4a5b17a6d
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-11-06 09:42:31 +0000

    Created tag 5.0.90

--- www/modules/customcss/scripts.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(\GO::modules()->files){
-	
-	$folder = \GO\Files\Model\Folder::model()->findByPath ('public/customcss', true);
-
-	$GO_SCRIPTS_JS .= 'GO.customcss.filesFolderId='.$folder->id.';';
-}
