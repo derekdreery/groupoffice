43e54d3231397d417ea2a1b7df6a09368f037058
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2014-11-26 13:18:25 +0000

    Created tag 6.1.6

---
+++ tags/groupoffice-com-6.1.6/modules/calendar/reports/WorkWeek.php
@@ -0,0 +1,19 @@
+<?php
+namespace GO\Calendar\Reports;
+/**
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @copyright Copyright Intermesh
+ * @version $Id$
+ * @author Michael de Hart <mdhart@intermesh.nl>
+ */
+class WorkWeek extends \GO\Calendar\Reports\Week {
+	
+	protected $dayCount = 5;
+
+}
