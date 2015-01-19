cf086826306ae1735d11aa8b5ed2eecf3bc8d5fb
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2015-01-19 09:32:57 +0000

    Removed invalid tag 6.1.15

--- tags/groupoffice-com-6.1.15/modules/freebusypermissions/language/es.php
+++
@@ -1,4 +0,0 @@
-<?php
-$l['name'] = 'Permisos de consulta de disponibilidad';
-$l['FreeBusyaccessrights'] = 'Permisos de consulta de disponibilidad';
-$l['info']='Permitir a otros usuarios y grupos consultar tu información de disponibilidad. Además las invitaciones que recibas de tos usuarios se añadirán a tu calendario directamente.';
--- tags/groupoffice-com-6.1.15/modules/mediawiki/language/ca.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-require($GO_LANGUAGE->get_fallback_language_file('mediawiki'));
-$lang['mediawiki']['name']='Mediawiki';
-$lang['mediawiki']['description']='Aquest mòdul integra Mediawiki amb Group-Office i automàticament hi identifica els usuaris de Group-Office. Vegeu modules/mediawiki/INSTALL.TXT per més informació.';
