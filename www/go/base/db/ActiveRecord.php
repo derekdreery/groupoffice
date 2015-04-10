2deb59fbaecee110d08244ab7ccfc325b848e8a6
wsmits <wsmits@94ee943c-857b-46a3-9e47-71061328e6f2>
2015-04-10 15:10:37 +0000

    Commented out error message when giving findParams to the findByPk function of an activeRecord

--- www/go/base/db/ActiveRecord.php
+++ www/go/base/db/ActiveRecord.php
@@ -1926,9 +1926,9 @@ ORDER BY `book`.`name` ASC ,`order`.`btime` DESC
 
 	public function findByPk($primaryKey, $findParams=false, $ignoreAcl=false, $noCache=false){
 
-		if(GO::config()->debug && $findParams != false){
-			throw new \Exception('Adding findparams to findByPk is not yet available');
-		}
+//		if(GO::config()->debug && $findParams != false){
+//			throw new \Exception('Adding findparams to findByPk is not yet available');
+//		}
 		
 //		GO::debug($this->className()."::findByPk($primaryKey)");
 		if(empty($primaryKey))
