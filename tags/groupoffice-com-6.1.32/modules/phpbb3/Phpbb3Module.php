6235f458d64fa773bbc1c10cbf1eda1230ccf183
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2015-05-07 14:46:44 +0000

    Created tag 6.1.32

---
+++ tags/groupoffice-com-6.1.32/modules/phpbb3/Phpbb3Module.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright Intermesh
+ *
+ * This file is part of Group-Office. You should have received a copy of the
+ * Group-Office license along with Group-Office. See the file /LICENSE.TXT
+ *
+ * If you have questions write an e-mail to info@intermesh.nl
+ *
+ * @version $Id$
+ * @copyright Copyright Intermesh
+ * @author Wesley Smits <wsmits@intermesh.nl>
+ */
+
+/**
+ * 
+ * The Dokuwiki module maintenance class
+ * 
+ */
+
+namespace GO\Phpbb3;
+
+
+class Phpbb3Module extends \GO\Base\Module{
+	
+	public function package() {
+		return self::PACKAGE_UNSUPPORTED;
+	}
+	
+	public function autoInstall() {
+		return false;
+	}
+}
