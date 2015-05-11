21fc03bca873a70a528d70ba1154ff80cf927122
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2015-05-11 07:32:51 +0000

    updated german and various caldav fixes

--- www/modules/email/language/de.php
+++ www/modules/email/language/de.php
@@ -272,8 +272,8 @@ $l['saveAttachmentTo']= 'Anhang zu diesen Elementen anhängen';
 $l['untilDateError']='Habe versucht, das folgende Enddatum einzutragen, aber der Prozess stoppte mit einem Fehler';
 
 $l['label']= 'Schlagwort';
-$l['labels']= 'Schlagwörter';
-$l['labelsLimit']= 'Es können maximal %d Schlagwörter angelegt werden';
+$l['labels']= 'Schlagworte';
+$l['labelsLimit']= 'Es können maximal %d Schlagworte angelegt werden';
 $l['saveToPersonal']= 'Im persönlichen Order speichern';
 $l['thisIsSpam1']= 'Diese Nachricht wurde als Spam identifiziert. Klicken Sie';
 $l['thisIsSpam2']= 'hier';
@@ -287,3 +287,5 @@ $l['moveToInboxTitle']= 'Aus dem Spam-Order verschieben?';
 $l['moveToInboxMsg']= 'Sind Sie sicher, dass Sie die Spam-Markierung entfernen und diese Nachricht in den Posteingang verschieben wollen?';
 
 $l['fts']= 'Ganze Nachricht';
+
+$l['icalendarOpenEvent']='Termin öffnen';
