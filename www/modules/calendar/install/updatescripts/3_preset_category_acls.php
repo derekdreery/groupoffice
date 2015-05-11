6844f43a488bbfa38a3b938f621764f0e467b763
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2015-05-11 08:21:03 +0000

    Upgrade calendar categories

--- www/modules/calendar/install/updatescripts/3_preset_category_acls.php
+++ www/modules/calendar/install/updatescripts/3_preset_category_acls.php
@@ -17,8 +17,7 @@ $stmt = \GO\Calendar\Model\Category::model()->find(
 	\GO\Base\Db\FindParams::newInstance()->ignoreAcl()
 );
 foreach ($stmt as $categoryModel) {
-	$aclModel = $categoryModel->setNewAcl();
+	$aclModel = $categoryModel->setNewAcl($categoryModel->user_id);
 //	$aclModel->addGroup(2, \GO\Base\Model\Acl::WRITE_PERMISSION); // Give 'everybody' group (id: 2) permission.
-	$aclModel->save();
 	$categoryModel->save();
 }
