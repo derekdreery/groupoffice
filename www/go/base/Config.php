a794cc4f3998ddd213205e4182c308ee7eff7e4c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2015-05-13 09:22:39 +0000

    6.1.33

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -987,7 +987,7 @@ class Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '6.1.32';
+	var $version = '6.1.33';
 
 	/**
 	 * Modification date
@@ -996,7 +996,7 @@ class Config {
 	 * @access  public
 	 */
 
-	var $mtime = '20150507';
+	var $mtime = '20150513';
 
 
 	#group configuration
