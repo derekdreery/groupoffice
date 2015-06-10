10f3acc5917628d493a659874591e70c778eab35
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2015-06-10 14:54:50 +0000

    Removed invalid tag 6.1.39

--- tags/groupoffice-com-6.1.39/modules/dav/language/en.php
+++
@@ -1,3 +0,0 @@
-<?php
-$l['name']='WebDAV server';
-$l['description']='Access your files from your desktop with the WebDAV server. The DAV services are built using the SabreDAV library.';
--- tags/groupoffice-com-6.1.39/modules/search/language/fr.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-$l["title"]= 'Recherche';
-$l['name']='Recherche';
-$l['description']='Barre de recherche pour tous les éléments ajoutés dans {product_name}. Cette barre apparaitra près du bouton "Applications" pour tous les utilisateurs avec les droits de lecture.';
