708dab9f7dfedfd5c0cbe7bf8d43cf9387bfd80c
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2015-06-17 11:42:23 +0000

    Improved etag values for caldav

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -987,7 +987,7 @@ class Config {
 	 * @var     string
 	 * @access  public
 	 */
-	var $version = '6.1.40';
+	var $version = '6.1.41';
 
 	/**
 	 * Modification date
@@ -996,7 +996,7 @@ class Config {
 	 * @access  public
 	 */
 
-	var $mtime = '20150615';
+	var $mtime = '20150617';
 
 
 	#group configuration
