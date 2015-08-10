f5290880176457909780d4769a42791eadb29cfc
mschering <mschering@94ee943c-857b-46a3-9e47-71061328e6f2>
2015-08-10 15:36:16 +0000

    Removed invalid tag 6.1.44

--- tags/groupoffice-com-6.1.44/modules/comments/language/et.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$l['name']='Kommentaarid';
-$l['description']='Lisab teistele moodulitele kommenteerimise võimaluse';
-$l["comment"]="Kommentaar";
-$l["comments"]="Kommentaarid";
-$l["recentComments"]='Viimased kommentaarid';
-$l["browseComments"]= 'Sirvi kommentaare';
--- tags/groupoffice-com-6.1.44/modules/mediawiki/language/fr.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$l['name']='Mediawiki';
-$l['description']='Intégration de Mediawiki dans Group-Office et connection automatique des utilisateurs. Voir /modules/mediawiki/INSTALL.TXT pour plus d\'informations.';
-$l['externalUrl']= 'URL Mediawiki';
-$l['title']= 'Titre du module';
